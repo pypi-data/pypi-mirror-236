e9de78b88d78845ae2763d62ce9c1c05b9f8bc28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-18 14:05:28 +0000

    Add package hamal-utils/0.0.51/hamal_utils-0.0.51-py3-none-any.whl

---
+++ packages/hamal-utils/hamal_utils-0.0.51-py3-none-any.whl/hamal_utils/code/s3_utils/__init__.py
@@ -0,0 +1,5 @@
+from hamal_utils.code.s3_utils.s3_custom_client import CustomS3Client
+from hamal_utils.code.s3_utils.s3_custom_resource import CustomS3Resource
+
+s3_client = CustomS3Client()
+s3_resource = CustomS3Resource()
