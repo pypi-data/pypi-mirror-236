03f6d92e04f0c9f15363f7a45a620d82e07ba946
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-18 16:06:37 +0000

    Add package python-amazon-ad-api/0.5.2/python_amazon_ad_api-0.5.2-py3-none-any.whl

---
+++ packages/python-amazon-ad-api/python_amazon_ad_api-0.5.2-py3-none-any.whl/ad_api/base/base_client.py
@@ -0,0 +1,14 @@
+import ad_api.version as vd
+
+class BaseClient:
+    scheme = 'https://'
+    method = 'GET'
+    content_type = 'application/x-www-form-urlencoded;charset=UTF-8'
+    user_agent = 'python-ad-api'
+
+    def __init__(self):
+        try:
+            version = vd.__version__
+            self.user_agent += f'-{version}'
+        except:
+            pass
