96edca431829ecb48febe5f4dd335661c07550ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-18 17:07:45 +0000

    Add package ninjaconnect/2.1.17/ninjaconnect-2.1.17-py3-none-any.whl

---
+++ packages/ninjaconnect/ninjaconnect-2.1.17-py3-none-any.whl/wgexpection/wireguardexpections.py
@@ -0,0 +1,15 @@
+from rich import print
+
+class GitlabauthExpection(Exception):
+    def __init__(self, statuscode, message):
+        self.statuscode = statuscode
+        self.message = message
+        formatted_message = f"[bold red1]:small_blue_diamond: GitlabAuthexpection: Error Code {statuscode}, Message: {message}[/bold red1]"
+        super().__init__(formatted_message)
+        
+class AlreadyregisterExpection(TypeError):
+    def __init__(self, statuscode, message):
+        self.statuscode = statuscode
+        self.message = message
+        formatted_message = f"[bold red1]:small_blue_diamond: AlreadyregisterExpection: Error Code {statuscode}, Message: {message}[/bold red1]"
+        super().__init__(formatted_message)
