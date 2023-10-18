b146940cbb9bead5ea5bfab2955fa0c35c7721ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-18 14:04:10 +0000

    Add package flake8-type-checking/2.5.1/flake8_type_checking-2.5.1.tar.gz

---
+++ packages/flake8-type-checking/flake8_type_checking-2.5.1.tar.gz/flake8_type_checking-2.5.1/flake8_type_checking/types.py
@@ -0,0 +1,24 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, Literal
+
+if TYPE_CHECKING:
+    import ast
+    from typing import Any, Generator, Optional, Protocol, Tuple, TypedDict, Union
+
+    class FunctionRangesDict(TypedDict):
+        start: int
+        end: int
+
+    class FunctionScopeNamesDict(TypedDict):
+        names: list[str]
+
+    Import = Union[ast.Import, ast.ImportFrom]
+    Flake8Generator = Generator[Tuple[int, int, str, Any], None, None]
+
+    class Name(Protocol):
+        asname: Optional[str]
+        name: str
+
+
+ImportTypeValue = Literal['APPLICATION', 'THIRD_PARTY', 'BUILTIN', 'FUTURE']
