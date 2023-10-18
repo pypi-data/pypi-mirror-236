b7218a32d099748e8400929f344356bf2b3fc508
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-18 15:04:46 +0000

    Add package types-aiobotocore-efs/2.7.0/types_aiobotocore_efs-2.7.0-py3-none-any.whl

---
+++ packages/types-aiobotocore-efs/types_aiobotocore_efs-2.7.0-py3-none-any.whl/types_aiobotocore_efs/__init__.py
@@ -0,0 +1,44 @@
+"""
+Main interface for efs service.
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+    from types_aiobotocore_efs import (
+        Client,
+        DescribeFileSystemsPaginator,
+        DescribeMountTargetsPaginator,
+        DescribeTagsPaginator,
+        EFSClient,
+    )
+
+    session = get_session()
+    async with session.create_client("efs") as client:
+        client: EFSClient
+        ...
+
+
+    describe_file_systems_paginator: DescribeFileSystemsPaginator = client.get_paginator("describe_file_systems")
+    describe_mount_targets_paginator: DescribeMountTargetsPaginator = client.get_paginator("describe_mount_targets")
+    describe_tags_paginator: DescribeTagsPaginator = client.get_paginator("describe_tags")
+    ```
+"""
+
+from .client import EFSClient
+from .paginator import (
+    DescribeFileSystemsPaginator,
+    DescribeMountTargetsPaginator,
+    DescribeTagsPaginator,
+)
+
+Client = EFSClient
+
+
+__all__ = (
+    "Client",
+    "DescribeFileSystemsPaginator",
+    "DescribeMountTargetsPaginator",
+    "DescribeTagsPaginator",
+    "EFSClient",
+)
---
+++ packages/types-aiobotocore-efs/types_aiobotocore_efs-2.7.0-py3-none-any.whl/types_aiobotocore_efs/paginator.pyi
@@ -0,0 +1,99 @@
+"""
+Type annotations for efs service client paginators.
+
+[Open documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_efs/paginators/)
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+
+    from types_aiobotocore_efs.client import EFSClient
+    from types_aiobotocore_efs.paginator import (
+        DescribeFileSystemsPaginator,
+        DescribeMountTargetsPaginator,
+        DescribeTagsPaginator,
+    )
+
+    session = get_session()
+    with session.create_client("efs") as client:
+        client: EFSClient
+
+        describe_file_systems_paginator: DescribeFileSystemsPaginator = client.get_paginator("describe_file_systems")
+        describe_mount_targets_paginator: DescribeMountTargetsPaginator = client.get_paginator("describe_mount_targets")
+        describe_tags_paginator: DescribeTagsPaginator = client.get_paginator("describe_tags")
+    ```
+"""
+
+from typing import AsyncIterator, Generic, Iterator, TypeVar
+
+from aiobotocore.paginate import AioPaginator
+from botocore.paginate import PageIterator
+
+from .type_defs import (
+    DescribeFileSystemsResponseTypeDef,
+    DescribeMountTargetsResponseTypeDef,
+    DescribeTagsResponseTypeDef,
+    PaginatorConfigTypeDef,
+)
+
+__all__ = ("DescribeFileSystemsPaginator", "DescribeMountTargetsPaginator", "DescribeTagsPaginator")
+
+_ItemTypeDef = TypeVar("_ItemTypeDef")
+
+class _PageIterator(Generic[_ItemTypeDef], PageIterator):
+    def __iter__(self) -> Iterator[_ItemTypeDef]:
+        """
+        Proxy method to specify iterator item type.
+        """
+
+class DescribeFileSystemsPaginator(AioPaginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/efs.html#EFS.Paginator.DescribeFileSystems)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_efs/paginators/#describefilesystemspaginator)
+    """
+
+    def paginate(
+        self,
+        *,
+        CreationToken: str = ...,
+        FileSystemId: str = ...,
+        PaginationConfig: PaginatorConfigTypeDef = ...
+    ) -> AsyncIterator[DescribeFileSystemsResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/efs.html#EFS.Paginator.DescribeFileSystems.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_efs/paginators/#describefilesystemspaginator)
+        """
+
+class DescribeMountTargetsPaginator(AioPaginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/efs.html#EFS.Paginator.DescribeMountTargets)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_efs/paginators/#describemounttargetspaginator)
+    """
+
+    def paginate(
+        self,
+        *,
+        FileSystemId: str = ...,
+        MountTargetId: str = ...,
+        AccessPointId: str = ...,
+        PaginationConfig: PaginatorConfigTypeDef = ...
+    ) -> AsyncIterator[DescribeMountTargetsResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/efs.html#EFS.Paginator.DescribeMountTargets.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_efs/paginators/#describemounttargetspaginator)
+        """
+
+class DescribeTagsPaginator(AioPaginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/efs.html#EFS.Paginator.DescribeTags)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_efs/paginators/#describetagspaginator)
+    """
+
+    def paginate(
+        self, *, FileSystemId: str, PaginationConfig: PaginatorConfigTypeDef = ...
+    ) -> AsyncIterator[DescribeTagsResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/efs.html#EFS.Paginator.DescribeTags.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_efs/paginators/#describetagspaginator)
+        """
