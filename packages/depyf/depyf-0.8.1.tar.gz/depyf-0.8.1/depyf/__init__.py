ed4d98e1bbdc932af2ace54c1c75981b6cd3268f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-18 17:07:06 +0000

    Add package depyf/0.8.1/depyf-0.8.1.tar.gz

---
+++ packages/depyf/depyf-0.8.1.tar.gz/depyf-0.8.1/depyf/__init__.py
@@ -0,0 +1,54 @@
+from typing import List, Tuple, Dict, Union, Callable, Optional
+from types import CodeType
+
+from .decompiler import Decompiler
+
+def decompile(code: Union[CodeType, Callable]):
+    """Decompile a code object or a function."""
+    return Decompiler(code).decompile()
+
+
+import types
+
+
+def pytorch_bytecode_src_hook(code: types.CodeType, new_code: types.CodeType):
+    import torch
+    bytecode_log = torch._logging.getArtifactLogger(
+        "torch._dynamo.convert_frame", "bytecode"
+    )
+    import logging
+
+    if bytecode_log.isEnabledFor(logging.DEBUG):
+        try:
+            decompiled_src = decompile(new_code)
+            bytecode_log.debug("possible source code:")
+            bytecode_log.debug(decompiled_src)
+        except Exception as e:
+            bytecode_log.debug("Decompilation fails due to: %s", str(e))
+        finally:
+            bytecode_log.debug(
+                "If you find the decompiled code is wrong,"
+                "please submit an issue at "
+                "https://github.com/thuml/depyf/issues."
+            )
+
+_handle = None
+
+def install():
+    import torch
+    global _handle
+    if _handle is not None:
+        return
+    _handle = torch._dynamo.convert_frame.register_bytecode_hook(pytorch_bytecode_src_hook)
+
+
+def uninstall():
+    global _handle
+    if _handle is None:
+        return
+    _handle.remove()
+    _handle = None
+
+import os
+
+__version__ = open(f"{os.path.dirname(__file__)}/VERSION.txt").read().strip()
---
+++ packages/depyf/depyf-0.8.1.tar.gz/depyf-0.8.1/depyf/explain/utils.py
@@ -0,0 +1,185 @@
+import torch
+from torch._dynamo.eval_frame import innermost_fn
+from torch._dynamo.eval_frame import _debug_get_cache_entry_list
+import inspect
+from importlib import import_module
+
+import dis
+from types import CodeType
+from typing import List, Callable, Dict, Union, Set
+from dataclasses import dataclass
+import contextlib
+
+import depyf
+
+class CodeProxy:
+    instances: Dict[str, "CodeProxy"] = {}
+    used_instances: Set[str] = set()
+    def __init__(self, code: str, name: str):
+        i = 0
+        new_name = name
+        if new_name.endswith(":"):
+            name = name[:-1]
+            while True:
+                new_name = f"{name}_{i}"
+                if new_name not in CodeProxy.instances:
+                    break
+                i += 1
+        self.name = new_name
+        code = "".join(["  " + line + "\n" for line in code.splitlines() if line.strip() != ""])
+        self.raw_code = code
+        self.code = f"""<details>
+  <summary>{self.name}</summary>
+
+  ```python
+{code}
+  ```
+</details>
+"""
+        CodeProxy.instances[self.name] = self
+    
+    def __str__(self):
+        CodeProxy.used_instances.add(self.name)
+        return self.name
+
+    @contextlib.contextmanager
+    @staticmethod
+    def record():
+        CodeProxy.used_instances = set()
+        yield CodeProxy.used_instances
+
+def display_func(self):
+    from IPython.display import display, JSON, Markdown, Code
+    display(Markdown("# transformed source code:"))
+    with CodeProxy.record() as instances:
+        data = self.to_data()
+    display(JSON(data))
+    display(Markdown("# source code of referenced function:"))
+    markdown = ""
+    for instance in instances:
+        markdown += CodeProxy.instances[instance].code
+    display(Markdown(markdown))
+
+
+@dataclass
+class CacheResult:
+    code: CodeType
+    guard: List[str]
+    compiled_subgraph: Callable
+    compiled_subgraph_proxy: CodeProxy
+    compiled_code: str
+    compiled_code_proxy: CodeProxy
+    referenced_global_functions: Dict[str, "DynamoOptimizationResult"]
+
+    def __init__(self, fn, cache):
+        guard = cache.check_fn.code_parts
+        code = cache.code
+        compiled_subgraphs = [name for name in code.co_names if name.startswith("__compiled")]
+        assert len(compiled_subgraphs) == 1
+        module = import_module(fn.__module__)
+        self.compiled_subgraph = innermost_fn(getattr(module, compiled_subgraphs[0]))
+        self.compiled_subgraph_proxy = CodeProxy(innermost_fn(self.compiled_subgraph).__self__.code, compiled_subgraphs[0])
+        try:
+            compiled_code = depyf.decompile(code)
+        except Exception as e:
+            print(str(e))
+            compiled_code = "'Failed to decompile.'\n"
+        self.compiled_code = compiled_code
+        self.compiled_code_proxy = CodeProxy(compiled_code, "compiled_code:")
+        resume_fns = [name for name in code.co_names if name.startswith("__resume")]
+        self.referenced_global_functions = {name: DynamoOptimizationResult(getattr(module, name), name) for name in resume_fns}
+        self.code = code
+        self.guard = guard
+
+    def to_data(self):
+        return {
+            "guard": self.guard,
+            "compiled_code": str(self.compiled_code_proxy),
+            "compiled_subgraph": str(self.compiled_subgraph_proxy),
+            "referenced_global_functions": {name: fn.to_data() for name, fn in self.referenced_global_functions.items()}
+        }
+
+    _ipython_display_ = display_func
+
+@dataclass
+class DynamoOptimizationResult:
+    name: str
+    fn: Callable
+    code: CodeType
+    source_code: str
+    source_code_proxy: CodeProxy
+    compiled_code_entries: List[CacheResult]
+
+    def __init__(self, fn, name=None):
+        self.fn = fn
+        if name is None:
+            self.name = fn.__name__
+        else:
+            self.name = name
+        caches = _debug_get_cache_entry_list(fn.__code__)
+        self.compiled_code_entries = [CacheResult(fn, cache) for cache in caches]
+        self.code = fn.__code__
+        try:
+            decompiled_source_code = depyf.Decompiler(self.code).decompile(overwite_fn_name=self.name)
+        except Exception as e:
+            print(str(e))
+            decompiled_source_code = "'Failed to decompile.'\n"
+        self.source_code = decompiled_source_code
+        self.source_code_proxy = CodeProxy(self.source_code, self.name)
+    
+    def to_data(self):
+        data = {
+            "name": self.name,
+            "source_code": str(self.source_code_proxy),
+            "compiled_code_entries": [entry.to_data() for entry in self.compiled_code_entries]
+        }
+        return data
+
+    def to_src(self):
+        raw_code = self.source_code_proxy.raw_code
+        signature = raw_code.splitlines()[0].replace("def ", "def compiled_", 1)
+        code = signature.strip()
+        code_obj = self.fn.__code__
+        normal_arg_count = code_obj.co_argcount + code_obj.co_kwonlyargcount
+        arg_names = code_obj.co_varnames[:normal_arg_count]
+        arg_dict = "L = {" + ", ".join([f'"{name}": {name}' for name in arg_names]) + "}"
+        code += "\n" + " " * 4 + arg_dict
+
+        additional_code = ""
+
+        for entry in self.compiled_code_entries:
+            guard = (" \\\n" + " " * 8 + "and ").join(["(" + x + ")" for x in entry.guard])
+            guard_func_name = CodeProxy(guard, "guard:").name
+            additional_code += f"\ndef {guard_func_name}(L):\n" + " " * 4 + "return " + guard + "\n"
+
+            compiled_subgraph_name = entry.compiled_subgraph_proxy.name
+            compiled_subgraph_code = entry.compiled_subgraph_proxy.raw_code
+            compiled_subgraph_code = f"def {compiled_subgraph_name}(" + compiled_subgraph_code[compiled_subgraph_code.index("(self,") + 6:].lstrip()
+            additional_code += "\n" + remove_indentation(compiled_subgraph_code) + "\n"
+
+            func_name = entry.compiled_code_proxy.name
+            compiled_code = entry.compiled_code_proxy.raw_code
+            compiled_code = f"def {func_name}" + compiled_code[compiled_code.index("("):]
+            additional_code += "\n" + remove_indentation(compiled_code) + "\n"
+
+            for name, func in entry.referenced_global_functions.items():
+                additional_code = func.to_src() + additional_code
+
+            code += "\n" + " " * 4 + f"if {guard_func_name}(L):\n" + " " * 8 + f"return {func_name}({', '.join(arg_names)})"
+        
+        original_code_lines = remove_indentation(self.source_code_proxy.raw_code).splitlines()
+        original_code_lines = [original_code_lines[0], " " * 4 + "# Note: if there is a compiled version below, this function might well not be executed directly. Please check the compiled version if possible."] + original_code_lines[1:]
+        original_code = "".join([x + "\n" for x in original_code_lines])
+
+        additional_code += "\n" + original_code + "\n"
+
+        original_func_name = self.source_code_proxy.name
+        code += "\n" + " " * 4 + "# Note: this function might well not be executed directly. It might well be compiled again, i.e. adding one more guards and compiled code.\n" + " " * 4 + f"return {original_func_name}({', '.join(arg_names)})"
+        return additional_code + code + f"\n\n#============ end of {self.name} ============#\n"
+
+    _ipython_display_ = display_func
+
+def remove_indentation(code: str):
+    lines = code.splitlines()
+    indent = len(lines[0]) - len(lines[0].lstrip())
+    return "".join([line[indent:] + "\n" for line in lines])
