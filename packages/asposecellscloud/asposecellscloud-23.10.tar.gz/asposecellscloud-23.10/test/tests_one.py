6cd9b18b06c6f0d25dd7e1c0305e7f27c1a5cb28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-18 17:08:05 +0000

    Add package asposecellscloud/23.10/asposecellscloud-23.10.tar.gz

---
+++ packages/asposecellscloud/asposecellscloud-23.10.tar.gz/asposecellscloud-23.10/test/tests_one.py
@@ -0,0 +1,34 @@
+# coding: utf-8
+
+from __future__ import absolute_import
+
+import os
+import sys
+import unittest
+import warnings
+
+ABSPATH = os.path.abspath(os.path.realpath(os.path.dirname(__file__)) + "/..")
+sys.path.append(ABSPATH)
+
+from asposecellscloud.rest import ApiException
+from asposecellscloud.apis.cells_api import CellsApi
+import AuthUtil
+from asposecellscloud.models import *
+from asposecellscloud.requests import *
+
+global_api = None
+
+class TestAutoFilterControllerApi(unittest.TestCase):
+    def setUp(self):
+        warnings.simplefilter('ignore', ResourceWarning)
+        global global_api
+        if global_api is None:
+           global_api = CellsApi(AuthUtil.GetClientId(),AuthUtil.GetClientSecret(),"v3.0",AuthUtil.GetBaseUrl())
+        self.api = global_api
+
+    def tearDown(self):
+        pass
+
+   
+if __name__ == '__main__':
+    unittest.main()
