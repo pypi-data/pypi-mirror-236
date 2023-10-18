e29ae89c37723366debab7e0e5ebf0f7848d14fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-18 16:07:10 +0000

    Add package project-utils-2023/0.2.7/project-utils-2023-0.2.7.tar.gz

---
+++ packages/project-utils-2023/project-utils-2023-0.2.7.tar.gz/project-utils-2023-0.2.7/utils/web/django/middle/_base_middle.py
@@ -0,0 +1,36 @@
+import asyncio
+
+from abc import ABCMeta, abstractmethod
+
+from typing import Optional, Callable
+
+from django.http.request import HttpRequest
+from django.http.response import HttpResponse
+from django.utils.deprecation import MiddlewareMixin
+
+from utils.web.django.conf import DjangoConfig
+
+
+class BaseMiddleware(MiddlewareMixin, metaclass=ABCMeta):
+    config: DjangoConfig
+
+    @abstractmethod
+    def process_request(self, request: HttpRequest) -> Optional[HttpResponse]:
+        return
+
+    def process_view(self, request: HttpRequest, callback: Callable, callback_args: ..., callback_kwargs: ...) -> \
+            Optional[HttpResponse]:
+        if asyncio.iscoroutinefunction(callback):
+            return self.config.loop.run_until_complete(callback(request, *callback_args, **callback_kwargs))
+
+    @abstractmethod
+    def process_response(self, request: HttpRequest, response: HttpResponse) -> HttpResponse:
+        return response
+
+    @abstractmethod
+    def process_exception(self, request: HttpRequest, exception: Exception) -> HttpResponse:
+        return HttpResponse(str(exception))
+
+    @abstractmethod
+    def process_template_response(self, request: HttpRequest, response: HttpResponse) -> HttpResponse:
+        return response
