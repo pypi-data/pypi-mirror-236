c4b0c3709b72375de92a717b6b3931f447cbbff9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-18 17:06:22 +0000

    Add package moneybox-sdk/0.0.1rc18/moneybox_sdk-0.0.1rc18.tar.gz

---
+++ packages/moneybox-sdk/moneybox_sdk-0.0.1rc18.tar.gz/moneybox_sdk-0.0.1rc18/openapi_client/api/__init__.py
@@ -0,0 +1,14 @@
+# flake8: noqa
+
+# import apis into api package
+from openapi_client.api.auth_api import AuthApi
+from openapi_client.api.currency_api import CurrencyApi
+from openapi_client.api.expenses_api import ExpensesApi
+from openapi_client.api.incomes_api import IncomesApi
+from openapi_client.api.invites_api import InvitesApi
+from openapi_client.api.reports_api import ReportsApi
+from openapi_client.api.transfers_api import TransfersApi
+from openapi_client.api.users_api import UsersApi
+from openapi_client.api.wallets_api import WalletsApi
+from openapi_client.api.group_api import GroupApi
+
