64184f617838cfc9e638810ccc5869dac7c594c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-18 14:04:51 +0000

    Add package windmill-api/1.184.0/windmill_api-1.184.0.tar.gz

---
+++ packages/windmill-api/windmill_api-1.184.0.tar.gz/windmill_api-1.184.0/windmill_api/models/list_hub_resource_types_response_200_item.py
@@ -0,0 +1,75 @@
+from typing import Any, Dict, List, Type, TypeVar, Union
+
+from attrs import define as _attrs_define
+from attrs import field as _attrs_field
+
+from ..types import UNSET, Unset
+
+T = TypeVar("T", bound="ListHubResourceTypesResponse200Item")
+
+
+@_attrs_define
+class ListHubResourceTypesResponse200Item:
+    """
+    Attributes:
+        id (str):
+        name (str):
+        schema (Union[Unset, Any]):
+    """
+
+    id: str
+    name: str
+    schema: Union[Unset, Any] = UNSET
+    additional_properties: Dict[str, Any] = _attrs_field(init=False, factory=dict)
+
+    def to_dict(self) -> Dict[str, Any]:
+        id = self.id
+        name = self.name
+        schema = self.schema
+
+        field_dict: Dict[str, Any] = {}
+        field_dict.update(self.additional_properties)
+        field_dict.update(
+            {
+                "id": id,
+                "name": name,
+            }
+        )
+        if schema is not UNSET:
+            field_dict["schema"] = schema
+
+        return field_dict
+
+    @classmethod
+    def from_dict(cls: Type[T], src_dict: Dict[str, Any]) -> T:
+        d = src_dict.copy()
+        id = d.pop("id")
+
+        name = d.pop("name")
+
+        schema = d.pop("schema", UNSET)
+
+        list_hub_resource_types_response_200_item = cls(
+            id=id,
+            name=name,
+            schema=schema,
+        )
+
+        list_hub_resource_types_response_200_item.additional_properties = d
+        return list_hub_resource_types_response_200_item
+
+    @property
+    def additional_keys(self) -> List[str]:
+        return list(self.additional_properties.keys())
+
+    def __getitem__(self, key: str) -> Any:
+        return self.additional_properties[key]
+
+    def __setitem__(self, key: str, value: Any) -> None:
+        self.additional_properties[key] = value
+
+    def __delitem__(self, key: str) -> None:
+        del self.additional_properties[key]
+
+    def __contains__(self, key: str) -> bool:
+        return key in self.additional_properties
