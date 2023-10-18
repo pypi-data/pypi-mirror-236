7e8673321fda4d74ee95ceb752760c8e90c5ffb4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-18 15:05:15 +0000

    Add package SpiralFilm/0.1.22/SpiralFilm-0.1.22-py3-none-any.whl

---
+++ packages/SpiralFilm/SpiralFilm-0.1.22-py3-none-any.whl/spiralfilm/__init__.py
@@ -0,0 +1,10 @@
+# spiral_film/__init__.py
+
+# Import main classes and functions from the relevant modules
+from .core import FilmCore
+from .config import FilmConfig
+from .embed import FilmEmbed, FilmEmbedConfig
+from .parser import FilmParser
+
+# Specify the version of the package
+__version__ = "0.1.22"
