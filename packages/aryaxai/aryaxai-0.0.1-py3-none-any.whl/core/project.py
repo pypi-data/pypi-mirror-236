d04da8a57a43385001d78197dbe15d27721ea028
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-18 17:05:27 +0000

    Add package aryaxai/0.0.1/aryaxai-0.0.1-py3-none-any.whl

---
+++ packages/aryaxai/aryaxai-0.0.1-py3-none-any.whl/core/project.py
@@ -0,0 +1,43 @@
+from pydantic import BaseModel
+from typing import List
+from aryaxai.common.xai_uris import (
+    GET_DATA_DIAGNOSIS_URI,
+    GET_DATA_SUMMARY_URI,
+    GET_DATA_DRIFT_DIAGNOSIS_REPORT_URI,
+)
+
+
+class Project(BaseModel):
+    created_by: str
+    project_name: str
+    user_project_name: str
+    user_workspace_name: str
+    workspace_name: str
+    project_data_dir_path: str
+    collections_name: List[str]
+    user_access: List[str]
+    created_at: str
+    updated_at: str
+    metadata: dict
+    access_type: str
+
+    # def get_data_summary(self):
+    #     res = self.api_client.post(f"{GET_DATA_SUMMARY}/?project_name={self.user_project_name}&refresh=true'")
+    #     return res
+
+    # def get_data_diagnosis(self):
+    #     res = self.api_client.get(f"{GET_DATA_DIAGNOSIS_URI}/?project_name={self.user_project_name}")
+    #     return res
+
+    # def get_data_diagnosis_drift_report(self):
+    #     res = self.api_client.get(f"{GET_DATA_DRIFT_DIAGNOSIS_REPORT_URI}/?project_name={self.user_project_name}")
+    #     return res
+
+    def __print__(self) -> str:
+        return f"Project(user_project_name='{self.user_project_name}', created_by='{self.created_by}', created_at='{self.created_at}')"
+
+    def __str__(self) -> str:
+        return self.__print__()
+
+    def __repr__(self) -> str:
+        return self.__print__()
---
+++ packages/aryaxai/aryaxai-0.0.1-py3-none-any.whl/core/xai.py
@@ -0,0 +1,62 @@
+import os
+from pydantic import BaseModel
+from aryaxai.client.client import APIClient
+from aryaxai.common.environment import Environment
+
+from aryaxai.common.xai_uris import CREATE_WORKSPACE_URI, LOGIN_URI, GET_WORKSPACES_URI
+import getpass
+from typing import List
+
+from aryaxai.core.workspace import Workspace
+
+
+class XAI(BaseModel):
+    """Base class to connect with AryaXAI platform"""
+
+    env: Environment = Environment()
+    api_client: APIClient = APIClient(base_url=env.get_base_url())
+
+    def login(self):
+        """login to AryaXAI platform
+
+        :param api_key: API key, defaults to XAI_ACCESS_TOKEN environment variable
+        """
+        access_token = os.environ.get("XAI_ACCESS_TOKEN", None) or getpass.getpass(
+            "Enter your Arya XAI Access Token: "
+        )
+
+        if not access_token:
+            raise ValueError("Either set XAI_ACCESS_TOKEN or pass the Access token")
+
+        res = self.api_client.post(LOGIN_URI, payload={"access_token": access_token})
+        self.api_client.update_headers(res["access_token"])
+        self.api_client.set_access_token(access_token)
+
+        print("Authenticated successfully.")
+
+    def get_workspaces(self):
+        """get user workspaces
+
+        :return: list of workspace
+        """
+        user_workspaces = []
+
+        workspaces = self.api_client.get(GET_WORKSPACES_URI)
+        user_workspaces = [
+            Workspace(api_client=self.api_client, **workspace)
+            for workspace in workspaces["details"]
+        ]
+
+        return user_workspaces
+
+    def create_workspace(self, workspace_name: str) -> Workspace:
+        """create user workspace
+
+        :param workspace_name: name for the workspace
+        :return: response
+        """
+
+        res = self.api_client.post(
+            CREATE_WORKSPACE_URI, {"workspace_name": workspace_name}
+        )
+        return res.get("details")
