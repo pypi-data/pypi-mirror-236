4dfc10c44b633cb490db9ff73d4e284d1be1067d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-18 14:05:17 +0000

    Add package LolzteamApi/1.1.11/LolzteamApi-1.1.11.tar.gz

---
+++ packages/LolzteamApi/LolzteamApi-1.1.11.tar.gz/LolzteamApi-1.1.11/LolzteamApi/__init__.py
@@ -0,0 +1,28 @@
+def auto_update():
+    from importlib.metadata import version
+    import requests
+    from bs4 import BeautifulSoup
+    current_version = version('LolzteamApi')
+    response = requests.get("https://pypi.org/project/LolzteamApi/")
+    newest_version = BeautifulSoup(response.text, 'html.parser').select(
+        selector="#content > div.banner > div > div.package-header__left > h1")[0].text.replace("LolzteamApi",
+                                                                                                "").replace(" ",
+                                                                                                            "").replace(
+        "\n", "")
+    if current_version != newest_version:
+        print(f"Updating LolzteamApi from {current_version} to {newest_version}")
+        import subprocess
+        import sys
+        import importlib
+        subprocess.call("pip install LolzteamApi --upgrade", shell=True, stdout=subprocess.DEVNULL,
+                        stderr=subprocess.STDOUT)
+        importlib.reload(sys.modules["LolzteamApi"])
+        print(f"LolzteamApi updated to {newest_version}")
+
+try:
+    auto_update()
+except:
+    pass
+from .LolzteamApi import LolzteamApi
+from .AntipublicApi import AntipublicApi
+from . import Types
