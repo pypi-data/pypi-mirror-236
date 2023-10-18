6897b11ffb38b2b3cd1f12646d538b3b129ef8b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-18 15:04:50 +0000

    Add package types-aiobotocore-gamesparks/2.7.0/types-aiobotocore-gamesparks-2.7.0.tar.gz

---
+++ packages/types-aiobotocore-gamesparks/types-aiobotocore-gamesparks-2.7.0.tar.gz/types-aiobotocore-gamesparks-2.7.0/types_aiobotocore_gamesparks/paginator.pyi
@@ -0,0 +1,166 @@
+"""
+Type annotations for gamesparks service client paginators.
+
+[Open documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_gamesparks/paginators/)
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+
+    from types_aiobotocore_gamesparks.client import GameSparksClient
+    from types_aiobotocore_gamesparks.paginator import (
+        ListExtensionVersionsPaginator,
+        ListExtensionsPaginator,
+        ListGamesPaginator,
+        ListGeneratedCodeJobsPaginator,
+        ListSnapshotsPaginator,
+        ListStageDeploymentsPaginator,
+        ListStagesPaginator,
+    )
+
+    session = get_session()
+    with session.create_client("gamesparks") as client:
+        client: GameSparksClient
+
+        list_extension_versions_paginator: ListExtensionVersionsPaginator = client.get_paginator("list_extension_versions")
+        list_extensions_paginator: ListExtensionsPaginator = client.get_paginator("list_extensions")
+        list_games_paginator: ListGamesPaginator = client.get_paginator("list_games")
+        list_generated_code_jobs_paginator: ListGeneratedCodeJobsPaginator = client.get_paginator("list_generated_code_jobs")
+        list_snapshots_paginator: ListSnapshotsPaginator = client.get_paginator("list_snapshots")
+        list_stage_deployments_paginator: ListStageDeploymentsPaginator = client.get_paginator("list_stage_deployments")
+        list_stages_paginator: ListStagesPaginator = client.get_paginator("list_stages")
+    ```
+"""
+
+from typing import AsyncIterator, Generic, Iterator, TypeVar
+
+from aiobotocore.paginate import AioPaginator
+from botocore.paginate import PageIterator
+
+from .type_defs import (
+    ListExtensionsResultTypeDef,
+    ListExtensionVersionsResultTypeDef,
+    ListGamesResultTypeDef,
+    ListGeneratedCodeJobsResultTypeDef,
+    ListSnapshotsResultTypeDef,
+    ListStageDeploymentsResultTypeDef,
+    ListStagesResultTypeDef,
+    PaginatorConfigTypeDef,
+)
+
+__all__ = (
+    "ListExtensionVersionsPaginator",
+    "ListExtensionsPaginator",
+    "ListGamesPaginator",
+    "ListGeneratedCodeJobsPaginator",
+    "ListSnapshotsPaginator",
+    "ListStageDeploymentsPaginator",
+    "ListStagesPaginator",
+)
+
+_ItemTypeDef = TypeVar("_ItemTypeDef")
+
+class _PageIterator(Generic[_ItemTypeDef], PageIterator):
+    def __iter__(self) -> Iterator[_ItemTypeDef]:
+        """
+        Proxy method to specify iterator item type.
+        """
+
+class ListExtensionVersionsPaginator(AioPaginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/gamesparks.html#GameSparks.Paginator.ListExtensionVersions)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_gamesparks/paginators/#listextensionversionspaginator)
+    """
+
+    def paginate(
+        self, *, Name: str, Namespace: str, PaginationConfig: PaginatorConfigTypeDef = ...
+    ) -> AsyncIterator[ListExtensionVersionsResultTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/gamesparks.html#GameSparks.Paginator.ListExtensionVersions.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_gamesparks/paginators/#listextensionversionspaginator)
+        """
+
+class ListExtensionsPaginator(AioPaginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/gamesparks.html#GameSparks.Paginator.ListExtensions)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_gamesparks/paginators/#listextensionspaginator)
+    """
+
+    def paginate(
+        self, *, PaginationConfig: PaginatorConfigTypeDef = ...
+    ) -> AsyncIterator[ListExtensionsResultTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/gamesparks.html#GameSparks.Paginator.ListExtensions.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_gamesparks/paginators/#listextensionspaginator)
+        """
+
+class ListGamesPaginator(AioPaginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/gamesparks.html#GameSparks.Paginator.ListGames)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_gamesparks/paginators/#listgamespaginator)
+    """
+
+    def paginate(
+        self, *, PaginationConfig: PaginatorConfigTypeDef = ...
+    ) -> AsyncIterator[ListGamesResultTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/gamesparks.html#GameSparks.Paginator.ListGames.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_gamesparks/paginators/#listgamespaginator)
+        """
+
+class ListGeneratedCodeJobsPaginator(AioPaginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/gamesparks.html#GameSparks.Paginator.ListGeneratedCodeJobs)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_gamesparks/paginators/#listgeneratedcodejobspaginator)
+    """
+
+    def paginate(
+        self, *, GameName: str, SnapshotId: str, PaginationConfig: PaginatorConfigTypeDef = ...
+    ) -> AsyncIterator[ListGeneratedCodeJobsResultTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/gamesparks.html#GameSparks.Paginator.ListGeneratedCodeJobs.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_gamesparks/paginators/#listgeneratedcodejobspaginator)
+        """
+
+class ListSnapshotsPaginator(AioPaginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/gamesparks.html#GameSparks.Paginator.ListSnapshots)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_gamesparks/paginators/#listsnapshotspaginator)
+    """
+
+    def paginate(
+        self, *, GameName: str, PaginationConfig: PaginatorConfigTypeDef = ...
+    ) -> AsyncIterator[ListSnapshotsResultTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/gamesparks.html#GameSparks.Paginator.ListSnapshots.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_gamesparks/paginators/#listsnapshotspaginator)
+        """
+
+class ListStageDeploymentsPaginator(AioPaginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/gamesparks.html#GameSparks.Paginator.ListStageDeployments)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_gamesparks/paginators/#liststagedeploymentspaginator)
+    """
+
+    def paginate(
+        self, *, GameName: str, StageName: str, PaginationConfig: PaginatorConfigTypeDef = ...
+    ) -> AsyncIterator[ListStageDeploymentsResultTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/gamesparks.html#GameSparks.Paginator.ListStageDeployments.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_gamesparks/paginators/#liststagedeploymentspaginator)
+        """
+
+class ListStagesPaginator(AioPaginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/gamesparks.html#GameSparks.Paginator.ListStages)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_gamesparks/paginators/#liststagespaginator)
+    """
+
+    def paginate(
+        self, *, GameName: str, PaginationConfig: PaginatorConfigTypeDef = ...
+    ) -> AsyncIterator[ListStagesResultTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/gamesparks.html#GameSparks.Paginator.ListStages.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_gamesparks/paginators/#liststagespaginator)
+        """
