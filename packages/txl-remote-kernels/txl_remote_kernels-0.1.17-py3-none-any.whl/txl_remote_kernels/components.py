e81471bdd8ec946c43eb617a4cc85e9ffe3e167e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-18 18:07:27 +0000

    Add package txl-remote-kernels/0.1.17/txl_remote_kernels-0.1.17-py3-none-any.whl

---
+++ packages/txl-remote-kernels/txl_remote_kernels-0.1.17-py3-none-any.whl/txl_remote_kernels/components.py
@@ -0,0 +1,39 @@
+import y_py as Y
+from asphalt.core import Component, Context
+
+from txl.base import Kernels
+
+from .driver import KernelDriver
+
+
+class RemoteKernels(Kernels):
+
+    comm_handlers = []
+
+    def __init__(
+        self,
+        url: str,
+        kernel_name: str | None,
+    ):
+        self.kernel = KernelDriver(url, kernel_name, comm_handlers=self.comm_handlers)
+
+    async def execute(self, ydoc: Y.YDoc, ycell: Y.YMap):
+        await self.kernel.execute(ydoc, ycell)
+
+
+class RemoteKernelsComponent(Component):
+    def __init__(self, url: str = "http://127.0.0.1:8000"):
+        super().__init__()
+        self.url = url
+
+    async def start(
+        self,
+        ctx: Context,
+    ) -> None:
+        url = self.url
+
+        class _RemoteKernels(RemoteKernels):
+            def __init__(self, *args, **kwargs):
+                super().__init__(url, *args, **kwargs)
+
+        ctx.add_resource(_RemoteKernels, types=Kernels)
