7fadb8e047cfddafcdaa92c27b63908085688374
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-18 14:04:55 +0000

    Add package pirg/1.2.0/pirg-1.2.0-py3-none-any.whl

---
+++ packages/pirg/pirg-1.2.0-py3-none-any.whl/pirg/models.py
@@ -0,0 +1,26 @@
+from dataclasses import dataclass
+from packaging.specifiers import SpecifierSet
+from typing import Optional
+
+
+@dataclass
+class Package:
+    name: str
+    suffix: Optional[str] = None
+    specifier_set: Optional[SpecifierSet] = None
+
+    def __eq__(self, other) -> bool:
+        return (
+            self.name == other.name
+            and self.specifier_set == other.specifier_set
+            and self.suffix == other.suffix
+        )
+
+    def __hash__(self) -> int:
+        return hash(self.name + str(self.suffix) + str(self.specifier_set))
+
+    def __str__(self) -> str:
+        string = f"{self.name}"
+        string += f"[{self.suffix}]" if self.suffix else ""
+        string += f"{str(self.specifier_set)}" if self.specifier_set else ""
+        return string
