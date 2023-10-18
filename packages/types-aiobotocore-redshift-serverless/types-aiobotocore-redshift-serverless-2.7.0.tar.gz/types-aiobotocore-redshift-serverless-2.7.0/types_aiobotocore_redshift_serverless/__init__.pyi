0491a65538ed278665673efa88eb993c0a80aecd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-18 15:05:22 +0000

    Add package types-aiobotocore-redshift-serverless/2.7.0/types-aiobotocore-redshift-serverless-2.7.0.tar.gz

---
+++ packages/types-aiobotocore-redshift-serverless/types-aiobotocore-redshift-serverless-2.7.0.tar.gz/types-aiobotocore-redshift-serverless-2.7.0/types_aiobotocore_redshift_serverless/__init__.pyi
@@ -0,0 +1,59 @@
+"""
+Main interface for redshift-serverless service.
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+    from types_aiobotocore_redshift_serverless import (
+        Client,
+        ListEndpointAccessPaginator,
+        ListNamespacesPaginator,
+        ListRecoveryPointsPaginator,
+        ListSnapshotsPaginator,
+        ListTableRestoreStatusPaginator,
+        ListUsageLimitsPaginator,
+        ListWorkgroupsPaginator,
+        RedshiftServerlessClient,
+    )
+
+    session = get_session()
+    async with session.create_client("redshift-serverless") as client:
+        client: RedshiftServerlessClient
+        ...
+
+
+    list_endpoint_access_paginator: ListEndpointAccessPaginator = client.get_paginator("list_endpoint_access")
+    list_namespaces_paginator: ListNamespacesPaginator = client.get_paginator("list_namespaces")
+    list_recovery_points_paginator: ListRecoveryPointsPaginator = client.get_paginator("list_recovery_points")
+    list_snapshots_paginator: ListSnapshotsPaginator = client.get_paginator("list_snapshots")
+    list_table_restore_status_paginator: ListTableRestoreStatusPaginator = client.get_paginator("list_table_restore_status")
+    list_usage_limits_paginator: ListUsageLimitsPaginator = client.get_paginator("list_usage_limits")
+    list_workgroups_paginator: ListWorkgroupsPaginator = client.get_paginator("list_workgroups")
+    ```
+"""
+
+from .client import RedshiftServerlessClient
+from .paginator import (
+    ListEndpointAccessPaginator,
+    ListNamespacesPaginator,
+    ListRecoveryPointsPaginator,
+    ListSnapshotsPaginator,
+    ListTableRestoreStatusPaginator,
+    ListUsageLimitsPaginator,
+    ListWorkgroupsPaginator,
+)
+
+Client = RedshiftServerlessClient
+
+__all__ = (
+    "Client",
+    "ListEndpointAccessPaginator",
+    "ListNamespacesPaginator",
+    "ListRecoveryPointsPaginator",
+    "ListSnapshotsPaginator",
+    "ListTableRestoreStatusPaginator",
+    "ListUsageLimitsPaginator",
+    "ListWorkgroupsPaginator",
+    "RedshiftServerlessClient",
+)
