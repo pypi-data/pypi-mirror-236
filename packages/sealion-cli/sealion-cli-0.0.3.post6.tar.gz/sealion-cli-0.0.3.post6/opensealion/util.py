7f344153d7eb516c376b4fceb0361ec4d80d425b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-18 16:06:09 +0000

    Add package sealion-cli/0.0.3.post6/sealion-cli-0.0.3.post6.tar.gz

---
+++ packages/sealion-cli/sealion-cli-0.0.3.post6.tar.gz/sealion-cli-0.0.3.post6/opensealion/util.py
@@ -0,0 +1,20 @@
+import opensealion.config as config
+
+
+def get_main_menu(current_row: int) -> list:
+    if current_row == 0:
+        return config.frontend
+    elif current_row == 1:
+        return config.backend
+    elif current_row == 2:
+        return config.setting
+
+
+def create_menu_dict(*menu_lists) -> dict:
+    menu_dict = dict()
+    for menu_list in menu_lists:
+        for item in menu_list:
+            # 去除字符串中的空格
+            item = item.strip()
+            menu_dict[item] = menu_list
+    return menu_dict
