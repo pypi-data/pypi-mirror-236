a38d9eedf8c2b3ce8d559b6c1973fadfde3e7ddc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-18 17:05:46 +0000

    Add package amiyabot/1.7.8/amiyabot-1.7.8-py3-none-any.whl

---
+++ packages/amiyabot/amiyabot-1.7.8-py3-none-any.whl/amiyabot/adapters/tencent/builder.py
@@ -0,0 +1,119 @@
+from amiyabot.adapters import MessageCallback
+from amiyabot.builtin.messageChain import Chain
+from amiyabot.builtin.messageChain.element import *
+
+
+class TencentMessageCallback(MessageCallback):
+    async def recall(self):
+        if not self.response:
+            log.warning('can not recall message because the response is None.')
+            return False
+        await self.instance.recall_message(self.response['id'], self.response['channel_id'])
+
+
+@dataclass
+class MessageSendRequest:
+    data: dict
+    direct: bool
+    user_id: str
+    upload_image: bool = False
+
+
+class MessageSendRequestGroup:
+    def __init__(self, user_id: str, message_id: str, reference: bool, direct: bool):
+        self.req_list: List[MessageSendRequest] = []
+
+        self.text: str = ''
+        self.user_id: str = user_id
+        self.message_id: str = message_id
+        self.reference: bool = reference
+        self.direct: bool = direct
+
+    def __insert_req(self, content: str = '', image: Optional[Union[str, bytes]] = None):
+        # noinspection PyArgumentList
+        req = MessageSendRequest(data={'msg_id': self.message_id}, direct=self.direct, user_id=self.user_id)
+
+        if content:
+            req.data['content'] = content
+
+        if isinstance(image, str):
+            req.data['image'] = image
+
+        if isinstance(image, bytes):
+            req.data['file_image'] = image
+            req.upload_image = True
+
+        if self.reference:
+            req.data['message_reference'] = {
+                'message_id': self.message_id,
+                'ignore_get_message_error': False,
+            }
+
+        self.req_list.append(req)
+
+    def add_text(self, text: str):
+        if self.req_list:
+            req = self.req_list[-1]
+
+            if 'content' not in req.data:
+                req.data['content'] = ''
+
+            req.data['content'] += text
+            return None
+
+        self.text += text
+
+    def add_image(self, image: Union[str, bytes]):
+        self.__insert_req(content=self.text, image=image)
+        self.text = ''
+
+    def done(self):
+        if self.text:
+            self.__insert_req(content=self.text)
+
+
+async def build_message_send(chain: Chain, custom_chain: Optional[CHAIN_LIST] = None):
+    chain_list = custom_chain or chain.chain
+
+    messages = MessageSendRequestGroup(chain.data.user_id, chain.data.message_id, chain.reference, chain.data.is_direct)
+
+    for item in chain_list:
+        # At
+        if isinstance(item, At):
+            messages.add_text(f'<@{item.target}>')
+
+        # AtAll
+        if isinstance(item, AtAll):
+            messages.add_text('<@everyone>')
+
+        # Tag
+        if isinstance(item, Tag):
+            messages.add_text(f'<#{item.target}>')
+
+        # Face
+        if isinstance(item, Face):
+            messages.add_text(f'<emoji:{item.face_id}>')
+
+        # Text
+        if isinstance(item, Text):
+            messages.add_text(item.content)
+
+        # Image
+        if isinstance(item, Image):
+            messages.add_image(await item.get())
+
+        # Voice
+        if isinstance(item, Voice):
+            pass
+
+        # Html
+        if isinstance(item, Html):
+            result = await item.create_html_image()
+            if result:
+                messages.add_image(result)
+            else:
+                log.warning('html convert fail.')
+
+    messages.done()
+
+    return messages
---
+++ packages/amiyabot/amiyabot-1.7.8-py3-none-any.whl/amiyabot/network/httpRequests.py
@@ -0,0 +1,117 @@
+import json
+import aiohttp
+
+from typing import Optional, Union, Any
+from amiyabot import log
+
+
+class HttpRequests:
+    success = [200, 204]
+    async_success = [201, 202, 304023, 304024]
+
+    @classmethod
+    async def request(
+        cls,
+        url: str,
+        method: str = 'post',
+        request_name: Optional[str] = None,
+        ignore_error: bool = False,
+        **kwargs,
+    ):
+        try:
+            request_name = (request_name or method).upper()
+
+            async with aiohttp.ClientSession(trust_env=True) as session:
+                async with session.request(method, url, **kwargs) as res:
+                    if res.status in cls.success:
+                        return await res.text()
+                    if res.status in cls.async_success:
+                        return ''
+                    if not ignore_error:
+                        log.warning(f'Request failed <{url}>[{request_name}]. Got code {res.status} {res.reason}')
+        except aiohttp.ClientConnectorError:
+            if not ignore_error:
+                log.error(f'Unable to request <{url}>[{request_name}]')
+        except Exception as e:
+            if not ignore_error:
+                log.error(e)
+
+    @classmethod
+    async def get(cls, interface: str, params: Optional[Union[dict, list]] = None, **kwargs):
+        return await cls.request(interface, 'get', params=params, **kwargs)
+
+    @classmethod
+    async def post(
+        cls,
+        interface: str,
+        payload: Optional[Union[dict, list]] = None,
+        headers: Optional[dict] = None,
+        **kwargs,
+    ):
+        _headers = {'Content-Type': 'application/json', **(headers or {})}
+        _payload = {**(payload or {})}
+        return await cls.request(
+            interface,
+            'post',
+            request_name='post',
+            data=json.dumps(_payload),
+            headers=_headers,
+            **kwargs,
+        )
+
+    @classmethod
+    async def post_form(
+        cls,
+        interface: str,
+        payload: Optional[dict] = None,
+        headers: Optional[dict] = None,
+        **kwargs,
+    ):
+        _headers = {**(headers or {})}
+
+        data = cls.__build_form_data(payload)
+
+        return await cls.request(interface, 'post', 'post-form', data=data, headers=_headers, **kwargs)
+
+    @classmethod
+    async def post_upload(
+        cls,
+        interface: str,
+        file: bytes,
+        file_field: str = 'file',
+        payload: Optional[dict] = None,
+        headers: Optional[dict] = None,
+        **kwargs,
+    ):
+        _headers = {**(headers or {})}
+
+        data = cls.__build_form_data(payload)
+        data.add_field(file_field, file, content_type='application/octet-stream')
+
+        return await cls.request(interface, 'post', 'post-upload', data=data, headers=_headers, **kwargs)
+
+    @classmethod
+    def __build_form_data(cls, payload: Optional[dict]):
+        data = aiohttp.FormData()
+
+        for field, value in (payload or {}).items():
+            if value is None:
+                continue
+            if type(value) in [dict, list]:
+                value = json.dumps(value, ensure_ascii=False)
+            data.add_field(field, value)
+
+        return data
+
+
+class ResponseException(Exception):
+    def __init__(self, code: int, message: str, data: Optional[Any] = None):
+        self.code = code
+        self.message = message
+        self.data = data
+
+    def __str__(self):
+        return f'[{self.code}] {self.message} -- data: {self.data}'
+
+
+http_requests = HttpRequests
