190268d9a65321f8af1d37544ba54cd8284b2261
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-18 14:05:09 +0000

    Add package sagemaker-jupyterlab-extension-common/0.1.1/sagemaker_jupyterlab_extension_common-0.1.1-py3-none-any.whl

---
+++ packages/sagemaker-jupyterlab-extension-common/sagemaker_jupyterlab_extension_common-0.1.1-py3-none-any.whl/sagemaker_jupyterlab_extension_common/constants.py
@@ -0,0 +1,10 @@
+SERVER_LOG_FILE_NAME = "jupyter-server.log"
+API_LOG_FILE_NAME = "jupyter-server.api.log"
+REQUEST_LOG_FILE_NAME = "jupyter-server.requests.log"
+LOGFILE_ENV_NAME = "SAGEMAKER_LOG_FILE"
+SCHEMAS_DIR = "schemas/"
+SERVER_LOG_SCHEMA = "http://sagemaker.studio.jupyterserver.log.schema"
+HANDLER_METRICS_SCHEMA = "http://sagemaker.studio.jupyterserver.api.metric.schema"
+REQUEST_METRICS_SCHEMA = (
+    "http://sagemaker.studio.jupyterserver.httprequest.metric.schema"
+)
