6ea1815d4b766f9f8f02714597fcfb7150b841d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-19 03:10:35 +0000

    Add package mangoplateapi/1.0.8/mangoplateapi-1.0.8-py3-none-any.whl

---
+++ packages/mangoplateapi/mangoplateapi-1.0.8-py3-none-any.whl/mangoplateapi/mixins/review.py
@@ -0,0 +1,21 @@
+import requests
+
+
+class ReviewMixin:
+    def get_reviews(self, restaurant_code, start_index: int, page_size: int, proxy=None):
+        url = f"https://stage.mangoplate.com/api/v5/restaurants/{restaurant_code}/reviews.json"
+        data = {
+            "language": "kor",
+            "start_index": start_index,
+            "request_count": page_size,
+        }
+        response = requests.get(url, data=data, proxies={"http":proxy})
+        response_dict = response.json()
+        
+        # ID 값 설정
+        for item in response_dict:
+            _id = item['action_id']
+            item['_id'] = _id
+            item['id'] = _id
+
+        return response_dict
