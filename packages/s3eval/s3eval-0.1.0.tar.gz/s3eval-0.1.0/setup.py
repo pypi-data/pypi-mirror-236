06eb5a3dcdf64d4dc6c4a8ae09a0824836a7f5d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-19 15:05:47 +0000

    Add package s3eval/0.1.0/s3eval-0.1.0.tar.gz

---
+++ packages/s3eval/s3eval-0.1.0.tar.gz/s3eval-0.1.0/setup.py
@@ -0,0 +1,57 @@
+from pathlib import Path
+from setuptools import find_packages, setup
+
+project_root = Path(__file__).parent
+install_requires = [
+    "pandas",
+    "sqlite3"
+]
+
+setup(
+    name="s3eval",
+    version="0.1.0",
+    python_requires=">=3.7",
+    description="S3Eval: A Synthetic, Scalable and Systematic Suite for Evaluating Language Models",
+    author="Fangyu Lei, Qian Liu",
+    author_email="leifangyu@qq.com",
+    long_description=(project_root / "README.md").read_text(encoding="utf-8"),
+    long_description_content_type="text/markdown",
+    url="https://github.com/lfy79001/S3Eval",
+    license="Apache-2.0 License",
+    packages=find_packages(exclude=["test", "test.*"]),
+    # package_dir={"": "src"},
+    # package_data={
+    #     "tabgenie": ["static/css/*", "static/img/*", "static/js/*", "templates/*"],
+    # },
+    # data_files=[("tabgenie", ["tabgenie/config.yml"])],
+    include_package_data=True,
+    entry_points={
+        "console_scripts": [
+            "tabgenie=tabgenie.cli:run",
+        ],
+        "flask.commands": ["export=tabgenie.cli:export", "sheet=tabgenie.cli:sheet", "info=tabgenie.cli:info"],
+    },
+    install_requires=install_requires,
+    extras_require={
+        "dev": [
+            "wheel",
+            "black",
+        ],
+        "deploy": [
+            "gunicorn",
+        ],
+    },
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Intended Audience :: Science/Research",
+        "Operating System :: POSIX :: Linux",
+        "License :: OSI Approved :: Apache Software License",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Typing :: Typed",
+    ],
+)
