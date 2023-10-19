456180abcd3713d13fb1dabe2f15fdfd3bce83d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-19 06:07:21 +0000

    Add package ansys-fluent-core/0.19.dev1/ansys-fluent-core-0.19.dev1.tar.gz

---
+++ packages/ansys-fluent-core/ansys-fluent-core-0.19.dev1.tar.gz/ansys-fluent-core-0.19.dev1/src/ansys/fluent/core/solver/settings_232/defaults_child.py
@@ -0,0 +1,21 @@
+#
+# This is an auto-generated file.  DO NOT EDIT!
+#
+
+from ansys.fluent.core.solver.flobject import *
+
+from ansys.fluent.core.solver.flobject import _ChildNamedObjectAccessorMixin
+
+from ansys.fluent.core.solver.flobject import _CreatableNamedObjectMixin
+
+from ansys.fluent.core.solver.flobject import _NonCreatableNamedObjectMixin
+
+from ansys.fluent.core.solver.flobject import _HasAllowedValuesMixin
+
+class defaults_child(Real):
+    """
+    'child_object_type' of /home/ansys/actions-runner/_work/pyfluent/pyfluent/src/ansys/fluent/core/solver/settings_232/defaults.
+    """
+
+    fluent_name = "child-object-type"
+
