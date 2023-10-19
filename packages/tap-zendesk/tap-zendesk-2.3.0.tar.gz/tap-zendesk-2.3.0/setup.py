9a0864906124c6edc2aa3251c4d25a0a0f799454
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-19 04:05:57 +0000

    Add package tap-zendesk/2.3.0/tap-zendesk-2.3.0.tar.gz

---
+++ packages/tap-zendesk/tap-zendesk-2.3.0.tar.gz/tap-zendesk-2.3.0/setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='tap-zendesk',
+      version='2.3.0',
+      description='Singer.io tap for extracting data from the Zendesk API',
+      author='Stitch',
+      url='https://singer.io',
+      classifiers=['Programming Language :: Python :: 3 :: Only'],
+      py_modules=['tap_zendesk'],
+      install_requires=[
+          'singer-python==5.12.2',
+          'zenpy==2.0.24',
+          'backoff==1.8.0',
+          'requests==2.25.1',
+      ],
+      extras_require={
+          'dev': [
+              'ipdb',
+          ],
+          'test': [
+              'pylint==2.8.3',
+              'nose',
+              'nose-watch',
+              'pytest'
+          ]
+      },
+      entry_points='''
+          [console_scripts]
+          tap-zendesk=tap_zendesk:main
+      ''',
+      packages=['tap_zendesk'],
+      include_package_data=True,
+)
