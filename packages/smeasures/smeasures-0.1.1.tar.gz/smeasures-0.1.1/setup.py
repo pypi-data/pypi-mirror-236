1fda88774e7dbc61a52abfb388996ea32a23c71d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-19 15:05:55 +0000

    Add package smeasures/0.1.1/smeasures-0.1.1.tar.gz

---
+++ packages/smeasures/smeasures-0.1.1.tar.gz/smeasures-0.1.1/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='smeasures',
+    version='0.1.1',
+    description='Python package to easily capture respource utilization and time taken for given code block or method.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    install_requires=[
+        # List your package dependencies here
+        'psutil',
+        'threading',
+        'gc'
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+    url='https://github.com/csseeker/smeasures',
+    author='Shripad Kandharkar',
+    author_email='shripad.kandharkar@gmail.com',
+)
