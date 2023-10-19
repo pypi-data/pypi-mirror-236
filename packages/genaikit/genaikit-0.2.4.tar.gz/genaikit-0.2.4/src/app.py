7e32e644d7b395ba9a37331936231b0529e184da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-19 03:09:11 +0000

    Add package genaikit/0.2.4/genaikit-0.2.4.tar.gz

---
+++ packages/genaikit/genaikit-0.2.4.tar.gz/genaikit-0.2.4/src/app.py
@@ -0,0 +1,21 @@
+"""
+    Package aissistant
+
+    This can be an sample app using the package
+    or the app itself to feed the __main__ and gui
+    modules.
+"""
+
+import logging
+from logging.config import dictConfig
+
+from genaikit import CONFIG_LOG
+
+dictConfig(CONFIG_LOG)
+
+logger = logging.getLogger('client')
+report = logging.getLogger('report')
+
+class MyApp:
+    def __init__(self, *args, **kwargs):
+        pass
