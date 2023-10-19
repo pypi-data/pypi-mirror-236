14576b90f5d10ffd7a3274196ef771143a5482b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-19 04:06:02 +0000

    Add package mqtasks/0.0.1.dev28/mqtasks-0.0.1.dev28-py3-none-any.whl

---
+++ packages/mqtasks/mqtasks-0.0.1.dev28-py3-none-any.whl/example/example_client.py
@@ -0,0 +1,37 @@
+import asyncio
+
+from example.example_config import CONNECTION, QUEUE_NANE
+from mqtasks import MqTasksClient
+
+loop = asyncio.get_event_loop()
+client = MqTasksClient(
+    loop=loop,
+    amqp_connection=CONNECTION,
+    verbose=True
+)
+
+
+async def main_async(task_name: str, body: str | object | None = None) -> None:
+    channel = await client.queue(QUEUE_NANE)
+    response = await channel.run_task_async(
+        task_name=task_name,
+        body=body,
+        message_handler=lambda msg: print(msg)
+    )
+    print(response)
+
+
+# ==============================================================================
+
+
+loop.run_until_complete(main_async(task_name="hello_sync", body={"message": "hello sync task1"}))
+loop.run_until_complete(main_async(task_name="hello_async", body={"message": "hello async task2"}))
+loop.run_until_complete(main_async(task_name="hello_sync", body={"message": "hello sync task3"}))
+loop.run_until_complete(main_async(task_name="hello_async", body={"message": "hello async task4"}))
+
+loop.run_until_complete(main_async(task_name="data_async", body={"message": "async progress task"}))
+
+
+loop.run_until_complete(client.close())
+
+loop.close()
