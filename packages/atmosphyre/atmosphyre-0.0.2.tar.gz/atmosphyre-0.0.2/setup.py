50ae85113e1a283d83005fa4c8fce5b631f35558
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-19 16:07:42 +0000

    Add package atmosphyre/0.0.2/atmosphyre-0.0.2.tar.gz

---
+++ packages/atmosphyre/atmosphyre-0.0.2.tar.gz/atmosphyre-0.0.2/setup.py
@@ -0,0 +1,17 @@
+import setuptools 
+  
+with open("README.md", "r") as fh: 
+    description = fh.read() 
+  
+setuptools.setup( 
+    name="atmosphyre", 
+    version="0.0.2", 
+    author="Jay Stephan", 
+    author_email="Jay.Stephan@stfc.ac.uk", 
+    packages=["atmosphyre"], 
+    description="A sample test package", 
+    long_description=description, 
+    long_description_content_type="text/markdown", 
+    license='MIT', 
+    install_requires=['numpy'] 
+) 
