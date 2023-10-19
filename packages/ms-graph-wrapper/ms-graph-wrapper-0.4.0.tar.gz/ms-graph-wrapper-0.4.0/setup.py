4f3fb2493964825825114dd062086644e38bd2ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-19 03:11:06 +0000

    Add package ms-graph-wrapper/0.4.0/ms-graph-wrapper-0.4.0.tar.gz

---
+++ packages/ms-graph-wrapper/ms-graph-wrapper-0.4.0.tar.gz/ms-graph-wrapper-0.4.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ms-graph-wrapper",
+    version="0.4.0",
+    author="Sanket Joshi",
+    description="A Python wrapper for the Microsoft Graph API that simplifies pagination and provides a more Pythonic interface.",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    fullname="ms_graph_wrapper",
+    install_requires=["requests","msal","configparser","json"],
+)
