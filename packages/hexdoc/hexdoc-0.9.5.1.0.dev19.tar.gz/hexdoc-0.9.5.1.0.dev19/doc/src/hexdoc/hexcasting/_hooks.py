945e5452af5d1cc5653bee925a847aa7a5874d83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-19 15:07:43 +0000

    Add package hexdoc/0.9.5.1.0.dev19/hexdoc-0.9.5.1.0.dev19.tar.gz

---
+++ packages/hexdoc/hexdoc-0.9.5.1.0.dev19.tar.gz/hexdoc-0.9.5.1.0.dev19/doc/src/hexdoc/hexcasting/_hooks.py
@@ -0,0 +1,47 @@
+from importlib.resources import Package
+
+import hexdoc
+from hexdoc.__gradle_version__ import GRADLE_VERSION
+from hexdoc.plugin import (
+    HookReturn,
+    LoadJinjaTemplatesImpl,
+    LoadResourceDirsImpl,
+    LoadTaggedUnionsImpl,
+    ModVersionImpl,
+    hookimpl,
+)
+
+from . import hex_recipes
+from .page import hex_pages
+
+
+class HexcastingPlugin(
+    LoadResourceDirsImpl,
+    LoadTaggedUnionsImpl,
+    ModVersionImpl,
+    LoadJinjaTemplatesImpl,
+):
+    @staticmethod
+    @hookimpl
+    def hexdoc_mod_version() -> str:
+        return GRADLE_VERSION
+
+    @staticmethod
+    @hookimpl
+    def hexdoc_load_resource_dirs() -> HookReturn[Package]:
+        # lazy import because generated may not exist when this file is loaded
+        # eg. when generating the contents of generated
+        # so we only want to import it if we actually need it
+        from hexdoc._export import generated, resources
+
+        return [generated, resources]
+
+    @staticmethod
+    @hookimpl
+    def hexdoc_load_tagged_unions() -> HookReturn[Package]:
+        return [hex_recipes, hex_pages]
+
+    @staticmethod
+    @hookimpl
+    def hexdoc_load_jinja_templates() -> HookReturn[tuple[Package, str]]:
+        return hexdoc, "_templates"
