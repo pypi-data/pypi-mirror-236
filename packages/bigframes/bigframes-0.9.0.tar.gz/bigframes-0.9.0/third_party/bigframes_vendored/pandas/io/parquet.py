0b21fdf2bc028b90a6689229d7e81ac0f946d8ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-19 05:06:24 +0000

    Add package bigframes/0.9.0/bigframes-0.9.0.tar.gz

---
+++ packages/bigframes/bigframes-0.9.0.tar.gz/bigframes-0.9.0/third_party/bigframes_vendored/pandas/io/parquet.py
@@ -0,0 +1,27 @@
+# Contains code from https://github.com/pandas-dev/pandas/blob/main/pandas/io/parquet.py
+""" parquet compat """
+from __future__ import annotations
+
+from bigframes import constants
+
+
+class ParquetIOMixin:
+    def read_parquet(
+        self,
+        path: str,
+    ):
+        r"""Load a Parquet object from the file path (local or Cloud Storage), returning a DataFrame.
+
+        .. note::
+            This method will not guarantee the same ordering as the file.
+            Instead, set a serialized index column as the index and sort by
+            that in the resulting DataFrame.
+
+        Args:
+            path (str):
+                Local or Cloud Storage path to Parquet file.
+
+        Returns:
+            bigframes.dataframe.DataFrame: A BigQuery DataFrames.
+        """
+        raise NotImplementedError(constants.ABSTRACT_METHOD_ERROR_MESSAGE)
