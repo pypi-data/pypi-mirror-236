b1d4a589e3d15d965c7a9fbc73c5fa685cb421a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-19 15:06:24 +0000

    Add package cloudnetpy/1.55.16/cloudnetpy-1.55.16.tar.gz

---
+++ packages/cloudnetpy/cloudnetpy-1.55.16.tar.gz/cloudnetpy-1.55.16/cloudnetpy/exceptions.py
@@ -0,0 +1,46 @@
+class InconsistentDataError(Exception):
+    """Internal exception class."""
+
+    def __init__(self, msg: str = ""):
+        self.message = msg
+        super().__init__(self.message)
+
+
+class DisdrometerDataError(Exception):
+    """Internal exception class."""
+
+    def __init__(self, msg: str = ""):
+        self.message = msg
+        super().__init__(self.message)
+
+
+class WeatherStationDataError(Exception):
+    """Internal exception class."""
+
+    def __init__(self, msg: str = ""):
+        self.message = msg
+        super().__init__(self.message)
+
+
+class ModelDataError(Exception):
+    """Internal exception class."""
+
+    def __init__(self, msg: str = "Invalid model file: not enough proper profiles"):
+        self.message = msg
+        super().__init__(self.message)
+
+
+class ValidTimeStampError(Exception):
+    """Internal exception class."""
+
+    def __init__(self, msg: str = "No valid timestamps found"):
+        self.message = msg
+        super().__init__(self.message)
+
+
+class MissingInputFileError(Exception):
+    """Internal exception class."""
+
+    def __init__(self, msg: str = "Missing required input files"):
+        self.message = msg
+        super().__init__(self.message)
