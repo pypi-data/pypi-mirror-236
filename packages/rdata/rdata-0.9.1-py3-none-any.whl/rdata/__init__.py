ca6f7914a06c896d4743a2d97dffbd193d869653
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-19 16:06:46 +0000

    Add package rdata/0.9.1/rdata-0.9.1-py3-none-any.whl

---
+++ packages/rdata/rdata-0.9.1-py3-none-any.whl/rdata/__init__.py
@@ -0,0 +1,18 @@
+"""rdata: Read R datasets from Python."""
+import os as _os
+import pathlib as _pathlib
+
+from . import conversion, parser
+
+
+def _get_test_data_path() -> _pathlib.Path:
+    return _pathlib.Path(_os.path.dirname(__file__)) / "tests" / "data"
+
+
+TESTDATA_PATH = _get_test_data_path()
+"""
+Path of the test data.
+
+"""
+
+__version__ = "0.9.1"
