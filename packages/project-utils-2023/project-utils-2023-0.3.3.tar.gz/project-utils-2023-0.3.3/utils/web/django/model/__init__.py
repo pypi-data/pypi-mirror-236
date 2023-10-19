6ea4f7b4105f76a3179fe16b5d31acdd8c8ca5b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-19 16:06:43 +0000

    Add package project-utils-2023/0.3.3/project-utils-2023-0.3.3.tar.gz

---
+++ packages/project-utils-2023/project-utils-2023-0.3.3.tar.gz/project-utils-2023-0.3.3/utils/web/django/model/__init__.py
@@ -0,0 +1,12 @@
+from ._base_model import BaseModel
+from ._base_meta import BaseMeta
+from ._id_model import IDModel
+
+Model = BaseModel
+
+__all__ = [
+    "Model",
+    "BaseModel",
+    "BaseMeta",
+    "IDModel",
+]
