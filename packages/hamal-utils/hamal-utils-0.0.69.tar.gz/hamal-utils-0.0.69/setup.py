3295629f69f2e6cfd12c5fd083678a9754b0e21d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-19 04:07:12 +0000

    Add package hamal-utils/0.0.69/hamal-utils-0.0.69.tar.gz

---
+++ packages/hamal-utils/hamal-utils-0.0.69.tar.gz/hamal-utils-0.0.69/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+from hamal_utils.code.version import __VERSION__
+
+setup(
+    name='hamal-utils',
+    version=__VERSION__,
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+        'boto3',
+        'prefect',
+        'numpy',
+        'gitversion',
+    ],
+    author_email='daniel@cain-technologies.com',
+    license='MIT',
+)
