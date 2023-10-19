025dcecc3879523a49736fa85bb88658d2d8ea2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-19 04:06:01 +0000

    Add package jupyterlab-ca-theme/0.3.0/jupyterlab_ca_theme-0.3.0-py3-none-any.whl

---
+++ packages/jupyterlab-ca-theme/jupyterlab_ca_theme-0.3.0-py3-none-any.whl/jupyterlab_ca_theme-0.3.0.data/data/share/jupyter/labextensions/jupyterlab-ca-theme/static/lib_index_js.8a795686dbdd04582b89.js
@@ -0,0 +1,42 @@
+"use strict";
+(self["webpackChunkjupyterlab_ca_theme"] = self["webpackChunkjupyterlab_ca_theme"] || []).push([["lib_index_js"],{
+
+/***/ "./lib/index.js":
+/*!**********************!*\
+  !*** ./lib/index.js ***!
+  \**********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var _jupyterlab_apputils__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @jupyterlab/apputils */ "webpack/sharing/consume/default/@jupyterlab/apputils");
+/* harmony import */ var _jupyterlab_apputils__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_jupyterlab_apputils__WEBPACK_IMPORTED_MODULE_0__);
+
+/**
+ * Initialization data for the jupyterlab-ca-theme extension.
+ */
+const plugin = {
+    id: 'jupyterlab-ca-theme:plugin',
+    description: 'A JupyterLab theme extension for Composable Analytics.',
+    autoStart: true,
+    requires: [_jupyterlab_apputils__WEBPACK_IMPORTED_MODULE_0__.IThemeManager],
+    activate: (app, manager) => {
+        console.log('JupyterLab extension jupyterlab-ca-theme is activated!');
+        const style = 'jupyterlab-ca-theme/index.css';
+        manager.register({
+            name: 'jupyterlab-ca-theme',
+            isLight: true,
+            load: () => manager.loadCSS(style),
+            unload: () => Promise.resolve(undefined)
+        });
+    }
+};
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (plugin);
+
+
+/***/ })
+
+}]);
+//# sourceMappingURL=lib_index_js.8a795686dbdd04582b89.js.map
