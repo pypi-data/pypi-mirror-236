27faccd2c07931e054fc1b9b567e2318db95c09e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-19 04:05:49 +0000

    Add package neos_common/0.10.25/neos_common-0.10.25-py3-none-any.whl

---
+++ packages/neos_common/neos_common-0.10.25-py3-none-any.whl/neos_common/client/__init__.py
@@ -0,0 +1,3 @@
+from neos_common.client.iam_client import IAMClient  # noqa: F401
+from neos_common.client.keycloak_client import KeycloakClient  # noqa: F401
+from neos_common.client.registry_client import RegistryClient  # noqa: F401
