70df5751ea418b3f9394dda4fd1f8cd394836fae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-19 03:10:33 +0000

    Add package mangoplateapi/1.0.7/mangoplateapi-1.0.7.tar.gz

---
+++ packages/mangoplateapi/mangoplateapi-1.0.7.tar.gz/mangoplateapi-1.0.7/mangoplateapi/mixins/code.py
@@ -0,0 +1,39 @@
+import base64
+
+import requests
+
+
+def extract_code_id(data):
+    return data['type_name'], data['display_text']
+def encode_to_base64(input_string):
+    try:
+        # 문자열을 바이트로 인코딩합니다.
+        input_bytes = input_string.encode('utf-8')
+
+        # base64로 인코딩합니다.
+        encoded_bytes = base64.b64encode(input_bytes)
+
+        # 바이트를 문자열로 디코딩합니다.
+        encoded_string = encoded_bytes.decode('utf-8')
+
+        return encoded_string
+    except Exception as e:
+        return str(e)
+
+class CodeMixin:
+    def get_codes(self, proxy=None):
+        url = f"https://stage.mangoplate.com/api/common/codetable.js"
+        data = {
+            "language": "kor",
+            "order_by": "2"
+        }
+        response = requests.get(url, data=data, proxy={"http":proxy})
+        codes = response.json()
+
+        # ID 설정
+        for code in codes:
+            keyword, code_status = extract_code_id(code)
+            id = encode_to_base64(f'{keyword}_{code_status}')
+            code['_id'] = id
+            code['id'] = id
+        return codes
