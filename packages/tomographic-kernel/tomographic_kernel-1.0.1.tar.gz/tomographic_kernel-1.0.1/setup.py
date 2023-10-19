1ba69c22d416de2e1dfd727b909250f227b5eecf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-19 03:07:33 +0000

    Add package tomographic-kernel/1.0.1/tomographic_kernel-1.0.1.tar.gz

---
+++ packages/tomographic-kernel/tomographic_kernel-1.0.1.tar.gz/tomographic_kernel-1.0.1/setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+from setuptools import find_packages
+from setuptools import setup
+
+install_requires = [
+    'numpy',
+    'h5parm>=1.0.5',
+    'scipy',
+    'astropy',
+    'matplotlib',
+    'cmocean',
+    'tqdm',
+    'jax',
+    'jaxlib',
+    'tables',
+    'tensorflow_probability'
+]
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(name='tomographic_kernel',
+      version='1.0.1',
+      description='A Tomographic Kernel in JAX for tomographic Gaussian processes.',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url="https://github.com/JoshuaAlbert/tomographic_kernel",
+      author='Joshua G. Albert',
+      author_email='albert@strw.leidenuniv.nl',
+      install_requires=install_requires,
+      tests_require=[
+          'pytest>=2.8',
+      ],
+      package_dir={'': './'},
+      packages=find_packages('./'),
+      classifiers=[
+          "Programming Language :: Python :: 3",
+          "License :: OSI Approved :: Apache Software License",
+          "Operating System :: OS Independent",
+      ],
+      python_requires='>=3.8'
+      )
