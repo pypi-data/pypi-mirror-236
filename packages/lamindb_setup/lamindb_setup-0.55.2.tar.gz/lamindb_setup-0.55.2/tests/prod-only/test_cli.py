5deb852b702fe4eb002aaab8ea40f38b5d9b1739
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-19 16:06:49 +0000

    Add package lamindb_setup/0.55.2/lamindb_setup-0.55.2.tar.gz

---
+++ packages/lamindb_setup/lamindb_setup-0.55.2.tar.gz/lamindb_setup-0.55.2/tests/prod-only/test_cli.py
@@ -0,0 +1,18 @@
+import os
+
+
+def test_entrypoint():
+    exit_status = os.system("lamin --help")
+    assert exit_status == 0
+
+
+def test_migrate_create():
+    exit_status = os.system("lamin migrate create")
+    assert exit_status == 0
+
+
+# def test_migrate_squash():
+#     exit_status = os.system(
+#         "yes | lamin migrate squash --package-name lnschema_core --end-number 0023"
+#     )
+#     assert exit_status == 0
