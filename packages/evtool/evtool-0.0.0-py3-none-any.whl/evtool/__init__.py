55738edbf9ae94df1a6b34bad0a56224c8e15c15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-19 04:06:41 +0000

    Add package evtool/0.0.0/evtool-0.0.0-py3-none-any.whl

---
+++ packages/evtool/evtool-0.0.0-py3-none-any.whl/evtool/__init__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+# SPDX-License-Identifier: EUPL-1.2
+#  
+#  Copyright (c) 2023  Marc van der Sluys - Nikhef/Utrecht University - marc.vandersluys.nl
+#   
+#  This file is part of the evTool Python package:
+#  Analyse and display output from the binary stellar-evolution code ev (a.k.a. STARS or TWIN).
+#  See: https://github.com/MarcvdSluys/evTool-python
+#   
+#  This is free software: you can redistribute it and/or modify it under the terms of the European Union
+#  Public Licence 1.2 (EUPL 1.2).  This software is distributed in the hope that it will be useful, but
+#  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+#  PURPOSE.  See the EU Public Licence for more details.  You should have received a copy of the European
+#  Union Public Licence along with this code.  If not, see <https://www.eupl.eu/1.2/en/>.
+
+
+"""evTool package
+
+Analyse and display output from the binary stellar-evolution code ev (a.k.a. STARS or TWIN).
+
+The code is being developed by `Marc van der Sluys <http://marc.vandersluys.nl>`_ of the Department of Physics
+at Utrecht University and the Netherlands Institute of Nuclear and High-Energy Physics (Nikhef), both The
+Netherlands.  The evTool package can be used under the conditions of the EUPL 1.2 licence.  These pages
+contain the API documentation.  For more information on the Python package, licence and source code, see the
+`evTool GitHub page <https://github.com/MarcvdSluys/evTool-python>`_.
+"""
+
+
+name = 'evtool'
+
