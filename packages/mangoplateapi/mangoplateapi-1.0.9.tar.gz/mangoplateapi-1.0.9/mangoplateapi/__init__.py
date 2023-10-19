28ea5d2172e3ae802179b8ea7ee8615c49e3f4c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-19 03:10:49 +0000

    Add package mangoplateapi/1.0.9/mangoplateapi-1.0.9.tar.gz

---
+++ packages/mangoplateapi/mangoplateapi-1.0.9.tar.gz/mangoplateapi-1.0.9/mangoplateapi/__init__.py
@@ -0,0 +1,21 @@
+import logging
+
+from mangoplateapi.mixins.code import CodeMixin
+from mangoplateapi.mixins.keyword import KeywordMixin
+from mangoplateapi.mixins.restaurant import RestaurantMixin
+from mangoplateapi.mixins.review import ReviewMixin
+from mangoplateapi.mixins.story import StoryMixin
+from mangoplateapi.mixins.theme import ThemeMixin
+
+DEFAULT_LOGGER = logging.getLogger("mangoplateapi")
+
+
+class Client(
+    CodeMixin,
+    RestaurantMixin,
+    ReviewMixin,
+    StoryMixin,
+    ThemeMixin,
+    KeywordMixin
+):
+    pass
