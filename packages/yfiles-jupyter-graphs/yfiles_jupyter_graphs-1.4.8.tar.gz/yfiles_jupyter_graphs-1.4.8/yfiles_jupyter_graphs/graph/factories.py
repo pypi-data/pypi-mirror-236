1f7e0b05ec0e3342b4091618a9fbca3d932801d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-19 03:09:47 +0000

    Add package yfiles-jupyter-graphs/1.4.8/yfiles_jupyter_graphs-1.4.8.tar.gz

---
+++ packages/yfiles-jupyter-graphs/yfiles_jupyter_graphs-1.4.8.tar.gz/yfiles_jupyter_graphs-1.4.8/yfiles_jupyter_graphs/graph/factories.py
@@ -0,0 +1,22 @@
+"""
+Includes corresponding factories for the graph importers.
+"""
+
+from .importer import \
+    GraphToolsGraphImporter, \
+    IGraphGraphImporter, \
+    NetworkxGraphImporter, \
+    Neo4jGraphImporter, \
+    PyGraphvizGraphImporter
+from .interfaces import \
+    GraphImporterInterface
+
+def _get_importer(graph) -> GraphImporterInterface:
+    """function to select factory based on graph type"""
+    importers = [GraphToolsGraphImporter(), IGraphGraphImporter(), NetworkxGraphImporter(), PyGraphvizGraphImporter(), Neo4jGraphImporter()]
+
+    for importer in importers:
+        if importer.isInstance(graph):
+            return importer
+
+    raise NotImplementedError('Could not find a graph importer factory for type {}'.format(type(graph)))
