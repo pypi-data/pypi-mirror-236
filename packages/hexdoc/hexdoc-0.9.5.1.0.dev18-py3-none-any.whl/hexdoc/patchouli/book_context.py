29bdf47a796fd61b3d03c4d31eeb1a1b47a9c8f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-19 14:07:07 +0000

    Add package hexdoc/0.9.5.1.0.dev18/hexdoc-0.9.5.1.0.dev18-py3-none-any.whl

---
+++ packages/hexdoc/hexdoc-0.9.5.1.0.dev18-py3-none-any.whl/hexdoc/patchouli/book_context.py
@@ -0,0 +1,23 @@
+from typing import Self
+
+from pydantic import Field, model_validator
+
+from hexdoc.minecraft import Tag
+from hexdoc.plugin.manager import PluginManagerContext
+from hexdoc.utils import ResourceLocation
+
+from .text.formatting import FormattingContext
+
+
+class BookContext(FormattingContext, PluginManagerContext):
+    spoilered_advancements: set[ResourceLocation] = Field(default_factory=set)
+
+    @model_validator(mode="after")
+    def _post_root_load_tags(self) -> Self:
+        self.spoilered_advancements |= Tag.load(
+            registry="hexdoc",
+            id=ResourceLocation("hexcasting", "spoilered_advancements"),
+            context=self,
+        ).values
+
+        return self
