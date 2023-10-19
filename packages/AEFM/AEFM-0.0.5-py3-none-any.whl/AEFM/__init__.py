bc3096def21836501a4c3b57233c94a6fa0f6959
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-19 15:06:39 +0000

    Add package AEFM/0.0.5/AEFM-0.0.5-py3-none-any.whl

---
+++ packages/AEFM/AEFM-0.0.5-py3-none-any.whl/AEFM/__init__.py
@@ -0,0 +1,15 @@
+from . import configs
+from . import data_collector
+from . import deployer
+from . import inf_generator
+from . import manager
+from . import models
+from . import utils
+from . import workload_generator
+from typing import Literal
+
+
+def set_log_level(level: Literal["debug", "info", "key", "warn", "error", "off"]):
+    from .utils.logger import log
+
+    log.level = level
