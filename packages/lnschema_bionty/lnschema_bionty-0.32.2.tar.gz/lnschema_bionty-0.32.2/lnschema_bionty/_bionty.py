ca4b64149027628022c161fcb9557d40a3244a53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-19 03:08:17 +0000

    Add package lnschema_bionty/0.32.2/lnschema_bionty-0.32.2.tar.gz

---
+++ packages/lnschema_bionty/lnschema_bionty-0.32.2.tar.gz/lnschema_bionty-0.32.2/lnschema_bionty/_bionty.py
@@ -0,0 +1,117 @@
+from typing import Dict, Optional, Union
+
+import bionty as bt
+from django.core.exceptions import ObjectDoesNotExist
+from lamin_utils import logger
+from lnschema_core.models import Registry  # TODO: import Registry instead of ORM
+
+from . import ids
+
+
+def create_or_get_species_record(species: Optional[Union[str, Registry]], orm: Registry) -> Optional[Registry]:
+    # return None if an Registry doesn't have species field
+    species_record = None
+    if hasattr(orm, "species"):
+        # using global setting of species
+        from .dev._settings import settings
+
+        if species is None and settings.species is not None:
+            logger.debug(f"using global setting species = {settings.species.name}")
+            return settings.species
+
+        if isinstance(species, Registry):
+            species_record = species
+        elif isinstance(species, str):
+            from lnschema_bionty import Species
+
+            try:
+                # existing species record
+                species_record = Species.objects.get(name=species)
+            except ObjectDoesNotExist:
+                try:
+                    # create a species record from bionty reference
+                    species_record = Species.from_bionty(name=species)
+                    # link the species record to the default bionty source
+                    species_record.bionty_source = get_bionty_source_record(bt.Species())  # type:ignore
+                    species_record.save()  # type:ignore
+                except KeyError:
+                    # no such species is found in bionty reference
+                    species_record = None
+
+        if species_record is None:
+            raise AssertionError(f"{orm.__name__} requires to specify a species name via `species=` or `lb.settings.species=`!")
+
+    return species_record
+
+
+def get_bionty_source_record(bionty_object: bt.Bionty):
+    kwargs = dict(
+        entity=bionty_object.__class__.__name__,
+        species=bionty_object.species,
+        source=bionty_object.source,
+        version=bionty_object.version,
+    )
+    from .models import BiontySource
+
+    source_record = BiontySource.objects.filter(**kwargs).get()
+    return source_record
+
+
+def encode_uid(orm: Registry, kwargs: dict):
+    if kwargs.get("uid") is not None:
+        # if uid is passed
+        return kwargs
+    try:
+        name = orm.__name__.lower()
+    except AttributeError:
+        name = orm.__class__.__name__.lower()
+    ontology = False
+    concat_str = ""
+    if name == "gene":
+        concat_str = f"{kwargs.get('stable_id', '')}{kwargs.get('ensembl_gene_id', '')}{kwargs.get('symbol', '')}"
+    elif name == "protein":
+        concat_str = kwargs.get("uniprotkb_id", "")
+    elif name == "cellmarker":
+        concat_str = kwargs.get("name", "")
+    elif name == "biontysource":
+        concat_str = f'{kwargs.get("entity", "")}{kwargs.get("source", "")}{kwargs.get("species", "")}{kwargs.get("version", "")}'  # noqa
+    elif kwargs.get("ontology_id") is not None:
+        concat_str = f"{kwargs.get('name', '')}{kwargs.get('ontology_id', '')}"
+        ontology = True
+    elif (kwargs.get("id") is not None) and (name == "species") and (kwargs.get("uid") is None):
+        # species, it's not "uid", here, but the taxon id
+        concat_str = kwargs.pop("id")
+    if len(concat_str) > 0:
+        if ontology:
+            id_encoder = ids.ontology
+        else:
+            try:
+                id_encoder = getattr(ids, name)
+            except Exception:
+                return kwargs
+        kwargs["uid"] = id_encoder(concat_str)
+    return kwargs
+
+
+def lookup2kwargs(orm: Registry, *args, **kwargs) -> Dict:
+    """Pass bionty search/lookup results."""
+    arg = args[0]
+    if isinstance(arg, tuple):
+        bionty_kwargs = arg._asdict()  # type:ignore
+    else:
+        bionty_kwargs = arg[0]._asdict()
+
+    if len(bionty_kwargs) > 0:
+        import bionty as bt
+
+        # add species and bionty_source
+        species_record = create_or_get_species_record(orm=orm.__class__, species=kwargs.get("species"))
+        if species_record is not None:
+            bionty_kwargs["species"] = species_record
+        bionty_object = getattr(bt, orm.__class__.__name__)(species=species_record.name if species_record is not None else None)
+        bionty_kwargs["bionty_source"] = get_bionty_source_record(bionty_object)
+
+        model_field_names = {i.name for i in orm._meta.fields}
+        model_field_names.add("parents")
+        bionty_kwargs = {k: v for k, v in bionty_kwargs.items() if k in model_field_names}
+    return encode_uid(orm=orm, kwargs=bionty_kwargs)
---
+++ packages/lnschema_bionty/lnschema_bionty-0.32.2.tar.gz/lnschema_bionty-0.32.2/lnschema_bionty/dev/_settings.py
@@ -0,0 +1,61 @@
+from typing import Optional, Union
+
+from lamin_utils import logger
+
+from ..models import Species
+
+
+class Settings:
+    """Settings.
+
+    Directly use `lb.settings` rather than instantiating this class yourself.
+    """
+
+    def __init__(self):
+        self._species = None
+        self._auto_save_parents = True
+
+    @property
+    def auto_save_parents(self) -> bool:
+        """Automatically save parents."""
+        return self._auto_save_parents
+
+    @auto_save_parents.setter
+    def auto_save_parents(self, value: bool):
+        self._auto_save_parents = value
+
+    @property
+    def species(self) -> Optional[Species]:
+        """Default species argument (default `None`).
+
+        Default record to use when `species` argument is required in `lamindb` functionality.
+
+        Only takes effect if explicitly set!
+
+        Examples:
+            >>> lb.settings.species = "mouse"
+            ✅ set species: Species(id=vado, name=mouse, taxon_id=10090, scientific_name=mus_musculus, updated_at=2023-07-21 11:37:08, bionty_source_id=CXWj, created_by_id=DzTjkKse) # noqa
+        """
+        return self._species
+
+    @species.setter
+    def species(self, name: Union[str, Species]):
+        import lamindb as ln
+
+        import lnschema_bionty as lb
+
+        # do not show the validated message for species
+        verbosity = ln.settings.verbosity
+        ln.settings.verbosity = 1
+        species = lb.Species.from_bionty(name=name)
+        ln.settings.verbosity = verbosity
+        if species is None:
+            raise ValueError(f"No species with name='{name}' is found, please create a species record!")
+        if species._state.adding:  # type:ignore
+            species.save()  # type:ignore
+        logger.debug(f"set species: {species}")
+        self._species = species
+
+
+settings = Settings()
+settings.__doc__ = """Global :class:`~lnschema_bionty.dev.Settings`."""
