39fd8a80d2ecc411aed9240323c12e6df0f83c23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-19 05:05:51 +0000

    Add package sendJhy/0.0.7/sendJhy-0.0.7.tar.gz

---
+++ packages/sendJhy/sendJhy-0.0.7.tar.gz/sendJhy-0.0.7/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+    name                = 'sendJhy',
+    version             = '0.0.7',
+    description         = '로컬 api sqlite db 사용, 교수 회원가입 및 기본 inNout 데이터 삽입, tree 변경',
+    author              = 'lucete28',
+    author_email        = '2580jhy@naver.com',
+    # url                 = 'https://github.com/jeakwon/ccpy',
+    # download_url        = 'https://github.com/jeakwon/ccpy/archive/0.0.tar.gz',
+    install_requires    =  [        ],
+    packages            = find_packages(exclude = []),
+    keywords            = ['Code','Lucete','lucete','lucete28','Lucete28','jhy'],
+    python_requires     = '>=3',
+    package_data        = {},
+    zip_safe            = False,
+    classifiers         = [
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+    ],
+)
