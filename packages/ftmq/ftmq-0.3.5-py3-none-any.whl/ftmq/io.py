05b6099f8736e789e46bd18d543154467fdd51e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-19 16:07:24 +0000

    Add package ftmq/0.3.5/ftmq-0.3.5-py3-none-any.whl

---
+++ packages/ftmq/ftmq-0.3.5-py3-none-any.whl/ftmq/io.py
@@ -0,0 +1,151 @@
+import contextlib
+import logging
+import sys
+from collections.abc import Iterable
+from typing import Any, Literal
+
+import orjson
+from banal import is_listish
+from fsspec import open
+from nomenklatura.entity import CE, CompositeEntity
+from nomenklatura.util import PathLike
+
+from ftmq.query import Query
+from ftmq.store import Store, get_store
+from ftmq.types import CEGenerator, SDict
+from ftmq.util import get_statements, make_dataset, make_proxy
+
+log = logging.getLogger(__name__)
+
+
+@contextlib.contextmanager
+def smart_open(
+    uri: str,
+    sys_io: Literal[sys.stdin.buffer, sys.stdout.buffer] | None = sys.stdin,
+    *args,
+    **kwargs,
+):
+    is_buffer = False
+    kwargs["mode"] = kwargs.get("mode", "rb")
+    if not uri:
+        raise ValueError("Missing uri")
+    if uri != "-":
+        fh = open(uri, *args, **kwargs)
+    else:
+        fh = sys_io
+        is_buffer = True
+
+    try:
+        if is_buffer:
+            yield fh
+        else:
+            yield fh.open()
+    finally:
+        if not is_buffer:
+            fh.close()
+
+
+def _smart_stream(uri, *args, **kwargs) -> Any:
+    kwargs["mode"] = kwargs.get("mode", "rb")
+    with smart_open(uri, sys.stdin.buffer, *args, **kwargs) as fh:
+        while line := fh.readline():
+            yield line
+
+
+def smart_read(uri, *args, **kwargs) -> Any:
+    kwargs["mode"] = kwargs.get("mode", "rb")
+    stream = kwargs.pop("stream", False)
+    if stream:
+        return _smart_stream(uri, *args, **kwargs)
+
+    with smart_open(uri, sys.stdin.buffer, *args, **kwargs) as fh:
+        return fh.read()
+
+
+def smart_write(uri, content: bytes | str, *args, **kwargs) -> Any:
+    kwargs["mode"] = kwargs.get("mode", "wb")
+    with smart_open(uri, sys.stdout.buffer, *args, **kwargs) as fh:
+        fh.write(content)
+
+
+def smart_get_store(uri: PathLike, **kwargs) -> Store | None:
+    try:
+        return get_store(uri, **kwargs)
+    except NotImplementedError:
+        return
+
+
+def smart_read_proxies(
+    uri: PathLike | Iterable[PathLike],
+    mode: str | None = "rb",
+    serialize: bool | None = True,
+    query: Query | None = None,
+    **store_kwargs,
+) -> CEGenerator:
+    if is_listish(uri):
+        for u in uri:
+            yield from smart_read_proxies(u, mode, serialize, query)
+        return
+
+    store = smart_get_store(uri, **store_kwargs)
+    if store is not None:
+        view = store.query()
+        yield from view.entities(query)
+        return
+
+    with smart_open(uri, sys.stdin.buffer, mode=mode) as fh:
+
+        def _iter():
+            while line := fh.readline():
+                data = orjson.loads(line)
+                if serialize or query:
+                    data = make_proxy(data)
+                yield data
+
+        q = query or Query()
+        yield from q.apply_iter(_iter())
+
+
+def smart_write_proxies(
+    uri: PathLike,
+    proxies: Iterable[CE | SDict],
+    mode: str | None = "wb",
+    serialize: bool | None = False,
+    **store_kwargs,
+) -> int:
+    ix = 0
+    if proxies is None:  # FIXME how could this happen
+        return ix
+
+    store = smart_get_store(uri, **store_kwargs)
+    if store is not None:
+        dataset = store_kwargs.get("dataset")
+        if dataset is not None:
+            proxies = apply_datasets(proxies, dataset, replace=True)
+        with store.writer() as bulk:
+            for proxy in proxies:
+                ix += 1
+                bulk.add_entity(proxy)
+                if ix % 1_000 == 0:
+                    log.info("Writing proxy %d ..." % ix)
+        return ix
+
+    with smart_open(uri, sys.stdout.buffer, mode=mode) as fh:
+        for proxy in proxies:
+            ix += 1
+            if serialize:
+                proxy = proxy.to_dict()
+            fh.write(orjson.dumps(proxy, option=orjson.OPT_APPEND_NEWLINE))
+    return ix
+
+
+def apply_datasets(
+    proxies: Iterable[CE], *datasets: Iterable[str], replace: bool | None = False
+) -> CEGenerator:
+    for proxy in proxies:
+        if datasets:
+            if not replace:
+                datasets = proxy.datasets | set(datasets)
+            statements = get_statements(proxy, *datasets)
+            dataset = make_dataset(list(datasets)[0])
+        yield CompositeEntity.from_statements(dataset, statements)
---
+++ packages/ftmq/ftmq-0.3.5-py3-none-any.whl/ftmq/model/coverage.py
@@ -0,0 +1,129 @@
+from collections import Counter
+from typing import Any
+
+from followthemoney import model
+from nomenklatura.dataset.coverage import DataCoverage as NKCoverage
+from pydantic import PrivateAttr
+
+from ftmq.enums import Properties
+from ftmq.model.mixins import BaseModel, NKModel
+from ftmq.types import CE, CEGenerator, DateLike, Frequencies
+from ftmq.util import get_country_name
+
+
+class Schema(BaseModel):
+    name: str
+    count: int
+    label: str
+    plural: str
+
+    def __init__(self, **data):
+        schema = model.get(data["name"])
+        data["label"] = schema.label
+        data["plural"] = schema.plural
+        super().__init__(**data)
+
+
+class Country(BaseModel):
+    code: str
+    count: int
+    label: str | None
+
+    def __init__(self, **data):
+        data["label"] = get_country_name(data["code"])
+        super().__init__(**data)
+
+
+class Collector:
+    schemata: Counter = None
+    countries: Counter = None
+    start: set[DateLike] = None
+    end: set[DateLike] = None
+
+    def __init__(self):
+        self.schemata = Counter()
+        self.countries = Counter()
+        self.start = set()
+        self.end = set()
+
+    def collect(self, proxy: CE) -> None:
+        self.schemata[proxy.schema.name] += 1
+        for country in proxy.countries:
+            self.countries[country] += 1
+        self.start.update(proxy.get(Properties.startDate, quiet=True))
+        self.start.update(proxy.get(Properties.date, quiet=True))
+        self.end.update(proxy.get(Properties.endDate, quiet=True))
+        self.end.update(proxy.get(Properties.date, quiet=True))
+
+    def export(self) -> "Coverage":
+        start = min(self.start) if self.start else None
+        end = max(self.end) if self.end else None
+        return Coverage(
+            start=start,
+            end=end,
+            schemata=[Schema(name=k, count=v) for k, v in self.schemata.items()],
+            countries=[Country(code=k, count=v) for k, v in self.countries.items()],
+            entities=self.schemata.total(),
+            years=(
+                start[:4] if start else None,
+                end[:4] if end else None,
+            ),
+        )
+
+    def to_dict(self) -> dict[str, Any]:
+        data = self.export()
+        return data.dict()
+
+    def apply(self, proxies: CEGenerator) -> CEGenerator:
+        """
+        Generate coverage from an input stream of proxies
+        This returns a generator again, so actual collection of coverage stats
+        will happen if the actual generator is executed
+        """
+        for proxy in proxies:
+            self.collect(proxy)
+            yield proxy
+
+
+class Coverage(NKModel):
+    _nk_model = NKCoverage
+    _collector: Collector | None = PrivateAttr()
+
+    start: DateLike | None = None
+    end: DateLike | None = None
+    frequency: Frequencies | None = "unknown"
+
+    # own additions:
+    schemata: list[Schema] | None = []
+    countries: list[Country] | None = []
+    entities: int = 0
+    years: tuple[int | None, int | None] | None = (None, None)
+
+    def __enter__(self):
+        self._collector = Collector()
+        return self._collector
+
+    def __exit__(self, *args, **kwargs):
+        res = self._collector.export()
+        self._collector = None
+        self.start = res.start
+        self.end = res.end
+        self.years = res.years
+        self.schemata = res.schemata
+        self.entities = res.entities
+        self.countries = res.countries
+
+    def apply(self, proxies: CEGenerator) -> "Coverage":
+        """
+        Generate coverage from an input stream of proxies
+        """
+        if self._collector is None:
+            self._collector = Collector()
+        for proxy in proxies:
+            self._collector.collect(proxy)
+        self.__exit__()
+
+    def to_nk(self) -> NKCoverage:
+        data = self.dict()
+        data["countries"] = [c["code"] for c in data["countries"]]
+        return NKCoverage(data)
