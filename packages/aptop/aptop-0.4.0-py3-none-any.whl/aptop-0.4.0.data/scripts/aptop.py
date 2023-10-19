73d362c53b2b0da15370ff747ae014de8c1eedd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-19 05:06:02 +0000

    Add package aptop/0.4.0/aptop-0.4.0-py3-none-any.whl

---
+++ packages/aptop/aptop-0.4.0-py3-none-any.whl/aptop-0.4.0.data/scripts/aptop.py
@@ -0,0 +1,14 @@
+#!python
+
+__author__ = "branko.toic@gmail.org (https://toic.org)"
+__date__ = "Oct 17, 2023 0:10 PM$"
+__version__ = "0.4.0"
+
+
+from ApTop import ApacheStatus
+from ApTop import AptopCurses
+
+
+if __name__ == "__main__":
+    aptop = ApacheStatus()
+    window = AptopCurses(aptop)
