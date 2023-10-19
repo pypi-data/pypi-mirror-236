0af27920fc2b56a807623e3c30fb7e47597adfa3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-19 06:07:11 +0000

    Add package pyecoforest/0.4.0/pyecoforest-0.4.0-py3-none-any.whl

---
+++ packages/pyecoforest/pyecoforest-0.4.0-py3-none-any.whl/pyecoforest/const.py
@@ -0,0 +1,24 @@
+import httpx
+
+# API CGI base path
+URL_CGI = "/recepcion_datos_4.cgi"
+
+MODEL_NAME = "Cordoba glass"
+
+SUPPORTED_MODELS = ["CC2014_v2"]  # Cordoba glass
+
+# API status operation code
+API_STATUS_OP = 1002
+API_STATS_OP = 1020
+API_ALARMS_OP = 1079
+API_SET_STATE_OP = 1013
+API_SET_TEMP_OP = 1019
+API_SET_POWER_OP = 1004
+
+LOCAL_TIMEOUT = httpx.Timeout(
+    # The device can be slow to respond but fast to connect to we
+    # need to set a long timeout for the read and a short timeout
+    # for the connect
+    timeout=10.0,
+    read=60.0,
+)
