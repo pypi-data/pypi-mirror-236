ee1ed378e729d729a3b2b9cc4da7472cc7be11f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-19 05:05:09 +0000

    Add package pyqcolor/1.1/pyqcolor-1.1.tar.gz

---
+++ packages/pyqcolor/pyqcolor-1.1.tar.gz/pyqcolor-1.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+setup(
+    name="pyqcolor",
+    version='1.1',
+    license='Eclipse Public License 2.0',
+    authors=["smithsha"],
+    author_email="smithsha@gmail.com",
+    description="Bring Colorful Code",
+    long_description='Documentation: https://github.com/smithsha/pyqcolor',
+    packages=['pyqcolor']
+)
+
