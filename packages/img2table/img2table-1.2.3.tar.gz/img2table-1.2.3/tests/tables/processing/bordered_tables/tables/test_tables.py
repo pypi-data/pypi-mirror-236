e9137090758383ecd145a2533c6a9d4caa8d25c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-19 06:07:11 +0000

    Add package img2table/1.2.3/img2table-1.2.3.tar.gz

---
+++ packages/img2table/img2table-1.2.3.tar.gz/img2table-1.2.3/tests/tables/processing/bordered_tables/tables/test_tables.py
@@ -0,0 +1,23 @@
+# coding: utf-8
+import json
+
+from img2table.tables.objects.cell import Cell
+from img2table.tables.objects.row import Row
+from img2table.tables.objects.table import Table
+from img2table.tables.processing.bordered_tables.tables import get_tables
+
+
+def test_get_tables():
+    with open("test_data/cells.json", 'r') as f:
+        cells = [Cell(**el) for el in json.load(f)]
+    with open("test_data/contours.json", "r") as f:
+        contours = [Cell(**el) for el in json.load(f)]
+
+    result = get_tables(cells=cells, elements=contours)
+
+    with open("test_data/expected.json", "r") as f:
+        expected = [Table(rows=[Row(cells=[Cell(**el) for el in row]) for row in tb])
+                    for tb in json.load(f)]
+
+    assert result == expected
+
