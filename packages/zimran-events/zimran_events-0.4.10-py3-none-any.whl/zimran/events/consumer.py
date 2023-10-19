cf3dbd263858bae5ce34b8ac091ea3929275f640
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-19 03:11:08 +0000

    Add package zimran-events/0.4.10/zimran_events-0.4.10-py3-none-any.whl

---
+++ packages/zimran-events/zimran_events-0.4.10-py3-none-any.whl/zimran/events/consumer.py
@@ -0,0 +1,155 @@
+import asyncio
+import time
+
+import aio_pika
+from pika.adapters.blocking_connection import BlockingChannel
+
+
+try:
+    from loguru import logger
+except ImportError:
+    import logging
+
+    logger = logging.getLogger(__name__)
+
+
+from zimran.events.connection import AsyncConnection, Connection
+from zimran.events.constants import DEFAULT_DEAD_LETTER_EXCHANGE_NAME
+from zimran.events.router import Router
+from zimran.events.utils import cleanup_and_normalize_queue_name
+
+
+class Consumer(Connection):
+    def __init__(
+        self,
+        *,
+        service_name: str,
+        broker_url: str,
+        router: Router,
+        channel_number: int = 1,
+        prefetch_count: int = 10,
+    ):
+        super().__init__(broker_url=broker_url, channel_number=channel_number)
+
+        self._service_name = service_name.replace('-', '_').lower()
+        self._prefetch_count = prefetch_count
+        self._router = router
+
+    def run(self, *, max_retries: int = 5, retry_delay: int = 3):
+        retries = 0
+        while retries <= max_retries:
+            try:
+                self._run()
+            except Exception as e:
+                logger.error(f'Error connecting to RabbitMQ: {e}')
+                retries += 1
+                if retries <= max_retries:
+                    logger.info(f'Retrying in {retry_delay} seconds...')
+                    time.sleep(retry_delay)
+                else:
+                    logger.error('Max retries exceeded, giving up')
+                    break
+
+    def _run(self):
+        channel: BlockingChannel = self.get_channel()
+        channel.basic_qos(prefetch_count=self._prefetch_count)
+        self._run_routines(channel)
+
+        for routing_key, event in self._router.handlers.items():
+            queue_name = cleanup_and_normalize_queue_name(f'{self._service_name}.{routing_key}')
+            channel.queue_declare(
+                queue_name,
+                durable=True,
+                arguments={
+                    'x-dead-letter-exchange': DEFAULT_DEAD_LETTER_EXCHANGE_NAME,
+                    'x-ha-policy': 'exactly 2',
+                },
+            )
+
+            if exchange := event.exchange:
+                channel.exchange_declare(
+                    exchange=exchange.name,
+                    exchange_type=exchange.type,
+                    **exchange.as_dict(exclude=['name', 'type', 'timeout']),
+                )
+                channel.queue_bind(queue=queue_name, exchange=exchange.name, routing_key=routing_key)
+
+            channel.basic_consume(queue_name, event.handler)
+            logger.info(f'Registering consumer | queue: {queue_name} | routing_key: {routing_key}')
+
+        channel.start_consuming()
+
+    def _run_routines(self, channel: BlockingChannel):
+        self._declare_unroutable_queue(channel)
+        self._declare_dead_letter_exchange(channel)
+
+
+class AsyncConsumer(AsyncConnection):
+    def __init__(
+        self,
+        *,
+        service_name: str,
+        broker_url: str,
+        router: Router,
+        channel_number: int = 1,
+        prefetch_count: int = 10,
+    ):
+        super().__init__(broker_url=broker_url, channel_number=channel_number)
+
+        self._service_name = service_name.replace('-', '_').lower()
+        self._prefetch_count = prefetch_count
+        self._router = router
+
+    async def run(self, *, max_retries: int = 5, retry_delay: int = 3):
+        retries = 0
+        while retries <= max_retries:
+            try:
+                await self._run()
+            except asyncio.CancelledError as e:
+                logger.error(f'Consumer cancelled: {e}')
+                break
+            except Exception as e:
+                logger.error(f'Error connecting to RabbitMQ: {e}')
+                retries += 1
+                if retries <= max_retries:
+                    logger.info(f'Retrying in {retry_delay} seconds...')
+                    await asyncio.sleep(retry_delay)
+                else:
+                    logger.error('Max retries exceeded, giving up')
+                    break
+
+    async def _run(self):
+        channel = await self.get_channel()
+        await channel.set_qos(prefetch_count=self._prefetch_count)
+        await self._run_routines(channel)
+
+        for routing_key, event in self._router.handlers.items():
+            queue_name = cleanup_and_normalize_queue_name(f'{self._service_name}.{routing_key}')
+            queue = await channel.declare_queue(
+                queue_name,
+                durable=True,
+                arguments={
+                    'x-dead-letter-exchange': DEFAULT_DEAD_LETTER_EXCHANGE_NAME,
+                    'x-ha-policy': 'exactly 2',
+                },
+            )
+
+            if _exchange := event.exchange:
+                exchange = await channel.declare_exchange(**_exchange.as_dict(exclude_none=True))
+                await queue.bind(exchange=exchange, routing_key=routing_key)
+
+            await queue.consume(event.handler)
+            logger.info(f'Registering consumer | queue: {queue_name} | routing_key: {routing_key}')
+
+        try:
+            await asyncio.Future()
+        except asyncio.CancelledError as error:
+            logger.error('Consumer cancelled')
+            raise error
+
+    async def _run_routines(self, channel: aio_pika.Channel):
+        await asyncio.gather(
+            self._declare_unroutable_queue(channel),
+            self._declare_dead_letter_exchange(channel),
+            return_exceptions=True,
+        )
---
+++ packages/zimran-events/zimran_events-0.4.10-py3-none-any.whl/zimran/events/producer.py
@@ -0,0 +1,106 @@
+import asyncio
+import json
+
+import aio_pika
+import pika
+from aioretry import retry
+
+
+try:
+    from loguru import logger
+except ImportError:
+    import logging
+
+    logger = logging.getLogger(__name__)
+
+
+from .connection import AsyncConnection, Connection
+from .dto import ChannelProperties, Exchange
+from .utils import retry_policy, validate_channel_properties, validate_exchange
+
+
+class Producer(Connection):
+    def __init__(self, *, broker_url: str, channel_number: int = 1):
+        super().__init__(broker_url=broker_url, channel_number=channel_number)
+
+    def publish(
+        self,
+        routing_key: str,
+        *,
+        payload: dict,
+        exchange: Exchange | None = None,
+        properties: ChannelProperties | None = None,
+    ):
+        if properties is None:
+            properties = ChannelProperties()
+        else:
+            validate_channel_properties(properties)
+
+        basic_properties = pika.BasicProperties(**properties.as_dict(exclude_none=True))
+        body = json.dumps(payload, default=str)
+
+        channel = self.get_channel()
+        if exchange is None:
+            channel.basic_publish(exchange='', routing_key=routing_key, body=body, properties=basic_properties)
+            logger.info(f'Message published to basic exchange | routing_key: {routing_key}')
+            return
+
+        validate_exchange(exchange)
+        self._declare_unroutable_queue(channel=channel)
+        self._declare_dead_letter_exchange(channel=channel)
+
+        channel.exchange_declare(
+            exchange=exchange.name,
+            exchange_type=exchange.type,
+            **exchange.as_dict(exclude=['name', 'type', 'timeout'], exclude_none=True),
+        )
+
+        channel.basic_publish(
+            exchange=exchange.name,
+            routing_key=routing_key,
+            body=body,
+            properties=basic_properties,
+        )
+        logger.info(f'Message published to {exchange.name} exchange | routing_key: {routing_key}')
+
+
+class AsyncProducer(AsyncConnection):
+    def __init__(self, *, broker_url: str, channel_number: int = 1):
+        super().__init__(broker_url=broker_url, channel_number=channel_number)
+
+    @retry(retry_policy)
+    async def publish(
+        self,
+        routing_key: str,
+        *,
+        payload: dict,
+        exchange: Exchange | None = None,
+        properties: ChannelProperties | None = None,
+    ):
+        if properties is None:
+            properties = ChannelProperties()
+        else:
+            validate_channel_properties(properties)
+
+        message = self._get_message(properties=properties, payload=payload)
+
+        channel = await self.get_channel()
+        if exchange is None:
+            await channel.default_exchange.publish(message=message, routing_key=routing_key)
+            logger.info(f'Message published to basic exchange | routing_key: {routing_key}')
+            return
+
+        validate_exchange(exchange)
+        declared_exchange, *_ = await asyncio.gather(
+            channel.declare_exchange(**exchange.as_dict(exclude_none=True)),
+            self._declare_unroutable_queue(channel=channel),
+            self._declare_dead_letter_exchange(channel=channel),
+            return_exceptions=True,
+        )
+
+        await declared_exchange.publish(message=message, routing_key=routing_key)
+        logger.info(f'Message published to {exchange.name} exchange | routing_key: {routing_key}')
+
+    @staticmethod
+    def _get_message(properties: ChannelProperties, payload: dict):
+        return aio_pika.Message(body=json.dumps(payload, default=str).encode(), **properties.as_dict(exclude_none=True))
