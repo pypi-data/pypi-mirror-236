b37dc720c1a6d39bd8b68b433e4edb02a7bdb9ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-19 15:07:20 +0000

    Add package streamlit-image-viewer/0.2.3/streamlit_image_viewer-0.2.3.tar.gz

---
+++ packages/streamlit-image-viewer/streamlit_image_viewer-0.2.3.tar.gz/streamlit_image_viewer-0.2.3/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="streamlit_image_viewer",
+    version="0.2.3",
+    author="katsuji takeda",
+    description="streamlit components for image viewer",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/TakedaKatsuji/Streamlit-ImageViewer",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    classifiers=[],
+    keywords=['Python', 'Streamlit', 'React', 'TypeScript'],
+    python_requires=">=3.9",
+    install_requires=[
+        "streamlit >= 0.63",
+    ],
+)
