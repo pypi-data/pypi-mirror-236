4c64bf928486d0a64ae4d5a6b7d6ff2250e420ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-19 03:08:06 +0000

    Add package fast-tmfg/0.0.10/fast_tmfg-0.0.10.tar.gz

---
+++ packages/fast-tmfg/fast_tmfg-0.0.10.tar.gz/fast_tmfg-0.0.10/fast_tmfg/__init__.py
@@ -0,0 +1,1 @@
+from .TMFG_core import TMFG
---
+++ packages/fast-tmfg/fast_tmfg-0.0.10.tar.gz/fast_tmfg-0.0.10/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setuptools.setup(
+    name="fast_tmfg",
+    version="0.0.10",
+    author="Antonio Briola",
+    author_email="anto.briola96@gmail.com",
+    description="The official package to compute the Triangulated Maximally Filtered Graph (TMFG).",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/FinancialComputingUCL/Triangulated_Maximally_Filtered_Graph",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+    install_requires=["numpy", "pandas", "networkx"],
+    packages=["fast_tmfg"],
+    )
