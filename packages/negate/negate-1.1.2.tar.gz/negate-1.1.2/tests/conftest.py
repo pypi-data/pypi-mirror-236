f5f9e3435b3f1565df1ab08de0e70d96b2e61196
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-19 06:06:26 +0000

    Add package negate/1.1.2/negate-1.1.2.tar.gz

---
+++ packages/negate/negate-1.1.2.tar.gz/negate-1.1.2/tests/conftest.py
@@ -0,0 +1,29 @@
+"""Set up tests."""
+
+from negate import Negator
+
+
+# Reuse negator model for all tests.
+negator_model: Negator = None
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        "--transformers", action="store_true", help="use Transformers")
+
+
+def pytest_generate_tests(metafunc):
+    if "negator" in metafunc.fixturenames:
+        # Initialize negator only once.
+        global negator_model
+        if negator_model is None:
+            use_transformers = metafunc.config.getoption("transformers")
+            try:
+                # `use_gpu` ignored if `use_transformers` is False.
+                negator_model = Negator(
+                    use_transformers=use_transformers, use_gpu=True)
+                negator_model.negate_sentence("I will now check GPU support!")
+            except (ValueError, NotImplementedError):  # GPU not supported
+                negator_model = Negator(
+                    use_transformers=use_transformers, use_gpu=False)
+        metafunc.parametrize("negator", [negator_model])
