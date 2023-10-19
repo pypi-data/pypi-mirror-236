576017f62ccffcadd93091876171dd2347ce45e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-19 05:05:33 +0000

    Add package atyp/0.0.10/atyp-0.0.10-py3-none-any.whl

---
+++ packages/atyp/atyp-0.0.10-py3-none-any.whl/atyp/__init__.py
@@ -0,0 +1,11 @@
+__version__ = "0.0.10"
+from .atyp import *
+from .ctyp import *
+from .qtyp import *
+from .ztyp import *
+
+from .rtyp import *
+from .ptyp import *
+from .dtyp import *
+from .btyp import *
+from .mtyp import *
