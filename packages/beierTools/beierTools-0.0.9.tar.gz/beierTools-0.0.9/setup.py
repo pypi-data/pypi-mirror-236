be1e4d7eeccb3daa135cd1c8812b07b309537699
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-19 15:05:36 +0000

    Add package beierTools/0.0.9/beierTools-0.0.9.tar.gz

---
+++ packages/beierTools/beierTools-0.0.9.tar.gz/beierTools-0.0.9/setup.py
@@ -0,0 +1,14 @@
+# setup.py文件内容
+from setuptools import setup
+
+setup(
+    name='beierTools',  # 应用名（pip安装和卸载时的名字）
+    version='0.0.9',  # 当前版本
+    author='Beier',  # 作者
+    author_email='1601684622@qq.com',  # 作者邮箱
+    licence='MIT License',  # 许可协议
+    url='https://github.com/srx-2000/beier_tools',  # 应用主页链接
+    description='Beier developments tools',
+    install_requires=["pycrypto>=1.4.1"],  # 依赖包
+    py_modules=["encrypt", "operation"],
+)
