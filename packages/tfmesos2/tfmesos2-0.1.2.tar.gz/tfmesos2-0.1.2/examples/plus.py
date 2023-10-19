3857fee7f1138edf1ee063967d59e0d77cf37b99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-19 03:11:26 +0000

    Add package tfmesos2/0.1.2/tfmesos2-0.1.2.tar.gz

---
+++ packages/tfmesos2/tfmesos2-0.1.2.tar.gz/tfmesos2-0.1.2/examples/plus.py
@@ -0,0 +1,44 @@
+from __future__ import print_function
+
+import json
+import os
+import tensorflow as tf
+from tfmesos2 import cluster
+
+
+def main():
+    jobs_def = [
+        {
+            "name": "ps",
+            "num":1 
+        },
+        {
+            "name": "worker",
+            "num":2 
+        },
+    ]
+
+    with cluster(jobs_def) as c:
+        os.environ["TF_CONFIG"] = json.dumps({
+            "cluster": c.cluster_def
+        })
+
+        print(os.environ["TF_CONFIG"])
+
+        cluster_resolver = tf.distribute.cluster_resolver.TFConfigClusterResolver()
+
+        strategy = tf.distribute.ParameterServerStrategy(cluster_resolver)
+
+        with strategy.scope():
+            constant_a = tf.constant(10)
+            constant_b = tf.constant(32)
+
+            op = constant_a + constant_b
+
+        result = op.numpy()
+        print("Result is: ")
+        print(result)
+        c.shutdown()
+
+if __name__ == '__main__':
+    main()
