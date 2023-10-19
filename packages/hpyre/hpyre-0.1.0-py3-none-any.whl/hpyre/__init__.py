004faa6f62b610b6ce6f1a171410cbe79fe42d27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-19 05:06:20 +0000

    Add package hpyre/0.1.0/hpyre-0.1.0-py3-none-any.whl

---
+++ packages/hpyre/hpyre-0.1.0-py3-none-any.whl/hpyre/__init__.py
@@ -0,0 +1,11 @@
+import numpy as np
+from itertools import repeat
+import pandas as pd
+from scipy.signal import find_peaks
+from scipy import signal
+import matplotlib.pyplot as plt
+from tkinter import filedialog as fd
+import re 
+from lecroyscope import Trace
+import isfreader 
+from itertools import groupby
