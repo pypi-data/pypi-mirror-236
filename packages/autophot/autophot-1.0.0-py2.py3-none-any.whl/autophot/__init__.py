e62f7b4c890d1ebf46063ff6f5201f803d87e00f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-19 03:08:37 +0000

    Add package autophot/1.0.0/autophot-1.0.0-py2.py3-none-any.whl

---
+++ packages/autophot/autophot-1.0.0-py2.py3-none-any.whl/autophot/__init__.py
@@ -0,0 +1,5 @@
+
+print("Sorry, AutoPhot has moved to 'AstroPhot'. Please run: pip install astrophot")
+
+def run_from_terminal():
+    print("Sorry, AutoPhot has moved to 'AstroPhot'. Please run: pip install astrophot")
