7ed027c51a87bc5dcdffefe059e37d6d81906d75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-19 16:07:25 +0000

    Add package ultranest/3.6.3/ultranest-3.6.3.tar.gz

---
+++ packages/ultranest/ultranest-3.6.3.tar.gz/ultranest-3.6.3/tests/.pytype/pyi/test_hotstart.pyi
@@ -0,0 +1,32 @@
+# (generated with --quick)
+
+import __future__
+import numpy as np
+import os
+import scipy
+import tempfile
+from typing import Any, List
+
+Ndata: int
+ReactiveNestedSampler: Any
+compute_quantile_intervals: Any
+division: __future__._Feature
+get_auxiliary_contbox_parameterization: Any
+get_extended_auxiliary_problem: Any
+log10: Any
+mean_true: float
+parameters: List[str]
+print_function: __future__._Feature
+resume_from_hot_file: Any
+reuse_samples: Any
+rng_data: Any
+sigma_true: float
+vectorize: Any
+y: Any
+
+def extended_log_likelihood(params) -> Any: ...
+def extended_prior_transform(x) -> Any: ...
+def log_likelihood(params) -> Any: ...
+def prior_transform(x) -> Any: ...
+def test_contbox_hotstart() -> None: ...
+def test_hotstart_SLOW() -> None: ...
---
+++ packages/ultranest/ultranest-3.6.3.tar.gz/ultranest-3.6.3/tests/.pytype/pyi/test_run.pyi
@@ -0,0 +1,28 @@
+# (generated with --quick)
+
+import json
+import numpy as np
+import os
+import pandas
+import pytest
+import shutil
+import tempfile
+import ultranest
+from typing import Any
+
+NestedSampler: Any
+ReactiveNestedSampler: Any
+assert_allclose: Any
+read_file: Any
+resume_from_hot_file: Any
+test_reactive_run_resume_eggbox: Any
+test_run_resume: Any
+
+def test_dlogz_reactive_run_SLOW() -> None: ...
+def test_reactive_run() -> None: ...
+def test_reactive_run_extraparams() -> None: ...
+def test_reactive_run_warmstart_gauss() -> None: ...
+def test_return_summary() -> None: ...
+def test_run() -> None: ...
+def test_run_compat() -> None: ...
+def test_run_hotstart_gauss_SLOW() -> None: ...
