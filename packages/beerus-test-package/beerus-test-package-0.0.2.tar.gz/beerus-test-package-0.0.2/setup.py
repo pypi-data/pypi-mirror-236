4daec7736b9da24444cf115153d33c430cb15025
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-19 16:06:45 +0000

    Add package beerus-test-package/0.0.2/beerus-test-package-0.0.2.tar.gz

---
+++ packages/beerus-test-package/beerus-test-package-0.0.2.tar.gz/beerus-test-package-0.0.2/setup.py
@@ -0,0 +1,20 @@
+
+import setuptools 
+  
+with open("README.md", "r") as fh: 
+    description = fh.read() 
+  
+setuptools.setup( 
+    name="beerus-test-package", 
+    version="0.0.2", 
+    author="Beerus", 
+    author_email="contact@gfg.com", 
+    packages=["utils"], 
+    description="A sample test package", 
+    long_description=description, 
+    long_description_content_type="text/markdown", 
+    url="", 
+    license='MIT', 
+    python_requires='>=3.8', 
+    install_requires=[] 
+) 
