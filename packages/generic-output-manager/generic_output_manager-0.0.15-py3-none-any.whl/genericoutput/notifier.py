9f990f853708cf94e06925d6bc561a7079e5c46e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-19 03:08:13 +0000

    Add package generic-output-manager/0.0.15/generic_output_manager-0.0.15-py3-none-any.whl

---
+++ packages/generic-output-manager/generic_output_manager-0.0.15-py3-none-any.whl/genericoutput/notifier.py
@@ -0,0 +1,19 @@
+from kafka import KafkaProducer
+from bdaserviceutils import get_kafka_binder_brokers
+from alidaargparser import get_asset_property
+import json
+
+class Notifier:
+    def __init__(self) -> None:
+        self.isActive = False
+
+        if get_asset_property(asset_name="go_manager", property="brokers") is not None:
+            self.producer = KafkaProducer(bootstrap_servers=get_asset_property(asset_name="go_manager", property="brokers").split(","))
+            self.topic = get_asset_property(asset_name="go_manager", property="topic")
+            self.isActive = True
+
+    def something_has_changed(self, metadata):
+        if self.isActive:
+            message = {"message": metadata}
+            self.producer.send(self.topic, json.dumps(message).encode('utf-8'))
+
