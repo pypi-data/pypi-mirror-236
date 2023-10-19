6c6003491b86f0ab6e12aa47d3f25cca7312a457
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-19 03:10:54 +0000

    Add package hollihop-api-client/0.0.14/hollihop_api_client-0.0.14-py3-none-any.whl

---
+++ packages/hollihop-api-client/hollihop_api_client-0.0.14-py3-none-any.whl/hollihop_api_client/methods/__init__.py
@@ -0,0 +1,13 @@
+from .ed_unit_students import StudentsCategory
+from .ed_units import EdUnitsCategory
+from .leads import LeadsCategory
+from .locations import LocationsCategory
+from .offices import OfficesCategory
+from .disciplines import DisciplinesCategory
+from .levels import LevelsCategory
+from .learning_types import LearningTypesCategory
+
+__all__ = ['StudentsCategory', 'EdUnitsCategory',
+           'LeadsCategory', 'LocationsCategory', 'OfficesCategory',
+           'DisciplinesCategory', 'LevelsCategory',
+           'LearningTypesCategory']
