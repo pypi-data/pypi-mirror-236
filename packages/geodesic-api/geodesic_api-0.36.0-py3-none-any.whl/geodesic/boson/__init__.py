e147af1935450d3fcfc180bf6a7a2e4290f7aaf0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-19 04:06:57 +0000

    Add package geodesic-api/0.36.0/geodesic_api-0.36.0-py3-none-any.whl

---
+++ packages/geodesic-api/geodesic_api-0.36.0-py3-none-any.whl/geodesic/boson/__init__.py
@@ -0,0 +1,15 @@
+from geodesic.boson.boson import (
+    BosonConfig,
+    BosonDescr,
+    DEFAULT_CREDENTIAL_KEY,
+    STORAGE_CREDENTIAL_KEY,
+    API_CREDENTIAL_KEY,
+)
+
+__all__ = [
+    "BosonConfig",
+    "BosonDescr",
+    "DEFAULT_CREDENTIAL_KEY",
+    "STORAGE_CREDENTIAL_KEY",
+    "API_CREDENTIAL_KEY",
+]
