cc82f7265e59dcf78bd8c509abc8ebee98eb6c2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-19 15:06:07 +0000

    Add package DaemonStarter/0.0.0.1/DaemonStarter-0.0.0.1.tar.gz

---
+++ packages/DaemonStarter/DaemonStarter-0.0.0.1.tar.gz/DaemonStarter-0.0.0.1/daemon_starter/__init__.py
@@ -0,0 +1,13 @@
+# @Time : 2022-07-08 14:25
+# @Author  : inflower
+# @Desc : ==============================================
+# Life is Short I Use Python!!!                      ===
+# ======================================================
+# @Project : JsonSimple
+# @FileName: __init__.py
+# @Software: PyCharm
+from .daemon import starter
+
+__all__ = (
+    'starter',
+)
