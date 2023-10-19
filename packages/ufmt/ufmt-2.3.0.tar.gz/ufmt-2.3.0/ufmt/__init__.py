7901b5614f17e2ac51742eb99e08d2abea2875d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-19 15:05:46 +0000

    Add package ufmt/2.3.0/ufmt-2.3.0.tar.gz

---
+++ packages/ufmt/ufmt-2.3.0.tar.gz/ufmt-2.3.0/ufmt/__init__.py
@@ -0,0 +1,37 @@
+"""
+Safe, atomic formatting with black and µsort
+"""
+
+__author__ = "Amethyst Reese"
+from .__version__ import __version__
+from .core import ufmt_bytes, ufmt_file, ufmt_paths, ufmt_stdin, ufmt_string
+from .types import (
+    BlackConfig,
+    BlackConfigFactory,
+    Encoding,
+    FileContent,
+    Newline,
+    Processor,
+    Result,
+    SkipFormatting,
+    UsortConfig,
+    UsortConfigFactory,
+)
+
+__all__ = [
+    "BlackConfig",
+    "BlackConfigFactory",
+    "Encoding",
+    "FileContent",
+    "Newline",
+    "Processor",
+    "Result",
+    "SkipFormatting",
+    "ufmt_bytes",
+    "ufmt_file",
+    "ufmt_paths",
+    "ufmt_stdin",
+    "ufmt_string",
+    "UsortConfig",
+    "UsortConfigFactory",
+]
