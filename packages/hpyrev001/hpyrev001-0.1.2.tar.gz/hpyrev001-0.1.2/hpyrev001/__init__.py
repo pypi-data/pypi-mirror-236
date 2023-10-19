b08a45343c9565b725ec61d09358cf3454503608
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-19 05:06:36 +0000

    Add package hpyrev001/0.1.2/hpyrev001-0.1.2.tar.gz

---
+++ packages/hpyrev001/hpyrev001-0.1.2.tar.gz/hpyrev001-0.1.2/hpyrev001/__init__.py
@@ -0,0 +1,1 @@
+import hpyre as hp
---
+++ packages/hpyrev001/hpyrev001-0.1.2.tar.gz/hpyrev001-0.1.2/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+VERSION = '0.1.2'
+DESCRIPTION = 'Electroporation data analysis'
+LONG_DESCRIPTION = 'A package that allows to analyze electroporation data(voltage and current).'
+
+# Setting up
+setup(
+    name="hpyrev001",
+    version=VERSION,
+    author="Pedro Paulo",
+    author_email="<ppaulo437@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['numpy', 'pandas', 'scipy', 'matplotlib', 'tk', 'lecroyscope', 'isfreader'],
+    keywords=['python', 'electroporation', 'fft'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
