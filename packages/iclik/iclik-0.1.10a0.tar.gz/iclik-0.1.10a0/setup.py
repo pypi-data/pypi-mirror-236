469c3f9ca45de0d6cb234ff84b8dc8231ffa6a6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-19 04:06:24 +0000

    Add package iclik/0.1.10a0/iclik-0.1.10a0.tar.gz

---
+++ packages/iclik/iclik-0.1.10a0.tar.gz/iclik-0.1.10a0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='iclik',
+    version='0.1.10-alpha',
+    packages=['iclik'],
+    description="Information criteria for composite likelihood models",
+    long_description=open('README.rst').read(),
+    long_description_content_type='text/x-rst',
+    install_requires=[
+        "numdifftools",
+        "numpy",
+        "scipy"
+    ]
+)
