71a5e004aca9e11d19d2c3ff3f72e438cd95884e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-19 03:10:54 +0000

    Add package cg/51.6.9/cg-51.6.9.tar.gz

---
+++ packages/cg/cg-51.6.9.tar.gz/cg-51.6.9/cg/store/api/core.py
@@ -0,0 +1,32 @@
+import logging
+
+from cg.store.api.delete import DeleteDataHandler
+from cg.store.api.find_business_data import FindBusinessDataHandler
+from cg.store.database import get_session
+
+from .add import AddHandler
+from .find_basic_data import FindBasicDataHandler
+from .status import StatusHandler
+
+LOG = logging.getLogger(__name__)
+
+
+class CoreHandler(
+    AddHandler,
+    DeleteDataHandler,
+    FindBasicDataHandler,
+    FindBusinessDataHandler,
+    StatusHandler,
+):
+    """Aggregating class for the store api handlers."""
+
+    def __init__(self, session):
+        DeleteDataHandler(session)
+        FindBasicDataHandler(session)
+        FindBusinessDataHandler(session)
+        StatusHandler(session)
+
+
+class Store(CoreHandler):
+    def __init__(self):
+        self.session = get_session()
