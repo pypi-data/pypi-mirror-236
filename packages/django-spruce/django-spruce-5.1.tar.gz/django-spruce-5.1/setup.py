ec571092eabed3521715e9882f5319b2afdbe185
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-19 03:07:35 +0000

    Add package django-spruce/5.1/django-spruce-5.1.tar.gz

---
+++ packages/django-spruce/django-spruce-5.1.tar.gz/django-spruce-5.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+
+from setuptools import setup, find_packages
+
+setup(
+    name='django-spruce',
+    version='5.1',
+    packages=['spruce_ui'],
+    include_package_data=True,
+    install_requires=[
+        'Django',
+        'pyarmor',
+        'requests',
+        # 添加其他依赖
+    ],
+    author='江绿岸',
+    author_email='jlarjjszx@tom.com',
+    zip_safe=False,
+)
---
+++ packages/django-spruce/django-spruce-5.1.tar.gz/django-spruce-5.1/spruce_ui/pyarmor_runtime_000000/__init__.py
@@ -0,0 +1,2 @@
+# Pyarmor 8.3.11 (trial), 000000, 2023-10-18T18:12:06.885259
+from .pyarmor_runtime import __pyarmor__
