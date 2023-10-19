b24f581cad2550b221e2d8fdfad8df1446eaafb9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-19 16:06:58 +0000

    Add package pydantic-handler-converter/0.1.90/pydantic_handler_converter-0.1.90.tar.gz

---
+++ packages/pydantic-handler-converter/pydantic_handler_converter-0.1.90.tar.gz/pydantic_handler_converter-0.1.90/pydantic_handler_converter/abstract_handler.py
@@ -0,0 +1,45 @@
+from abc import ABC, abstractmethod
+from typing import Optional, Type, TypeVar, Generic
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.state import StatesGroup
+from pydantic import BaseModel
+
+from pydantic_handler_converter.types import Event
+
+from .view.abstract import AbstractView
+
+
+TBaseSchema = TypeVar("TBaseSchema", bound=BaseModel)
+
+
+class AbstractPydanticFormHandlers(ABC, Generic[TBaseSchema]):
+    Schema: Type[TBaseSchema]
+    views: list[AbstractView]
+    states: StatesGroup
+
+    __abstract__ = True
+    def __init_subclass__(cls) -> None:
+        if cls.__abstract__ and '__abstract__' in cls.__dict__:
+            return
+
+        e = NotImplementedError(f"`{cls.__name__}` requires Pydantic.BaseModel")
+ 
+        if (
+            not (bases := getattr(cls, "__orig_bases__"))
+            or not (generics := bases[0].__args__)
+        ):
+            raise e
+
+        cls.Schema = generics[-1]
+
+        if not issubclass(cls.Schema, BaseModel):
+            raise e
+
+    @abstractmethod
+    async def next(self, event: Event, state: FSMContext, current_step: Optional[str] = None):
+        raise NotImplementedError
+
+    @abstractmethod
+    async def finish(self, event: Event, state: FSMContext):
+        raise NotImplementedError
+
