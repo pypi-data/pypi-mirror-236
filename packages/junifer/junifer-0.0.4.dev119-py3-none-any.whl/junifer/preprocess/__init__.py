38407c75b1c80cb9564cd0ec110113ca9e86c982
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-19 03:08:13 +0000

    Add package junifer/0.0.4.dev119/junifer-0.0.4.dev119-py3-none-any.whl

---
+++ packages/junifer/junifer-0.0.4.dev119-py3-none-any.whl/junifer/preprocess/__init__.py
@@ -0,0 +1,8 @@
+"""Provide imports for preprocess sub-package."""
+
+# Authors: Federico Raimondo <f.raimondo@fz-juelich.de>
+#          Leonard Sasse <l.sasse@fz-juelich.de>
+# License: AGPL
+
+from .base import BasePreprocessor
+from .confounds import fMRIPrepConfoundRemover
