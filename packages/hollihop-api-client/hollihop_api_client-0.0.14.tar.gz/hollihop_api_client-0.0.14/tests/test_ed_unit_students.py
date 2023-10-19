eb4c71f501c7f32c6db2c0ec515af992474e2573
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-19 03:10:54 +0000

    Add package hollihop-api-client/0.0.14/hollihop_api_client-0.0.14.tar.gz

---
+++ packages/hollihop-api-client/hollihop_api_client-0.0.14.tar.gz/hollihop_api_client-0.0.14/tests/test_ed_unit_students.py
@@ -0,0 +1,11 @@
+def test_ed_unit_students(hh_client, test_data):
+    response = hh_client.ed_unit_students.get_students(ed_unit_id=test_data.test_group_id)
+    assert response[-1].student_name == test_data.test_student_name
+
+
+def test_ed_unit_students_debt_date(hh_client, test_data):
+    response = hh_client.ed_unit_students.get_students(
+        ed_unit_id=test_data.test_group_id,
+        query_payers=True
+    )
+    assert response[-1].payers[-1].debt_date == test_data.test_debt_date
