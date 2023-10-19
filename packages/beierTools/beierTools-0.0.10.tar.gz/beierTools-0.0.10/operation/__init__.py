e2662e0de6c96a02d4f4d2a0178d68dd14ae9178
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-19 15:06:09 +0000

    Add package beierTools/0.0.10/beierTools-0.0.10.tar.gz

---
+++ packages/beierTools/beierTools-0.0.10.tar.gz/beierTools-0.0.10/operation/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+'''
+@Project :beier_tools 
+@File    :__init__.py
+@IDE     :PyCharm 
+@Author  :Beier
+@Date    :2023/10/19 9:59 
+@github  :https://github.com/srx-2000
+'''
