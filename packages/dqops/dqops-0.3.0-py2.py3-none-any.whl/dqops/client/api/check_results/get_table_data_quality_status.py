ba5d3cb1f66e780ef4c78aaa37d5fa5372a7eca5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-19 03:08:26 +0000

    Add package dqops/0.3.0/dqops-0.3.0-py2.py3-none-any.whl

---
+++ packages/dqops/dqops-0.3.0-py2.py3-none-any.whl/dqops/client/api/check_results/get_table_data_quality_status.py
@@ -0,0 +1,264 @@
+from http import HTTPStatus
+from typing import Any, Dict, Optional, Union
+
+import httpx
+
+from ... import errors
+from ...client import AuthenticatedClient, Client
+from ...models.check_time_scale import CheckTimeScale
+from ...models.check_type import CheckType
+from ...models.table_data_quality_status_model import TableDataQualityStatusModel
+from ...types import UNSET, Response, Unset
+
+
+def _get_kwargs(
+    connection_name: str,
+    schema_name: str,
+    table_name: str,
+    *,
+    months: Union[Unset, None, int] = UNSET,
+    check_type: Union[Unset, None, CheckType] = UNSET,
+    check_time_scale: Union[Unset, None, CheckTimeScale] = UNSET,
+) -> Dict[str, Any]:
+    pass
+
+    params: Dict[str, Any] = {}
+    params["months"] = months
+
+    json_check_type: Union[Unset, None, str] = UNSET
+    if not isinstance(check_type, Unset):
+        json_check_type = check_type.value if check_type else None
+
+    params["checkType"] = json_check_type
+
+    json_check_time_scale: Union[Unset, None, str] = UNSET
+    if not isinstance(check_time_scale, Unset):
+        json_check_time_scale = check_time_scale.value if check_time_scale else None
+
+    params["checkTimeScale"] = json_check_time_scale
+
+    params = {k: v for k, v in params.items() if v is not UNSET and v is not None}
+
+    return {
+        "method": "get",
+        "url": "api/connections/{connectionName}/schemas/{schemaName}/tables/{tableName}/status".format(
+            connectionName=connection_name,
+            schemaName=schema_name,
+            tableName=table_name,
+        ),
+        "params": params,
+    }
+
+
+def _parse_response(
+    *, client: Union[AuthenticatedClient, Client], response: httpx.Response
+) -> Optional[TableDataQualityStatusModel]:
+    if response.status_code == HTTPStatus.OK:
+        response_200 = TableDataQualityStatusModel.from_dict(response.json())
+
+        return response_200
+    if client.raise_on_unexpected_status:
+        raise errors.UnexpectedStatus(response.status_code, response.content)
+    else:
+        return None
+
+
+def _build_response(
+    *, client: Union[AuthenticatedClient, Client], response: httpx.Response
+) -> Response[TableDataQualityStatusModel]:
+    return Response(
+        status_code=HTTPStatus(response.status_code),
+        content=response.content,
+        headers=response.headers,
+        parsed=_parse_response(client=client, response=response),
+    )
+
+
+def sync_detailed(
+    connection_name: str,
+    schema_name: str,
+    table_name: str,
+    *,
+    client: AuthenticatedClient,
+    months: Union[Unset, None, int] = UNSET,
+    check_type: Union[Unset, None, CheckType] = UNSET,
+    check_time_scale: Union[Unset, None, CheckTimeScale] = UNSET,
+) -> Response[TableDataQualityStatusModel]:
+    """getTableDataQualityStatus
+
+     Read the most recent results of executed data quality checks on the table and return the current
+    table's data quality status - the number of failed data quality checks if the table has active data
+    quality issues. Also returns the names of data quality checks that did not pass most recently. This
+    operation verifies only the status of the most recently executed data quality checks. Previous data
+    quality issues are not counted.
+
+    Args:
+        connection_name (str):
+        schema_name (str):
+        table_name (str):
+        months (Union[Unset, None, int]):
+        check_type (Union[Unset, None, CheckType]):
+        check_time_scale (Union[Unset, None, CheckTimeScale]):
+
+    Raises:
+        errors.UnexpectedStatus: If the server returns an undocumented status code and Client.raise_on_unexpected_status is True.
+        httpx.TimeoutException: If the request takes longer than Client.timeout.
+
+    Returns:
+        Response[TableDataQualityStatusModel]
+    """
+
+    kwargs = _get_kwargs(
+        connection_name=connection_name,
+        schema_name=schema_name,
+        table_name=table_name,
+        months=months,
+        check_type=check_type,
+        check_time_scale=check_time_scale,
+    )
+
+    response = client.get_httpx_client().request(
+        **kwargs,
+    )
+
+    return _build_response(client=client, response=response)
+
+
+def sync(
+    connection_name: str,
+    schema_name: str,
+    table_name: str,
+    *,
+    client: AuthenticatedClient,
+    months: Union[Unset, None, int] = UNSET,
+    check_type: Union[Unset, None, CheckType] = UNSET,
+    check_time_scale: Union[Unset, None, CheckTimeScale] = UNSET,
+) -> Optional[TableDataQualityStatusModel]:
+    """getTableDataQualityStatus
+
+     Read the most recent results of executed data quality checks on the table and return the current
+    table's data quality status - the number of failed data quality checks if the table has active data
+    quality issues. Also returns the names of data quality checks that did not pass most recently. This
+    operation verifies only the status of the most recently executed data quality checks. Previous data
+    quality issues are not counted.
+
+    Args:
+        connection_name (str):
+        schema_name (str):
+        table_name (str):
+        months (Union[Unset, None, int]):
+        check_type (Union[Unset, None, CheckType]):
+        check_time_scale (Union[Unset, None, CheckTimeScale]):
+
+    Raises:
+        errors.UnexpectedStatus: If the server returns an undocumented status code and Client.raise_on_unexpected_status is True.
+        httpx.TimeoutException: If the request takes longer than Client.timeout.
+
+    Returns:
+        TableDataQualityStatusModel
+    """
+
+    return sync_detailed(
+        connection_name=connection_name,
+        schema_name=schema_name,
+        table_name=table_name,
+        client=client,
+        months=months,
+        check_type=check_type,
+        check_time_scale=check_time_scale,
+    ).parsed
+
+
+async def asyncio_detailed(
+    connection_name: str,
+    schema_name: str,
+    table_name: str,
+    *,
+    client: AuthenticatedClient,
+    months: Union[Unset, None, int] = UNSET,
+    check_type: Union[Unset, None, CheckType] = UNSET,
+    check_time_scale: Union[Unset, None, CheckTimeScale] = UNSET,
+) -> Response[TableDataQualityStatusModel]:
+    """getTableDataQualityStatus
+
+     Read the most recent results of executed data quality checks on the table and return the current
+    table's data quality status - the number of failed data quality checks if the table has active data
+    quality issues. Also returns the names of data quality checks that did not pass most recently. This
+    operation verifies only the status of the most recently executed data quality checks. Previous data
+    quality issues are not counted.
+
+    Args:
+        connection_name (str):
+        schema_name (str):
+        table_name (str):
+        months (Union[Unset, None, int]):
+        check_type (Union[Unset, None, CheckType]):
+        check_time_scale (Union[Unset, None, CheckTimeScale]):
+
+    Raises:
+        errors.UnexpectedStatus: If the server returns an undocumented status code and Client.raise_on_unexpected_status is True.
+        httpx.TimeoutException: If the request takes longer than Client.timeout.
+
+    Returns:
+        Response[TableDataQualityStatusModel]
+    """
+
+    kwargs = _get_kwargs(
+        connection_name=connection_name,
+        schema_name=schema_name,
+        table_name=table_name,
+        months=months,
+        check_type=check_type,
+        check_time_scale=check_time_scale,
+    )
+
+    response = await client.get_async_httpx_client().request(**kwargs)
+
+    return _build_response(client=client, response=response)
+
+
+async def asyncio(
+    connection_name: str,
+    schema_name: str,
+    table_name: str,
+    *,
+    client: AuthenticatedClient,
+    months: Union[Unset, None, int] = UNSET,
+    check_type: Union[Unset, None, CheckType] = UNSET,
+    check_time_scale: Union[Unset, None, CheckTimeScale] = UNSET,
+) -> Optional[TableDataQualityStatusModel]:
+    """getTableDataQualityStatus
+
+     Read the most recent results of executed data quality checks on the table and return the current
+    table's data quality status - the number of failed data quality checks if the table has active data
+    quality issues. Also returns the names of data quality checks that did not pass most recently. This
+    operation verifies only the status of the most recently executed data quality checks. Previous data
+    quality issues are not counted.
+
+    Args:
+        connection_name (str):
+        schema_name (str):
+        table_name (str):
+        months (Union[Unset, None, int]):
+        check_type (Union[Unset, None, CheckType]):
+        check_time_scale (Union[Unset, None, CheckTimeScale]):
+
+    Raises:
+        errors.UnexpectedStatus: If the server returns an undocumented status code and Client.raise_on_unexpected_status is True.
+        httpx.TimeoutException: If the request takes longer than Client.timeout.
+
+    Returns:
+        TableDataQualityStatusModel
+    """
+
+    return (
+        await asyncio_detailed(
+            connection_name=connection_name,
+            schema_name=schema_name,
+            table_name=table_name,
+            client=client,
+            months=months,
+            check_type=check_type,
+            check_time_scale=check_time_scale,
+        )
+    ).parsed
---
+++ packages/dqops/dqops-0.3.0-py2.py3-none-any.whl/dqops/client/models/table_statistics_collectors_root_categories_spec.py
@@ -0,0 +1,75 @@
+from typing import TYPE_CHECKING, Any, Dict, List, Type, TypeVar, Union
+
+from attrs import define as _attrs_define
+from attrs import field as _attrs_field
+
+from ..types import UNSET, Unset
+
+if TYPE_CHECKING:
+    from ..models.table_volume_statistics_collectors_spec import (
+        TableVolumeStatisticsCollectorsSpec,
+    )
+
+
+T = TypeVar("T", bound="TableStatisticsCollectorsRootCategoriesSpec")
+
+
+@_attrs_define
+class TableStatisticsCollectorsRootCategoriesSpec:
+    """
+    Attributes:
+        volume (Union[Unset, TableVolumeStatisticsCollectorsSpec]):
+    """
+
+    volume: Union[Unset, "TableVolumeStatisticsCollectorsSpec"] = UNSET
+    additional_properties: Dict[str, Any] = _attrs_field(init=False, factory=dict)
+
+    def to_dict(self) -> Dict[str, Any]:
+        volume: Union[Unset, Dict[str, Any]] = UNSET
+        if not isinstance(self.volume, Unset):
+            volume = self.volume.to_dict()
+
+        field_dict: Dict[str, Any] = {}
+        field_dict.update(self.additional_properties)
+        field_dict.update({})
+        if volume is not UNSET:
+            field_dict["volume"] = volume
+
+        return field_dict
+
+    @classmethod
+    def from_dict(cls: Type[T], src_dict: Dict[str, Any]) -> T:
+        from ..models.table_volume_statistics_collectors_spec import (
+            TableVolumeStatisticsCollectorsSpec,
+        )
+
+        d = src_dict.copy()
+        _volume = d.pop("volume", UNSET)
+        volume: Union[Unset, TableVolumeStatisticsCollectorsSpec]
+        if isinstance(_volume, Unset):
+            volume = UNSET
+        else:
+            volume = TableVolumeStatisticsCollectorsSpec.from_dict(_volume)
+
+        table_statistics_collectors_root_categories_spec = cls(
+            volume=volume,
+        )
+
+        table_statistics_collectors_root_categories_spec.additional_properties = d
+        return table_statistics_collectors_root_categories_spec
+
+    @property
+    def additional_keys(self) -> List[str]:
+        return list(self.additional_properties.keys())
+
+    def __getitem__(self, key: str) -> Any:
+        return self.additional_properties[key]
+
+    def __setitem__(self, key: str, value: Any) -> None:
+        self.additional_properties[key] = value
+
+    def __delitem__(self, key: str) -> None:
+        del self.additional_properties[key]
+
+    def __contains__(self, key: str) -> bool:
+        return key in self.additional_properties
