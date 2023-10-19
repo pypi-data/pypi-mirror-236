626c96caa13d6f3c23befd423b6469f317152626
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-19 04:06:02 +0000

    Add package Samco-modular/0.0.17/Samco_modular-0.0.17.tar.gz

---
+++ packages/Samco-modular/Samco_modular-0.0.17.tar.gz/Samco_modular-0.0.17/Samco_modular/pip_installs.py
@@ -0,0 +1,10 @@
+def pip_install_func():
+    %pip install jupyterthemes 
+    %pip install py5paisa
+    %pip install pandas
+    %pip install numpy
+
+    %pip install py5paisa --upgrade
+    
+    output="RESTART KERNEL"
+    return output
