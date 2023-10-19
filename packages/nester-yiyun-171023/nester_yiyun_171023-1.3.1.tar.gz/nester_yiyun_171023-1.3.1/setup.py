35bd8671ae10cbaa7767df2031a6c2bc895bcbe6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-19 06:06:35 +0000

    Add package nester-yiyun-171023/1.3.1/nester_yiyun_171023-1.3.1.tar.gz

---
+++ packages/nester-yiyun-171023/nester_yiyun_171023-1.3.1.tar.gz/nester_yiyun_171023-1.3.1/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name		= 'nester_yiyun_171023',
+	version		= '1.3.1',
+	py_modules	= ['nester_yiyun_171023'],
+	author		= 'yiyunxiao08ko', 
+	author_email	= 'yiyunxiao08ko@yahoo.de', 
+	url		= 'http://www.headfirstlabs.com', 
+	description	= 'A simple printer of nested lists',
+	)
