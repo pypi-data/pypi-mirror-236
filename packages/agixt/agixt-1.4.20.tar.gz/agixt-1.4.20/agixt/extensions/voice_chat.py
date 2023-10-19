dfadf9d7ef255fe7d20ef4102ffdc2490f8f8044
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-19 05:05:01 +0000

    Add package agixt/1.4.20/agixt-1.4.20.tar.gz

---
+++ packages/agixt/agixt-1.4.20.tar.gz/agixt-1.4.20/agixt/extensions/voice_chat.py
@@ -0,0 +1,85 @@
+from ApiClient import ApiClient
+from Extensions import Extensions
+import logging
+
+
+class voice_chat(Extensions):
+    def __init__(self, **kwargs):
+        if "agent_name" in kwargs:
+            self.agent_name = kwargs["agent_name"]
+        else:
+            self.agent_name = "gpt4free"
+        self.voice_prompt = "Voice Chat without Commands"
+        if "enabled_commands" in kwargs:
+            if len(kwargs["enabled_commands"]) > 0:
+                self.voice_prompt = "Voice Chat"
+        self.tts_command = "Speak with TTS with Streamlabs Text to Speech"
+        if "USE_STREAMLABS_TTS" in kwargs:
+            if isinstance(kwargs["USE_STREAMLABS_TTS"], bool):
+                if kwargs["USE_STREAMLABS_TTS"]:
+                    self.tts_command = "Speak with TTS with Streamlabs Text to Speech"
+            else:
+                if kwargs["USE_STREAMLABS_TTS"].lower() == "true":
+                    self.tts_command = "Speak with TTS with Streamlabs Text to Speech"
+        if "USE_GTTS" in kwargs:
+            if isinstance(kwargs["USE_GTTS"], bool):
+                if kwargs["USE_GTTS"]:
+                    self.tts_command = "Speak with GTTS"
+            else:
+                if kwargs["USE_GTTS"].lower() == "true":
+                    self.tts_command = "Speak with GTTS"
+        if "USE_HUGGINGFACE_TTS" in kwargs:
+            if isinstance(kwargs["USE_HUGGINGFACE_TTS"], bool):
+                if kwargs["USE_HUGGINGFACE_TTS"] and "HUGGINGFACE_API_KEY" in kwargs:
+                    if kwargs["ELEVENLABS_API_KEY"] != "":
+                        self.tts_command = "Read Audio with Huggingface"
+            else:
+                if (
+                    kwargs["USE_HUGGINGFACE_TTS"].lower() == "true"
+                    and "HUGGINGFACE_API_KEY" in kwargs
+                ):
+                    if kwargs["HUGGINGFACE_API_KEY"] != "":
+                        self.tts_command = "Read Audio with Huggingface"
+        if "ELEVENLABS_API_KEY" in kwargs:
+            if kwargs["ELEVENLABS_API_KEY"] != "":
+                self.tts_command = "Speak with TTS Using Elevenlabs"
+        self.commands = {
+            "Chat with Voice": self.chat_with_voice,
+        }
+        self.conversation_name = f"Voice Chat with {self.agent_name}"
+        if "conversation_name" in kwargs:
+            self.conversation_name = kwargs["conversation_name"]
+
+    async def chat_with_voice(
+        self,
+        base64_audio,
+        conversation_results=3,
+        context_results=10,
+    ):
+        # Transcribe the audio to text.
+        user_input = ApiClient.execute_command(
+            agent_name=self.agent_name,
+            command_name="Transcribe Base64 Audio",
+            command_args={"base64_audio": base64_audio},
+        )
+        logging.info(f"[Whisper]: Transcribed User Input: {user_input}")
+        # Send the transcribed text to the agent.
+        text_response = ApiClient.prompt_agent(
+            agent_name=self.agent_name,
+            prompt_name=self.voice_prompt,
+            prompt_args={
+                "user_input": user_input,
+                "conversation_name": self.conversation_name,
+                "conversation_results": conversation_results,
+                "context_results": context_results,
+            },
+        )
+        logging.info(f"[Whisper]: Text Response from LLM: {text_response}")
+        # Get the audio response from the TTS engine and return it.
+        audio_response = ApiClient.execute_command(
+            agent_name=self.agent_name,
+            command_name=self.tts_command,
+            command_args={"text": text_response},
+        )
+        logging.info(f"[Whisper]: Audio Response from TTS: {audio_response}")
+        return audio_response
---
+++ packages/agixt/agixt-1.4.20.tar.gz/agixt-1.4.20/agixt/extensions/whisper_stt.py
@@ -0,0 +1,74 @@
+try:
+    from whisper_cpp import Whisper
+except ImportError:
+    import sys
+    import subprocess
+
+    subprocess.check_call(
+        [
+            sys.executable,
+            "-m",
+            "pip",
+            "install",
+            "whisper-cpp-pybind",
+        ]
+    )
+    from whisper_cpp import Whisper
+
+import base64
+import requests
+import os
+from Extensions import Extensions
+
+
+class whisper_stt(Extensions):
+    def __init__(self, WHISPER_MODEL="base.en", **kwargs):
+        self.commands = {
+            "Transcribe Audio from File": self.transcribe_audio_from_file,
+            "Transcribe Base64 Audio": self.transcribe_base64_audio,
+        }
+        # https://huggingface.co/ggerganov/whisper.cpp
+        # Models: tiny, tiny.en, base, base.en, small, small.en, medium, medium.en, large, large-v1
+        if WHISPER_MODEL not in [
+            "tiny",
+            "tiny.en",
+            "base",
+            "base.en",
+            "small",
+            "small.en",
+            "medium",
+            "medium.en",
+            "large",
+            "large-v1",
+        ]:
+            self.WHISPER_MODEL = "base.en"
+        else:
+            self.WHISPER_MODEL = WHISPER_MODEL
+        os.makedirs(os.path.join(os.getcwd(), "models", "whispercpp"), exist_ok=True)
+        model_path = os.path.join(
+            os.getcwd(), "models", "whispercpp", f"ggml-{WHISPER_MODEL}.bin"
+        )
+        if not os.path.exists(model_path):
+            r = requests.get(
+                f"https://huggingface.co/ggerganov/whisper.cpp/resolve/main/ggml-{WHISPER_MODEL}.bin",
+                allow_redirects=True,
+            )
+            open(model_path, "wb").write(r.content)
+
+    async def transcribe_audio_from_file(self, filename: str = "recording.wav"):
+        w = Whisper(model_path=os.path.join(os.getcwd(), "models"))
+        file_path = os.path.join(os.getcwd(), "WORKSPACE", filename)
+        if not os.path.exists(file_path):
+            raise RuntimeError(f"Failed to load audio: {filename} does not exist.")
+        w.transcribe(file_path)
+        return w.output()
+
+    async def transcribe_base64_audio(self, base64_audio: str):
+        # Save the audio as a file then run transcribe_audio_from_file.
+        audio = base64.b64decode(base64_audio)
+        filename = "recording.wav"
+        with open(filename, "wb") as f:
+            f.write(audio)
+        output = self.transcribe_audio_from_file(filename)
+        os.remove(filename)
+        return output
