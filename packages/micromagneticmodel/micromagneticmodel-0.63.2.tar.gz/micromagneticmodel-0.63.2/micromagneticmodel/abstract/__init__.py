e3b9f0feeab70355f768c6beb6c0cc8384191e20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-20 03:10:02 +0000

    Add package micromagneticmodel/0.63.2/micromagneticmodel-0.63.2.tar.gz

---
+++ packages/micromagneticmodel/micromagneticmodel-0.63.2.tar.gz/micromagneticmodel-0.63.2/micromagneticmodel/abstract/__init__.py
@@ -0,0 +1,4 @@
+"""Abstract classes"""
+from .abstract import Abstract
+from .container import Container
+from .term import Term
---
+++ packages/micromagneticmodel/micromagneticmodel-0.63.2.tar.gz/micromagneticmodel-0.63.2/micromagneticmodel/dynamics/__init__.py
@@ -0,0 +1,6 @@
+from .damping import Damping
+from .dynamics import Dynamics
+from .dynamicsterm import DynamicsTerm
+from .precession import Precession
+from .slonczewski import Slonczewski
+from .zhangli import ZhangLi
