111a299862110f2b7ef3cb1816f39b816ecb1c9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-20 16:08:03 +0000

    Add package pygrabber/0.2/pygrabber-0.2.tar.gz

---
+++ packages/pygrabber/pygrabber-0.2.tar.gz/pygrabber-0.2/setup.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+from setuptools import setup
+
+setup(name='pygrabber',
+      version='0.2',
+      description='Module for grabbing live video images using DirectShow',
+      long_description=(Path(__file__).parent / "README.md").read_text(),
+      long_description_content_type='text/markdown',
+      url='https://github.com/andreaschiavinato/python_grabber',
+      author='andreaschiavinato',
+      author_email='',
+      license='MIT',
+      packages=['pygrabber'],
+      package_data={"pygrabber": ["py.typed"]},
+      install_requires=[
+          "comtypes>=1.1.7",
+          "numpy>=1.17.3"
+      ],
+      zip_safe=False)
