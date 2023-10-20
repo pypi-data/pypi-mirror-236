26a95c24168e71b76845816d905fe588161d70eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-20 05:06:45 +0000

    Add package pybritive/1.5.0/pybritive-1.5.0-py3-none-any.whl

---
+++ packages/pybritive/pybritive-1.5.0-py3-none-any.whl/pybritive/commands/cache.py
@@ -0,0 +1,24 @@
+import click
+from ..helpers.build_britive import build_britive
+from ..options.britive_options import britive_options
+
+
+@click.group()
+def cache():
+    """Manage local cache settings."""
+    pass
+
+
+@cache.command()
+@build_britive
+@britive_options(names='tenant,token,silent,passphrase,federation_provider')
+def profiles(ctx, tenant, token, silent, passphrase, federation_provider):
+    """Cache profiles locally to facilitate auto-completion of profile names on checkin/checkout."""
+    ctx.obj.britive.cache_profiles()
+
+
+@cache.command()
+@build_britive
+def clear(ctx):
+    """Clears the local cache."""
+    ctx.obj.britive.cache_clear()
