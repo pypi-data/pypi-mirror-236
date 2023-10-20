9e3fbf6c0e71c94ef2e7a2cf2afe7f50c213a5c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-20 05:06:32 +0000

    Add package geodemov2/0.0.1/geodemov2-0.0.1.tar.gz

---
+++ packages/geodemov2/geodemov2-0.0.1.tar.gz/geodemov2-0.0.1/README.md
@@ -0,0 +1,17 @@
+# geodemov2
+
+
+[![image](https://img.shields.io/pypi/v/geodemov2.svg)](https://pypi.python.org/pypi/geodemov2)
+[![image](https://img.shields.io/conda/vn/conda-forge/geodemov2.svg)](https://anaconda.org/conda-forge/geodemov2)
+
+
+**example geo python packages**
+
+
+-   Free software: MIT license
+-   Documentation: https://Hamilton97.github.io/geodemov2
+    
+
+## Features
+
+-   TODO
---
+++ packages/geodemov2/geodemov2-0.0.1.tar.gz/geodemov2-0.0.1/tests/test_geodemov2.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+"""Tests for `geodemov2` package."""
+
+
+import unittest
+
+from geodemov2 import geodemov2
+
+
+class TestGeodemov2(unittest.TestCase):
+    """Tests for `geodemov2` package."""
+
+    def setUp(self):
+        """Set up test fixtures, if any."""
+
+    def tearDown(self):
+        """Tear down test fixtures, if any."""
+
+    def test_000_something(self):
+        """Test something."""
