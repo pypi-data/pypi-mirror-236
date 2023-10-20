954ab967e614c38a4c99bd1f62feb6c154721f1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-20 05:06:36 +0000

    Add package flatpack/2.3.4/flatpack-2.3.4.tar.gz

---
+++ packages/flatpack/flatpack-2.3.4.tar.gz/flatpack-2.3.4/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name="flatpack",
+    version="2.3.4",
+    packages=find_packages(),
+    install_requires=[
+        "asyncio",
+        "httpx",
+        "requests",
+        "toml",
+        "torch"
+    ],
+    author="Romlin Group AB",
+    author_email="hello@romlin.com",
+    description="Train AI models - not your patience",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    entry_points={
+        "console_scripts": [
+            "flatpack=flatpack.main:main"
+        ],
+    }
+)
