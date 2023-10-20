8d565dd97ac86b0ec19a6f83a3b31825ea481ded
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-20 16:10:34 +0000

    Add package stimulsoft-data-adapters/2023.4.2/stimulsoft_data_adapters-2023.4.2.tar.gz

---
+++ packages/stimulsoft-data-adapters/stimulsoft_data_adapters-2023.4.2.tar.gz/stimulsoft_data_adapters-2023.4.2/stimulsoft_data_adapters/events/StiDataEventArgs.py
@@ -0,0 +1,28 @@
+from .StiEventArgs import StiEventArgs
+from ..classes.StiDataResult import StiDataResult
+
+class StiDataEventArgs(StiEventArgs):
+
+    command: str = None
+    """The current command for the data adapter. The supported commands are in the 'StiDataCommand' enum."""
+
+    database: str = None
+    """The database type for which the command will be executed. The supported database types are in the 'StiDatabaseType' enum."""
+
+    connection: str = None
+    """The name of the current database connection."""
+
+    dataSource: str = None
+    """The name of the current data source."""
+
+    connectionString: str = None
+    """The connection string for the current data source."""
+
+    queryString: str = None
+    """The SQL query that will be executed to get the data array of the current data source."""
+
+    parameters: dict[str, object] = None
+    """A set of parameters for the current SQL query."""
+
+    result: StiDataResult = None
+    """The result of executing an event handler request."""
