edf656f1a7dc2baf2f923d0248ee3f956762b98b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-20 14:06:57 +0000

    Add package studioai/0.2.9/studioai-0.2.9.tar.gz

---
+++ packages/studioai/studioai-0.2.9.tar.gz/studioai-0.2.9/studioai/analysis/__init__.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+# ================================================================================================ #
+# Project    : Artificial Intelligence & Data Science Studio                                       #
+# Version    : 0.1.0                                                                               #
+# Python     : 3.10.12                                                                             #
+# Filename   : /studioai/analysis/__init__.py                                                      #
+# ------------------------------------------------------------------------------------------------ #
+# Author     : John James                                                                          #
+# Email      : john.james.ai.studio@gmail.com                                                      #
+# URL        : https://github.com/john-james-ai/studioai                                           #
+# ------------------------------------------------------------------------------------------------ #
+# Created    : Thursday October 19th 2023 06:49:08 pm                                              #
+# Modified   : Thursday October 19th 2023 07:34:06 pm                                              #
+# ------------------------------------------------------------------------------------------------ #
+# License    : MIT License                                                                         #
+# Copyright  : (c) 2023 John James                                                                 #
+# ================================================================================================ #
+
+from studioai.analysis.container import AnalysisContainer
+
+container = AnalysisContainer()
+container.init_resources()
+container.wire(packages=["studioai.analysis.explore.eda"])
