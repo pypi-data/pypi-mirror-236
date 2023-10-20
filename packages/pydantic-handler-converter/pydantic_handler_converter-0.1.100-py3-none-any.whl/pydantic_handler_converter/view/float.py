02a6e0c1eda34d9b7405068d52a2080efa686b44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-20 04:06:28 +0000

    Add package pydantic-handler-converter/0.1.100/pydantic_handler_converter-0.1.100-py3-none-any.whl

---
+++ packages/pydantic-handler-converter/pydantic_handler_converter-0.1.100-py3-none-any.whl/pydantic_handler_converter/view/float.py
@@ -0,0 +1,8 @@
+from .base import BaseView
+
+
+class FloatView(BaseView):
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        self.text = self.dialects.INPUT_STR.format(field_name=self.field_name)
+
