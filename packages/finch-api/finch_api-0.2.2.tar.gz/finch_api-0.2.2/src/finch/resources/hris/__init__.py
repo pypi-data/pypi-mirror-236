d0dcacea8873a732f11e6a582c3b56bfe27d6a5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-20 05:05:33 +0000

    Add package finch-api/0.2.2/finch_api-0.2.2.tar.gz

---
+++ packages/finch-api/finch_api-0.2.2.tar.gz/finch_api-0.2.2/src/finch/resources/hris/__init__.py
@@ -0,0 +1,29 @@
+# File generated from our OpenAPI spec by Stainless.
+
+from .hris import HRIS, AsyncHRIS
+from .company import CompanyResource, AsyncCompanyResource
+from .benefits import Benefits, AsyncBenefits
+from .payments import Payments, AsyncPayments
+from .directory import Directory, AsyncDirectory
+from .employments import Employments, AsyncEmployments
+from .individuals import Individuals, AsyncIndividuals
+from .pay_statements import PayStatements, AsyncPayStatements
+
+__all__ = [
+    "CompanyResource",
+    "AsyncCompanyResource",
+    "Directory",
+    "AsyncDirectory",
+    "Individuals",
+    "AsyncIndividuals",
+    "Employments",
+    "AsyncEmployments",
+    "Payments",
+    "AsyncPayments",
+    "PayStatements",
+    "AsyncPayStatements",
+    "Benefits",
+    "AsyncBenefits",
+    "HRIS",
+    "AsyncHRIS",
+]
