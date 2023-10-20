7a0e84e0687209dd843253cd88628569a9d2deb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-20 05:06:06 +0000

    Add package bafcode/1.0.3/bafcode-1.0.3-py3-none-any.whl

---
+++ packages/bafcode/bafcode-1.0.3-py3-none-any.whl/cli/main.py
@@ -0,0 +1,21 @@
+from .commands import setup, start, generate_key
+
+def main():
+    import sys
+
+    if len(sys.argv) < 2:
+        print("Please provide a command. Options: setup, start, generate-key")
+        return
+
+    command = sys.argv[1]
+    if command == "setup":
+        setup.setup()
+    elif command == "start":
+        start.start_framework()
+    elif command == "generate:key":
+        generate_key.generate_fernet_key()
+    else:
+        print(f"Unknown command: {command}")
+
+if __name__ == "__main__":
+    main()
