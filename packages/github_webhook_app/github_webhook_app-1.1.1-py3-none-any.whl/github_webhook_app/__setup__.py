3866bb3653c2275d185cdf8b7bef84d97b56f6a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-20 04:07:47 +0000

    Add package github_webhook_app/1.1.1/github_webhook_app-1.1.1-py3-none-any.whl

---
+++ packages/github_webhook_app/github_webhook_app-1.1.1-py3-none-any.whl/github_webhook_app/__setup__.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+
+setup(
+    name='github_webhook_app',
+
+    version='1',
+
+    description='Quickly create github webhook apps',
+    long_description='',
+
+    author='Josh Ghiloni',
+    author_email='ghiloni@gmail.com',
+
+    license='MIT',
+
+    packages=['github_webhook_app'],
+    zip_safe=False,
+)
