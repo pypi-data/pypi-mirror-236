11036e5190fb4cac828fbb65f65362e1c32c14a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-20 05:05:36 +0000

    Add package rethink-note/0.1.11/rethink-note-0.1.11.tar.gz

---
+++ packages/rethink-note/rethink-note-0.1.11.tar.gz/rethink-note-0.1.11/tests/test_api.py
@@ -0,0 +1,295 @@
+import datetime
+import unittest
+
+from fastapi.testclient import TestClient
+from httpx import Response
+
+from rethink import const
+from rethink.application import app
+from rethink.models import database
+from rethink.models.utils import jwt_decode
+from . import utils
+
+
+class PublicApiTest(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls) -> None:
+        utils.set_env(".env.test.local")
+        database.init()
+
+    def setUp(self) -> None:
+        self.client = TestClient(app)
+
+    @classmethod
+    def tearDownClass(cls) -> None:
+        database.drop_all()
+        utils.drop_env(".env.test.local")
+
+    def test_home(self):
+        resp = self.client.get("/")
+        self.assertEqual(200, resp.status_code)
+        self.assertEqual("text/html; charset=utf-8", resp.headers["content-type"])
+        self.assertEqual(Response, type(resp))
+
+    def test_register(self):
+        resp = self.client.put("/api/user", json={
+            "email": "a@c.com",
+            "password": "a",
+            "language": const.Language.EN.value,
+            "requestId": "xxx"
+        })
+        rj = resp.json()
+        self.assertEqual(const.Code.ONE_USER_MODE.value, rj["code"])
+        self.assertEqual(len(rj["token"]), 0)
+        self.assertEqual("xxx", rj["requestId"])
+
+
+class TokenApiTest(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls) -> None:
+        utils.set_env(".env.test.local")
+
+    def setUp(self) -> None:
+        database.init()
+        self.client = TestClient(app)
+        resp = self.client.post("/api/login", json={
+            "email": const.DEFAULT_USER["email"],
+            "password": "",
+        })
+        rj = resp.json()
+        self.assertEqual(0, rj["code"])
+        self.token = rj["token"]
+
+    def tearDown(self) -> None:
+        database.drop_all()
+
+    @classmethod
+    def tearDownClass(cls) -> None:
+        utils.drop_env(".env.test.local")
+
+    def test_get_user(self):
+        resp = self.client.get(
+            "/api/user",
+            params={"rid": "xxx"},
+            headers={"token": self.token})
+        rj = resp.json()
+        self.assertEqual(0, rj["code"])
+        self.assertEqual("rethink", rj["user"]["nickname"])
+        self.assertEqual("xxx", rj["requestId"])
+        self.assertGreater(
+            datetime.datetime.strptime(rj["user"]["createdAt"], "%Y-%m-%dT%H:%M:%SZ").timestamp(),
+            0., msg=rj["user"]["createdAt"])
+
+    def test_update_user(self):
+        resp = self.client.post("/api/user", json={
+            "requestId": "xxx",
+            "nickname": "new nickname",
+            "avatar": "http://new.avatar/aa.png",
+        }, headers={"token": self.token})
+        rj = resp.json()
+        self.assertEqual(0, rj["code"])
+        self.assertEqual("xxx", rj["requestId"])
+
+        resp = self.client.get("/api/user", params={"rid": "xxx"}, headers={"token": self.token})
+        rj = resp.json()
+        self.assertEqual(0, rj["code"])
+        self.assertEqual("new nickname", rj["user"]["nickname"])
+        self.assertEqual("http://new.avatar/aa.png", rj["user"]["avatar"])
+        self.assertEqual("xxx", rj["requestId"])
+
+    def test_recent_search(self):
+        resp = self.client.put("/api/node", json={
+            "requestId": "xxx",
+            "md": "node1\ntext",
+            "type": const.NodeType.MARKDOWN.value,
+        }, headers={"token": self.token})
+        rj = resp.json()
+        self.assertEqual(0, rj["code"])
+
+        resp = self.client.put(
+            "/api/search/recent",
+            json={
+                "requestId": "xxx",
+                "nid": rj["node"]["id"],
+            }, headers={"token": self.token})
+        rj = resp.json()
+        self.assertEqual(0, rj["code"])
+
+        resp = self.client.get(
+            "/api/search/recent",
+            params={"rid": "xxx"},
+            headers={"token": self.token}
+        )
+        rj = resp.json()
+        self.assertEqual(0, rj["code"])
+        self.assertEqual("xxx", rj["requestId"])
+        self.assertEqual(len(rj["nodes"]), 1)
+        self.assertEqual(rj["nodes"][0]["title"], "node1")
+
+    def test_node(self):
+        resp = self.client.post(
+            "/api/search/node",
+            json={
+                "query": "",
+                "requestId": "xxx",
+                "sortKey": "createdAt",
+                "sortOrder": -1, "page": 0, "pageSize": 5},
+            headers={"token": self.token})
+        rj = resp.json()
+        self.assertGreater(len(rj["data"]["nodes"]), 0)
+
+        self.client.post(
+            "/api/search/node",
+            json={
+                "query": "",
+                "requestId": "xxx",
+                "sortKey": "createdAt",
+                "sortOrder": -1, "page": 0, "pageSize": 5},
+            headers={"token": self.token})
+
+        resp = self.client.put("/api/node", json={
+            "requestId": "xxx",
+            "md": "node1\ntext",
+            "type": const.NodeType.MARKDOWN.value,
+        }, headers={"token": self.token})
+        rj = resp.json()
+        self.assertEqual(0, rj["code"])
+        self.assertEqual("xxx", rj["requestId"])
+        node = rj["node"]
+
+        resp = self.client.get(
+            "/api/node",
+            params={"rid": "xxx", "nid": node["id"]},
+            headers={"token": self.token}
+        )
+        rj = resp.json()
+        self.assertEqual(0, rj["code"])
+        self.assertEqual("xxx", rj["requestId"])
+        n = rj["node"]
+        self.assertEqual("node1", n["title"], msg=rj)
+        self.assertEqual("node1\ntext", n["md"])
+        self.assertEqual(const.NodeType.MARKDOWN.value, n["type"])
+
+        resp = self.client.post(
+            "/api/node",
+            json={
+                "requestId": "xxx",
+                "nid": node["id"],
+                "md": "node2\ntext"
+            },
+            headers={"token": self.token}
+        )
+        rj = resp.json()
+        self.assertEqual(0, rj["code"])
+        self.assertEqual("xxx", rj["requestId"])
+
+        resp = self.client.get(
+            "/api/node",
+            params={"rid": "xxx", "nid": node["id"]},
+            headers={"token": self.token}
+        )
+        rj = resp.json()
+        n = rj["node"]
+        self.assertEqual(0, rj["code"])
+        self.assertEqual("xxx", rj["requestId"])
+        self.assertEqual("node2", n["title"])
+        self.assertEqual("node2\ntext", n["md"])
+        self.assertEqual(const.NodeType.MARKDOWN.value, n["type"])
+
+        resp = self.client.put(
+            "/api/trash",
+            json={
+                "requestId": "xxx",
+                "nid": node["id"],
+            },
+            headers={"token": self.token}
+        )
+        rj = resp.json()
+        self.assertEqual(0, rj["code"])
+
+        resp = self.client.get(
+            "/api/trash",
+            params={"rid": "xxx", "p": 0, "ps": 10},
+            headers={"token": self.token}
+        )
+        rj = resp.json()
+        self.assertEqual(0, rj["code"])
+        self.assertEqual(1, len(rj["data"]["nodes"]))
+
+        resp = self.client.post(
+            "/api/trashRestore",
+            json={
+                "requestId": "xxx",
+                "nid": node["id"],
+            },
+            headers={"token": self.token}
+        )
+        rj = resp.json()
+        self.assertEqual(0, rj["code"])
+
+        resp = self.client.post(
+            "/api/search/cursor",
+            json={
+                "requestId": "xxx",
+                "nid": node["id"],
+                "textBeforeCursor": "@How",
+            },
+            headers={"token": self.token},
+        )
+        rj = resp.json()
+        self.assertEqual(0, rj["code"])
+        self.assertEqual("xxx", rj["requestId"])
+        self.assertEqual("How", rj["result"]["query"])
+        self.assertEqual(1, len(rj["result"]["nodes"]))
+        self.assertEqual("How do I record", rj["result"]["nodes"][0]["title"])
+
+        self.client.put(
+            "/api/trash",
+            json={
+                "requestId": "xxx",
+                "nid": node["id"],
+            },
+            headers={"token": self.token}
+        )
+        resp = self.client.delete(
+            f"/api/trash/{node['id']}",
+            headers={"token": self.token}
+        )
+        rj = resp.json()
+        self.assertEqual(0, rj["code"])
+
+        resp = self.client.delete(
+            "/api/trash/ssa",
+            headers={"token": self.token}
+        )
+        rj = resp.json()
+        self.assertEqual(const.Code.NODE_NOT_EXIST.value, rj["code"])
+
+        resp = self.client.get(
+            "/api/node",
+            params={"rid": "xxx", "nid": node["id"]},
+            headers={"token": self.token}
+        )
+        rj = resp.json()
+        self.assertEqual(const.Code.NODE_NOT_EXIST.value, rj["code"])
+
+    def test_captcha(self):
+        resp = self.client.get(
+            "/api/captcha/img",
+        )
+        self.assertEqual(200, resp.status_code)
+        self.assertEqual("image/png", resp.headers["content-type"])
+        self.assertGreater(len(resp.headers["x-captcha-token"]), 0)
+
+        code_str = jwt_decode(resp.headers["x-captcha-token"])["code"]
+        resp = self.client.post(
+            "/api/captcha/img",
+            json={
+                "requestId": "xxx",
+                "token": resp.headers["x-captcha-token"],
+                "codeStr": code_str,
+            }
+        )
+        rj = resp.json()
+        self.assertEqual(0, rj["code"])
+        self.assertEqual("xxx", rj["requestId"])
---
+++ packages/rethink-note/rethink-note-0.1.11.tar.gz/rethink-note-0.1.11/tests/test_models_local.py
@@ -0,0 +1,265 @@
+import unittest
+from textwrap import dedent
+
+from rethink import const, models
+from rethink.models.utils import short_uuid
+from . import utils
+
+
+class LocalModelsTest(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls) -> None:
+        utils.set_env(".env.test.local")
+        models.database.drop_all()
+
+    @classmethod
+    def tearDownClass(cls) -> None:
+        utils.drop_env(".env.test.local")
+
+    def setUp(self) -> None:
+        models.database.init()
+        u, _ = models.user.get_by_email(email=const.DEFAULT_USER["email"])
+        self.uid = u["id"]
+
+    def tearDown(self) -> None:
+        models.database.drop_all()
+
+    def test_user(self):
+        _id, code = models.user.add(
+            account="aaa", source=const.UserSource.EMAIL.value,
+            email="aaa", hashed="bbb", nickname="ccc", avatar="ddd", language=const.Language.EN.value)
+        self.assertNotEqual("", _id)
+        self.assertEqual(const.Code.OK, code)
+
+        u, code = models.user.get_by_email(email=const.DEFAULT_USER["email"])
+        self.assertEqual(const.Code.OK, code)
+        self.assertEqual("rethink", u["nickname"])
+        self.assertIsNotNone(u)
+
+        u, code = models.user.get(_id)
+        self.assertEqual(const.Code.OK, code)
+        self.assertEqual("ccc", u["nickname"])
+
+        u, code = models.user.update(uid=_id, email="a@a.com", hashed="1", nickname="2", avatar="3")
+        self.assertEqual(const.Code.OK, code)
+
+        u, code = models.user.get(_id)
+        self.assertEqual(const.Code.OK, code)
+        self.assertEqual("a@a.com", u["email"])
+        self.assertEqual("1", u["hashed"])
+        self.assertEqual("2", u["nickname"])
+        self.assertEqual("3", u["avatar"])
+
+        code = models.user.disable(uid=_id)
+        self.assertEqual(const.Code.OK, code)
+
+        u, code = models.user.get(uid=_id)
+        self.assertEqual(const.Code.ACCOUNT_OR_PASSWORD_ERROR, code)
+        self.assertIsNone(u)
+
+        code = models.user.enable(uid=_id)
+        self.assertEqual(const.Code.OK, code)
+
+        code = models.user.disable(uid="sdwqdqw")
+        self.assertEqual(const.Code.OPERATION_FAILED, code)
+
+        code = models.user.delete(uid=_id)
+        self.assertEqual(const.Code.OK, code)
+
+    def test_node(self):
+        node, code = models.node.add(
+            uid=self.uid, md="a" * (const.MD_MAX_LENGTH + 1), type_=const.NodeType.MARKDOWN.value
+        )
+        self.assertEqual(const.Code.NOTE_EXCEED_MAX_LENGTH, code)
+        self.assertIsNone(node)
+
+        node, code = models.node.add(
+            uid=self.uid, md="[title](/qqq)\nbody", type_=const.NodeType.MARKDOWN.value
+        )
+        self.assertEqual(const.Code.OK, code)
+
+        n, code = models.node.get(uid=self.uid, nid=node["id"])
+        self.assertEqual(const.Code.OK, code)
+        self.assertEqual("title", n["title"])
+        self.assertEqual("body", n["snippet"])
+
+        ns, total = models.search.user_node(uid=self.uid)
+        self.assertEqual(3, len(ns))
+        self.assertEqual(3, total)
+
+        n, code = models.node.update(uid=self.uid, nid=node["id"], md="title2\nbody2")
+        self.assertEqual(const.Code.OK, code)
+        self.assertEqual(const.Code.OK, code)
+        self.assertEqual("title2", n["title"])
+        self.assertEqual("title2\nbody2", n["md"])
+        self.assertEqual(const.NodeType.MARKDOWN.value, n["type"])
+
+        code = models.node.disable(uid=self.uid, nid=node["id"])
+        self.assertEqual(const.Code.OK, code)
+        n, code = models.node.get(uid=self.uid, nid=node["id"])
+        self.assertEqual(const.Code.NODE_NOT_EXIST, code)
+
+        code = models.node.to_trash(uid=self.uid, nid=node["id"])
+        self.assertEqual(const.Code.OK, code)
+
+        code = models.node.delete(uid=self.uid, nid=node["id"])
+        self.assertEqual(const.Code.OK, code)
+        n, code = models.node.get(uid=self.uid, nid=node["id"])
+        self.assertIsNone(n)
+        self.assertEqual(const.Code.NODE_NOT_EXIST, code)
+
+    def test_parse_at(self):
+        nid1, _ = models.node.add(
+            uid=self.uid, md="c", type_=const.NodeType.MARKDOWN.value,
+        )
+        nid2, _ = models.node.add(
+            uid=self.uid, md="我133", type_=const.NodeType.MARKDOWN.value,
+        )
+        md = dedent(f"""title
+        fffqw [@c](/n/{nid1['id']})
+        fff 
+        [@我133](/n/{nid2['id']})
+        ffq
+        """)
+        node, code = models.node.add(
+            uid=self.uid, md=md, type_=const.NodeType.MARKDOWN.value
+        )
+        self.assertEqual(const.Code.OK, code)
+        nodes, total = models.search.user_node(uid=self.uid)
+        self.assertEqual(5, len(nodes))
+        self.assertEqual(5, total)
+        found, total = models.search.user_node(uid=self.uid, query="我")
+        self.assertEqual(2, len(found), msg=found)
+        self.assertEqual(5, total)
+
+        n, code = models.node.get(uid=self.uid, nid=node["id"])
+        self.assertEqual(const.Code.OK, code)
+        self.assertEqual(2, len(n["toNodeIds"]))
+
+        cache = n["md"]
+        n, code = models.node.update(uid=self.uid, nid=node["id"], md=f'{cache}xxxx')
+        self.assertEqual(const.Code.OK, code)
+        self.assertEqual(cache + "xxxx", n["md"])
+
+        n, code = models.node.get(uid=self.uid, nid=nid1['id'])
+        self.assertEqual(const.Code.OK, code)
+        self.assertEqual(1, len(n["fromNodeIds"]))
+
+        n, code = models.node.update(uid=self.uid, nid=node["id"], md=n["title"])
+        self.assertEqual(const.Code.OK, code)
+        self.assertEqual(0, len(n["toNodeIds"]))
+
+        n, code = models.node.get(uid=self.uid, nid=nid1['id'])
+        self.assertEqual(const.Code.OK, code)
+        self.assertEqual(0, len(n["fromNodeIds"]))
+
+    def test_add_set(self):
+        node, code = models.node.add(
+            uid=self.uid, md="title\ntext", type_=const.NodeType.MARKDOWN.value
+        )
+        self.assertEqual(0, len(node["toNodeIds"]))
+        self.assertEqual(const.Code.OK, code)
+
+        res = models.db_ops.node_add_to_set(node["id"], "toNodeIds", short_uuid())
+        self.assertEqual(1, res.modified_count)
+        node, code = models.node.get(uid=self.uid, nid=node["id"])
+        self.assertEqual(const.Code.OK, code)
+        self.assertEqual(1, len(node["toNodeIds"]))
+
+    def test_cursor_text(self):
+        n1, code = models.node.add(
+            uid=self.uid, md="title\ntext", type_=const.NodeType.MARKDOWN.value
+        )
+        self.assertEqual(const.Code.OK, code)
+        n2, code = models.node.add(
+            uid=self.uid, md="title2\ntext", type_=const.NodeType.MARKDOWN.value
+        )
+        self.assertEqual(const.Code.OK, code)
+
+        q, recom = models.node.cursor_query(
+            uid=self.uid,
+            nid=n2["id"],
+            cursor_text="te",
+        )
+        self.assertEqual(0, len(recom))
+        self.assertEqual(None, q)
+
+        q, recom = models.node.cursor_query(
+            uid=self.uid,
+            nid=n2["id"],
+            cursor_text="@te",
+        )
+        self.assertEqual(2, len(recom))
+        self.assertEqual("te", q)
+
+        q, recom = models.node.cursor_query(
+            uid=self.uid,
+            nid=n2["id"],  # exclude the second node
+            cursor_text="sw21 @te",
+        )
+        self.assertEqual("te", q)
+        self.assertEqual(2, len(recom))
+        self.assertEqual("title", recom[1]["title"])
+
+        q, recom = models.node.cursor_query(
+            uid=self.uid,
+            nid=n2["id"],  # exclude the second node
+            cursor_text="sw21 @",
+        )
+        self.assertEqual("", q)
+        self.assertEqual(2, len(recom))
+
+        code = models.search.add_recent_cursor_search(self.uid, n1["id"], n2["id"])
+        self.assertEqual(const.Code.OK, code)
+
+        q, recom = models.node.cursor_query(
+            uid=self.uid,
+            nid=n1["id"],  # exclude the second node
+            cursor_text="sw21 @",
+        )
+        self.assertEqual("", q)
+        self.assertEqual(3, len(recom))
+        self.assertEqual("Welcome to Rethink", recom[2]["title"])
+
+    def test_to_trash(self):
+        n1, code = models.node.add(
+            uid=self.uid, md="title\ntext", type_=const.NodeType.MARKDOWN.value
+        )
+        self.assertEqual(const.Code.OK, code)
+        n2, code = models.node.add(
+            uid=self.uid, md="title2\ntext", type_=const.NodeType.MARKDOWN.value
+        )
+        self.assertEqual(const.Code.OK, code)
+
+        code = models.node.to_trash(self.uid, n1["id"])
+        self.assertEqual(const.Code.OK, code)
+
+        ns, total = models.node.get_nodes_in_trash(self.uid, 0, 10)
+        self.assertEqual(1, len(ns))
+        self.assertEqual(1, total)
+        self.assertEqual(n1["id"], ns[0]["id"])
+
+        ns, total = models.search.user_node(self.uid)
+        self.assertEqual(3, len(ns))
+        self.assertEqual(3, total)
+
+        code = models.node.restore_from_trash(self.uid, n1["id"])
+        self.assertEqual(const.Code.OK, code)
+        nodes, total = models.search.user_node(self.uid)
+        self.assertEqual(4, len(nodes))
+        self.assertEqual(4, total)
+
+    def test_search(self):
+        code = models.search.put_recent_search(self.uid, "a")
+        self.assertEqual(const.Code.NODE_NOT_EXIST, code)
+
+        n1, code = models.node.add(
+            uid=self.uid, md="title\ntext", type_=const.NodeType.MARKDOWN.value
+        )
+        self.assertEqual(const.Code.OK, code)
+        code = models.search.put_recent_search(self.uid, n1["id"])
+        self.assertEqual(const.Code.OK, code)
+
+        nodes = models.search.get_recent_search(self.uid)
+        self.assertEqual(1, len(nodes))
+        self.assertEqual(n1["id"], nodes[0]["id"])
