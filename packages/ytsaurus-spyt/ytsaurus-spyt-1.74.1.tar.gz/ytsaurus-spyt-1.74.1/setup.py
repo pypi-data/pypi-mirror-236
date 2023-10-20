f2a7bf31d37794fa7fb31a47c1bf327782541bbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-20 04:09:53 +0000

    Add package ytsaurus-spyt/1.74.1/ytsaurus-spyt-1.74.1.tar.gz

---
+++ packages/ytsaurus-spyt/ytsaurus-spyt-1.74.1.tar.gz/ytsaurus-spyt-1.74.1/setup.py
@@ -0,0 +1,44 @@
+from distutils.util import convert_path
+import os
+import setuptools
+
+ver_path = convert_path('spyt/version.py')
+with open(ver_path) as ver_file:
+    exec(ver_file.read())
+
+SCRIPTS_PATH = convert_path('deps/bin')
+scripts = [os.path.join(SCRIPTS_PATH, x) for x in os.listdir(SCRIPTS_PATH)
+           if os.path.isfile(os.path.join(SCRIPTS_PATH, x))]
+
+with open("README.md") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name='ytsaurus-spyt',
+    version=__version__,
+    author='YTsaurus',
+    author_email='dev@ytsaurus.tech',
+    description='YTsaurus SPYT high-level client',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    keywords="yt ytsaurus python spyt",
+    url='https://github.com/ytsaurus/ytsaurus/tree/main/yt/spark/spark-over-yt',
+    packages=[
+        'spyt',
+        'spyt.jars'
+    ],
+    install_requires=[
+        'ytsaurus-pyspark=={}'.format(__spark_version__),
+        'pyarrow',
+        'pyyaml'
+    ],
+    scripts=scripts,
+    license='http://www.apache.org/licenses/LICENSE-2.0',
+    package_dir={
+        'spyt.jars': 'deps/jars'
+    },
+    package_data={
+        'spyt.jars': ['*.jar']
+    },
+    include_package_data=True,
+)
