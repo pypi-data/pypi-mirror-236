b747bd66fdc5a1e97980f35822703b30796cdbf2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-20 06:06:35 +0000

    Add package mknodes/0.45.1/mknodes-0.45.1-py3-none-any.whl

---
+++ packages/mknodes/mknodes-0.45.1-py3-none-any.whl/mknodes/paths.py
@@ -0,0 +1,12 @@
+"""Filesystem paths."""
+
+from __future__ import annotations
+
+import pathlib
+
+
+TEST_RESOURCES = pathlib.Path(__file__).parent.parent / "tests" / "data"
+RESOURCES = pathlib.Path(__file__).parent / "resources"
+CSS_DIR = RESOURCES / "css"
+DOCS_DIR = pathlib.Path(__file__).parent.parent / "docs"
+DEFAULT_BUILD_FN = "mknodes.navs.mkdefaultwebsite:MkDefaultWebsite.for_project"
