38c2372c56a6719ebabd69d88c0853c8b87fae5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-20 16:10:48 +0000

    Add package armcnc/1.0.9/armcnc-1.0.9-py3-none-any.whl

---
+++ packages/armcnc/armcnc-1.0.9-py3-none-any.whl/armcnc/cnc/ini.py
@@ -0,0 +1,14 @@
+"""
+******************************************************************************
+* @author  ARMCNC site:www.armcnc.net github:armcnc.github.io
+******************************************************************************
+"""
+
+import linuxcnc
+
+class Ini:
+
+    def __init__(self, framework):
+        self.framework = framework
+        self.linuxcnc = linuxcnc
+
