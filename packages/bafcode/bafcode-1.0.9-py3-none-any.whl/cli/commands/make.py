0de4d027f9d1e3cf5e267318ff99dedc2f53af20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-20 16:11:03 +0000

    Add package bafcode/1.0.9/bafcode-1.0.9-py3-none-any.whl

---
+++ packages/bafcode/bafcode-1.0.9-py3-none-any.whl/cli/commands/make.py
@@ -0,0 +1,47 @@
+import os
+import sys
+from .make_context import ApiContext, LlmContext, PromptContext, ToolContext
+
+def create_file(path,context):
+    # Ensure the directory exists
+    directory = os.path.dirname(path)
+    print(f"Attempting to create directory: {directory}")  # Add this line
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    # Check if file already exists
+    if os.path.exists(path):
+        print(f"Error: File '{path}' already exists.")
+        return
+
+    # Create the file
+    with open(path, 'w') as f:
+        f.write(context)
+    print(f"File '{path}' created successfully!")
+
+def make(type_,name):
+    print(f"sys.argv: {sys.argv}")  # Debug: print the full sys.argv
+
+
+    if type_ == "tool":
+        base_dir = "tools"
+        full_path = os.path.join(base_dir, name + ".py")
+        file_context = ToolContext.context(name)
+        create_file(full_path,file_context)
+    elif type_ == "api":
+        base_dir = "api"
+        full_path = os.path.join(base_dir, name + ".py")
+        file_context = ApiContext.context(name)
+        create_file(full_path,file_context)
+    elif type_ == "prompt":
+        base_dir = "prompts"
+        full_path = os.path.join(base_dir, name + ".py")
+        file_context = PromptContext.context(name)
+        create_file(full_path,file_context)
+    elif type_ == "llm":
+        base_dir = "llms"
+        full_path = os.path.join(base_dir, name + ".py")
+        file_context = LlmContext.context(name)
+        create_file(full_path,file_context)
+    else:
+        print(f"Error: Unknown type_ '{type_}'")
+        sys.exit(1)
