32311d19e1da1c634bc9186a7a3c555a7caf6aaf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-20 06:06:35 +0000

    Add package shepherd-core/2023.10.1/shepherd_core-2023.10.1.tar.gz

---
+++ packages/shepherd-core/shepherd_core-2023.10.1.tar.gz/shepherd_core-2023.10.1/tests/test_examples.py
@@ -0,0 +1,29 @@
+import os
+import subprocess
+from pathlib import Path
+
+import pytest
+
+
+@pytest.fixture
+def example_path() -> Path:
+    path = Path(__file__).resolve().parent.parent / "examples"
+    os.chdir(path)
+    return path
+
+
+examples = [
+    "experiment_generic_var1.py",
+    "experiment_models.py",
+    "firmware_model.py",
+    "firmware_modification.py",
+    "inventory.py",
+    "uart_decode_waveform.py",
+    "vharvester_simulation.py",
+    "vsource_simulation.py",
+]
+
+
+@pytest.mark.parametrize("file", examples)
+def test_example_scripts(example_path: Path, file: str) -> None:
+    subprocess.check_call(f"python {example_path / file}", shell=True)
