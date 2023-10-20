4d9dd45562bb5e57d72d885a568edbec54cc90b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-20 05:06:16 +0000

    Add package durkon/0.0.10/durkon-0.0.10.tar.gz

---
+++ packages/durkon/durkon-0.0.10.tar.gz/durkon-0.0.10/src/durkon/util.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import numpy as np
+import math
+
+def round_to_sf(x, sf=5):
+ if x==0:
+  return 0
+ else:
+  return round(x,sf-1-int(math.floor(math.log10(abs(x)))))
+
+def get_gradient(x, y):
+ aveX = float(sum(x))/len(x)
+ aveY = float(sum(y))/len(y)
+ 
+ numerator=sum((x-aveX)*(y-aveY))
+ denominator=sum((x-aveX)*(x-aveX))
+ 
+ return numerator/denominator
+
+
+def get_sorted_keys(d):
+ keys = [c for c in d]
+ keys.sort(key=str)
+ return keys
