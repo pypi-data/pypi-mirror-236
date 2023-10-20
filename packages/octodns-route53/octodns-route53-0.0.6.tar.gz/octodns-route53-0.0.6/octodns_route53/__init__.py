a5d023b0279480e15eb8b2e1d4f1d543e0b7834a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-20 05:05:31 +0000

    Add package octodns-route53/0.0.6/octodns-route53-0.0.6.tar.gz

---
+++ packages/octodns-route53/octodns-route53-0.0.6.tar.gz/octodns-route53-0.0.6/octodns_route53/__init__.py
@@ -0,0 +1,16 @@
+#
+#
+#
+
+from .provider import Route53Provider, Route53ProviderException
+from .record import Route53AliasRecord
+from .source import Ec2Source, ElbSource
+
+__VERSION__ = '0.0.6'
+
+# quell warnings
+Ec2Source
+ElbSource
+Route53AliasRecord
+Route53Provider
+Route53ProviderException
