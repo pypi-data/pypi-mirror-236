01251a9bc7290400abae67f749635987c135de68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-20 03:09:45 +0000

    Add package unique-passwords/1.0.1/unique-passwords-1.0.1.tar.gz

---
+++ packages/unique-passwords/unique-passwords-1.0.1.tar.gz/unique-passwords-1.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='unique-passwords',
+    version='1.0.1',
+    packages=[''],
+    url='https://github.com/VinayKhatrii/password',
+    license='MIT',
+    author='Vinay Khatri',
+    author_email='khatrivinay520@gmail.com',
+    description='A Python script to generate secure random passwords.',
+)
