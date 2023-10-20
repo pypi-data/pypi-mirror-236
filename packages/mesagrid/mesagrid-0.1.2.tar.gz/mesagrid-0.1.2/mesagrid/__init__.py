fbc00a9690cafb5779fb02dd78efb0004614dc27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-20 16:11:26 +0000

    Add package mesagrid/0.1.2/mesagrid-0.1.2.tar.gz

---
+++ packages/mesagrid/mesagrid-0.1.2.tar.gz/mesagrid-0.1.2/mesagrid/__init__.py
@@ -0,0 +1,9 @@
+"""Top-level package for mesagrid."""
+
+from importlib.metadata import version
+__version__ = version("pycounts")
+
+__author__ = """Earl Patrick Bellinger"""
+__email__ = 'earl.bellinger@yale.edu'
+
+from mesagrid import Grid, Track
