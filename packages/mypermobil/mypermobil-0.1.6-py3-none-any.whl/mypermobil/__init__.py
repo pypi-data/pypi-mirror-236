b9848b9f92a82369ca1232da63ff996deb152d28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-20 05:05:51 +0000

    Add package mypermobil/0.1.6/mypermobil-0.1.6-py3-none-any.whl

---
+++ packages/mypermobil/mypermobil-0.1.6-py3-none-any.whl/mypermobil/__init__.py
@@ -0,0 +1,11 @@
+__version__ = "0.1.6"
+
+from mypermobil.mypermobil import (
+    MyPermobil,
+    create_session,
+    MyPermobilException,
+    MyPermobilAPIException,
+    MyPermobilClientException,
+    MyPermobilConnectionException,
+)
+from mypermobil.const import *
