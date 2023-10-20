ab28078f1dba2176947835f3dcf0d55a5de609b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-20 03:11:02 +0000

    Add package epydemics/0.2/epydemics-0.2.tar.gz

---
+++ packages/epydemics/epydemics-0.2.tar.gz/epydemics-0.2/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='epydemics',
+    version='0.2',  # Start with a small version number, you can increment this as you make changes
+    packages=find_packages(),  # Automatically discover and include all packages in the package directory
+    install_requires=[
+        'numpy',
+        'pandas',
+        'matplotlib',
+        'scipy',
+        'seaborn',
+        'python-box',
+        'scikit-learn',
+        'statsmodels',
+        'itertools',
+        'logging',
+        'json'
+    ],
+    author='Juliho David Castillo Colmenares',
+    author_email='juliho.colmenares@gmail.com',
+    description='A module for modeling and analyzing epidemic data.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',  # If using a markdown README file
+    url='https://github.com/julihocc/epydemics',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',  # Choose the appropriate license
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',  # Specify a minimum Python version
+)
