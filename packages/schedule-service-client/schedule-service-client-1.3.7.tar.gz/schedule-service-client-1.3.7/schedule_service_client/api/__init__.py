3bbb2ca5ba693a083ac9302499f04e0d48a5ac6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-20 03:11:42 +0000

    Add package schedule-service-client/1.3.7/schedule-service-client-1.3.7.tar.gz

---
+++ packages/schedule-service-client/schedule-service-client-1.3.7.tar.gz/schedule-service-client-1.3.7/schedule_service_client/api/__init__.py
@@ -0,0 +1,7 @@
+# flake8: noqa
+
+# import apis into api package
+from schedule_service_client.api.default_api import DefaultApi
+from schedule_service_client.api.schedule_api import ScheduleApi
+from schedule_service_client.api.workbook_api import WorkbookApi
+
