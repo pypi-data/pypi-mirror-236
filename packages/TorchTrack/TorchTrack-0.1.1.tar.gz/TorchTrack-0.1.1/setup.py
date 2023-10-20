68f49fea9661f33e82919cf77209cf0a4c8bd60b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-20 16:09:05 +0000

    Add package TorchTrack/0.1.1/TorchTrack-0.1.1.tar.gz

---
+++ packages/TorchTrack/TorchTrack-0.1.1.tar.gz/TorchTrack-0.1.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="TorchTrack",
+    packages=find_packages(include=["TorchTrack"]),
+    version="0.1.1",
+    description="TorchTrack takes your models data and training data then send it to the TrackML web application",
+    author="NeuralNuts",
+    install_requires=[],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest==4.4.1'],
+    test_suite='test',
+)
