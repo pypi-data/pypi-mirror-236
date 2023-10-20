844384adfe5629d48cf7d828030c176c75fdae1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-20 15:07:30 +0000

    Add package lona/1.16/lona-1.16.tar.gz

---
+++ packages/lona/lona-1.16.tar.gz/lona-1.16/lona/__init__.py
@@ -0,0 +1,14 @@
+# TODO: remove LonaView and LonaApp in 2.0
+
+from .exceptions import *  # NOQA: F403
+from .responses import *  # NOQA: F403
+from .routing import MATCH_ALL, Route
+from .errors import *  # NOQA: F403
+from .view import View as LonaView
+from .app import App as LonaApp
+from .channels import Channel
+from .view import View
+from .app import App
+
+VERSION = (1, 16)
+VERSION_STRING = '.'.join(str(i) for i in VERSION)
---
+++ packages/lona/lona-1.16.tar.gz/lona-1.16/lona/request.py
@@ -0,0 +1,44 @@
+class Request:
+    def __init__(self, view_runtime, connection):
+        self._view_runtime = view_runtime
+        self.connection = connection
+
+        self.url = self._view_runtime.url
+
+        if self.url:
+            self.GET = dict(self._view_runtime.url.query)
+            self.POST = self._view_runtime.post_data or {}
+
+        else:
+            self.GET = {}
+            self.POST = {}
+
+        self.method = 'POST' if self.POST else 'GET'
+
+    @property
+    def interactive(self):
+        return self.connection.interactive
+
+    @property
+    def user(self):
+        return getattr(self.connection, 'user', None)
+
+    @user.setter
+    def user(self, new_value):
+        self.connection.user = new_value
+
+    @property
+    def frontend(self):
+        return self._view_runtime.frontend
+
+    @property
+    def server(self):
+        return self._view_runtime.server
+
+    @property
+    def route(self):
+        return self._view_runtime.route
+
+    @property
+    def match_info(self):
+        return self._view_runtime.match_info
