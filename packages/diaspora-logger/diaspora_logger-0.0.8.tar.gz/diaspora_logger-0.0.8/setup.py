05be8aac5b2c7e9b182e12d14ee5fb3c1ee0ff3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-20 05:06:12 +0000

    Add package diaspora-logger/0.0.8/diaspora_logger-0.0.8.tar.gz

---
+++ packages/diaspora-logger/diaspora_logger-0.0.8.tar.gz/diaspora_logger-0.0.8/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='diaspora_logger',
+    version='0.0.8',
+    description='A Python logging library for sending logs to Diaspora Streams (Kafka)',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    include_package_data=True,  # This flag is used to include non-code files
+    license='LICENSE',  # Specify the path to your LICENSE file
+    url='https://github.com/globus-labs/diaspora_logger',
+    install_requires=[
+        'kafka-python',
+    ],
+)
