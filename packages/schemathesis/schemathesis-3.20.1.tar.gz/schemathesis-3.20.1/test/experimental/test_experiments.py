b4cd841402c62d2daeaf58d0b0ac82bfd67a0d3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-20 14:06:12 +0000

    Add package schemathesis/3.20.1/schemathesis-3.20.1.tar.gz

---
+++ packages/schemathesis/schemathesis-3.20.1.tar.gz/schemathesis-3.20.1/test/experimental/test_experiments.py
@@ -0,0 +1,33 @@
+import pytest
+from pytest import ExitCode
+
+from schemathesis.experimental import OPEN_API_3_1, ExperimentSet
+
+
+def test_experiments():
+    experiments = ExperimentSet()
+    example = experiments.create_experiment("Example", "", "FOO", "", "")
+
+    del experiments
+
+    assert not example.is_enabled
+    example.enable()
+    assert example.is_enabled
+    example.disable()
+    assert not example.is_enabled
+
+
+@pytest.mark.parametrize(
+    "args, kwargs",
+    (
+        ((f"--experimental={OPEN_API_3_1.name}",), {}),
+        ((), {"env": {OPEN_API_3_1.env_var: "true"}}),
+    ),
+)
+@pytest.mark.openapi_version("3.0")
+@pytest.mark.operations("success")
+def test_enable_via_cli(cli, schema_url, args, kwargs):
+    result = cli.run(schema_url, *args, **kwargs)
+    assert result.exit_code == ExitCode.OK, result.stdout
+    assert "Experimental Features:" in result.stdout
+    assert OPEN_API_3_1.is_enabled
