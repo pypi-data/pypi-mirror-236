be09665be87011263b63ff748308431a4756addb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-20 16:12:07 +0000

    Add package flatpack/2.8.0/flatpack-2.8.0.tar.gz

---
+++ packages/flatpack/flatpack-2.8.0.tar.gz/flatpack-2.8.0/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name="flatpack",
+    version="2.8.0",
+    packages=find_packages(),
+    install_requires=[
+        "httpx",
+        "requests",
+        "toml",
+        "torch"
+    ],
+    author="Romlin Group AB",
+    author_email="hello@romlin.com",
+    description="Train AI models - not your patience",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    entry_points={
+        "console_scripts": [
+            "flatpack=flatpack.main:main"
+        ],
+    }
+)
