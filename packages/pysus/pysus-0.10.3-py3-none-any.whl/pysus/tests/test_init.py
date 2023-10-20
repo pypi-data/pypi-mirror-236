f28843b930183566bd97a1fded6b35a25dad9d3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-20 05:05:57 +0000

    Add package pysus/0.10.3/pysus-0.10.3-py3-none-any.whl

---
+++ packages/pysus/pysus-0.10.3-py3-none-any.whl/pysus/tests/test_init.py
@@ -0,0 +1,32 @@
+import unittest
+import pytest
+
+import pandas as pd
+from numpy import dtype
+from pysus.online_data import FTP_Inspect
+
+
+class TestInitFunctions(unittest.TestCase):
+    @pytest.mark.skip(reason="This test takes too long")
+    @pytest.mark.timeout(5)
+    def test_last_update(self):
+        for db in [
+            "SINAN",
+            "SIM",
+            "SINASC",
+            "SIH",
+            "SIA",
+            "PNI",
+            "CNES",
+            "CIHA",
+        ]:
+            df = FTP_Inspect(db).last_update_df()
+            self.assertIsInstance(df, pd.DataFrame)
+            self.assertGreater(df.size, 0)
+            self.assertIn("folder", df.columns)
+            self.assertIsInstance(df["date"][0], pd.Timestamp)
+            self.assertEqual(df.file_size.dtype, dtype("int64"))
+
+
+if __name__ == "__main__":
+    unittest.main()
