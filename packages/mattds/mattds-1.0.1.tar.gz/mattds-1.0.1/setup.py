1382578d962fe040c5b6f3966e7606e77a48c33a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-20 04:09:00 +0000

    Add package mattds/1.0.1/mattds-1.0.1.tar.gz

---
+++ packages/mattds/mattds-1.0.1.tar.gz/mattds-1.0.1/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='mattds',
+    version= '1.0.1',
+    author='Mattli',
+    author_email='matt.li76@icloud.com',
+    description='userful tools for data science projects',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/mattli/deeplearning',
+
+    packages=find_packages(),    # include_package_data=True,
+    install_requires=[
+        'python-box==6.0.2',
+        'ensure==1.0.2',
+        'pyYAML',
+        'joblib',
+    ],
+    # entry_points={
+    #     'console_scripts': [
+    #         'command_name=mattli-ds.command_line_interface:main',
+    #     ]
+    # },
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Operating System :: OS Independent",
+    ],
+)
