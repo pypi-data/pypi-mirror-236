5342ce2e9c1fa98d31d094696e0581fbc8b1f7a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-20 03:11:00 +0000

    Add package morecopy/0.4.0/morecopy-0.4.0-py3-none-any.whl

---
+++ packages/morecopy/morecopy-0.4.0-py3-none-any.whl/morecopy/__init__.py
@@ -0,0 +1,13 @@
+__all__ = [
+    "copy",
+    "copiers",
+    "copier_for",
+]
+__version__ = "0.4.0"
+
+
+# submodules
+from . import copy
+from . import copiers
+from .copy import *
+from .copiers import *
