0b68f2b58615de3171389ac67e6c9d7a511d38b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-20 04:08:32 +0000

    Add package tonpy/0.0.0.1.2b0/tonpy-0.0.0.1.2b0-cp311-cp311-macosx_11_0_arm64.whl

---
+++ packages/tonpy/tonpy-0.0.0.1.2b0-cp311-cp311-macosx_11_0_arm64.whl/tonpy-0.0.0.1.2b0.data/purelib/tonpy/utils/token.py
@@ -0,0 +1,10 @@
+from tonpy.libs.python_ton import parse_token_data as parse_token_data_cell
+
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from tonpy import Cell
+
+
+def parse_token_data(cell: "Cell"):
+    return parse_token_data_cell(cell.cell)
