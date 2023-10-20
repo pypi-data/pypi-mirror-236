62f25220b5213985107f92ee6ac494ed4f5ba68d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-20 03:10:17 +0000

    Add package assert-files/0.1.0/assert-files-0.1.0.tar.gz

---
+++ packages/assert-files/assert-files-0.1.0.tar.gz/assert-files-0.1.0/setup.py
@@ -0,0 +1,38 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='assert-files',
+    packages=find_packages(include=['assert_files']),
+    version='0.1.0',
+    description='My first Python library',
+    long_description='my first Python library',
+    long_description_content_type='text/x-rst',
+    url='',
+    author='avillanova',
+    install_requires=['pypdf~=3.16.4'],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest'],
+    test_suite='tests',
+    license='MIT',
+    keywords='assert files test automation compare',
+    python_requires='>=3',
+    classifiers=[
+    # How mature is this project? Common values are
+    #   3 - Alpha
+    #   4 - Beta
+    #   5 - Production/Stable
+    'Development Status :: 1 - Planning',
+
+    # Indicate who your project is intended for
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Quality Assurance',
+
+    # Pick your license as you wish (should match "license" above)
+    'License :: OSI Approved :: MIT License',
+
+    # Specify the Python versions you support here. In particular, ensure
+    # that you indicate whether you support Python 2, Python 3 or both.
+    'Programming Language :: Python :: 3.9',
+    'Programming Language :: Python :: 3.10',
+],
+)
