75c80fe66275c4e8eecf3d0785817484265be7b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-20 04:09:43 +0000

    Add package datagit/0.19.0a11/datagit-0.19.0a11.tar.gz

---
+++ packages/datagit/datagit-0.19.0a11.tar.gz/datagit-0.19.0a11/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="datagit",
+    version="0.19.0-alpha11",
+    packages=find_packages(),
+    author="Sammy Teillet",
+    author_email="sammy.teillet@gmail.com",
+    description="Git based metric store",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    entry_points={"console_scripts": ["datagit=datagit.cli:cli_entrypoint"]},
+    url="https://github.com/data-drift/data-drift/tree/main/tools/datagit",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_data={
+        "datagit": [
+            "bin/data-drift-mac-m1",
+            "bin/data-drift-mac-intel",
+            "bin/frontend/dist/*",
+        ],
+    },
+    python_requires=">=3.6",
+    install_requires=["pandas", "PyGithub", "click", "GitPython"],
+)
