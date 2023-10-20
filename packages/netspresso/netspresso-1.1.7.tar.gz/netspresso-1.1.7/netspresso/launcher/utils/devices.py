f73ca5a720a9e7a3ab00168b25b626441552eb69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-20 03:10:27 +0000

    Add package netspresso/1.1.7/netspresso-1.1.7.tar.gz

---
+++ packages/netspresso/netspresso-1.1.7.tar.gz/netspresso-1.1.7/netspresso/launcher/utils/devices.py
@@ -0,0 +1,33 @@
+from typing import List
+from netspresso.launcher.schemas import DeviceName
+from netspresso.launcher.schemas.model import TargetDevice
+
+def filter_devices_with_device_name(name: DeviceName, devices: List[TargetDevice]) -> List[TargetDevice]:
+    """Filter the devices with given device name.
+
+        Args:
+            name (DeviceName): The device name.
+            devices (List[TargetDevice]): The list of devices
+        Returns:
+            List[TargetDevice]: filtered list of devices.
+    """
+    filtered_devices: List = []
+    for device in devices:
+        if device.device_name == name:
+            filtered_devices.append(device)
+    return filtered_devices
+
+def filter_devices_with_device_software_version(software_version: str, devices: List[TargetDevice]) -> List[TargetDevice]:
+    """Filter the devices with given software version.
+
+        Args:
+            software_version (DeviceName): The software version of device.
+            devices (List[TargetDevice]): The list of devices
+        Returns:
+            List[TargetDevice]: filtered list of devices.
+    """
+    filtered_devices: List = []
+    for device in devices:
+        if device.software_version == software_version:
+            filtered_devices.append(device)
+    return filtered_devices
