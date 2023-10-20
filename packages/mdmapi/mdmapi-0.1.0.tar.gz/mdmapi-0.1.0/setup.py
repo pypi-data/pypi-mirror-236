602c0b77fcb765ccb203ca8086f0701e4e3564fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-20 05:05:38 +0000

    Add package mdmapi/0.1.0/mdmapi-0.1.0.tar.gz

---
+++ packages/mdmapi/mdmapi-0.1.0.tar.gz/mdmapi-0.1.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mdmapi',
+    version='0.1.0',
+    description='Coming soon',
+    packages=find_packages(),
+    classifiers=[
+        "License :: Other/Proprietary License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Topic :: Internet",
+        "Development Status :: 1 - Planning",
+    ],
+    python_requires='>=3.6',
+)
