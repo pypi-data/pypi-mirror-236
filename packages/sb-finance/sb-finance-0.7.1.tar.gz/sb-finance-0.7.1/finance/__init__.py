3b1a0b6ccc1164f7747c54fd9afcd2abf6ed081b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-20 05:06:01 +0000

    Add package sb-finance/0.7.1/sb-finance-0.7.1.tar.gz

---
+++ packages/sb-finance/sb-finance-0.7.1.tar.gz/sb-finance-0.7.1/finance/__init__.py
@@ -0,0 +1,17 @@
+import sys
+
+# FIXME: Any better way to handle this..?
+try:
+    from logbook import Logger, StreamHandler
+except ImportError:
+    import warnings
+
+    warnings.warn("Could not import logbook")
+else:
+    StreamHandler(sys.stderr).push_application()
+    log = Logger("finance")
+
+
+__version__ = "0.7.1"
+__author__ = "Sumin Byeon"
+__email__ = "suminb@gmail.com"
