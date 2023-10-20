773d8da5b8e582d9b6c7b5a990a496fb3e461337
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-20 16:10:30 +0000

    Add package pipen-report/0.14.0/pipen_report-0.14.0-py3-none-any.whl

---
+++ packages/pipen-report/pipen_report-0.14.0-py3-none-any.whl/pipen_report/frontend/src/layouts/ProcLayout.svelte
@@ -0,0 +1,36 @@
+<script>
+  import { Column, Content, Grid, Row, SideNav } from "carbon-components-svelte";
+
+  import ProcToc from "./ProcToc.svelte";
+  import Header from './Header.svelte';
+  import Footer from './Footer.svelte';
+
+  export let logo;
+  export let logotext = null;
+  export let versions;
+  export let entries;
+  export let pipeline_name;
+  export let report_toc;
+  export let toc;
+  export let page;
+  export let isSideNavOpen;
+  export let runinfo;
+</script>
+
+<Header {logo} {logotext} {entries} {pipeline_name} bind:isSideNavOpen />
+{#if report_toc && toc.length > 0}
+<SideNav bind:isOpen={isSideNavOpen}>
+  <ProcToc {toc} {page}  />
+</SideNav>
+{/if}
+
+<Content>
+  <Grid>
+    <Row>
+      <Column>
+        <slot />
+      </Column>
+    </Row>
+  </Grid>
+  <Footer {versions} {runinfo} />
+</Content>
