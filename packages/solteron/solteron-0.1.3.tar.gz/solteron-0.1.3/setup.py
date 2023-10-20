fd3dcb5b2c1635882c8ad5a5e5ea146edb894942
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-20 04:07:51 +0000

    Add package solteron/0.1.3/solteron-0.1.3.tar.gz

---
+++ packages/solteron/solteron-0.1.3.tar.gz/solteron-0.1.3/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Solteron',
+    description="setuptools.build_meta",
+    version='0.1.3',
+    author           = 'Hernán A. Teszkiewicz Novick',
+    author_email     = 'herni@cajadeideas.ar',
+    license          =  'MIT'    ,
+    url= 'https://github.com/Hernanatn/solteron.py',
+    download_url     =  'https://pypi.org/project/solteron/0.1/#files',
+    packages=['solteron'],
+)
