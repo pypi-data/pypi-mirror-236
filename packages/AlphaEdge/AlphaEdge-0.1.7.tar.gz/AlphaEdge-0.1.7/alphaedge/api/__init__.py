f555ea607f54e1532c90944d3f9adb5c819b214e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-20 03:11:03 +0000

    Add package AlphaEdge/0.1.7/AlphaEdge-0.1.7.tar.gz

---
+++ packages/AlphaEdge/AlphaEdge-0.1.7.tar.gz/AlphaEdge-0.1.7/alphaedge/api/__init__.py
@@ -0,0 +1,9 @@
+from ultron.strategy.deformer import FusionDump, FusionLoad
+
+from alphaedge.api.quantum import Base, Creator, Trainer, Predictor, Optimizer, Constraint, establish
+from alphaedge.api.chain import AtomLauncher, S1Launcher
+
+__all__ = [
+    'FusionDump', 'FusionLoad', 'Base', 'Creator', 'Trainer', 'Predictor',
+    'Optimizer', 'Constraint', 'establish', 'AtomLauncher', 'S1Launcher'
+]
