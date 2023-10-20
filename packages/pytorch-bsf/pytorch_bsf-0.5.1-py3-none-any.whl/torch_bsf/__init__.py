44b32823c0af435f53c7a584d7e76c6be3c3de86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-20 03:10:08 +0000

    Add package pytorch-bsf/0.5.1/pytorch_bsf-0.5.1-py3-none-any.whl

---
+++ packages/pytorch-bsf/pytorch_bsf-0.5.1-py3-none-any.whl/torch_bsf/__init__.py
@@ -0,0 +1,7 @@
+"""torch_bsf: PyTorch implementation of Bezier simplex fitting.
+
+"""
+from torch_bsf.bezier_simplex import BezierSimplex, BezierSimplexDataModule, fit
+
+__version__ = "0.5.1"
+__all__ = ["BezierSimplex", "BezierSimplexDataModule", "fit"]
