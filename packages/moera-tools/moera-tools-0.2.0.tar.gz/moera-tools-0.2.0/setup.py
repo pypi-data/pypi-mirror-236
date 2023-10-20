cd182f91b692d7b3a72df0a722f0f1b402ec5dd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-20 14:07:01 +0000

    Add package moera-tools/0.2.0/moera-tools-0.2.0.tar.gz

---
+++ packages/moera-tools/moera-tools-0.2.0.tar.gz/moera-tools-0.2.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='moera-tools',
+    version='0.2.0',
+    install_requires=[
+        'requests>=2.31.0',
+        'camel-converter',
+        'python-dateutil',
+        'jsonschema>=4.19.0',
+        'PyYAML',
+        'first',
+    ],
+)
