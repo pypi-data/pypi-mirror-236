ba14a85ddc6837388658e267e8b1b7443edc1e9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-20 16:10:48 +0000

    Add package plot-likert/0.5.1.dev20231020/plot_likert-0.5.1.dev20231020-py3-none-any.whl

---
+++ packages/plot-likert/plot_likert-0.5.1.dev20231020-py3-none-any.whl/plot_likert/__init__.py
@@ -0,0 +1,12 @@
+import plot_likert.plot_likert as __internal__
+from .plot_likert import (
+    plot_likert,
+    plot_counts,
+    likert_counts,
+    likert_percentages,
+    likert_response,
+    raw_scale,
+    PlotLikertError,
+)
+
+name = "plot_likert"
