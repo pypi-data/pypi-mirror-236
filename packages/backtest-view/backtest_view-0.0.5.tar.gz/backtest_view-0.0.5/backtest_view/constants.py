653d73258b59dc41562ccf98d1398535d96ac5ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-20 14:05:59 +0000

    Add package backtest-view/0.0.5/backtest_view-0.0.5.tar.gz

---
+++ packages/backtest-view/backtest_view-0.0.5.tar.gz/backtest_view-0.0.5/backtest_view/constants.py
@@ -0,0 +1,19 @@
+import enum
+
+
+class FileFormats(enum.Enum):
+    JSON = 'json'
+
+
+class Timeframe(enum.Enum):
+    YEAR = '1Y'
+    MONTH = '1M'
+    DAY = '1d'
+    HOUR = '1h'
+    MINUTE = '1m'
+
+
+class PlotType(enum.Enum):
+    CANDLE_PLOTS = 'candle_plots'
+    SUB_PLOTS = 'sub_plots'
+
