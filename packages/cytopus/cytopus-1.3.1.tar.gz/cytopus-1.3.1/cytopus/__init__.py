df86ddd0cac27720fbf7b176dd5008786f086d06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-20 14:05:58 +0000

    Add package cytopus/1.3.1/cytopus-1.3.1.tar.gz

---
+++ packages/cytopus/cytopus-1.3.1.tar.gz/cytopus-1.3.1/cytopus/__init__.py
@@ -0,0 +1,4 @@
+"""A Knowledge Base for Single Cell Biology"""
+
+from . import tools
+from . import KnowledgeBase
---
+++ packages/cytopus/cytopus-1.3.1.tar.gz/cytopus-1.3.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+VERSION = '1.3.1'
+
+setup(
+    name='cytopus',
+    version=VERSION,
+    author="Thomas Walle",
+    packages=["cytopus"],
+    install_requires = [
+        #"pandas>1.3",
+        #"numpy>1.2",
+        "networkx>2.7",
+        #"matplotlib>3.4"
+        ]        
+)
