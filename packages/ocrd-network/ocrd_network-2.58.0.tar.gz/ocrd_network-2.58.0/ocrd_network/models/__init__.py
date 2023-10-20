20b1d6e5cfc6312305cc516c0dd5915d1518c281
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-20 16:11:27 +0000

    Add package ocrd-network/2.58.0/ocrd_network-2.58.0.tar.gz

---
+++ packages/ocrd-network/ocrd_network-2.58.0.tar.gz/ocrd_network-2.58.0/ocrd_network/models/__init__.py
@@ -0,0 +1,28 @@
+"""
+DB prefix stands for Database Models
+PY prefix stands for Pydantic Models
+"""
+
+__all__ = [
+    'DBProcessorJob',
+    'DBWorkflowJob',
+    'DBWorkspace',
+    'PYJobInput',
+    'PYJobOutput',
+    'PYOcrdTool',
+    'PYResultMessage',
+    'PYWorkflowJobOutput',
+    'StateEnum',
+]
+
+from .job import (
+    DBProcessorJob,
+    DBWorkflowJob,
+    PYJobInput,
+    PYJobOutput,
+    PYWorkflowJobOutput,
+    StateEnum
+)
+from .messages import PYResultMessage
+from .ocrd_tool import PYOcrdTool
+from .workspace import DBWorkspace
