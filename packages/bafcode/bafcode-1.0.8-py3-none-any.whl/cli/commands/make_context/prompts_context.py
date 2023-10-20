1e6ddef993c46ca62f41a697151164ed7d64e825
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-20 16:10:58 +0000

    Add package bafcode/1.0.8/bafcode-1.0.8-py3-none-any.whl

---
+++ packages/bafcode/bafcode-1.0.8-py3-none-any.whl/cli/commands/make_context/prompts_context.py
@@ -0,0 +1,44 @@
+
+
+
+
+class PromptContext:
+    @staticmethod
+    def snake_to_camel(word):
+     formatted_word = word.replace('/', '_')
+    
+     return ''.join(x.capitalize() for x in formatted_word.split('_'))
+    
+    def format_word(word):
+        formatted_word = word.replace('/', '_')
+        return formatted_word
+    
+
+    
+    
+    def context(prompt_name):
+        camel_case_name = PromptContext.snake_to_camel(prompt_name)
+        formatted_word = PromptContext.format_word(prompt_name)
+        file_context= """ 
+from core import BafLog
+# Optionally, import any other required modules or packages
+
+
+class {prompt_name}: # Replace {prompt_name} with the name of your prompt
+   
+  def {function}(data):
+    prompt = {string}
+
+      
+
+            {prompt_name} Data:
+            {data}
+
+
+           {string}
+      
+    return prompt.format(data=data)
+
+        """
+
+        return file_context.format(prompt_name=camel_case_name, string='"""',function=formatted_word)
