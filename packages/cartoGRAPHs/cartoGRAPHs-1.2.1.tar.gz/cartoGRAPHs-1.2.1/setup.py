44dfb0e62dc3a3b882e0918e254fb478e232a4fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-20 16:09:05 +0000

    Add package cartoGRAPHs/1.2.1/cartoGRAPHs-1.2.1.tar.gz

---
+++ packages/cartoGRAPHs/cartoGRAPHs-1.2.1.tar.gz/cartoGRAPHs-1.2.1/setup.py
@@ -0,0 +1,44 @@
+import pathlib
+from setuptools import setup, find_packages
+
+HERE = pathlib.Path(__file__).parent
+
+VERSION = '1.2.1'
+PACKAGE_NAME = 'cartoGRAPHs'
+AUTHOR = 'Chris H.'
+AUTHOR_EMAIL = 'chris@menchelab.com'
+URL = 'https://github.com/menchelab/cartoGRAPHs'
+
+LICENSE = 'MIT License'
+DESCRIPTION = 'A Network Layout and Visualization Package'
+LONG_DESCRIPTION = (HERE / "README.md").read_text()
+LONG_DESC_TYPE = "text/markdown"
+
+INSTALL_REQUIRES = [
+        'numpy',
+        'pandas',
+        'matplotlib',
+        'seaborn',
+        'scipy',
+        'networkx',
+        'plotly',
+        'colormath',
+        'umap-learn',
+        'scikit_learn',
+        'shapely',
+        'colormath',
+        'numba'
+]
+
+setup(name=PACKAGE_NAME,
+      version=VERSION,
+      description=DESCRIPTION,
+      long_description=LONG_DESCRIPTION,
+      long_description_content_type=LONG_DESC_TYPE,
+      author=AUTHOR,
+      license=LICENSE,
+      author_email=AUTHOR_EMAIL,
+      url=URL,
+      install_requires=INSTALL_REQUIRES,
+      packages=find_packages()
+      )
