a4503a5eda4ad174fe73e0ffa85d33aaf93c80a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-20 05:06:01 +0000

    Add package bafcode/1.0.2/bafcode-1.0.2.tar.gz

---
+++ packages/bafcode/bafcode-1.0.2.tar.gz/bafcode-1.0.2/cli/commands/setup.py
@@ -0,0 +1,41 @@
+import subprocess
+import os
+
+def setup():
+    try:
+        # Cloning the git repository
+        print("Cloning the BafCode repository...")
+        subprocess.check_call(['git', 'clone', 'https://github.com/aitelabrandig/bafcode.git'])
+        print("Cloning completed successfully!")
+
+    except subprocess.CalledProcessError:
+        print("Error: Failed to clone the BafCode repository. Ensure you have 'git' installed and internet access.")
+        return
+
+    try:
+        # Installing the requirements
+        print("Installing the requirements...")
+        subprocess.check_call(['pip', 'install', '-r', 'bafcode/src/requirements.txt'])
+        print("Requirements installed successfully!")
+
+    except subprocess.CalledProcessError:
+        print("Error: Failed to install the required packages. Ensure you have 'pip' installed.")
+        return
+
+    try:
+        # Change directory to run the baf command
+        print("Changing directory to 'bafcode/src/'...")
+        os.chdir('bafcode/src/')
+    except OSError:
+        print("Error: Failed to change directory to 'bafcode/src/'. Ensure the repository was cloned correctly.")
+        return
+
+    try:
+        # Run the baf command
+        print("Generating Baf key...")
+        subprocess.check_call(['bafcode', 'generate_key'])
+        print("Baf key generated successfully!")
+
+    except subprocess.CalledProcessError:
+        print("Error: Failed to generate the Baf key. Ensure the 'baf' command is in the PATH.")
+
