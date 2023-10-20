342ea2a322fb1d55a284cec97080a8a6175623cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-20 16:10:58 +0000

    Add package nobinobi-daily-follow-up/0.1.2.14/nobinobi-daily-follow-up-0.1.2.14.tar.gz

---
+++ packages/nobinobi-daily-follow-up/nobinobi-daily-follow-up-0.1.2.14.tar.gz/nobinobi-daily-follow-up-0.1.2.14/tests/test_models.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+test_nobinobi-daily-follow-up
+------------
+
+Tests for `nobinobi-daily-follow-up` models module.
+"""
+
+from django.test import TestCase
+
+from nobinobi_daily_follow_up import models
+
+
+class TestNobinobi_daily_follow_up(TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_something(self):
+        pass
+
+    def tearDown(self):
+        pass
