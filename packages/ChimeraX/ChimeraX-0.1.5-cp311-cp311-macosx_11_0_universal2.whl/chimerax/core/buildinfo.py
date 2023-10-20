67a39e6d6f7a36dd189295360164d562a4b470c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-20 06:06:46 +0000

    Add package ChimeraX/0.1.5/ChimeraX-0.1.5-cp311-cp311-macosx_11_0_universal2.whl

---
+++ packages/ChimeraX/ChimeraX-0.1.5-cp311-cp311-macosx_11_0_universal2.whl/chimerax/core/buildinfo.py
@@ -0,0 +1,40 @@
+# vim: set expandtab shiftwidth=4 softtabstop=4:
+
+# === UCSF ChimeraX Copyright ===
+# Copyright 2022 Regents of the University of California. All rights reserved.
+# The ChimeraX application is provided pursuant to the ChimeraX license
+# agreement, which covers academic and commercial uses. For more details, see
+# <http://www.rbvi.ucsf.edu/chimerax/docs/licensing.html>
+#
+# This particular file is part of the ChimeraX library. You can also
+# redistribute and/or modify it under the terms of the GNU Lesser General
+# Public License version 2.1 as published by the Free Software Foundation.
+# For more details, see
+# <https://www.gnu.org/licenses/old-licenses/lgpl-2.1.html>
+#
+# THIS SOFTWARE IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, EITHER
+# EXPRESSED OR IMPLIED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+# OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. ADDITIONAL LIABILITY
+# LIMITATIONS ARE DESCRIBED IN THE GNU LESSER GENERAL PUBLIC LICENSE
+# 1.7.dev202310192100 2.1
+#
+# This notice must be embedded in or attached to all copies, including partial
+# copies, of the software or any revisions or derivations thereof.
+# === UCSF ChimeraX Copyright ===
+
+# information about the current build
+
+version = "1.7.dev202310192100" # Chimerax core version
+commit = "c82d2a1c8d2091f16ef920f67863dc07877d4b9f"   # last commit
+date = "2023-10-19 21:00:48 UTC"    # date of last commit
+branch = "modified develop"   # (modified) branch of last commit
+copyright = "© 2016-2023 Regents of the University of California.  All rights reserved."
+
+# general information
+
+synopsis = (
+    "An application for molecular visualization and analysis,"
+    " with an emphasis on molecular assemblies."
+)
+
+web_site = "http://www.rbvi.ucsf.edu/chimerax/"
