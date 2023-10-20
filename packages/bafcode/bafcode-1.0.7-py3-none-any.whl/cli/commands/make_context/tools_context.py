aee9d3c67026db8e8c6cd38d9892b74b0799ff7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-20 16:10:49 +0000

    Add package bafcode/1.0.7/bafcode-1.0.7-py3-none-any.whl

---
+++ packages/bafcode/bafcode-1.0.7-py3-none-any.whl/cli/commands/make_context/tools_context.py
@@ -0,0 +1,33 @@
+
+
+
+
+class ToolContext:
+    @staticmethod
+    def snake_to_camel(word):
+        """
+        Convert snake_case or a single word to CamelCase.
+        """
+        return ''.join(x.capitalize() for x in word.split('_'))
+    
+    def context(tool_name):
+        camel_case_name = ToolContext.snake_to_camel(tool_name)
+        file_context= """ 
+        from core import BafLog
+        # Optionally, import any other required modules or packages
+        # E.g., from api import YourAPI
+        # E.g., from prompts import YourPrompt
+
+        class {tool_name}: # Replace {tool_name} with the name of your tool
+            def __init__(self):
+                self.logger = BafLog
+
+            def execute(self, data):
+               
+                prompt = 'Use your imported prompt here e.g., YourPrompt.your_function(data)' 
+                return prompt
+
+
+        """
+
+        return file_context.replace("{tool_name}", camel_case_name)
