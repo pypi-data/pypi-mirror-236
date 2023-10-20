d8331031345dc3d2d7f65924502eddbaef4cdbb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-20 03:11:41 +0000

    Add package fastpython/1.1.3/fastpython-1.1.3.tar.gz

---
+++ packages/fastpython/fastpython-1.1.3.tar.gz/fastpython-1.1.3/fastpython/fastpython.py
@@ -0,0 +1,70 @@
+import os as os2, time, string, sys, platform, binascii, codecs, base64
+
+class OS:
+	def GetOSInfo():
+		print(f"OS Device Identifier : {os2.name}")
+		print(f"OS : {platform.system()}")
+	def ClearScreen():
+		if os2.name.lower() == "nt":
+			os2.system("cls")
+		else:
+			os2.system("clear")
+class ANSI:
+	def ListColors():
+		for color in range(256):
+			print(f"\033[38;5;{color}m{color}\033[0m ", end='')
+	def ColoredOutput(color, text=""):
+		print(f"\033[38;5;{color}m{text}")
+	def ResetColors():
+		print("\033[0m")
+class Message:
+	def Error(errtext="Unspecified Error"):
+		print(f"\033[0;37m[\033[1;31mX\033[0;37m] \033[1;31m{errtext}\033[0m")
+	def Warn(warntext="Unspecified Warning"):
+		print(f"\033[0;37m[\033[1;33m!\033[0;37m] \033[1;33m{warntext}\033[0m")
+	def Info(infotext="Unspecified Information"):
+		print(f"\033[0;37m[\033[1;34mi\033[0;37m] \033[1;34m{infotext}\033[0m")
+class Encoding:
+	class b16:
+		def encode16(txtinput):
+			output = base64.b16encode(bytes(txtinput, 'utf-8')).decode('utf-8')
+			return output
+		def decode16(b16input):
+			output = base64.b16decode(bytes(b16input, 'utf-8')).decode('utf-8')
+			return output
+	class b32:
+		def encode32(txtinput):
+			output = base64.b32encode(bytes(txtinput, 'utf-8')).decode('utf-8')
+			return output
+		def decode32(b32input):
+			output = base64.b32decode(bytes(b32input, 'utf-8')).decode('utf-8')
+			return output
+	class b64:
+		def encode64(txtinput):
+			output = base64.b64encode(bytes(txtinput, 'utf-8')).decode('utf-8')
+			return output
+		def decode64(b64input):
+			output = base64.b64decode(bytes(b64input, 'utf-8')).decode('utf-8')
+			return output
+	class hex:
+		def encodeHex(txtinput):
+			output = binascii.hexlify(bytes(txtinput, 'utf-8')).decode('utf-8')
+			return output
+		def decodeHex(hexinput):
+			output = binascii.unhexlify(bytes(hexinput, 'utf-8')).decode('utf-8')
+			return output
+	class binary:
+		def encodeBin(txtinput):
+			output = ''.join(format(i, '08b') for i in bytearray(txtinput, encoding ='utf-8'))
+			return output
+		def decodeBin(bininput):
+			integer = integer = int(bininput, base=2)
+			output = integer.to_bytes((integer.bit_length() + 7)//8, 'big').decode()
+			return output
+
+
+
+
+if __name__=="__main__":
+	print("Ooops, you ran the wrong file.\n\nCreate a new file and import this one to use it. It will not work here.")
+	sys.exit(1)
