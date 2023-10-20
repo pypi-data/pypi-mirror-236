0a4f08f7855f5c3b98d3455d027ddf0a2de4f579
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-20 15:07:29 +0000

    Add package gplates-ws-proxy/0.0.2/gplates_ws_proxy-0.0.2-py3-none-any.whl

---
+++ packages/gplates-ws-proxy/gplates_ws_proxy-0.0.2-py3-none-any.whl/gplates_ws_proxy/coastlines.py
@@ -0,0 +1,85 @@
+import requests
+from shapely.geometry import shape
+
+from . import _auth as a
+from ._auth import auth
+
+
+@auth
+def get_paleo_coastlines(
+    time,
+    model="MULLER2022",
+    format="geojson",
+    facecolor="lime",
+    edgecolor="none",
+    alpha=0.5,
+    extent=(-180, 180, -90, 90),
+    wrap=True,
+    central_meridian=0.0,
+    anchor_plate_id=0,
+):
+    """Get paleo-coastlines
+
+    By default, the polygons are wrapped along (180/-180). If you would like to wrap them
+    at other locations, be careful that some plotting packages might not work well with them.
+
+    :param time: the input paleo age
+    :param model: the name of rotation model
+    :param format: the return data format, such as geojson, shapely, png
+    :param facecolor: face color -- only for png format
+    :param edgecolor: edge color -- only for png format
+    :param alpha: alpha -- only for png format
+    :param extent: (left, right, bottom, top) -- only for png format
+    :param anchor_plate_id: anchor plate id
+    :param central_meridian: central meridian
+    :param wrap: flag to indicate if wrap the polygons along dateline
+
+    :returns: paleo-coastlines
+    :rtype: geojson
+
+    """
+
+    params = {
+        "time": time,
+        "model": model,
+        "anchor_plate_id": anchor_plate_id,
+        "wrap": wrap,
+        "central_meridian": central_meridian,
+    }
+    if format == "png":
+        params["fmt"] = "png"
+        params["facecolor"] = facecolor
+        params["edgecolor"] = edgecolor
+        params["alpha"] = alpha
+        params["extent"] = f"{extent[0]},{extent[1]},{extent[2]},{extent[3]}"
+        headers = {
+            "Accept": "image/png",
+        }
+    else:
+        headers = {
+            "Accept": "application/json",
+        }
+
+    ret = requests.get(
+        a.server_url + "/reconstruct/coastlines/",
+        params=params,
+        verify=True,
+        headers=headers,
+        proxies={"http": a.proxy},
+    )
+
+    if ret.status_code in [200, 201]:
+        if format == "shapely":
+            json_data = ret.json()
+            geoms = [
+                shape(feature["geometry"]).buffer(0)
+                for feature in json_data["features"]
+            ]
+            return geoms
+        elif format == "png":
+            return ret.content
+        else:
+            json_data = ret.json()
+            return json_data
+    else:
+        raise Exception("Failed to get paleo-coastlines.")
---
+++ packages/gplates-ws-proxy/gplates_ws_proxy-0.0.2-py3-none-any.whl/gplates_ws_proxy/plate_model.py
@@ -0,0 +1,70 @@
+import json
+
+import requests
+import shapely
+
+from . import _auth as a
+from ._auth import auth
+from .paleoearth import get_paleo_labels
+from .reconstruction import get_paleo_coordinates, reconstruct_points
+from .topology import get_subduction_zones
+
+
+class PlateModel:
+    def __init__(self, name="MULLER2019"):
+        self.name = name
+
+    @auth
+    @staticmethod
+    def list_models():
+        """return a list which contains all the available rotation models"""
+        ret = requests.get(
+            a.server_url + "/model/list",
+            verify=True,
+            proxies={"http": a.proxy},
+        )
+        return json.loads(str(ret.text))
+
+    @auth
+    def get_cfg(self):
+        """Given a rotation model name, return the details about the model"""
+        params = {"model": self.name}
+
+        ret = requests.get(
+            a.server_url + f"/model/show",
+            params=params,
+            verify=True,
+            proxies={"http": a.proxy},
+        )
+
+        return json.loads(str(ret.text))
+
+    def reconstruct(
+        self,
+        lats: list[float],
+        lons: list[float],
+        age: float,
+        pids: list[int] = [],
+    ) -> dict[str, list]:
+        """reconstruct a list of lats and lons to age
+
+        :returns: for example, {'lats':[1.1,2.2], 'lons':[2.1,2.3]}
+        """
+        return get_paleo_coordinates(lats, lons, age, model=self.name, pids=pids)
+
+    def get_paleo_labels(self):
+        """get a list of paleo-labels
+
+        :returns: a dict object, {"names": names, "lons": lons, "lats": lats}
+        """
+        return get_paleo_labels()
+
+    def get_subduction_zones(self, time):
+        return get_subduction_zones(self.name, time)
+
+
+def reconstruct_shapely_points(
+    model: PlateModel, points: list[shapely.Point], time: float, pids: list[int] = []
+) -> list[shapely.Point]:
+    """reconstruct a list of shapely points to {time}Ma"""
+    return reconstruct_points(points, time, model=model.name, pids=pids)
