98a3d354011eb9c0e954c61cf27191b4ce2a02d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-20 14:06:05 +0000

    Add package biglist/0.8.7/biglist-0.8.7.tar.gz

---
+++ packages/biglist/biglist-0.8.7.tar.gz/biglist-0.8.7/README.rst
@@ -0,0 +1,30 @@
+biglist
+=======
+
+The package ``biglist`` provides persisted, out-of-memory Python data structures
+that implement the ``Sequence`` and ``Iterable`` interfaces with the capabilities of
+concurrent and distributed reading and writing.
+The main use case is processing large amounts of data that can not fit in memory.
+Persistence can be on local disk or in cloud storage.
+
+
+Read the `documentation <https://biglist.readthedocs.io/en/latest/>`_.
+
+
+Reference
+---------
+
+A very early version of this work is described in `a blog post <https://zpz.github.io/blog/biglist/>`_.
+
+Status
+------
+
+Production ready.
+
+
+Python version
+--------------
+
+Development and testing were conducted in Python 3.8 until version 0.7.8.
+Starting with 0.7.9, development and testing happen in Python 3.10.
+Code continues to NOT intentionally use features beyond Python 3.8.
---
+++ packages/biglist/biglist-0.8.7.tar.gz/biglist-0.8.7/src/biglist/__init__.py
@@ -0,0 +1,54 @@
+"""
+The package `biglist <https://github.com/zpz/biglist>`_ provides persisted, out-of-memory Python data structures
+that implement the :class:`Seq` interface (a simplified |Sequence|_) with the capabilities of
+concurrent and distributed reading and writing.
+The main use case is processing large amounts of data that can not fit in memory.
+
+Currently, two kinds of "biglists" are provided, namely :class:`Biglist` and :class:`ParquetBiglist`.
+
+:class:`Biglist` manages writing and reading.
+Here, "writing" refers to adding data to this facility and be managed by it.
+The class manages its data files in addition to meta info.
+Writing is append-only; updating existing data is not supported.
+Appending can be conducted by a number of distributed workers.
+
+:class:`ParquetBiglist` defines a kind of "external biglist". When given the paths to a set of
+pre-existing data files in the Apache Parquet format,
+this class provides a rich set of facilities for reading the data.
+
+:class:`Biglist` and :class:`ParquetBiglist` share the same core API for *reading*.
+Although random element access is supported, it is not optimized
+and is not the target usage pattern. The intended way of data consumption is by iteration.
+Iteration can be done by distributed workers *collectively*.
+
+Persistence can be on local disk or in cloud storage.
+Thanks to the package `upathlib <https://github.com/zpz/upathlib>`_, the user API as well as
+the implementation is agnostic to the location of storage.
+
+Additional utilities provide mechanisms for "slicing and dicing" a biglist,
+as well as "chaining up" a series of biglists. These utilities work not only for biglist,
+but also for any :class:`Seq`.
+"""
+from __future__ import annotations
+
+from ._base import BiglistBase, FileReader, FileSeq
+from ._biglist import Biglist, BiglistFileReader, BiglistFileSeq, Multiplexer
+from ._parquet import (
+    ParquetBatchData,
+    ParquetBiglist,
+    ParquetFileReader,
+    ParquetFileSeq,
+    make_parquet_field,
+    make_parquet_schema,
+    make_parquet_type,
+    read_parquet_file,
+    write_arrays_to_parquet,
+    write_pylist_to_parquet,
+)
+from ._util import (
+    Chain,
+    Seq,
+    Slicer,
+)
+
+__version__ = "0.8.7"
