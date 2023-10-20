49a0dd430f8b46877fe0a97d7a62fdce53073fb1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-20 16:12:05 +0000

    Add package mcorelib/0.3.0/mcorelib-0.3.0.tar.gz

---
+++ packages/mcorelib/mcorelib-0.3.0.tar.gz/mcorelib-0.3.0/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['mcorelib']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'mcorelib',
+    'version': '0.3.0',
+    'description': '',
+    'long_description': '',
+    'author': 'Volodymyr Paslavskyy',
+    'author_email': 'vpaslavskyy@elitex.systems',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
