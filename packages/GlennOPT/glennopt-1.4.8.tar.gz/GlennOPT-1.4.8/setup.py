37fa043fb8159b9de2062af0c884d9b960cc1ad9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-20 04:06:48 +0000

    Add package GlennOPT/1.4.8/glennopt-1.4.8.tar.gz

---
+++ packages/GlennOPT/glennopt-1.4.8.tar.gz/GlennOPT-1.4.8/setup.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['glennopt',
+ 'glennopt.DOE',
+ 'glennopt.base',
+ 'glennopt.helpers',
+ 'glennopt.io',
+ 'glennopt.optimizers']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['dataclasses_json',
+ 'diversipy',
+ 'doepy',
+ 'matplotlib>=3.3.1,<4.0.0',
+ 'numpy',
+ 'pandas',
+ 'psutil',
+ 'pydoe',
+ 'scikit-learn',
+ 'torch',
+ 'tqdm']
+
+setup_kwargs = {
+    'name': 'glennopt',
+    'version': '1.4.8',
+    'description': 'Multi and single objective optimization tool for cfd/computer simulations.',
+    'long_description': 'None',
+    'author': 'Paht Juangphanich',
+    'author_email': 'paht.juangphanich@nasa.gov',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.6.9,<4.0.0',
+}
+
+
+setup(**setup_kwargs)
