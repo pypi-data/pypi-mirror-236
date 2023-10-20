a6ca86f7e3e5d5552004af9c586637469170e36b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-20 04:09:57 +0000

    Add package fringes-gui/0.3.3/fringes_gui-0.3.3-py3-none-any.whl

---
+++ packages/fringes-gui/fringes_gui-0.3.3-py3-none-any.whl/fringes_gui/__init__.py
@@ -0,0 +1,19 @@
+import importlib
+import os
+
+import toml
+
+from .gui import FringesGUI
+
+try:  # PackageNotFoundError
+    fname = os.path.join(os.path.dirname(__file__), "..", "pyproject.toml")
+    version = toml.load(fname)["tool"]["poetry"]["version"]
+except FileNotFoundError or KeyError:
+    version = importlib.metadata.version("fringes_gui")
+
+__version__ = version
+
+
+def run():
+    gui = FringesGUI()
+    gui.show()
