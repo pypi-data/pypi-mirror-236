b592609076981763560c547c3eff185e6da9a2e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-21 05:08:13 +0000

    Add package pyprefs/1.0.4/pyprefs-1.0.4.tar.gz

---
+++ packages/pyprefs/pyprefs-1.0.4.tar.gz/pyprefs-1.0.4/src/pyprefs/__main__.py
@@ -0,0 +1,35 @@
+from os import path, mkdir
+
+local_path = 'C:\\Program Files\\mc-corp\\'
+except_prefix = '\033[31m[error]: '
+
+def save(saveName, data):
+    if not path.isdir(local_path + 'pyprefs'):
+        mkdir(local_path)
+        mkdir(local_path + 'pyprefs')
+
+    with open(local_path + 'pyprefs\\' + saveName + '.txt', 'w') as pypfile:
+        pypfile.write(str(data))
+        pypfile.close()
+        return True
+
+def load(saveName):
+    if not path.isdir(local_path + 'pyprefs'):
+        mkdir(local_path)
+        mkdir(local_path + 'pyprefs')
+    if not path.isfile(local_path + 'pyprefs\\' + saveName + '.txt'):
+        print(except_prefix + f'Save {saveName} not found.')
+        exit()
+
+    with open(local_path + 'pyprefs\\' + saveName + '.txt', 'r') as pypfile:
+        return pypfile.read()
+
+def check(saveName):
+    if path.isfile(local_path + 'pyprefs\\' + saveName + '.txt'):
+        return True
+    else:
+        return False
+
+if __name__ == "__main__":
+    # Этот код будет выполняться при запуске вашего пакета
+    pass  # Эту строку можно оставить пустой, если вам не нужен вывод сообщения
