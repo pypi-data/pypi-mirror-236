26a898114d4e2b876586bd9675f8729d80d3885e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-21 05:08:02 +0000

    Add package ffsim/0.0.9/ffsim-0.0.9-cp38-abi3-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/ffsim/ffsim-0.0.9-cp38-abi3-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/ffsim/protocols/__init__.py
@@ -0,0 +1,16 @@
+# (C) Copyright IBM 2023.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+
+"""Protocols."""
+
+from ffsim.protocols.apply_unitary import SupportsApplyUnitary, apply_unitary
+from ffsim.protocols.approximate_equality import SupportsApproximateEquality, approx_eq
+from ffsim.protocols.linear_operator import SupportsLinearOperator, linear_operator
+from ffsim.protocols.trace import SupportsTrace, trace
