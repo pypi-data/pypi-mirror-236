4f49785b59762198cebcab28c866a9dd86699bbb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-21 03:13:03 +0000

    Add package MuonDataLib/0.1.0b1/MuonDataLib-0.1.0b1.tar.gz

---
+++ packages/MuonDataLib/MuonDataLib-0.1.0b1.tar.gz/MuonDataLib-0.1.0b1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import find_packages, setup
+
+VERSION = "0.1.0b1"
+
+PACKAGE_NAME = 'MuonDataLib'
+
+
+setup(
+        name=PACKAGE_NAME,
+        requires=['numpy'],
+        setup_requires=['numpy>=1.12'],
+        install_requires=['numpy>=1.12'],
+        packages=find_packages(where='src'),
+        description='A package for MuSR data',
+        long_description='This package provides code for reading '
+                         'and analysing data from MuSR experiments. ',
+        author='Anthony Lim',
+        #  ext_modules=extensions,
+        author_email="anthony.lim@stfc.ac.uk",
+        version=VERSION,
+        license='BSD',
+        package_dir={'': 'src'}
+        )
