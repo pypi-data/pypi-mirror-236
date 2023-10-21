049b70a3f3225f2f325fa720c56d07c63511561e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-21 03:12:32 +0000

    Add package envpicker/0.1.2/envpicker-0.1.2.tar.gz

---
+++ packages/envpicker/envpicker-0.1.2.tar.gz/envpicker-0.1.2/envpicker/manager/__init__.py
@@ -0,0 +1,32 @@
+from __future__ import annotations
+from typing import Optional
+from .conda_mngr import CondaManager, MambaManager
+from ..logger import ENVPICKER_LOGGER
+
+PREFERENCE_ORDER = [
+    "mamba",
+    "conda",
+    #   "poetry",
+    "venv",
+]
+
+
+_MANAGER_CLASSES = {
+    "conda": CondaManager if CondaManager.is_available() else None,
+    "mamba": MambaManager if MambaManager.is_available() else None,
+    #    "poetry": PoetryManager,
+    #    "venv": VenvManager,
+}
+
+
+def get_manager(path: Optional[str] = None, preferences: Optional[list[str]] = None):
+    """Return the first available manager."""
+    if preferences is None:
+        preferences = PREFERENCE_ORDER
+
+    ENVPICKER_LOGGER.debug("Getting manager from %s", preferences)
+    for manager in preferences:
+        if manager in _MANAGER_CLASSES and _MANAGER_CLASSES[manager] is not None:
+            ENVPICKER_LOGGER.info(f"Using %s as environment manager.", manager)
+            return _MANAGER_CLASSES[manager](path=path)
+    raise RuntimeError("No available environment managers found.")
