36e3796648bd9124aa08c0fcc1ec5ffcb8d5ead8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-21 03:11:35 +0000

    Add package odoo14-addon-ssi-work-log-cost/14.0.1.6.0/odoo14_addon_ssi_work_log_cost-14.0.1.6.0-py3-none-any.whl

---
+++ packages/odoo14-addon-ssi-work-log-cost/odoo14_addon_ssi_work_log_cost-14.0.1.6.0-py3-none-any.whl/odoo/addons/ssi_work_log_cost/__manifest__.py
@@ -0,0 +1,31 @@
+# Copyright 2022 OpenSynergy Indonesia
+# Copyright 2022 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Work Log Cost",
+    "version": "14.0.1.6.0",
+    "website": "https://simetri-sinergi.id",
+    "author": "OpenSynergy Indonesia, PT. Simetri Sinergi Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "ssi_work_log_mixin",
+        "ssi_product_line_account_mixin",
+        "ssi_transaction_ready_mixin",
+    ],
+    "data": [
+        "security/ir_module_category_data.xml",
+        "security/res_group_data.xml",
+        "security/ir.model.access.csv",
+        "security/ir_rule_data.xml",
+        "data/ir_sequence_data.xml",
+        "data/sequence_template_data.xml",
+        "data/policy_template_data.xml",
+        "data/approval_template_data.xml",
+        "views/hr_work_log_views.xml",
+        "views/ir_model_views.xml",
+        "views/work_log_rate_views.xml",
+        "views/hr_employee_views.xml",
+    ],
+}
