5bfbb039568d8aa67935346140bcf31452aa4a5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-21 04:07:48 +0000

    Add package avilla-satori/1.0.0a9/avilla_satori-1.0.0a9.tar.gz

---
+++ packages/avilla-satori/avilla_satori-1.0.0a9.tar.gz/avilla_satori-1.0.0a9/avilla/satori/collector/connection.py
@@ -0,0 +1,38 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, ClassVar, TypeVar
+
+from avilla.core.ryanvk.collector.base import AvillaBaseCollector
+from graia.ryanvk import Access, BasePerform
+from satori.account import Account
+
+if TYPE_CHECKING:
+    from avilla.satori.protocol import SatoriProtocol
+
+
+T = TypeVar("T")
+T1 = TypeVar("T1")
+
+
+class ConnectionBasedPerformTemplate(BasePerform, native=True):
+    __collector__: ClassVar[ConnectionCollector]
+
+    protocol: Access[SatoriProtocol] = Access()
+    connection: Access[Account] = Access()
+
+
+class ConnectionCollector(AvillaBaseCollector):
+    post_applying: bool = False
+
+    @property
+    def _(self):
+        upper = super()._
+
+        class PerformTemplate(
+            ConnectionBasedPerformTemplate,
+            upper,
+            native=True,
+        ):
+            ...
+
+        return PerformTemplate
