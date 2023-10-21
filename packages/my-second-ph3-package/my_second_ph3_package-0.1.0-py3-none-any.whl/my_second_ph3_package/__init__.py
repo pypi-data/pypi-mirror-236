69dc85a0d8e17846489cbd8266fefbb1700a3831
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-21 03:12:32 +0000

    Add package my-second-ph3-package/0.1.0/my_second_ph3_package-0.1.0-py3-none-any.whl

---
+++ packages/my-second-ph3-package/my_second_ph3_package-0.1.0-py3-none-any.whl/my_second_ph3_package/__init__.py
@@ -0,0 +1,10 @@
+"""Top-level package for My Second PH3 Package."""
+
+__author__ = """Fabien Mathieu"""
+__email__ = 'loufab@gmail.com'
+__version__ = '0.1.0'
+
+
+from my_second_ph3_package.sub_package_1.my_class_1 import MyClass1
+from my_second_ph3_package.sub_package_2.my_class_2 import MyClass2
+from my_second_ph3_package.sub_package_2.my_class_3 import MyClass3
