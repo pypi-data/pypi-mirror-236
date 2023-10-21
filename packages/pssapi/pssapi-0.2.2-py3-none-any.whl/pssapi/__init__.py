e97b7d70e8d01b4260d7cdff24de1254d272f171
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-21 04:09:10 +0000

    Add package pssapi/0.2.2/pssapi-0.2.2-py3-none-any.whl

---
+++ packages/pssapi/pssapi-0.2.2-py3-none-any.whl/pssapi/__init__.py
@@ -0,0 +1,12 @@
+from . import entities, enums, raw, utils
+from .client import PssApiClient
+
+__all__ = [
+    "entities",
+    "enums",
+    "raw",
+    "utils",
+    "PssApiClient",
+]
+
+__version__ = "0.2.2"
