ba2f33ebd9e7e83d1b81c57c8ba246cecc858ce7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-21 04:07:41 +0000

    Add package urluckynum/1.3.1/urluckynum-1.3.1.tar.gz

---
+++ packages/urluckynum/urluckynum-1.3.1.tar.gz/urluckynum-1.3.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+import os
+
+requirements_path = os.path.join("src", "urluckynum")
+requirements_path = os.path.join(requirements_path, "requirements.txt")
+
+# Read the dependencies from requirements.txt
+with open(requirements_path, "r") as f:
+    dependencies = f.read().splitlines()
+
+
+setup(
+    name='urluckynum',
+    version='1.3.1',
+    description='My Application with a DB',
+    author='LuckyWave Group',
+    packages=[
+        'urluckynum',
+    ],
+    install_requires=dependencies,
+    package_dir={'urluckynum': 'src/urluckynum'},
+)
