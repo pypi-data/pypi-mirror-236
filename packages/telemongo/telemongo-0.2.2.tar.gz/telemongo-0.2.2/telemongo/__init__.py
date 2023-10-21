d1fc8eb45e02db2a3bff2b8896717bc1fb3dc7e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-21 15:07:07 +0000

    Add package telemongo/0.2.2/telemongo-0.2.2.tar.gz

---
+++ packages/telemongo/telemongo-0.2.2.tar.gz/telemongo-0.2.2/telemongo/__init__.py
@@ -0,0 +1,7 @@
+from .telemongo import MongoSession, Entity, SentFile, Session, UpdateState, Version
+
+__version__ = "0.2.1"
+__author__ = "Chris Watson <cawatson1993@gmail.com>"
+__all__ = [
+    "MongoSession",
+]
