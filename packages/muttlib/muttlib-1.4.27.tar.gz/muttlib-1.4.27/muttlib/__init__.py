1983c93ae2c2d17a8228a982d77c77463e7447a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-21 05:08:01 +0000

    Add package muttlib/1.4.27/muttlib-1.4.27.tar.gz

---
+++ packages/muttlib/muttlib-1.4.27.tar.gz/muttlib-1.4.27/muttlib/__init__.py
@@ -0,0 +1,7 @@
+"""Version."""
+try:
+    import importlib.metadata as importlib_metadata
+except ModuleNotFoundError:
+    import importlib_metadata  # type: ignore
+
+__version__ = importlib_metadata.version(__name__)  # type: ignore
