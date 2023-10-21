e659478d449e48fa724abb7c5b6f2b4f291b504d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-21 15:07:26 +0000

    Add package accrete/0.0.9/accrete-0.0.9-py3-none-any.whl

---
+++ packages/accrete/accrete-0.0.9-py3-none-any.whl/accrete/contrib/ui/__init__.py
@@ -0,0 +1,12 @@
+from . import config
+from .filter import Filter
+from .helper import (
+    ClientAction,
+    DetailContext,
+    FormContext,
+    ListContext,
+    BreadCrumb,
+    build_querystring,
+    parse_client_actions,
+    queryset_from_querystring
+)
