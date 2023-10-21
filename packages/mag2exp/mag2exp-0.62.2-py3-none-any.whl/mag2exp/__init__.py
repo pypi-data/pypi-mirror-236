7da937353e624aaf57d2584161595a27364aa47c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-21 04:07:34 +0000

    Add package mag2exp/0.62.2/mag2exp-0.62.2-py3-none-any.whl

---
+++ packages/mag2exp/mag2exp-0.62.2-py3-none-any.whl/mag2exp/__init__.py
@@ -0,0 +1,23 @@
+"""Simulate experimental measurements."""
+import importlib
+
+import pytest
+
+from . import ltem, magnetometry, mfm, moke, quick_plots, sans, util, x_ray
+
+__version__ = importlib.metadata.version(__package__)
+
+
+def test():
+    """Run all package tests.
+
+    Examples
+    --------
+    1. Run all tests.
+
+    >>> import mag2exp
+    ...
+    >>> # mag2exp.test()
+
+    """
+    return pytest.main(["-v", "--pyargs", "mag2exp", "-l"])  # pragma: no cover
