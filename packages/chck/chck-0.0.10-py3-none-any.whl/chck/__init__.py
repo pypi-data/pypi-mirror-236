0796c7011dc0e568d3e972d6bb0363b519ba0979
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-21 03:10:50 +0000

    Add package chck/0.0.10/chck-0.0.10-py3-none-any.whl

---
+++ packages/chck/chck-0.0.10-py3-none-any.whl/chck/__init__.py
@@ -0,0 +1,17 @@
+__version__ = "0.0.10"
+from .bool import *
+from .core import *
+from .comp import *
+
+from .insp import *
+from .strs import *
+from .path import *
+
+from .dcls import *
+from .misc import *
+
+# specific libs:
+from .isnp import *
+from .ispd import *
+from .isad import *
+from .trch import *
