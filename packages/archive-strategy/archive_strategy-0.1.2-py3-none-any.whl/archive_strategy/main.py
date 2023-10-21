06e4f8f9c36b722f8570228086057348dbdd0181
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-21 03:10:50 +0000

    Add package archive-strategy/0.1.2/archive_strategy-0.1.2-py3-none-any.whl

---
+++ packages/archive-strategy/archive_strategy-0.1.2-py3-none-any.whl/archive_strategy/main.py
@@ -0,0 +1,24 @@
+import os
+import ast
+from archive_strategy.archiver import Archiver
+
+
+BASE_DIR = os.environ.get("ARCHIVE_BASE_DIR", "/backups")
+ARCHIVE_DIR = os.environ.get("ARCHIVE_DIR", "/archives")
+ARCHIVE_USER = os.environ.get("ARCHIVE_USER", "root")
+ARCHIVE_GROUP = os.environ.get("ARCHIVE_GROUP", "root")
+TEST_MODE = ast.literal_eval(os.environ.get("ARCHIVE_TEST_MODE", "False"))
+DELETE = ast.literal_eval(os.environ.get("ARCHIVE_DELETE", "False"))
+
+
+def main():
+    archiver = Archiver(
+        archive_dir=ARCHIVE_DIR,
+        base_dir=BASE_DIR,
+        archive_user=ARCHIVE_USER,
+        archive_group=ARCHIVE_GROUP,
+        test_mode=TEST_MODE,
+        delete=DELETE,
+    )
+    archiver.archive()
+    archiver.organise()
