b71d620428716460c9f19c4abf455efeeb22979f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-21 05:08:47 +0000

    Add package versioned/0.5.4/versioned-0.5.4-py3-none-any.whl

---
+++ packages/versioned/versioned-0.5.4-py3-none-any.whl/versioned/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+"""
+The version and alias best practice for immutable artifacts and deployment.
+"""
+
+from ._version import __version__
+
+__short_description__ = "The version and alias best practice for immutable artifacts and deployment."
+__license__ = "MIT"
+__author__ = "Sanhe Hu"
+__author_email__ = "husanhe@gmail.com"
+__maintainer__ = "Sanhe Hu"
+__maintainer_email__ = "husanhe@gmail.com"
+__github_username__ = "MacHu-GWU"
