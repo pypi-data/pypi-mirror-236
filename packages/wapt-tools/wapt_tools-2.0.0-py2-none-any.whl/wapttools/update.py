09b5fcd6c7b03a6d8d5d854627bd932127f05f1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-21 04:07:02 +0000

    Add package wapt-tools/2.0.0/wapt_tools-2.0.0-py2-none-any.whl

---
+++ packages/wapt-tools/wapt_tools-2.0.0-py2-none-any.whl/wapttools/update.py
@@ -0,0 +1,13 @@
+import logging
+# TODO
+# import plumbum
+
+log = logging.getLogger()
+
+
+def update():
+    logging.debug('[TODO] waptpython update-package-sources .')
+    logging.debug('[TODO] git checkout WAPT/control and set WAPT/control/version to latest')
+    logging.debug('[TODO] compute checksum on check-version.json files')
+
+    logging.info('Not yet implemented')
