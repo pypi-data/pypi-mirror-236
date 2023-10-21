1de53af1bef34e91e479433f7bee56f7e867b550
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-21 15:08:32 +0000

    Add package cloudantic/0.0.3/cloudantic-0.0.3.tar.gz

---
+++ packages/cloudantic/cloudantic-0.0.3.tar.gz/cloudantic-0.0.3/cloudantic/router/api.py
@@ -0,0 +1,29 @@
+from fastapi import FastAPI
+
+from .crud import CRUDRouter, DynaModel, TypeVar, async_io, handle
+
+D = TypeVar("D", bound=DynaModel)
+
+
+def setup_routes(app: FastAPI):
+    """Automatically setup CRUD routes for all subclasses of DynaModel."""
+    for model in DynaModel.__subclasses__():
+        app.include_router(CRUDRouter[model](model))
+    return app
+
+
+def setup_table(app: FastAPI):
+    """Automatically hooks up a startup event to create the DynamoDB Single Table and adds the schemas to the openapi spec.
+    [NOTE]: It's a chainable function, so you can do setup_table(setup_routes(app))
+    """
+
+    @handle
+    @async_io
+    def _setup_table():
+        return DynaModel.create_table()
+
+    @app.on_event("startup")
+    async def startup():
+        await _setup_table()
+
+    return app
