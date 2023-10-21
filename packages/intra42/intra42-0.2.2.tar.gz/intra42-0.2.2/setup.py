37a68d560180d7dc6391cafc2a967f40125b4a7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-21 14:06:08 +0000

    Add package intra42/0.2.2/intra42-0.2.2.tar.gz

---
+++ packages/intra42/intra42-0.2.2.tar.gz/intra42-0.2.2/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='intra42',
+    version='0.2.2',
+    description='Intra 42 public and private api interface',
+    url='https://github.com/Xentiie/intra42',
+    author='Rémi Claire',
+    author_email='reclaire@student.42mulhouse.fr',
+    license='BSD 2-clause',
+    packages=['intra42'],
+    install_requires=['requests',
+                      'bs4',],
+    classifiers=[],
+)
