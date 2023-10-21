96ec25e9c24ef045f23ed7a0350fed345b3f6212
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-21 03:12:08 +0000

    Add package aos-prov/4.2.1b4/aos_prov-4.2.1b4-py3-none-any.whl

---
+++ packages/aos-prov/aos_prov-4.2.1b4-py3-none-any.whl/aos_prov/utils/common.py
@@ -0,0 +1,57 @@
+#
+#  Copyright (c) 2018-2023 Renesas Inc.
+#  Copyright (c) 2018-2023 EPAM Systems Inc.
+#
+
+import random
+import string
+from pathlib import Path
+
+from rich.console import Console
+
+CONTENT_ENCRYPTION_ALGORITHM = 'aes256_cbc'
+DOWNLOADS_PATH = Path.home() / '.aos' / 'downloads'
+AOS_DISKS_PATH = DOWNLOADS_PATH
+NODE0_IMAGE_FILENAME = 'aos-vm-node0-genericx86-64.wic.vmdk'
+NODE1_IMAGE_FILENAME = 'aos-vm-node1-genericx86-64.wic.vmdk'
+
+DISK_IMAGE_DOWNLOAD_URL = 'https://aos-prod-cdn-endpoint.azureedge.net/vm/aos-vm-v4.2.0-beta.tar.gz?' \
+                          '0b4234d360a3a61ddd5ebc8f0aac0acd3a8fa78d90e3865f855fceec8ee6b053082021ca7' \
+                           'f20015062273fd395e5cd6373fcf9b7c736e12bb0a7685162c982f5625b2d4a9b76f3'
+
+
+
+
+console = Console()
+error_console = Console(stderr=True, style='red')
+allow_print = True
+
+def print_message(formatted_text, end="\n", ljust: int = 0):
+    if allow_print:
+        if ljust > 0:
+            formatted_text = formatted_text.ljust(ljust)
+        console.print(formatted_text, end=end)
+
+def print_left(formatted_text, ljust=60):
+    print_message(formatted_text, end='', ljust = ljust)
+
+def print_done():
+    print_message('[green]DONE')
+
+def print_success(message):
+    print_message(f'[green]{str(message)}')
+
+def print_error(message):
+    if allow_print:
+        error_console.print(message)
+
+def generate_random_password() -> str:
+    """
+    Generate random password from letters and digits.
+
+    Returns:
+        str: Random string password
+    """
+    dictionary = string.ascii_letters + string.digits
+    password_length = random.randint(10, 15)
+    return ''.join(random.choice(dictionary) for _ in range(password_length))
