ea16c5606727a77d96afbda37a154c9982bf7038
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-21 15:07:55 +0000

    Add package pycify/1.0.2/pycify-1.0.2.tar.gz

---
+++ packages/pycify/pycify-1.0.2.tar.gz/pycify-1.0.2/src/pycify/cli.py
@@ -0,0 +1,18 @@
+"""CLI interface for pycify."""
+from __future__ import annotations
+
+import argparse
+
+from pycify import replace_py_with_pyc
+
+
+class PycifyArgs:
+    directory: str
+
+
+def cli(argv: list[str] | None = None) -> None:
+    """CLI to run pycify."""
+    parser = argparse.ArgumentParser()
+    parser.add_argument("directory", help="Folder to convert .py files in")
+    args = parser.parse_args(argv, namespace=PycifyArgs)
+    replace_py_with_pyc(args.directory)
