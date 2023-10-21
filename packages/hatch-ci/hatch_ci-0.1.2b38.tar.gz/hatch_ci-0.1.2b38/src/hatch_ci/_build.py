3e93edc09ea48b2995e5d710f72b72f71cf07fce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-21 03:13:06 +0000

    Add package hatch-ci/0.1.2b38/hatch_ci-0.1.2b38.tar.gz

---
+++ packages/hatch-ci/hatch_ci-0.1.2b38.tar.gz/hatch_ci-0.1.2b38/src/hatch_ci/_build.py
@@ -0,0 +1,9 @@
+# autogenerate build file
+branch = 'beta/0.1.2'
+build = '38'
+current = '0.1.2'
+ref = 'refs/heads/beta/0.1.2'
+runid = '6588567622'
+sha = '845fdc4cd10ceefcbab4b2112796f4866084c59f'
+version = '0.1.2b38'
+workflow = 'beta'
