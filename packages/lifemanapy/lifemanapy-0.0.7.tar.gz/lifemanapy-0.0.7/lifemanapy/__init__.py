bc1adf17225dd284a2bf8413419889acc6e8af61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-21 04:09:23 +0000

    Add package lifemanapy/0.0.7/lifemanapy-0.0.7.tar.gz

---
+++ packages/lifemanapy/lifemanapy-0.0.7.tar.gz/lifemanapy-0.0.7/lifemanapy/__init__.py
@@ -0,0 +1,8 @@
+from .lifemanatibia import main, set_dimensions
+from lifemanapy.configs import *
+from lifemanapy.constants import *
+from lifemanapy.converter import *
+from lifemanapy.dimensions import *
+
+
+
