e24b0647045eea94fcf9540fe228018930ef6ec8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-21 04:07:53 +0000

    Add package hestia-earth-utils/0.11.6/hestia_earth_utils-0.11.6-py3-none-any.whl

---
+++ packages/hestia-earth-utils/hestia_earth_utils-0.11.6-py3-none-any.whl/hestia_earth/utils/storage/_s3_client.py
@@ -0,0 +1,34 @@
+import os
+
+BUCKET = os.getenv('AWS_BUCKET')
+BUCKET_GLOSSARY = os.getenv('AWS_BUCKET_GLOSSARY')
+_s3_client = None
+
+
+# improves speed for connecting on subsequent calls
+def _get_s3_client():
+    global _s3_client
+    import boto3
+    _s3_client = boto3.session.Session().client('s3') if _s3_client is None else _s3_client
+    return _s3_client
+
+
+def _get_bucket(glossary: bool = False) -> str:
+    return BUCKET_GLOSSARY if glossary else BUCKET
+
+
+def _load_from_bucket(bucket: str, key: str):
+    from botocore.exceptions import ClientError
+    try:
+        return _get_s3_client().get_object(Bucket=bucket, Key=key)['Body'].read()
+    except ClientError:
+        return None
+
+
+def _exists_in_bucket(bucket: str, key: str):
+    from botocore.exceptions import ClientError
+    try:
+        _get_s3_client().head_object(Bucket=bucket, Key=key)
+        return True
+    except ClientError:
+        return False
