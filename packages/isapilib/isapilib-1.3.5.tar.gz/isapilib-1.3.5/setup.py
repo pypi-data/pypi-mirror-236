b501b93935a5ce8b71dd0bd8559b4b9eeb6eedc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-21 05:07:37 +0000

    Add package isapilib/1.3.5/isapilib-1.3.5.tar.gz

---
+++ packages/isapilib/isapilib-1.3.5.tar.gz/isapilib-1.3.5/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='isapilib',
+    version='1.3.5',
+    packages=find_packages(),
+    install_requires=[
+        'django>=4.2',
+    ],
+)
