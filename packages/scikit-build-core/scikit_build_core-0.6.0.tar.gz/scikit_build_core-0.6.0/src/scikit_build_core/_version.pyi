9289cafd2fa5bbcd50328db5345f3efb42298155
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-21 04:07:36 +0000

    Add package scikit-build-core/0.6.0/scikit_build_core-0.6.0.tar.gz

---
+++ packages/scikit-build-core/scikit_build_core-0.6.0.tar.gz/scikit_build_core-0.6.0/src/scikit_build_core/_version.pyi
@@ -0,0 +1,7 @@
+from __future__ import annotations
+
+version: str
+version_tuple: tuple[int, int, int, str, str] | tuple[int, int, int]
+
+# Note that newer versions of setuptools_scm also add __version__, but we are
+# not forcing new versions of setuptools_scm, so only these imports are allowed.
