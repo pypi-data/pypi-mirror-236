701bfe84f9b28414528153ca288c8ef585ed947d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-21 03:13:07 +0000

    Add package hatch-ci/0.1.2/hatch_ci-0.1.2-py3-none-any.whl

---
+++ packages/hatch-ci/hatch_ci-0.1.2-py3-none-any.whl/hatch_ci/_build.py
@@ -0,0 +1,9 @@
+# autogenerate build file
+branch = 'release/0.1.2'
+build = '8'
+current = '0.1.2'
+ref = 'refs/tags/release/0.1.2'
+runid = '6588651783'
+sha = 'a7f23e31eb6375bcfdc44945217ebd56911816c0'
+version = '0.1.2'
+workflow = 'tags'
