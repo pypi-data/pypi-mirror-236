ab4b8463744709352349bcb06e961419588501ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-21 03:12:45 +0000

    Add package jaxlob/0.10/jaxlob-0.10.tar.gz

---
+++ packages/jaxlob/jaxlob-0.10.tar.gz/jaxlob-0.10/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='jaxlob',
+    version='0.10',
+    packages=find_packages(),
+    install_requires=[
+        # list your project dependencies here
+    ],
+)
+
