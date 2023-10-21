7673449c520b36c757ddf841f8eec96ac25d6b03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-21 14:06:46 +0000

    Add package mknodes/0.45.3/mknodes-0.45.3-py3-none-any.whl

---
+++ packages/mknodes/mknodes-0.45.3-py3-none-any.whl/mknodes/project.py
@@ -0,0 +1,146 @@
+"""MkNodes project."""
+
+from __future__ import annotations
+
+from collections.abc import Callable
+import os
+import pathlib
+
+from typing import Any, Generic, TypeVar
+
+from mknodes import paths
+from mknodes.info import contexts, folderinfo, linkprovider
+from mknodes.jinja import environment
+from mknodes.navs import mknav
+from mknodes.theme import theme as theme_
+from mknodes.utils import classhelpers, helpers, jinjahelpers, log, reprhelpers
+
+
+logger = log.get_logger(__name__)
+
+
+T = TypeVar("T", bound=theme_.Theme)
+
+
+class Project(Generic[T]):
+    """MkNodes Project."""
+
+    def __init__(
+        self,
+        theme: T,
+        repo: str | os.PathLike | None | folderinfo.FolderInfo = None,
+        build_fn: str | Callable = paths.DEFAULT_BUILD_FN,
+        build_kwargs: dict[str, Any] | None = None,
+        base_url: str = "",
+        use_directory_urls: bool = True,
+        clone_depth: int = 100,
+    ):
+        """The main project to create a website.
+
+        Arguments:
+            theme: The theme to use
+            repo: Path to the git repository
+            build_fn: Callable to create the website with
+            build_kwargs: Keyword arguments for the build function
+            base_url: Base url of the website
+            use_directory_urls: Whether urls are in directory-style
+            clone_depth: Amount of commits to clone in case repository is remote.
+        """
+        self.linkprovider = linkprovider.LinkProvider(
+            base_url=base_url,
+            use_directory_urls=use_directory_urls,
+            include_stdlib=True,
+        )
+        self._root: mknav.MkNav | None = None
+        self.build_fn = classhelpers.to_callable(build_fn)
+        self.build_kwargs = build_kwargs or {}
+        self.env = environment.Environment(undefined="strict", load_templates=True)
+        self.theme: T = theme
+        self.theme.associated_project = self
+        match repo:
+            case folderinfo.FolderInfo():
+                self.folderinfo = repo
+            case _ if helpers.is_url(str(repo)):
+                self.folderinfo = folderinfo.FolderInfo.clone_from(
+                    str(repo),
+                    depth=clone_depth,
+                )
+            case _:
+                self.folderinfo = folderinfo.FolderInfo(repo)
+
+        self.context = contexts.ProjectContext(
+            metadata=self.folderinfo.context,
+            git=self.folderinfo.git.context,
+            # github=self.folderinfo.github.context,
+            theme=self.theme.context,
+            links=self.linkprovider,
+            env=self.env,
+        )
+        jinjahelpers.set_markdown_exec_namespace(self.env.globals)
+
+    def build(self):
+        logger.debug("Building page...")
+        self.build_fn(project=self, **self.build_kwargs)
+        logger.debug("Finished building page.")
+        if not self._root:
+            msg = "No root for project created."
+            raise RuntimeError(msg)
+        paths = [
+            pathlib.Path(node.resolved_file_path).stem
+            for _level, node in self._root.iter_nodes()
+            if hasattr(node, "resolved_file_path")
+        ]
+        self.linkprovider.set_excludes(paths)
+
+    def __repr__(self):
+        return reprhelpers.get_repr(self, repo_path=str(self.folderinfo.path))
+
+    @classmethod
+    def for_mknodes(cls) -> Project:
+        kls = cls(
+            base_url="",
+            use_directory_urls=True,
+            theme=theme_.Theme.get_theme("material", data={}),
+            build_fn=paths.DEFAULT_BUILD_FN,
+        )
+        kls.build()
+        return kls
+
+    @classmethod
+    def for_path(cls, path: str) -> Project:
+        kls = cls(
+            base_url="",
+            use_directory_urls=True,
+            theme=theme_.Theme.get_theme("material", data={}),
+            repo=path,
+        )
+        kls.build()
+        return kls
+
+    def set_root(self, nav: mknav.MkNav):
+        """Set the root MkNav."""
+        self._root = nav
+        nav.associated_project = self
+
+    def get_root(self, **kwargs: Any) -> mknav.MkNav:
+        """Return the root MkNav.
+
+        This MkNav should get populated in order to build
+        the website.
+
+        Arguments:
+            kwargs: Keyword arguments passed to MkNav constructor.
+        """
+        self._root = mknav.MkNav(project=self, **kwargs)
+        return self._root
+
+
+if __name__ == "__main__":
+    import mknodes as mk
+
+    from mknodes.manual import root
+
+    theme = mk.MaterialTheme()
+    project = Project(theme=theme)
+    log.basic()
+    root.build(project)
---
+++ packages/mknodes/mknodes-0.45.3-py3-none-any.whl/mknodes/utils/classhelpers.py
@@ -0,0 +1,309 @@
+from __future__ import annotations
+
+from collections.abc import Callable, Iterator, Sequence
+import contextlib
+import functools
+import importlib
+import importlib.util
+import inspect
+import os
+import pathlib
+import sys
+import types
+import typing
+
+from mknodes.utils import log
+
+
+T = typing.TypeVar("T", bound=type)
+
+
+logger = log.get_logger(__name__)
+
+
+def iter_subclasses(
+    klass: T,
+    *,
+    recursive: bool = True,
+    filter_abstract: bool = False,
+    filter_generic: bool = True,
+    filter_locals: bool = True,
+) -> typing.Iterator[T]:
+    """Recursively iter all subclasses of given klass.
+
+    Arguments:
+        klass: class to get subclasses from
+        filter_abstract: whether abstract base classes should be included.
+        filter_generic: whether generic base classes should be included.
+        filter_locals: whether local base classes should be included.
+        recursive: whether to also get subclasses of subclasses.
+    """
+    if getattr(klass.__subclasses__, "__self__", None) is None:
+        return
+    for kls in klass.__subclasses__():
+        if recursive:
+            yield from iter_subclasses(kls)
+        if filter_abstract and inspect.isabstract(kls):
+            continue
+        if filter_generic and kls.__qualname__.endswith("]"):
+            continue
+        if filter_locals and "<locals>" in kls.__qualname__:
+            continue
+        yield kls
+
+
+@typing.overload
+def to_module(module, return_none: typing.Literal[False] = ...) -> types.ModuleType: ...
+
+
+@typing.overload
+def to_module(
+    module,
+    return_none: typing.Literal[True] = ...,
+) -> types.ModuleType | None: ...
+
+
+@functools.cache
+def to_module(
+    module: str | Sequence[str] | types.ModuleType,
+    return_none: bool = True,
+) -> types.ModuleType | None:
+    """Returns a module for given module path. If module is given, just return it.
+
+    Arguments:
+        module: A ModuleType, str or sequence.
+        return_none: In case module cant get imported, return None.
+                     If False, Exception is thrown.
+    """
+    match module:
+        case (str(), *_) | str():  # type: ignore[attr-defined]
+            module_path = module if isinstance(module, str) else ".".join(module)
+            try:
+                return import_module(module_path)
+            except (ImportError, AttributeError) as e:
+                logger.warning("Could not import %s", module_path)
+                if return_none:
+                    return None
+                raise ImportError from e
+        case types.ModuleType():
+            return module
+        case _:
+            raise TypeError(module)
+
+
+@functools.cache
+def to_class(klass: type | str | tuple[str, ...] | list[str]):
+    """Convert given input to a class.
+
+    If input is a string or Sequence, interpret it as a dotted path.
+    """
+    match klass:
+        case type():
+            return klass
+        case str() if ":" in klass:
+            # path.to.mod:Classname
+            mod_path, klass_name = klass.split(":", maxsplit=1)
+            mod = import_module(mod_path)
+            return getattr(mod, klass_name)
+        case str():
+            # path.to.mod.Classname
+            parts = klass.split(".")
+            mod = import_module(".".join(parts[:-1]))
+            return getattr(mod, parts[-1])
+        case tuple() | list():
+            mod = import_module(".".join(klass[:-1]))
+            return getattr(mod, klass[-1])
+        case _:
+            raise TypeError(klass)
+
+
+def to_module_parts(  # type: ignore
+    module: Sequence[str] | str | types.ModuleType,
+) -> tuple[str, ...]:
+    """Returns a tuple describing the module path.
+
+    Result is of form ("module", "submodule", "subsubmodule")
+
+    Arguments:
+        module: A ModuleType, str or sequence.
+    """
+    match module:
+        case (str(), *_):  # type: ignore[attr-defined]
+            return tuple(module)
+        case str():
+            return tuple(module.split("."))
+        case types.ModuleType():
+            return tuple(module.__name__.split("."))
+        case pathlib.Path() if not module.is_absolute():
+            return module.parts
+        case _:
+            raise TypeError(module)
+
+
+def to_dotted_path(
+    obj: Sequence[str] | str | types.ModuleType | types.MethodType | type,
+) -> str:
+    """Return dotted path for given input.
+
+    Arguments:
+        obj: Input to return dotted path for
+    """
+    match obj:
+        case (str(), *_):  # type: ignore[attr-defined]
+            return ".".join(obj)
+        case str():
+            return obj
+        case types.ModuleType():
+            return obj.__name__
+        case type() | Callable():
+            return f"{obj.__module__}.{obj.__qualname__}"
+        case _:
+            raise TypeError(obj)
+
+
+@functools.cache
+def iter_classes(
+    module: types.ModuleType | str | tuple[str, ...],
+    *,
+    type_filter: type | None | types.UnionType = None,
+    module_filter: str | None = None,
+    filter_by___all__: bool = False,
+    recursive: bool = False,
+) -> Iterator[type]:
+    """Yield classes from given module.
+
+    Arguments:
+        module: either a module or a path to a module in form of str or
+                tuple of strings.
+        type_filter: only yield classes which are subclasses of given type.
+        module_filter: filter by a module prefix.
+        filter_by___all__: Whether to filter based on whats defined in __all__.
+        recursive: import all submodules recursively and also yield their classes.
+    """
+    mod = to_module(module)
+    if not mod:
+        return []
+    if recursive:
+        for _name, submod in get_members(mod, inspect.ismodule):
+            if submod.__name__.startswith(module_filter or ""):
+                yield from iter_classes(
+                    submod,
+                    type_filter=type_filter,
+                    module_filter=submod.__name__,
+                    filter_by___all__=filter_by___all__,
+                    recursive=True,
+                )
+    for klass_name, kls in get_members(mod, inspect.isclass):
+        has_all = hasattr(mod, "__all__")
+        if filter_by___all__ and (not has_all or klass_name not in mod.__all__):
+            continue
+        if type_filter is not None and not issubclass(kls, type_filter):
+            continue
+        if module_filter is not None and not kls.__module__.startswith(module_filter):
+            continue
+        yield kls
+
+
+@functools.cache
+def get_topmost_module_path(obj: Callable) -> str:
+    """Return path of topmost module containing given class.
+
+    If a class is imported in any of its parent modules, return that "shorter" path.
+
+    So for a class "submodule.classmodule.Class", it could return "submodule.Class"
+
+    Arguments:
+        obj: Klass to get the path for.
+    """
+    to_search_for = obj.__self__ if hasattr(obj, "__self__") else obj
+    path = obj.__module__
+    parts = path.split(".")
+    while parts:
+        with contextlib.suppress(TypeError):
+            new_path = ".".join(parts)
+            mod = import_module(new_path)
+            objs = [i for _i_name, i in get_members(mod)]
+            if to_search_for in objs:
+                path = new_path
+        parts = parts[:-1]
+    return path
+
+
+@functools.cache
+def get_submodules(
+    module: types.ModuleType | str | tuple[str, ...],
+) -> list[types.ModuleType]:
+    """Return list of submodules of given module.
+
+    Arguments:
+        module: Module to return submodules from.
+    """
+    module = to_module(module)
+    return [
+        mod
+        for name, mod in get_members(module, inspect.ismodule)
+        if name.startswith(module.__name__)
+    ]
+
+
+@functools.cache
+def import_module(mod: str):
+    return importlib.import_module(mod)
+
+
+@functools.cache
+def get_members(module, predicate=None):
+    return inspect.getmembers(module, predicate)
+
+
+@functools.cache
+def import_file(path: str | os.PathLike) -> types.ModuleType:
+    """Import a module based on a file path.
+
+    Arguments:
+        path: Path which should get imported
+    """
+    module_name = pathlib.Path(path).stem
+    spec = importlib.util.spec_from_file_location(module_name, path)
+    if spec is None:
+        raise RuntimeError
+    module = importlib.util.module_from_spec(spec)
+    sys.modules[module_name] = module
+    spec.loader.exec_module(module)  # type: ignore[union-attr]
+    return module
+
+
+@functools.cache
+def to_callable(path: str | Callable) -> Callable:
+    if callable(path):
+        return path
+    modname, _qualname_separator, qualname = path.partition(":")
+    obj = import_file(modname) if modname.endswith(".py") else import_module(modname)
+    for attr in qualname.split("."):
+        obj = getattr(obj, attr)
+    if not callable(obj):
+        msg = "Incorrect path"
+        raise TypeError(msg)
+    return obj
+
+
+def get_code_name(obj) -> str:
+    """Get a title for an object representing code."""
+    match obj:
+        case types.CodeType():
+            return obj.co_name
+        case types.TracebackType():
+            return obj.tb_frame.f_code.co_name
+        case types.FrameType():
+            return obj.f_code.co_name
+        case Callable():
+            return to_dotted_path(obj)
+        case _:
+            return obj.__name__
+
+
+if __name__ == "__main__":
+    import mknodes as mk
+
+    print(get_topmost_module_path(mk.MkCode.for_object))
+    print(mk.MkCode.for_object.__qualname__)
