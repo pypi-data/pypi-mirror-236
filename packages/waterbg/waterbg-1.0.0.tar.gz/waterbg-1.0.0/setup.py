2e987381d7ef316f21295b8368adb47afda1577f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-21 05:07:43 +0000

    Add package waterbg/1.0.0/waterbg-1.0.0.tar.gz

---
+++ packages/waterbg/waterbg-1.0.0.tar.gz/waterbg-1.0.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='waterbg',
+    version='1.0.0',
+    py_modules=['waterbg'],
+    license='MIT',
+    author='Szabolcs Dombi',
+    author_email='cprogrammer1994@gmail.com',
+    url='https://github.com/szabolcsdombi/pyodide-water-background',
+    project_urls={
+        'Source': 'https://github.com/szabolcsdombi/pyodide-water-background',
+    },
+    classifiers=['Topic :: Multimedia :: Graphics :: 3D Rendering'],
+    keywords=['pyodide', 'console', 'background'],
+)
