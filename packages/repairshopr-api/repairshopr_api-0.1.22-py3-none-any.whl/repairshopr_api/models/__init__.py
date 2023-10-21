137f6ee7ceb9ed6541c5b11cccb9df5e3a3737ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-21 15:07:12 +0000

    Add package repairshopr-api/0.1.22/repairshopr_api-0.1.22-py3-none-any.whl

---
+++ packages/repairshopr-api/repairshopr_api-0.1.22-py3-none-any.whl/repairshopr_api/models/__init__.py
@@ -0,0 +1,7 @@
+from .product import Product
+from .ticket import Ticket, Comment
+from .user import User
+from .invoice import Invoice, InvoiceLine
+from .customer import Customer, Contact
+from .estimate import Estimate
+from .payment import Payment
