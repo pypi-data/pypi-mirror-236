dd4503e70d71a23421dc4856c8500cc2a486f0a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-21 03:10:55 +0000

    Add package tarmtextract/0.0.8/tarmtextract-0.0.8.tar.gz

---
+++ packages/tarmtextract/tarmtextract-0.0.8.tar.gz/tarmtextract-0.0.8/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+# with open("README.md", "r", encoding="utf8") as file:
+    # long_description = file.read()
+
+VERSION = '0.0.8'
+DESCRIPTION = 'A package that allows to extract text from pdf/word file which is save in aws s3'
+
+# Setting up
+setup(
+    name="tarmtextract",
+    version=VERSION,
+    author="Paras Deshbhratar",
+    author_email="<paras.d13@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    # long_description=long_description,
+    packages=find_packages(),
+    install_requires=['pdfplumber', 'textract', 'boto3'],
+    keywords=['python', 'textract', 'pdfplumber', 'textextractor'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
