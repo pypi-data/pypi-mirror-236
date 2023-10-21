3cc9d9c02659e5c57d2b0eb379266f9d32eac273
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-21 14:06:45 +0000

    Add package kfsd/0.0.119/kfsd-0.0.119.tar.gz

---
+++ packages/kfsd/kfsd-0.0.119.tar.gz/kfsd-0.0.119/kfsd/apps/models/signals.py
@@ -0,0 +1,4 @@
+import kfsd.apps.endpoints.handlers.relations.hierarchy_init  # noqa: F401
+import kfsd.apps.endpoints.handlers.relations.hierarchy  # noqa: F401
+import kfsd.apps.endpoints.handlers.relations.rhel  # noqa: F401
+import kfsd.apps.endpoints.handlers.relations.relation  # noqa: F401
