e2d8707eb8e26fb2445fca6cd209ab1643998907
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-21 03:12:36 +0000

    Add package django-master-pay/1.0.4/django_master_pay-1.0.4.tar.gz

---
+++ packages/django-master-pay/django_master_pay-1.0.4.tar.gz/django_master_pay-1.0.4/django_master_pay/collections.py
@@ -0,0 +1,21 @@
+class PurseType(object):
+    TYPE_WEBMONEY = 'WebMoney'
+    TYPE_YANDEX_MONEY = 'Yandex'
+    TYPE_QIWI = 'QIWI'
+    TYPE_ZALEY_CASH = 'ZaleyCash'
+    TYPE_RU_CARD = 'RUCARD'
+    TYPE_UKR_CARD = 'UKRCARD'
+    TYPE_WORLD_CARD = 'WORLDCARD'
+    TYPE_CIS_CARD = 'SNGCARD'
+    TYPE_MTS = 'MTS'
+    TYPE_BEELINE = 'Beeline'
+    TYPE_MEGAFON = 'MegaFon'
+    TYPE_TELE2 = 'Tele2'
+    TYPE_CAPITALIST = 'Capitalist'
+
+
+class Currency(object):
+    RUB = 'RUB'
+    USD = 'USD'
+    EURO = 'EUR'
+    INR = 'INR'
