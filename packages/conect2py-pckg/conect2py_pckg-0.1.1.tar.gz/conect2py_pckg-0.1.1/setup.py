02ed7051e2ebc62044ff6564412dba2a5667d2ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-21 05:07:40 +0000

    Add package conect2py-pckg/0.1.1/conect2py_pckg-0.1.1.tar.gz

---
+++ packages/conect2py-pckg/conect2py_pckg-0.1.1.tar.gz/conect2py_pckg-0.1.1/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+from os.path import abspath, dirname, join
+
+README_MD = open(join(dirname(abspath(__file__)), "README.md")).read()
+
+setup(
+    # Name 
+    name="conect2py_pckg",
+
+    # Version
+    version="0.1.1",
+
+    # Packages
+    packages=find_packages(),
+
+    # Description
+    description="A python library for data compression using TAC (Tiny Anomaly Compression)",
+
+    # The content that will be shown for the project page.
+    long_description=README_MD,
+    long_description_content_type="text/markdown",
+
+    # The url field - Link to a git repository
+    url="https://github.com/conect2ai/Conect2Py-Package",
+
+    # The author name and email 
+    author_name="Conect2ai",
+    author_email="conect2ai@gmail.com",
+
+    # Classifiers
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3 :: Only"
+    ],
+
+    # Keywords are tags that identify your project and help searching for it
+    # This field is OPTIONAL
+    keywords="TEDA, TAC, Annomaly Detection, Data Compression, IoT, Eccentricity"
+
+)
