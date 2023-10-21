9ec210dfea27d8788f9f283860ac3770019c6ead
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-21 04:07:41 +0000

    Add package fmu-sumo/1.0.1/fmu_sumo-1.0.1-py3-none-any.whl

---
+++ packages/fmu-sumo/fmu_sumo-1.0.1-py3-none-any.whl/fmu/sumo/explorer/__init__.py
@@ -0,0 +1,5 @@
+"""Top-level package for fmu.sumo.explorer"""
+
+from fmu.sumo.explorer.explorer import Explorer
+from fmu.sumo.explorer.timefilter import TimeType, TimeFilter
+from fmu.sumo.explorer.objects.table_aggregated import AggregatedTable
