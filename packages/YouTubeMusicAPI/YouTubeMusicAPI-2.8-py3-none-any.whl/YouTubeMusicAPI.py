52ef26f305a8df55db5b03c595a2a66c09a57831
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-21 15:07:30 +0000

    Add package YouTubeMusicAPI/2.8/YouTubeMusicAPI-2.8-py3-none-any.whl

---
+++ packages/YouTubeMusicAPI/YouTubeMusicAPI-2.8-py3-none-any.whl/YouTubeMusicAPI.py
@@ -0,0 +1,30 @@
+import warnings
+import httpx
+import re
+
+warnings.filterwarnings("ignore", category=DeprecationWarning)
+
+def search(query: str) -> dict[str | None]:
+
+    headers: dict = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:108.0) Gecko/20100101 Firefox/108.0"}
+
+    page: str = httpx.get(f"https://music.youtube.com/search?q={query}", headers=headers,
+                          timeout=None).content.decode("unicode_escape")
+
+    trackId: str | None = re.search('"videoId":"(.*?)"', page)
+
+    if not trackId:
+        return {}
+
+    trackId: str = eval(f"{{{trackId.group()}}}")["videoId"]
+
+    track_info: dict = httpx.get(f"https://www.youtube.com/oembed?url=https://www.youtube.com/watch?v={trackId}",
+                                 headers=headers, timeout=None).json()
+
+    return dict(
+        title=track_info["title"],
+        id=trackId,
+        url=f"https://music.youtube.com/watch?v={trackId}",
+        artwork=f"https://img.youtube.com/vi/{trackId}/0.jpg",
+        author=dict(name=track_info["author_name"], url=track_info["author_url"])
+    )
