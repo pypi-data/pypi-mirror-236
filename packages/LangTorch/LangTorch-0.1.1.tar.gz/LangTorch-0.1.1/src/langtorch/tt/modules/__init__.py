b93201c2667dd37acca00a136b706477c2ff7483
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-22 04:06:49 +0000

    Add package LangTorch/0.1.1/LangTorch-0.1.1.tar.gz

---
+++ packages/LangTorch/LangTorch-0.1.1.tar.gz/LangTorch-0.1.1/src/langtorch/tt/modules/__init__.py
@@ -0,0 +1,10 @@
+from .module import TextModule
+from .code_module import CodeModule
+# from .linear import Linear
+from .activation import ActivationGPT
+# from .loss import Quality
+# from .pooling import AvgPool, MaxPool,
+# from .dropout import Dropout
+# from .rnn import  RNNBase
+from .distance import CosineSimilarity
+# from .transformer import TextTransformer
