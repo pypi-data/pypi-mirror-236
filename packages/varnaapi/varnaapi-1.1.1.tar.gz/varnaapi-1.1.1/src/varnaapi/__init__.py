0c755b1de11508335da5ec5ebe8b8a4c2591d95d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-22 03:13:43 +0000

    Add package varnaapi/1.1.1/varnaapi-1.1.1.tar.gz

---
+++ packages/varnaapi/varnaapi-1.1.1.tar.gz/varnaapi-1.1.1/src/varnaapi/__init__.py
@@ -0,0 +1,8 @@
+from .models import BasicDraw, Structure, Comparison, Motif, VARNA
+from .param import load_config
+from .settings import set_VARNA, enable_hack, check_settings_exists, load_settings
+
+__version__ = '1.1.1'
+
+check_settings_exists()
+load_settings()
