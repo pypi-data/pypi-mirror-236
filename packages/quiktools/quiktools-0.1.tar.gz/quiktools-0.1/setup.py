50c85550e046875f681603692a30788f1b9170a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-22 14:07:20 +0000

    Add package quiktools/0.1/quiktools-0.1.tar.gz

---
+++ packages/quiktools/quiktools-0.1.tar.gz/quiktools-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='quiktools',
+    version='0.1',
+    description="Quickly do things in 1 line that would've taken multiple lines.",
+    author='noerlol',
+    author_email='noerlol@proton.com',
+    packages=['quiktools'],
+    install_requires=[],
+)
