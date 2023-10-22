cc15b43b8d1fdd72865f47a9f78d4d84d548dbfc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-22 03:13:15 +0000

    Add package grpc-kit/0.1.1/grpc_kit-0.1.1.tar.gz

---
+++ packages/grpc-kit/grpc_kit-0.1.1.tar.gz/grpc_kit-0.1.1/src/grpc_kit/utils.py
@@ -0,0 +1,28 @@
+import glob
+import os
+from importlib import import_module
+
+
+def _import_module(proto_dir, module_path):
+    module_name = os.path.basename(module_path).split('.')[0]
+    module = import_module(f'{proto_dir}.{module_name}')
+    return module
+
+
+def get_service_name(module_path):
+    module_name = os.path.basename(module_path).split('.')[0]
+    return module_name.split('_')[0]
+
+
+def find_services(proto_dir: str) -> dict:
+    """寻找所有的服务"""
+    proto_module = import_module(proto_dir)
+    proto = {}
+    pd2_files = glob.glob(os.path.join(proto_module.__path__[0], '*_pb2.py'))
+    pd2_grpc_files = glob.glob(os.path.join(proto_module.__path__[0], '*_pb2_grpc.py'))
+    for pb2, pb2_grpc in list(zip(pd2_files, pd2_grpc_files)):
+        proto[get_service_name(pb2)] = {
+            'pb2': _import_module(proto_dir, pb2),
+            'pb2_grpc': _import_module(proto_dir, pb2_grpc)
+        }
+    return proto
