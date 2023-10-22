4411c2cf40ff8805f2ec9cd381f48c1deb867012
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-22 13:15:50 +0000

    Add package excel-productive-CLI/4.0.0/excel-productive-CLI-4.0.0.tar.gz

---
+++ packages/excel-productive-CLI/excel-productive-CLI-4.0.0.tar.gz/excel-productive-CLI-4.0.0/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='excel-productive-CLI',
+    version="4.0.0",
+    py_modules=['main'],
+    description='TimeTracker-CLI is a command-line interface (CLI) application designed to help users efficiently track and manage their time spent on various tasks and projects.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    install_requires=[
+        'pyfiglet',
+        'tqdm',
+        'inquirer',
+        'yaspin',
+        'rich',
+    ],
+    entry_points={
+        'console_scripts': [
+            'excel-productive-CLI-4.0.0=main:main',
+        ],
+    },
+)
