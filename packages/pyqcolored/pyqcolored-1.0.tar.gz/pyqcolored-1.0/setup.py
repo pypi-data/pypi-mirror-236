6eef19f6bf655fe5d8f842edc7887352020d5f31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-22 14:07:08 +0000

    Add package pyqcolored/1.0/pyqcolored-1.0.tar.gz

---
+++ packages/pyqcolored/pyqcolored-1.0.tar.gz/pyqcolored-1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+setup(
+    name="pyqcolored",
+    version='1.0',
+    license='Eclipse Public License 2.0',
+    authors=["smithsha"],
+    author_email="smithsha@gmail.com",
+    description="A command line tool for colors",
+    long_description='Documentation: https://github.com/johnsnow/pyqcolored',
+    packages=['pyqcolored']
+)
+
