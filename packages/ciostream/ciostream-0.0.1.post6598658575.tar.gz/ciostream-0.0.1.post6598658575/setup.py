13bc266a35a419e1bfa71de8b6390912f86ac74d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-22 04:06:36 +0000

    Add package ciostream/0.0.1.post6598658575/ciostream-0.0.1.post6598658575.tar.gz

---
+++ packages/ciostream/ciostream-0.0.1.post6598658575.tar.gz/ciostream-0.0.1.post6598658575/setup.py
@@ -0,0 +1,33 @@
+import os
+from setuptools import setup
+from Cython.Build import cythonize
+from setuptools.extension import Extension
+
+version="0.0.1"
+
+if "GITHUB_RUN_ID" in os.environ:
+    version += "-"
+    version += os.environ["GITHUB_RUN_ID"]
+
+ext = Extension(
+    "ciostream",
+    sources=[
+        "src/ciostream/__init__.pyx",
+        "src/ciostream/ciostream_native.cpp"],
+    language="c++")
+
+setup(
+    name="ciostream",
+    version=version,
+    author="Matthew Ballance",
+    author_email="matt.ballance@gmail.com",
+    description="Provides C++ iostream Cython wrappers",
+    zip_safe=False,
+    packages = ['ciostream'],
+    package_dir = {'': 'src'},
+    package_data = {
+        'ciostream': ['*.pxd', '*.cpp', '*.h']
+    },
+    ext_modules = cythonize([ext])
+)
+
