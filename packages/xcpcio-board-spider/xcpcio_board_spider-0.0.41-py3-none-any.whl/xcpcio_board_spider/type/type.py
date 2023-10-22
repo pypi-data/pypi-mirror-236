cb6a985c251c8265b06cbc360ab7bf1db097f8c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-22 14:08:38 +0000

    Add package xcpcio-board-spider/0.0.41/xcpcio_board_spider-0.0.41-py3-none-any.whl

---
+++ packages/xcpcio-board-spider/xcpcio_board_spider-0.0.41-py3-none-any.whl/xcpcio_board_spider/type/type.py
@@ -0,0 +1,70 @@
+import json
+import typing
+
+
+class Image:
+    def __init__(self, url: str = None, base64: str = None, preset: str = None):
+        self.url = url
+        self.base64 = base64
+        self.preset = preset
+
+    @property
+    def get_dict(self):
+        obj = {}
+
+        if self.url is not None:
+            obj['url'] = self.url
+
+        if self.base64 is not None:
+            obj['base64'] = self.base64
+
+        if self.preset is not None:
+            obj['preset'] = self.preset
+
+        return obj
+
+    def from_dict(self, d: typing.Any):
+        if "url" in d.keys():
+            self.url = d["url"]
+
+        if "base64" in d.keys():
+            self.base64 = d["base64"]
+
+        if "preset" in d.keys():
+            self.preset = d["preset"]
+
+    @property
+    def get_json(self):
+        return json.dumps(self.get_dict)
+
+    def from_json(self, json_string: str):
+        self.from_dict(json.loads(json_string))
+
+
+class Color:
+    def __init__(self, color: str, background_color: str):
+        self.color = color
+        self.background_color = background_color
+
+    @property
+    def get_dict(self):
+        obj = {}
+
+        obj["color"] = self.color
+        obj["background_color"] = self.background_color
+
+        return obj
+
+    def from_dict(self, d: typing.Any):
+        if "color" in d.keys():
+            self.color = d["color"]
+
+        if "background_color" in d.keys():
+            self.background_color = d["background_color"]
+
+    @property
+    def get_json(self):
+        return json.dumps(self.get_dict)
+
+    def from_json(self, json_string: str):
+        self.from_dict(json.loads(json_string))
