e7f11b64ef66eb8eb6f5970a443b9072d38140cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-22 13:15:17 +0000

    Add package rockettp/1.0.0/rockettp-1.0.0.tar.gz

---
+++ packages/rockettp/rockettp-1.0.0.tar.gz/rockettp-1.0.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+   name='rockettp',
+   version='1.0.0',
+   author='Mokadem Adel Abdelkader',
+   author_email='aa.mokadem@esi-sba.dz',
+   packages=['rockettp'],
+   url='http://pypi.python.org/pypi/rockettp/',
+   license='LICENSE.txt',
+   description='An awesome package that does something',
+   long_description=open('README.md').read(),
+   long_description_content_type="text/markdown",
+   install_requires=['rockettp']
+)
