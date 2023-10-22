f2d5a02f23576176d1b83ee5d7831e8cca3519b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-22 03:11:07 +0000

    Add package pyinterp/2023.10.2/pyinterp-2023.10.2.tar.gz

---
+++ packages/pyinterp/pyinterp-2023.10.2.tar.gz/pyinterp-2023.10.2/src/pyinterp/version.py
@@ -0,0 +1,18 @@
+# Copyright (c) 2023 CNES
+#
+# All rights reserved. Use of this source code is governed by a
+# BSD-style license that can be found in the LICENSE file.
+"""
+Get software version information
+================================
+"""
+
+
+def release() -> str:
+    """Returns the software version number"""
+    return "2023.10.2"
+
+
+def date() -> str:
+    """Returns the creation date of this release"""
+    return "21 October 2023"
