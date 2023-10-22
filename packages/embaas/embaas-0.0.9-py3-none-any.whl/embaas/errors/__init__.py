bf04c9c4a12b2d92ea517e94685bc09ef8a61488
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-22 13:15:37 +0000

    Add package embaas/0.0.9/embaas-0.0.9-py3-none-any.whl

---
+++ packages/embaas/embaas-0.0.9-py3-none-any.whl/embaas/errors/__init__.py
@@ -0,0 +1,6 @@
+
+
+from .api_error import ApiError
+from .unprocessable_entity_error import UnprocessableEntityError
+
+__all__ = ["UnprocessableEntityError", "ApiError"]
---
+++ packages/embaas/embaas-0.0.9-py3-none-any.whl/embaas/types/document_chunk.py
@@ -0,0 +1,27 @@
+
+
+import datetime as dt
+import typing
+
+import pydantic
+
+from ..core.datetime_utils import serialize_datetime
+
+
+class DocumentChunk(pydantic.BaseModel):
+    text: str
+    metadata: typing.Dict[str, typing.Any]
+    embedding: typing.Optional[typing.List[float]]
+    index: int
+
+    def json(self, **kwargs: typing.Any) -> str:
+        kwargs_with_defaults: typing.Any = {"by_alias": True, "exclude_unset": True, **kwargs}
+        return super().json(**kwargs_with_defaults)
+
+    def dict(self, **kwargs: typing.Any) -> typing.Dict[str, typing.Any]:
+        kwargs_with_defaults: typing.Any = {"by_alias": True, "exclude_unset": True, **kwargs}
+        return super().dict(**kwargs_with_defaults)
+
+    class Config:
+        frozen = True
+        json_encoders = {dt.datetime: serialize_datetime}
