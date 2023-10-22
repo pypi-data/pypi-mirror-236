38ea10110dbaa3c3ba16fb4cc9acf1c20c84e8e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-22 04:07:25 +0000

    Add package ua-currency-exporter/0.0.448b0/ua-currency-exporter-0.0.448b0.tar.gz

---
+++ packages/ua-currency-exporter/ua-currency-exporter-0.0.448b0.tar.gz/ua-currency-exporter-0.0.448b0/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\\n" + fh.read()
+
+setup(
+    name="ua-currency-exporter",
+    version='v0.0.448-beta',
+    author="John Doe",
+    author_email="johndoe@mail.com",
+    description="my amazing package",
+    url = "https://github.com/johndoe/my_package",
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=['numpy'],
+    keywords=['pypi', 'cicd', 'python'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows"
+    ]
+)
