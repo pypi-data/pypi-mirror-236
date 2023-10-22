41118a7382c89f90c2bd4c11b8d6dbbb0d3306b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-22 13:15:31 +0000

    Add package autograder-py/0.2.7/autograder_py-0.2.7-py3-none-any.whl

---
+++ packages/autograder-py/autograder_py-0.2.7-py3-none-any.whl/autograder/cli/__main__.py
@@ -0,0 +1,24 @@
+"""
+General commands to work with autograder projects.
+"""
+
+import os
+import sys
+
+import autograder.util.cli
+
+THIS_DIR = os.path.abspath(os.path.dirname(os.path.realpath(__file__)))
+
+def run():
+    print("The autograder CLI package contains several tools for interacting with the autograder.")
+    print("The following is a non-exhaustive list of CLI tools.")
+    print("Invoke each command with the `--help` option for more details.")
+
+    autograder.util.cli.list_dir(THIS_DIR, 'autograder.cli')
+    return 0
+
+def main():
+    return run()
+
+if (__name__ == '__main__'):
+    sys.exit(main())
