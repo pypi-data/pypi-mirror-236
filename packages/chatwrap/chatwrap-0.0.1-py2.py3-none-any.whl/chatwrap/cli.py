95806a82d7b407f2c136f51c9573f5a7492cf812
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-22 04:06:26 +0000

    Add package chatwrap/0.0.1/chatwrap-0.0.1-py2.py3-none-any.whl

---
+++ packages/chatwrap/chatwrap-0.0.1-py2.py3-none-any.whl/chatwrap/cli.py
@@ -0,0 +1,19 @@
+"""Console script for chatwrap."""
+import argparse
+import sys
+
+
+def main():
+    """Console script for chatwrap."""
+    parser = argparse.ArgumentParser()
+    parser.add_argument('_', nargs='*')
+    args = parser.parse_args()
+
+    print("Arguments: " + str(args._))
+    print("Replace this message by putting your code into "
+          "chatwrap.cli.main")
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())  # pragma: no cover
