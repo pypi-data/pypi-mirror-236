de8975f25c26c0e0d4e80b868ed4577702ba8cd7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-22 04:06:29 +0000

    Add package pymoat/0.1.0/pymoat-0.1.0.tar.gz

---
+++ packages/pymoat/pymoat-0.1.0.tar.gz/pymoat-0.1.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages 
+setup( 
+       name="pymoat",    # pypi project name
+       version="0.1.0", 
+       packages=find_packages(), 
+       description="moat package",
+       python_requires='>=3.6',
+)
