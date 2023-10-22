d0d2675b8fd6275672b086efe936879d3068dbbc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-22 13:15:07 +0000

    Add package jupyter-toc/1.1.2/jupyter-toc-1.1.2.tar.gz

---
+++ packages/jupyter-toc/jupyter-toc-1.1.2.tar.gz/jupyter-toc-1.1.2/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="jupyter-toc",
+    version="1.1.2",
+    packages=find_packages(),
+    install_requires=[
+        "nbformat",
+        "nbconvert"
+    ],
+    author="Odai Athamneh",
+    author_email="heyodai@gmail.com",
+    description="A Python package for generating a table of contents for Jupyter notebooks",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/heyodai/jupyter-toc",
+    entry_points={
+        "console_scripts": [
+            "jupyter-toc=jupyter_toc.cli:main"
+        ]
+    }
+)
