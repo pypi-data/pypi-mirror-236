aca02c83a9bffed5b16bafe59e668417f6a4d1e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-22 14:07:21 +0000

    Add package pyzjr/1.2.3/pyzjr-1.2.3.tar.gz

---
+++ packages/pyzjr/pyzjr-1.2.3.tar.gz/pyzjr-1.2.3/pyzjr/augmentation/Color/__init__.py
@@ -0,0 +1,8 @@
+
+
+
+from .Module import *
+from .mode import *
+from ._utils import *
+
+__all__ = ["grayscale", "rgb2bgr", "bgr2rgb", "bgr2hsv", "hsv2bgr", "rgb2hsv", "hsv2rgb", "ColorFind", "colorstr"]
