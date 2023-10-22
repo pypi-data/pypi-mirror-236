ffaa7ae1f3281e51c5ea0a10721a8c5262c45659
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-22 04:06:35 +0000

    Add package upyt/0.0.1/upyt-0.0.1-py3-none-any.whl

---
+++ packages/upyt/upyt-0.0.1-py3-none-any.whl/upyt/cli/__init__.py
@@ -0,0 +1,63 @@
+from argparse import ArgumentParser
+
+import os
+
+from importlib import import_module
+
+
+def main(argv: list[str] | None = None) -> None:
+    parser = ArgumentParser(
+        description="""
+            A multi-tool for programming and interacting with MicroPython
+            devices.
+        """
+    )
+    parser.add_argument(
+        "--device",
+        "-d",
+        default=os.getenv("UPYT_DEVICE"),
+        help="""
+            Device to connect to. For example `/dev/ttyACM0` or
+            `/dev/ttyACM0:115200`. Defaults to value of UPYT_DEVICE environment
+            variable, if set, required otherwise.
+        """,
+    )
+
+    subparsers = parser.add_subparsers(title="commands", required=True)
+
+    subcommands = [
+        (["terminal", "term", "t"], "a serial terminal for MicroPython."),
+        ("sync", "efficiently synchronise a directory to the device"),
+        ("reset", "perform a soft-reset"),
+        (["interrupt", "int"], "interrupt and return to the REPL"),
+        ("ls", "list files and directories"),
+        ("mkdir", "create a directory"),
+        ("rm", "remove files and directories"),
+        ("cat", "read (and concatenate) files"),
+        ("cp", "copy files to and from the device"),
+    ]
+
+    for command, help_text in subcommands:
+        if isinstance(command, str):
+            command = [command]
+
+        command_module = import_module(f"upyt.cli.{command[0]}")
+        subparser = subparsers.add_parser(
+            command[0],
+            help=help_text,
+            description=command_module.__doc__,
+            aliases=command[1:],
+        )
+        command_module.add_arguments(subparser)
+        subparser.set_defaults(cmd=command_module.main)
+
+    args = parser.parse_args(argv)
+
+    if args.device is None:
+        parser.error("--device is required if UPYT_DEVICE is not set")
+
+    args.cmd(args)
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/upyt/upyt-0.0.1-py3-none-any.whl/upyt/cli/rm.py
@@ -0,0 +1,74 @@
+"""
+Delete files and directories on a MicroPython device.
+"""
+
+import sys
+
+from argparse import ArgumentParser, Namespace
+
+from upyt.connection import Connection
+from upyt.upy_terminal import interrupt_and_enter_repl
+from upyt.upy_fs import upy_filesystem, PathType
+from upyt.cli.hybrid_filesystem_api import HybridFilesystemAPI
+
+
+def add_arguments(parser: ArgumentParser) -> None:
+    parser.add_argument(
+        "path",
+        nargs="+",
+        help="""
+            The files or directories to delete. Acts recursively. Prefix with
+            ':' for device paths.
+        """,
+    )
+    parser.add_argument(
+        "--recursive",
+        "-r",
+        action="store_true",
+        default=False,
+        help="""
+            If given, delete directories and their contents recursively.
+            Otherwise, will only delete files.
+        """,
+    )
+    parser.add_argument(
+        "--force",
+        "-f",
+        action="store_true",
+        default=False,
+        help="""
+            Suppress errors when deleting non-existant files.
+        """,
+    )
+
+
+def main(args: Namespace):
+    with Connection.from_specification(args.device) as conn:
+        interrupt_and_enter_repl(conn)
+        with upy_filesystem(conn) as fs:
+            if not any(path.startswith(":") for path in args.path):
+                print(
+                    "warning: path not on device (i.e. starting with ':')",
+                    file=sys.stderr,
+                )
+            hfs = HybridFilesystemAPI(fs)
+
+            for path in args.path:
+                filetype = hfs.get_type(path)
+
+                if filetype == PathType.absent:
+                    if not args.force:
+                        print(f"error: file not found: {path}", file=sys.stderr)
+                        sys.exit(1)
+                elif filetype.is_dir():
+                    if args.recursive:
+                        hfs.remove_recursive(path)
+                    else:
+                        print(
+                            f"error: cannot delete directory: {path} (try --recursive)",
+                            file=sys.stderr,
+                        )
+                        sys.exit(1)
+                elif filetype.is_file():
+                    hfs.remove_recursive(path)
+            hfs.sync()
