6257f99e0f9718cae839a5f2e05ebc46d12abc17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-22 14:07:07 +0000

    Add package assetutilities/0.0.1/assetutilities-0.0.1.tar.gz

---
+++ packages/assetutilities/assetutilities-0.0.1.tar.gz/assetutilities-0.0.1/src/assetutilities/__main__.py
@@ -0,0 +1,21 @@
+"""Generic python package
+
+Usage:
+------
+
+
+
+Contact:
+--------
+
+More information is available at:
+
+- https://pypi.org/project/assetutilities/
+- https://github.com/vamseeachanta/assetutilities
+
+
+Version:
+--------
+
+- assetutilities v0.0.1
+"""
