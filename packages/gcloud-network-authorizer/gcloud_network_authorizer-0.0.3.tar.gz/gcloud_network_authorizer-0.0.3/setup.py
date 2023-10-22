4ebf37a3ace1f184084627a36b126e8dcc645136
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-22 03:12:28 +0000

    Add package gcloud-network-authorizer/0.0.3/gcloud_network_authorizer-0.0.3.tar.gz

---
+++ packages/gcloud-network-authorizer/gcloud_network_authorizer-0.0.3.tar.gz/gcloud_network_authorizer-0.0.3/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="gcloud_network_authorizer",
+    version="0.0.3",
+    packages=find_packages(),
+    install_requires=["click", "requests"],
+    entry_points={
+        "console_scripts": ["authorize_networks=authorize_networks_cli.main:cli"],
+    },
+)
