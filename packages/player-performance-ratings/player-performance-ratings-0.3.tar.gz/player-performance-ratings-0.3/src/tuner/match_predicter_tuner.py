1fca912c31fbd305484ac317dc9a11c062551962
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-22 13:15:05 +0000

    Add package player-performance-ratings/0.3/player-performance-ratings-0.3.tar.gz

---
+++ packages/player-performance-ratings/player-performance-ratings-0.3.tar.gz/player-performance-ratings-0.3/src/tuner/match_predicter_tuner.py
@@ -0,0 +1,78 @@
+import logging
+from typing import Optional
+
+import pandas as pd
+
+from src.tuner import StartRatingTuner
+from src.tuner.player_rating_tuner import PlayerRatingTuner
+from src.tuner.pre_transformer_tuner import PreTransformerTuner
+from src.predictor.match_predictor import MatchPredictor
+from src.ratings.data_prepararer import MatchGenerator
+
+from src.ratings.match_rating.team_rating_generator import TeamRatingGenerator
+from src.ratings.rating_generator import RatingGenerator
+logging.basicConfig(level = logging.INFO)
+
+class MatchPredictorTuner():
+
+    def __init__(self,
+                 pre_transformer_tuner: Optional[PreTransformerTuner] = None,
+                 start_rating_tuner: Optional[StartRatingTuner] = None,
+                 player_rating_tuner: Optional[PlayerRatingTuner] = None,
+                 fit_best: bool = True
+                 ):
+        self.pre_transformer_tuner = pre_transformer_tuner
+        self.start_rating_tuner = start_rating_tuner
+        self.player_rating_tuner = player_rating_tuner
+        self.fit_best = fit_best
+
+    def tune(self, df: pd.DataFrame) -> MatchPredictor:
+        if self.pre_transformer_tuner:
+            column_names = self.pre_transformer_tuner.column_names
+            match_predictor = self.pre_transformer_tuner.match_predictor
+
+        elif self.player_rating_tuner:
+            column_names = self.player_rating_tuner.match_predictor.column_names
+            match_predictor = self.player_rating_tuner.match_predictor
+        else:
+            column_names = self.start_rating_tuner.column_names
+            match_predictor = self.start_rating_tuner.match_predictor
+
+        if self.pre_transformer_tuner:
+            logging.info("Tuning PreTransformers")
+            best_pre_transformers = self.pre_transformer_tuner.tune(df)
+            for pre_rating_transformer in best_pre_transformers:
+                df = pre_rating_transformer.transform(df)
+            match_generator = MatchGenerator(column_names=column_names)
+            matches = match_generator.generate(df=df)
+            if self.player_rating_tuner:
+                self.player_rating_tuner.match_predictor.pre_rating_transformers = best_pre_transformers
+            if self.start_rating_tuner:
+                self.start_rating_tuner.match_predictor.pre_rating_transformers = best_pre_transformers
+        else:
+            best_pre_transformers = None
+            matches = None
+
+        if self.player_rating_tuner:
+            logging.info("Tuning Player Rating")
+            best_player_rating_generator = self.player_rating_tuner.tune(df, matches=matches)
+            if self.start_rating_tuner:
+                self.start_rating_tuner.match_predictor.rating_generator.team_rating_generator.player_rating_generator = best_player_rating_generator
+        else:
+            best_player_rating_generator = None
+
+        if self.start_rating_tuner:
+            logging.info("Tuning Start Rating")
+            best_start_rating = self.start_rating_tuner.tune(df, matches=matches)
+            if best_player_rating_generator:
+                best_player_rating_generator.start_rating_generator = best_start_rating
+
+        team_rating_generator = TeamRatingGenerator(player_rating_generator=best_player_rating_generator)
+        rating_generator = RatingGenerator(team_rating_generator=team_rating_generator)
+        best_match_predictor = MatchPredictor(column_names=column_names, rating_generator=rating_generator,
+                                              pre_rating_transformers=best_pre_transformers,
+                                              predictor=match_predictor.predictor, target=match_predictor.target)
+        if self.fit_best:
+            best_match_predictor.generate(df=df, matches=matches)
+
+        return best_match_predictor
---
+++ packages/player-performance-ratings/player-performance-ratings-0.3.tar.gz/player-performance-ratings-0.3/src/tuner/start_rating_tuner.py
@@ -0,0 +1,95 @@
+import copy
+import logging
+from typing import Optional, Any
+
+import optuna
+import pandas as pd
+from optuna.samplers import TPESampler
+from optuna.trial import BaseTrial
+
+from src.tuner.optimizer.start_rating_optimizer import StartLeagueRatingOptimizer
+from src.tuner.base_tuner import ParameterSearchRange, add_custom_hyperparams
+from src.predictor.match_predictor import MatchPredictor
+from src.ratings.data_structures import ColumnNames, Match
+from src.ratings.enums import RatingColumnNames
+from src.ratings.match_rating.start_rating_calculator import StartRatingGenerator
+from src.ratings.data_prepararer import MatchGenerator
+from src.scorer.base_score import BaseScorer, LogLossScorer
+
+logging.basicConfig(level=logging.INFO)
+
+RC = RatingColumnNames
+
+
+class StartRatingTuner():
+
+    def __init__(self,
+                 column_names: ColumnNames,
+                 match_predictor: MatchPredictor,
+                 search_ranges: Optional[list[ParameterSearchRange]] = None,
+                 start_rating_parameters: Optional[dict[str, Any]] = None,
+                 start_rating_optimizer: Optional[StartLeagueRatingOptimizer] = None,
+                 scorer: Optional[BaseScorer] = None,
+                 n_trials: int = 0,
+                 ):
+
+        self.start_rating_parameters = start_rating_parameters
+        rating_column_names = [RC.rating_difference, RC.player_rating_change, RC.player_league, RC.opponent_league]
+        self.start_rating_optimizer = start_rating_optimizer or StartLeagueRatingOptimizer(column_names=column_names,
+                                                                                           match_predictor=match_predictor)
+        self.n_trials = n_trials
+        self.match_predictor = match_predictor
+        self.search_ranges = search_ranges
+        self.scorer = scorer or LogLossScorer(target=self.match_predictor.predictor.target, weight_cross_league=5,
+                                              pred_column=self.match_predictor.predictor.pred_column)
+
+        self.column_names = column_names
+
+    def tune(self, df: pd.DataFrame, matches: Optional[list[Match]] = None) -> StartRatingGenerator:
+
+        def objective(trial: BaseTrial, df: pd.DataFrame, league_start_ratings: dict[str, float]) -> float:
+            params = self.start_rating_parameters or {}
+            if league_start_ratings != {}:
+                params['league_ratings'] = league_start_ratings
+
+            params = add_custom_hyperparams(params=params,
+                                            trial=trial,
+                                            parameter_search_range=self.search_ranges,
+                                            )
+            start_rating_generator = StartRatingGenerator(**params)
+            match_predictor = copy.deepcopy(self.match_predictor)
+            match_predictor.rating_generator.team_rating_generator.player_rating_generator.start_rating_generator = start_rating_generator
+            df = match_predictor.generate(df=df)
+            return self.scorer.score(df)
+
+        if matches is None:
+            match_generator = MatchGenerator(column_names=self.column_names)
+            matches = match_generator.generate(df=df)
+
+        if self.start_rating_optimizer:
+            optimized_league_ratings = self.start_rating_optimizer.optimize(df,
+                                                                            matches=matches)
+        else:
+            optimized_league_ratings = self.match_predictor.rating_generator.team_rating_generator.player_rating_generator.start_rating_generator.league_ratings
+
+        direction = "minimize"
+        study_name = "optuna_study"
+        optuna_seed = 12
+        sampler = TPESampler(seed=optuna_seed)
+        study = optuna.create_study(direction=direction, study_name=study_name, sampler=sampler)
+        callbacks = []
+        logging.info(f"start league ratings {optimized_league_ratings}")
+        study.optimize(lambda trial: objective(trial, df, league_start_ratings=optimized_league_ratings),
+                       n_trials=self.n_trials, callbacks=callbacks)
+
+        return StartRatingGenerator(league_ratings=optimized_league_ratings, **study.best_params)
+
+
+def _get_cross_league_matches(self, matches: list[Match]) -> list[Match]:
+    cross_league_matches = []
+    for match in matches:
+        if len(match.teams) == 2:
+            if match.teams[0].league != match.teams[1].league:
+                cross_league_matches.append(match)
+
+    return cross_league_matches
