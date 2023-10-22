66dd20f75c821a9d27018060ef19c80840df800a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-22 04:06:33 +0000

    Add package moddb/0.10.0/moddb-0.10.0-py3-none-any.whl

---
+++ packages/moddb/moddb-0.10.0-py3-none-any.whl/moddb/__init__.py
@@ -0,0 +1,11 @@
+import requests
+
+from .base import front_page, login, logout, parse_page, parse_results, rss, search, search_tags
+from .client import Client, Thread
+from .enums import *
+from .pages import *
+from .utils import BASE_URL, LOGGER, Object, get_page, request, soup
+
+SESSION = requests.Session()
+
+__version__ = "0.10.0"
