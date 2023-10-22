f2feca65bca5977d4b0be6704d1502fde0eee5e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-22 13:15:30 +0000

    Add package essence-nlp/1.0.4/essence_nlp-1.0.4.tar.gz

---
+++ packages/essence-nlp/essence_nlp-1.0.4.tar.gz/essence_nlp-1.0.4/example.py
@@ -0,0 +1,4 @@
+from essence_nlp.example import add_one
+
+result = add_one(1)
+print(result)
---
+++ packages/essence-nlp/essence_nlp-1.0.4.tar.gz/essence_nlp-1.0.4/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+from setuptools_rust import Binding, RustExtension
+
+setup(
+    name="essence_nlp",
+    version="1.0.4",
+    rust_extensions=[RustExtension("essence_nlp.essence", "src/essence_nlp/essence/Cargo.toml", binding=Binding.PyO3)],
+    packages=["essence"],
+    zip_safe=False,
+)
