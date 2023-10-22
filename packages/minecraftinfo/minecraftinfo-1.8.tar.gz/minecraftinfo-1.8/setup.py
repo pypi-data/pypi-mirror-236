e6337ad6d910de1b306c139eaf89155716afa63b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-22 13:15:41 +0000

    Add package minecraftinfo/1.8/minecraftinfo-1.8.tar.gz

---
+++ packages/minecraftinfo/minecraftinfo-1.8.tar.gz/minecraftinfo-1.8/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='minecraftinfo',
+    version="1.8",
+    description="Request information Minecraft JavaEdition and BedrockEdition",
+    author='naisu',
+    packages=find_packages(),
+    license='MIT'
+)
