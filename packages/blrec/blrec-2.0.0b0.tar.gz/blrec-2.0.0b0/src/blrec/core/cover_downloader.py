3d2252f8d087d34b6be252e1071a058365f1c644
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-22 14:07:22 +0000

    Add package blrec/2.0.0b0/blrec-2.0.0b0.tar.gz

---
+++ packages/blrec/blrec-2.0.0b0.tar.gz/blrec-2.0.0b0/src/blrec/core/cover_downloader.py
@@ -0,0 +1,109 @@
+import logging
+from enum import Enum
+from threading import Lock
+from typing import Set
+
+import aiofiles
+import aiohttp
+from tenacity import retry, stop_after_attempt, wait_fixed
+
+from blrec.bili.live import Live
+from blrec.event.event_emitter import EventEmitter, EventListener
+from blrec.exception import submit_exception
+from blrec.logging.room_id import aio_task_with_room_id
+from blrec.path import cover_path
+from blrec.utils.hash import sha1sum
+from blrec.utils.mixins import SwitchableMixin
+
+from .stream_recorder import StreamRecorder, StreamRecorderEventListener
+
+__all__ = 'CoverDownloader', 'CoverDownloaderEventListener'
+
+
+logger = logging.getLogger(__name__)
+
+
+class CoverDownloaderEventListener(EventListener):
+    async def on_cover_image_downloaded(self, path: str) -> None:
+        ...
+
+
+class CoverSaveStrategy(Enum):
+    DEFAULT = 'default'
+    DEDUP = 'dedup'
+
+    def __str__(self) -> str:
+        return self.value
+
+    # workaround for value serialization
+    def __repr__(self) -> str:
+        return str(self)
+
+
+class CoverDownloader(
+    EventEmitter[CoverDownloaderEventListener],
+    StreamRecorderEventListener,
+    SwitchableMixin,
+):
+    def __init__(
+        self,
+        live: Live,
+        stream_recorder: StreamRecorder,
+        *,
+        save_cover: bool = False,
+        cover_save_strategy: CoverSaveStrategy = CoverSaveStrategy.DEFAULT,
+    ) -> None:
+        super().__init__()
+        self._live = live
+        self._stream_recorder = stream_recorder
+        self._lock: Lock = Lock()
+        self._sha1_set: Set[str] = set()
+        self.save_cover = save_cover
+        self.cover_save_strategy = cover_save_strategy
+
+    def _do_enable(self) -> None:
+        self._sha1_set.clear()
+        self._stream_recorder.add_listener(self)
+        logger.debug('Enabled cover downloader')
+
+    def _do_disable(self) -> None:
+        self._stream_recorder.remove_listener(self)
+        logger.debug('Disabled cover downloader')
+
+    async def on_video_file_completed(self, video_path: str) -> None:
+        with self._lock:
+            if not self.save_cover:
+                return
+            await self._save_cover(video_path)
+
+    @aio_task_with_room_id
+    async def _save_cover(self, video_path: str) -> None:
+        try:
+            await self._live.update_room_info()
+            cover_url = self._live.room_info.cover
+            data = await self._fetch_cover(cover_url)
+            sha1 = sha1sum(data)
+            if (
+                self.cover_save_strategy == CoverSaveStrategy.DEDUP
+                and sha1 in self._sha1_set
+            ):
+                return
+            path = cover_path(video_path, ext=cover_url.rsplit('.', 1)[-1])
+            await self._save_file(path, data)
+            self._sha1_set.add(sha1)
+        except Exception as e:
+            logger.error(f'Failed to save cover image: {repr(e)}')
+            submit_exception(e)
+        else:
+            logger.info(f'Saved cover image: {path}')
+            await self._emit('cover_image_downloaded', path)
+
+    @retry(reraise=True, wait=wait_fixed(1), stop=stop_after_attempt(3))
+    async def _fetch_cover(self, url: str) -> bytes:
+        async with aiohttp.ClientSession(raise_for_status=True) as session:
+            async with session.get(url) as response:
+                return await response.read()
+
+    async def _save_file(self, path: str, data: bytes) -> None:
+        async with aiofiles.open(path, 'wb') as file:
+            await file.write(data)
---
+++ packages/blrec/blrec-2.0.0b0.tar.gz/blrec-2.0.0b0/src/blrec/core/raw_danmaku_dumper.py
@@ -0,0 +1,121 @@
+import asyncio
+import json
+import logging
+from contextlib import suppress
+from threading import Lock
+
+import aiofiles
+from aiofiles.threadpool.text import AsyncTextIOWrapper
+from tenacity import AsyncRetrying, retry_if_not_exception_type, stop_after_attempt
+
+from blrec.bili.live import Live
+from blrec.event.event_emitter import EventEmitter, EventListener
+from blrec.exception import exception_callback, submit_exception
+from blrec.logging.room_id import aio_task_with_room_id
+from blrec.path import raw_danmaku_path
+from blrec.utils.mixins import SwitchableMixin
+
+from .raw_danmaku_receiver import RawDanmakuReceiver
+from .stream_recorder import StreamRecorder, StreamRecorderEventListener
+
+__all__ = 'RawDanmakuDumper', 'RawDanmakuDumperEventListener'
+
+
+logger = logging.getLogger(__name__)
+
+
+class RawDanmakuDumperEventListener(EventListener):
+    async def on_raw_danmaku_file_created(self, path: str) -> None:
+        ...
+
+    async def on_raw_danmaku_file_completed(self, path: str) -> None:
+        ...
+
+
+class RawDanmakuDumper(
+    EventEmitter[RawDanmakuDumperEventListener],
+    StreamRecorderEventListener,
+    SwitchableMixin,
+):
+    def __init__(
+        self,
+        live: Live,
+        stream_recorder: StreamRecorder,
+        danmaku_receiver: RawDanmakuReceiver,
+    ) -> None:
+        super().__init__()
+        self._live = live  # @aio_task_with_room_id
+        self._stream_recorder = stream_recorder
+        self._receiver = danmaku_receiver
+        self._lock: Lock = Lock()
+
+    def _do_enable(self) -> None:
+        self._stream_recorder.add_listener(self)
+        logger.debug('Enabled raw danmaku dumper')
+
+    def _do_disable(self) -> None:
+        self._stream_recorder.remove_listener(self)
+        asyncio.create_task(self._stop_dumping())
+        logger.debug('Disabled raw danmaku dumper')
+
+    async def on_video_file_created(
+        self, video_path: str, record_start_time: int
+    ) -> None:
+        with self._lock:
+            self._path = raw_danmaku_path(video_path)
+            await self._stop_dumping()
+            self._start_dumping()
+
+    async def on_video_file_completed(self, video_path: str) -> None:
+        with self._lock:
+            await self._stop_dumping()
+
+    def _start_dumping(self) -> None:
+        self._create_dump_task()
+
+    async def _stop_dumping(self) -> None:
+        if hasattr(self, '_dump_task'):
+            await self._cancel_dump_task()
+            del self._dump_task  # type: ignore
+
+    def _create_dump_task(self) -> None:
+        self._dump_task = asyncio.create_task(self._do_dump())
+        self._dump_task.add_done_callback(exception_callback)
+
+    async def _cancel_dump_task(self) -> None:
+        self._dump_task.cancel()
+        with suppress(asyncio.CancelledError):
+            await self._dump_task
+
+    @aio_task_with_room_id
+    async def _do_dump(self) -> None:
+        logger.debug('Started dumping raw danmaku')
+        try:
+            async with aiofiles.open(self._path, 'wt', encoding='utf8') as f:
+                logger.info(f"Raw danmaku file created: '{self._path}'")
+                await self._emit('raw_danmaku_file_created', self._path)
+
+                async for attempt in AsyncRetrying(
+                    retry=retry_if_not_exception_type((asyncio.CancelledError)),
+                    stop=stop_after_attempt(3),
+                ):
+                    with attempt:
+                        try:
+                            await self._dumping_loop(f)
+                        except Exception as e:
+                            submit_exception(e)
+                            raise
+                while True:
+                    danmu = await self._receiver.get_raw_danmaku()
+                    json_string = json.dumps(danmu, ensure_ascii=False)
+                    await f.write(json_string + '\n')
+        finally:
+            logger.info(f"Raw danmaku file completed: '{self._path}'")
+            await self._emit('raw_danmaku_file_completed', self._path)
+            logger.debug('Stopped dumping raw danmaku')
+
+    async def _dumping_loop(self, file: AsyncTextIOWrapper) -> None:
+        while True:
+            danmu = await self._receiver.get_raw_danmaku()
+            json_string = json.dumps(danmu, ensure_ascii=False)
+            await file.write(json_string + '\n')
