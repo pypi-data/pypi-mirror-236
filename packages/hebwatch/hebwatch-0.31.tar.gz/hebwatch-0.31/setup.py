cbb69edd1d56e315bf4b5874f400b2b39cf8f7b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-22 04:06:15 +0000

    Add package hebwatch/0.31/hebwatch-0.31.tar.gz

---
+++ packages/hebwatch/hebwatch-0.31.tar.gz/hebwatch-0.31/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(name = 'hebwatch',
+      version = '0.31',
+      description = 'this library calculates the relative hour clock from the jewish talmud',
+      author = 'Tal Amar',
+      packages = ['hebwatch'],
+      zip_safe = False,
+      include_package_data=True,
+          install_requires=[
+        'suntime',
+        'pyyaml',
+    ]
+)
+
+
+# pypi-AgEIcHlwaS5vcmcCJDZiOTNkYmRlLTA4MTEtNGEyZC1hNDA2LTVlM2U2OWU0NWFlMgACEFsxLFsiaGVid2F0Y2giXV0AAixbMixbImJjMGY4Y2JiLWI0ZjAtNDc5ZC05MDU4LTY3OWRmMjNmNTVkMyJdXQAABiCqgOhgKOkZI5Pnohs7WjgIjTofo3sT4S5_g5pMssE-Ng
