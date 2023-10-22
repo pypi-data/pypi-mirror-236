d1fd90ceb28eb3694690c476e9e9a46a28404260
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-22 04:07:42 +0000

    Add package Scenario-Library/1.0.0/Scenario_Library-1.0.0.tar.gz

---
+++ packages/Scenario-Library/Scenario_Library-1.0.0.tar.gz/Scenario_Library-1.0.0/README.md
@@ -0,0 +1,2 @@
+# Scenario-Library
+for developing
---
+++ packages/Scenario-Library/Scenario_Library-1.0.0.tar.gz/Scenario_Library-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='Scenario_Library',
+    version='1.0.0',
+    description='A simple Python library',
+    author='Sina Sohrab',
+    author_email='sinasohrab84@gmail.com',
+    packages=[''],
+)
