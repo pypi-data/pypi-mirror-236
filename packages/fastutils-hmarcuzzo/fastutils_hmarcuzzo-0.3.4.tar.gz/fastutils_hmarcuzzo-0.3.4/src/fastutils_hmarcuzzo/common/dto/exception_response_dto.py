47e2eb58f74d068870b4f408882c45be8a8eae02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-22 04:06:11 +0000

    Add package fastutils-hmarcuzzo/0.3.4/fastutils_hmarcuzzo-0.3.4.tar.gz

---
+++ packages/fastutils-hmarcuzzo/fastutils_hmarcuzzo-0.3.4.tar.gz/fastutils_hmarcuzzo-0.3.4/src/fastutils_hmarcuzzo/common/dto/exception_response_dto.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+from typing import List
+
+from pydantic import BaseModel, Extra, Field
+
+
+class DetailResponseDto(BaseModel):
+    loc: List[str] = Field(title="Location")
+    msg: str = Field(title="Message")
+    type: str = Field(title="Error Type")
+
+
+class ExceptionResponseDto(BaseModel):
+    detail: List[DetailResponseDto]
+    status_code: int = 422
+    timestamp: datetime = Field(title="Timestamp of the Request")
+    path: str = Field(title="Request Path")
+    method: str = Field(title="Request Method")
+
+    class Config:
+        extra = Extra.forbid
