8b74423c954017d73cf6875c836f677e2fdadff1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-22 14:09:06 +0000

    Add package rocketdina/1.0.0/rocketdina-1.0.0.tar.gz

---
+++ packages/rocketdina/rocketdina-1.0.0.tar.gz/rocketdina-1.0.0/rocketdina/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+   name='rocket',
+   version='1.0.0',
+   author='dinaftc',
+   author_email='d.fettache@esi-sba.dz',
+   packages=['rocket'],
+   url='http://pypi.python.org/pypi/rocket/',
+   license='LICENSE.txt',
+   description='An awesome package that does something',
+   long_description=open('README.md').read(),
+   long_description_content_type='text/markdown',
+   
+)
---
+++ packages/rocketdina/rocketdina-1.0.0.tar.gz/rocketdina-1.0.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+   name='rocketdina',
+   version='1.0.0',
+   author='dinaftc',
+   author_email='d.fettache@esi-sba.dz',
+   packages=['rocketdina'],
+   url='http://pypi.python.org/pypi/rocketdina/',
+   license='LICENSE.txt',
+   description='An awesome package that does something',
+   long_description=open('README.md').read(),
+   long_description_content_type='text/markdown',
+   
+)
