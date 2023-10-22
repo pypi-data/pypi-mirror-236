b75bc1ad5a0053948abac5012304cfec8f2ef1e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-22 04:07:29 +0000

    Add package blackscholes/0.1.6/blackscholes-0.1.6-py3-none-any.whl

---
+++ packages/blackscholes/blackscholes-0.1.6-py3-none-any.whl/blackscholes/__init__.py
@@ -0,0 +1,33 @@
+from .call import Black76Call, BlackScholesCall
+from .put import Black76Put, BlackScholesPut
+from .straddle import BlackScholesStraddleLong, BlackScholesStraddleShort
+from .strangle import BlackScholesStrangleLong, BlackScholesStrangleShort
+from .butterfly import BlackScholesButterflyLong, BlackScholesButterflyShort
+from .iron_condor import BlackScholesIronCondorLong, BlackScholesIronCondorShort
+from .spread import (BlackScholesBullSpread, BlackScholesBearSpread, 
+                     BlackScholesCalendarCallSpread, BlackScholesCalendarPutSpread) 
+from .iron_butterfly import (
+    BlackScholesIronButterflyLong,
+    BlackScholesIronButterflyShort,
+)
+
+__all__ = [
+    "BlackScholesCall",
+    "Black76Call",
+    "BlackScholesPut",
+    "Black76Put",
+    "BlackScholesStraddleLong",
+    "BlackScholesStraddleShort",
+    "BlackScholesStrangleLong",
+    "BlackScholesStrangleShort",
+    "BlackScholesButterflyLong",
+    "BlackScholesButterflyShort",
+    "BlackScholesIronCondorLong",
+    "BlackScholesIronCondorShort",
+    "BlackScholesBullSpread",
+    "BlackScholesBearSpread",
+    "BlackScholesCalendarCallSpread",
+    "BlackScholesCalendarPutSpread",
+    "BlackScholesIronButterflyLong",
+    "BlackScholesIronButterflyShort",
+]
