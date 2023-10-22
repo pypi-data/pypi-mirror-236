abedb791c23522f949d236b57f536636029fd163
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-22 04:07:33 +0000

    Add package pygame-text-centering/0.20/pygame-text-centering-0.20.tar.gz

---
+++ packages/pygame-text-centering/pygame-text-centering-0.20.tar.gz/pygame-text-centering-0.20/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pygame-text-centering',
+    version='0.20',
+    packages=find_packages(),
+    package_data={'pygamecentering': ['*.json']},
+    install_requires=[
+        'pygame', 'numpy', 'joblib', 'scipy', 'threadpoolctl', 'scikit-learn'
+    ],
+    author='Melvin Chen',
+    author_email='melvinchen610@gmail.com',
+    description="A Pygame library to fix Pygame's text-centering!",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/mchen610/pygame-text-centering',
+)
