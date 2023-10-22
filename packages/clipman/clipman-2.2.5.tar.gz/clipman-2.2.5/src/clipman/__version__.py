3cbc1fdc771f37f905d39c74e9811d20f9c5019b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-22 04:06:12 +0000

    Add package clipman/2.2.5/clipman-2.2.5.tar.gz

---
+++ packages/clipman/clipman-2.2.5.tar.gz/clipman-2.2.5/src/clipman/__version__.py
@@ -0,0 +1,7 @@
+"""
+Created for storing version number.
+The version number will be placed here based on the tag in Github using the tags_marker.py script.
+
+Having "2.2.5" in final releases is NOT allowed.
+"""
+__version__="2.2.5"
