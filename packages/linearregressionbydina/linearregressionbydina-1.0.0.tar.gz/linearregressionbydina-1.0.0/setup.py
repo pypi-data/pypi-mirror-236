18a025f314666e45726ccd868bd79f97e8cb093e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-22 14:09:41 +0000

    Add package linearregressionbydina/1.0.0/linearregressionbydina-1.0.0.tar.gz

---
+++ packages/linearregressionbydina/linearregressionbydina-1.0.0.tar.gz/linearregressionbydina-1.0.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+   name='linearregressionbydina',
+   version='1.0.0',
+   author='dinaftc',
+   author_email='d.fettache@esi-sba.dz',
+   packages=['linearregressionbydina'],
+   url='http://pypi.python.org/pypi/linearregressionbydina/',
+   license='LICENSE.txt',
+   description='An awesome package that does something',
+   long_description=open('README.md').read(),
+   long_description_content_type="text/markdown",
+   install_requires=['linearregressionbydina']
+)
