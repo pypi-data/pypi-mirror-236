85ba7fa4f54d7f0aa9efdc3433b15a4fa365df74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-22 14:07:24 +0000

    Add package jaxlob/0.13/jaxlob-0.13.tar.gz

---
+++ packages/jaxlob/jaxlob-0.13.tar.gz/jaxlob-0.13/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+
+# Read the content of README.md into long_description
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='jaxlob',
+    version='0.13',
+    packages=find_packages(),
+    install_requires=[
+        'setuptools',
+        'pandas',
+        'numpy',
+        'scipy',
+        'six',
+        'wheel',
+        'jax[cuda]==0.4.11',
+        'jaxlib==0.4.11',
+        'distrax',
+        'brax',
+        'chex',
+        'flax',
+        'optax',
+        'gym',
+        'notebook',
+        'matplotlib',
+        'tqdm',
+        'gymnax',
+        'jupyter',
+        'ipython',
+        'wandb'
+    ],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+)
+
