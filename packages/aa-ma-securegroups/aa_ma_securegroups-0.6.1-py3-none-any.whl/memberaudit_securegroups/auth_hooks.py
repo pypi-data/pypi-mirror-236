4de49a2e31f136cac9f06d078207145b63b3c0fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-22 13:15:15 +0000

    Add package aa-ma-securegroups/0.6.1/aa_ma_securegroups-0.6.1-py3-none-any.whl

---
+++ packages/aa-ma-securegroups/aa_ma_securegroups-0.6.1-py3-none-any.whl/memberaudit_securegroups/auth_hooks.py
@@ -0,0 +1,37 @@
+"""
+Hook into Alliance Auth
+"""
+
+# Alliance Auth
+from allianceauth import hooks
+
+# Memberaudit Securegroups
+from memberaudit_securegroups.models import (
+    ActivityFilter,
+    AgeFilter,
+    AssetFilter,
+    ComplianceFilter,
+    CorporationRoleFilter,
+    SkillPointFilter,
+    SkillSetFilter,
+)
+
+
+@hooks.register("secure_group_filters")
+def filters():
+    """
+    Secure group filter
+
+    :return:
+    :rtype:
+    """
+
+    return [
+        ActivityFilter,
+        AgeFilter,
+        AssetFilter,
+        ComplianceFilter,
+        CorporationRoleFilter,
+        SkillPointFilter,
+        SkillSetFilter,
+    ]
