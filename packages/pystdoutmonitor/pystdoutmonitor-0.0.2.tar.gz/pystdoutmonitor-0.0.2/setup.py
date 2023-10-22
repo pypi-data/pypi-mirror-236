d51d88c2500c8fabed05402ef4059ce09534abf7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-22 04:08:36 +0000

    Add package pystdoutmonitor/0.0.2/pystdoutmonitor-0.0.2.tar.gz

---
+++ packages/pystdoutmonitor/pystdoutmonitor-0.0.2.tar.gz/pystdoutmonitor-0.0.2/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='pystdoutmonitor',
+      version='0.0.2',
+      description='Launches a distinct thread and calls a user-defined callback each time it captures an output.',
+      packages=['stdoutmonitor'],
+      author_email='sevaserg@gmail.com',
+      zip_safe=False)
---
+++ packages/pystdoutmonitor/pystdoutmonitor-0.0.2.tar.gz/pystdoutmonitor-0.0.2/stdoutmonitor/__init__.py
@@ -0,0 +1,1 @@
+from stdoutmonitor import StdoutMonitorProcess
