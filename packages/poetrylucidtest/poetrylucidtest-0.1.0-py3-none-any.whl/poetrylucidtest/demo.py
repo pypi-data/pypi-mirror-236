7b9f3c5be4d794c30d96c34f036a761988882282
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-22 04:07:51 +0000

    Add package poetrylucidtest/0.1.0/poetrylucidtest-0.1.0-py3-none-any.whl

---
+++ packages/poetrylucidtest/poetrylucidtest-0.1.0-py3-none-any.whl/poetrylucidtest/demo.py
@@ -0,0 +1,21 @@
+import pyglet
+
+# w = pyglet.window.Window(300, 300)
+# w.set_location(w.screen.width//2-w.width//2, w.screen.height//2-w.height//2)
+
+# pyglet.app.run() 
+
+
+def hello_world():
+    print("Hello World from Gang Huang")
+
+
+
+def add(a_num, b_num):
+    """Adds two numbers."""
+    return a_num + b_num
+
+
+def sub(a_num, b_num):
+    """Subtract two numbers."""
+    return a_num - b_num
