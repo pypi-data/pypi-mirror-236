6d4191988ba7cd34d460ed2ccb6ef97a92525772
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-22 03:11:38 +0000

    Add package drive-bot-proto/0.0.9/drive_bot_proto-0.0.9.tar.gz

---
+++ packages/drive-bot-proto/drive_bot_proto-0.0.9.tar.gz/drive_bot_proto-0.0.9/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+
+setup(
+      name='drive_bot_proto',
+      version='0.0.9',
+      description='GRPC client for drive_bot_proto',
+      author='Pavel Anokhin',
+      author_email='p.a.anokhin@gmail.com',
+      packages=['drive_bot_proto'],
+      package_data={
+          'drive_bot_proto': ['*.pyi', 'py.typed'],
+      },
+      include_package_data=True,
+)
