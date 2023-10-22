48463fc5da1f6b7bc3a9fd5c6ed060816bb48e76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-22 13:15:25 +0000

    Add package linear-regression-tp/1.0.0/linear_regression_tp-1.0.0.tar.gz

---
+++ packages/linear-regression-tp/linear_regression_tp-1.0.0.tar.gz/linear_regression_tp-1.0.0/README.md
@@ -0,0 +1,29 @@
+# rocket
+
+`rockettp` is a Python library for ..........
+
+## Installation
+
+Use the package manager [pip](https://pip.pypa.io/en/stable/) to install `rockettp`.
+
+```bash
+pip install rockettp
+```
+
+## Usage
+
+```python
+import rockettp
+
+# returns 'words'
+rockettp.module_name('word')
+
+# returns 'geese'
+rockettp.module_name('goose')
+
+# returns 'phenomenon'
+rockettp.module_name('phenomena')
+```
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ packages/linear-regression-tp/linear_regression_tp-1.0.0.tar.gz/linear_regression_tp-1.0.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+   name='linear_regression_tp',
+   version='1.0.0',
+   author='Mokadem Adel Abdelkader',
+   author_email='aa.mokadem@esi-sba.dz',
+   packages=['linear_regression_tp'],
+   url='http://pypi.python.org/pypi/linear_regression_tp/',
+   license='LICENSE.txt',
+   description='An awesome package that does something',
+   long_description=open('README.md').read(),
+   long_description_content_type="text/markdown",
+   install_requires=['linear_regression_tp']
+)
