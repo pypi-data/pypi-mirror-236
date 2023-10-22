31a9f33cf13912a321cbeafe26a8e593bb4e16db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-22 13:15:25 +0000

    Add package android-xml-translater/1.0.3/android-xml-translater-1.0.3.tar.gz

---
+++ packages/android-xml-translater/android-xml-translater-1.0.3.tar.gz/android-xml-translater-1.0.3/README.md
@@ -0,0 +1,39 @@
+# android-xml-translater
+
+Inspired by [GTranslate-strings-xml](https://github.com/Ra-Na/GTranslate-strings-xml)
+
+[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)
+
+## Dependencies
+
+- [deep-translator](https://github.com/nidhaloff/deep-translator)
+- [beautifulsoup4](https://www.crummy.com/software/BeautifulSoup/bs4/doc/)
+- [lxml](https://lxml.de/)
+
+```shell
+pip install android-xml-translater
+```
+
+OR
+
+```shell
+git clone https://github.com/Harkame/android-xml-translater.git
+
+cd android-xml-translater
+
+pip install .
+```
+
+## Usage
+
+#### Basic usage
+
+```shell
+android-xml-translater -t fr es de
+```
+
+For more informations
+
+```shell
+android-xml-translater -h
+```
---
+++ packages/android-xml-translater/android-xml-translater-1.0.3.tar.gz/android-xml-translater-1.0.3/android_xml_translater/arguments_helper.py
@@ -0,0 +1,66 @@
+import argparse
+import os
+
+try:  # Normal way
+    from android_xml_translater import DEFAULT_INPUT_FILE, DEFAULT_DESTINATION, DEFAULT_SOURCE
+except ImportError:  # For main tests
+    from .axt import DEFAULT_INPUT_FILE, DEFAULT_DESTINATION, DEFAULT_SOURCE
+
+try:  # Normal way
+    from __version__ import __version__
+except ImportError:  # For tests
+    from .__version__ import __version__
+
+
+def get_arguments(arguments):
+    parser = argparse.ArgumentParser(
+        description="Script to translate Android strings.xml",
+        formatter_class=argparse.RawTextHelpFormatter,
+    )
+
+    parser.add_argument(
+        "-f",
+        "--file",
+        help="Input file" + os.linesep +
+             "example : android-xml-translater -f C:\\strings.xml" + os.linesep +
+             "(default: %(default)s)",
+        default=DEFAULT_INPUT_FILE
+    )
+
+    parser.add_argument(
+        "-s",
+        "--source",
+        help="Input language" + os.linesep +
+             "example : google-images-downloader -s en" + os.linesep +
+             "(default: %(default)s)",
+        default=DEFAULT_SOURCE
+    )
+
+    parser.add_argument(
+        "-t",
+        "--target",
+        help="Target languages" + os.linesep +
+             "example : android-xml-translater -t fr es de",
+        nargs='+',
+        required=True
+    )
+
+    parser.add_argument(
+        "-d",
+        "--destination",
+        help="Translations destination" + os.linesep +
+             "example : android-xml-translater -d C\\my\\destination" + os.linesep +
+             "(default: %(default)s)",
+        default=DEFAULT_DESTINATION
+    )
+
+    parser.add_argument(
+        "-v",
+        "--version",
+        help="Show android-xml-translater version",
+        action="version"
+    )
+
+    parser.version = __version__
+
+    return parser.parse_args(arguments)
