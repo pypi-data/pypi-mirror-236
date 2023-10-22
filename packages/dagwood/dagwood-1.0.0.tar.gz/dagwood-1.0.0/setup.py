46625994a58316b077e8a2c8febce6f3c2ab4c17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-22 04:08:00 +0000

    Add package dagwood/1.0.0/dagwood-1.0.0.tar.gz

---
+++ packages/dagwood/dagwood-1.0.0.tar.gz/dagwood-1.0.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='dagwood',
+    version='1.0.0',
+    packages=find_packages(),
+    install_requires=[
+        # any future dependencies
+    ],
+    author='Odai Athamneh',
+    author_email='heyodai@gmail.com',
+    description='Simple logging tool. Write to console and to file in one step.'
+)
