3236faaf0209454fcbb388b24d53db709d50d9cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-22 03:13:25 +0000

    Add package lorenzpy/0.0.2/lorenzpy-0.0.2-py3-none-any.whl

---
+++ packages/lorenzpy/lorenzpy-0.0.2-py3-none-any.whl/lorenzpy/__init__.py
@@ -0,0 +1,11 @@
+"""Python package to simulate and Measure chaotic time series.
+
+Modules exported by this package:
+
+- `simulations`: Simulate various discrete and continuous chaotic dynamical systems.
+- `measures`: Measures for the chaotic dynamical systems.
+"""
+
+from . import measures, simulations
+
+__version__ = "0.0.2"
