050d2b1f89d4787496ba5550aa04983c804d81e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-22 03:12:45 +0000

    Add package phoneinfopy/1.0.0.11/phoneinfopy-1.0.0.11.tar.gz

---
+++ packages/phoneinfopy/phoneinfopy-1.0.0.11.tar.gz/phoneinfopy-1.0.0.11/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+
+VERSION = '1.0.0.11'
+DESCRIPTION = "phoneinfopy is a Python package that allow you to get respones from truecaller APIs. You can able to do login, OTP verification, and phone number search using Truecaller API with this package."
+
+# Setting up
+setup(
+    name="phoneinfopy",
+    version=VERSION,
+    author="Mr_3rr0r_501 [yuvaraj]",
+    author_email="<yuvarajucet@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=['requests'],
+    keywords=['truecaller','phoneinfo', 'numberinfo', 'phonenumberinfo'],
+	classifiers = [
+	    "Topic :: Utilities",
+	    "Natural Language :: English",
+	    "Operating System :: OS Independent",
+	    "Programming Language :: Python :: 3",
+	]
+)
