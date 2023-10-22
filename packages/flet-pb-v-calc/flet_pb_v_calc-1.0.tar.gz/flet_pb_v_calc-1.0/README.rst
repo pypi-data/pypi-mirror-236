caa0f9b1a32bad9cc9cac47dcae68e72fe152f7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-22 03:11:57 +0000

    Add package flet-pb-v-calc/1.0/flet_pb_v_calc-1.0.tar.gz

---
+++ packages/flet-pb-v-calc/flet_pb_v_calc-1.0.tar.gz/flet_pb_v_calc-1.0/README.rst
@@ -0,0 +1,8 @@
+====================================================
+flet_pb_v_calc - Flet Progress Bar Value Calculator.
+====================================================
+.. code :: python
+
+    from flet_pb_v_calc import calculate_value
+
+    progress_bar_value = calculate_value(50, 100, 0) # Current progress bar value, maximal progress bar value, minimal progress bar value. Returns 0.5.
---
+++ packages/flet-pb-v-calc/flet_pb_v_calc-1.0.tar.gz/flet_pb_v_calc-1.0/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+setup(
+    name='flet_pb_v_calc',
+
+    packages=['flet_pb_v_calc'],
+
+    version='1.0',
+
+    license='MIT',
+
+    description='Flet Progress Bar Value Calculator.',
+
+    long_description_content_type='text/x-rst',
+    long_description=open('README.rst', 'r').read(),
+
+    author='Ivan Perzhinsky.',
+    author_email='name1not1found.com@gmail.com',
+
+    url='https://github.com/xzripper/flet_pb_v_calc',
+    download_url='https://github.com/xzripper/flet_pb_v_calc/archive/refs/tags/v1.0.tar.gz',
+
+    keywords = ['utility'],
+
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Topic :: Utilities',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3'
+    ]
+)
