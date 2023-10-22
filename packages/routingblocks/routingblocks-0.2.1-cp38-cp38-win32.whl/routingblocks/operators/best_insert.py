dbcaa08d230c3bdfeca8071ae95d0daf1026bd52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-22 04:06:12 +0000

    Add package routingblocks/0.2.1/routingblocks-0.2.1-cp38-cp38-win32.whl

---
+++ packages/routingblocks/routingblocks-0.2.1-cp38-cp38-win32.whl/routingblocks/operators/best_insert.py
@@ -0,0 +1,51 @@
+from typing import List, Iterable, Callable, TypeVar
+
+import routingblocks
+
+from .move_selectors import MoveSelector
+
+
+class BestInsertionOperator(routingblocks.RepairOperator):
+    """
+    Iteratively (one at a time) inserts vertices according to the cost incurred from inserting them.
+
+    The operator uses a :class:`routingblocks.operators.MoveSelector[routingblocks.InsertionMove]` to choose the
+    next vertex to insert. This selector receives as argument a list of :class:`routingblocks.InsertionMove` objects,
+    each one representing a possible location at which the next vertex can be inserted, ordered by cost in descending order.
+
+    This allows to customize the operator to different insertion strategies, such as inserting the vertex at the location
+    with the lowest cost, inserting the vertex at the location with the highest cost, or introducing randomness in the
+    selection process.
+    """
+
+    def __init__(self, instance: routingblocks.Instance, move_selector: MoveSelector[routingblocks.InsertionMove]):
+        """
+        :param instance: The problem instance
+        :param move_selector: The move selector used to choose the next insertion position
+        """
+        routingblocks.RepairOperator.__init__(self)
+        self._instance = instance
+        self._move_cache = routingblocks.InsertionCache(self._instance)
+        # Exposed
+        self.move_selector = move_selector
+
+    def apply(self, evaluation: routingblocks.Evaluation, solution: routingblocks.Solution,
+              vertex_ids: Iterable[int]) -> None:
+        vertex_ids = [x for x in vertex_ids if not self._instance.get_vertex(x).is_station]
+        self._move_cache.rebuild(evaluation, solution, vertex_ids)
+        for vertex_id in vertex_ids:
+            best_insertion = self.move_selector(self._move_cache.get_best_insertions_for_vertex(vertex_id))
+            best_route = best_insertion.after_node.route
+            # Stop tracking
+            self._move_cache.stop_tracking(vertex_id)
+            # Insert the vertex
+            solution.insert_vertex_after(best_insertion.after_node, vertex_id)
+            # Update the cache
+            self._move_cache.invalidate_route(solution[best_route],
+                                              best_route)
+
+    def name(self) -> str:
+        return "BestInsertionOperator"
+
+    def can_apply_to(self, solution: routingblocks.Solution) -> bool:
+        return True
---
+++ packages/routingblocks/routingblocks-0.2.1-cp38-cp38-win32.whl/routingblocks/operators/station_vicinity_removal.py
@@ -0,0 +1,73 @@
+from __future__ import annotations
+from typing import List, Callable, Iterable, Optional, Tuple, Union
+
+import routingblocks
+from .cluster_removal import ClusterRemovalOperator, DistanceBasedClusterMemberSelector
+
+
+class StationSeedSelector:
+    def __init__(self, stations: List[routingblocks.Vertex], randgen: Optional[routingblocks.Randgen] = None):
+        self._stations = stations
+        self._randgen = randgen
+
+    def _get_station_locations(self, solution: routingblocks.Solution):
+        return [
+            y for x in self._stations for y in solution.find(x.vertex_id)
+        ]
+
+    def __call__(self, evaluation: routingblocks.Evaluation, solution: routingblocks.Solution,
+                 removed_vertices: List[routingblocks.NodeLocation]) -> routingblocks.NodeLocation:
+        # Get all stations in the solution not removed yet
+        stations = [x for x in self._get_station_locations(solution) if
+                    x not in removed_vertices]
+        if len(stations) == 0:
+            raise StopIteration
+        # Return a random one
+        picked_station_location = stations[self._randgen.randint(0, len(stations) - 1)]
+        return picked_station_location
+
+
+class StationVicinityRemovalOperator(routingblocks.DestroyOperator):
+    """
+    Station vicinity removal is a specialized cluster removal operator designed to reorder customer visits in the
+    vicinity of replenishment stations. The operator defines the vicinity of a station by selecting a random radius
+    based on a percentage of the maximum distance between vertices. It then randomly chooses a recharging station
+    and removes the station along with vertices within the selected radius, repeating this process until the
+    desired number of vertices are removed.
+    """
+
+    def __init__(self, instance: routingblocks.Instance,
+                 get_distance: Callable[[routingblocks.Vertex, routingblocks.Vertex], float],
+                 min_radius_factor: float, max_radius_factor: float,
+                 randgen: Optional[routingblocks.Randgen]):
+        """
+        :param instance: Instance the operator will be applied to
+        :param get_distance: A function taking two vertices and returning the distance between them. The distance can be arbitrary, i.e., does not have to correspond to the evaluation function.
+        :param min_radius_factor: Minimum of the interval the radius is picked from
+        :param max_radius_factor: Maximum of the interval the radius is picked from
+        :param randgen: Random number generator
+        """
+        routingblocks.DestroyOperator.__init__(self)
+        self._cluster_removal_operator = ClusterRemovalOperator(
+            seed_selector=StationSeedSelector(list(instance.stations), randgen),
+            cluster_member_selector=DistanceBasedClusterMemberSelector(
+                vertices=[*instance.stations, *instance.customers],
+                get_distance=get_distance,
+                min_radius_factor=min_radius_factor,
+                max_radius_factor=max_radius_factor,
+                randgen=randgen)
+        )
+
+    def can_apply_to(self, solution: routingblocks.Solution) -> bool:
+        """
+        Station vicinity removal can be applied to any solution that contains at least one station.
+        """
+        return any(node.vertex.is_station
+                   for route in solution.routes for node in route)
+
+    def name(self) -> str:
+        return "StationVicinityRemovalOperator"
+
+    def apply(self, evaluation: routingblocks.Evaluation, solution: routingblocks.Solution,
+              number_of_removed_vertices: int) -> List[int]:
+        return self._cluster_removal_operator.apply(evaluation, solution, number_of_removed_vertices)
