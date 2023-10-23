3238e0fed93a7c064abc43465311912cef4771e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-23 15:06:55 +0000

    Add package promptmodel/0.0.8/promptmodel-0.0.8.tar.gz

---
+++ packages/promptmodel/promptmodel-0.0.8.tar.gz/promptmodel-0.0.8/promptmodel/database/orm.py
@@ -0,0 +1,34 @@
+import os 
+
+from .models import (
+    LLMModule,
+    LLMModuleVersion,
+    Prompt,
+    RunLog,
+    SampleInputs,
+    DeployedLLMModule,
+    DeployedLLMModuleVersion,
+    DeployedPrompt
+)
+from .config import db
+
+
+def initialize_db():
+    if not os.path.exists("./."):
+        os.mkdir("./.")
+    db.connect()
+    with db.atomic():
+        if not LLMModule.table_exists():
+            db.create_tables(
+                [
+                    LLMModule,
+                    LLMModuleVersion,
+                    Prompt,
+                    RunLog,
+                    SampleInputs,
+                    DeployedLLMModule,
+                    DeployedLLMModuleVersion,
+                    DeployedPrompt
+                    ]
+                )
+    db.close()
