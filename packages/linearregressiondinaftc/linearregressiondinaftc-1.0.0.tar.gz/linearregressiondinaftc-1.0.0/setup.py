f0fd524d659c5b5cad549929abd36821aaaba16a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-23 03:11:54 +0000

    Add package linearregressiondinaftc/1.0.0/linearregressiondinaftc-1.0.0.tar.gz

---
+++ packages/linearregressiondinaftc/linearregressiondinaftc-1.0.0.tar.gz/linearregressiondinaftc-1.0.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+   name='linearregressiondinaftc',
+   version='1.0.0',
+   author='dinaftc',
+   author_email='d.fettache@esi-sba.dz',
+   packages=['linearregressiondinaftc'],
+   url='http://pypi.python.org/pypi/linearregressiondinaftc/',
+   license='LICENSE.txt',
+   description='An awesome package that does something',
+   long_description=open('README.md').read(),
+   long_description_content_type="text/markdown",
+   install_requires=['linearregressiondinaftc']
+)
