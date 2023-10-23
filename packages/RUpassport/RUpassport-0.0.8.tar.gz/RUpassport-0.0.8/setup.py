63bf428335ee03ec6416a610f166d0b33f536cbf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-23 04:09:00 +0000

    Add package RUpassport/0.0.8/RUpassport-0.0.8.tar.gz

---
+++ packages/RUpassport/RUpassport-0.0.8.tar.gz/RUpassport-0.0.8/setup.py
@@ -0,0 +1,33 @@
+import os
+from setuptools import setup, find_packages
+
+def readme():
+    with open('README.md', 'r') as f:
+        return f.read()
+
+# Get the path of the calling script
+calling_script_dir = os.path.dirname(os.path.abspath(__file__))
+
+setup(
+    name='RUpassport',
+    version='0.0.8',
+    author='@GusGus153',
+    author_email='dimons2006@yandex.ru',
+    description='Recognizes data from Russian passports and returns them',
+    long_description=readme(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/Filin153/RUpassport',
+    packages=find_packages(),
+    install_requires=["ultralytics==8.0.200", "easyocr==1.7.1", "opencv-python==4.8.1.78"],
+    classifiers=[
+        'Programming Language :: Python :: 3.9',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent'
+    ],
+    keywords='ru rus RU RUS passport RUpassport recognition ',
+    project_urls={
+        'GitHub': 'https://github.com/Filin153/RUpassport'
+    },
+    python_requires='>=3.6',
+    package_data={'': [os.path.join(calling_script_dir, 'pasport_model.pt')]},  # Include the specified file
+)
