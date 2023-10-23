6e5de2fb0f232789a9b4c3189640408d0354662a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-23 04:09:07 +0000

    Add package bpp-iplweb/202310.1120/bpp_iplweb-202310.1120-py3-none-any.whl

---
+++ packages/bpp-iplweb/bpp_iplweb-202310.1120-py3-none-any.whl/pbn_api/models/__init__.py
@@ -0,0 +1,14 @@
+from .conference import Conference  # noqa
+from .country import Country  # noqa
+from .discipline import Discipline  # noqa
+from .institution import (  # noqa
+    Institution,
+    OswiadczenieInstytucji,
+    PublikacjaInstytucji,
+)
+from .journal import Journal  # noqa
+from .language import Language  # noqa
+from .publication import Publication  # noqa
+from .publisher import Publisher  # noqa
+from .scientist import Scientist  # noqa
+from .sentdata import SentData  # noqa
