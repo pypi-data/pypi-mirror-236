3e8bfd059f1fa16be3b4ac3dfd631b3526311edb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-23 04:09:58 +0000

    Add package codemodpydanticv1/0.1.3/codemodpydanticv1-0.1.3.tar.gz

---
+++ packages/codemodpydanticv1/codemodpydanticv1-0.1.3.tar.gz/codemodpydanticv1-0.1.3/codemodpydanticv1/cli.py
@@ -0,0 +1,18 @@
+import typer
+
+from codemodpydanticv1.codemodpydanticv1 import transform_code
+
+cli = typer.Typer()
+
+
+@cli.command()
+def transform(
+    file_path: str,
+):
+    with open(file_path) as f:
+        code = f.read()
+
+    transformed_code = transform_code(code)
+
+    with open(file_path, "w") as f:
+        f.write(transformed_code)
