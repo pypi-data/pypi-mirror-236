84b2fb77a792158522d90182dd1f51ddb4add1bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-23 16:08:34 +0000

    Add package scrapy-redis-expand/0.0.1/scrapy_redis_expand-0.0.1-py3-none-any.whl

---
+++ packages/scrapy-redis-expand/scrapy_redis_expand-0.0.1-py3-none-any.whl/scrapy_redis_expand/RedisPipeline.py
@@ -0,0 +1,13 @@
+import sys
+sys.path.append('.')
+
+
+class RedisPipeline(object):
+    __REDIS_SETTING_MAPS__ = {
+        'REDIS_HOST': 'host',
+        'REDIS_PORT': 'port',
+        'REDIS_DB': 'db'
+    }
+
+    def __init__(self):
+        print(settings)
