28f428af0a50579f7580c5e6bf8b872b5fad2815
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-23 04:08:07 +0000

    Add package tempered/0.2.1/tempered-0.2.1-py2.py3-none-any.whl

---
+++ packages/tempered/tempered-0.2.1-py2.py3-none-any.whl/tempered/__init__.py
@@ -0,0 +1,14 @@
+"Generate native python functions from HTML templates"
+__version__ = "0.2.1"
+from .main import (
+    add_template, add_template_folder,
+    register_type, build, load,
+)
+
+__all__ = [
+    "add_template",
+    "add_template_folder",
+    "register_type",
+    "build",
+    "load",
+]
