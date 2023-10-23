bc2e873f3a295a368043cde671dcf452a2023141
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-23 14:09:34 +0000

    Add package lark-oapi/1.0.24/lark_oapi-1.0.24-py3-none-any.whl

---
+++ packages/lark-oapi/lark_oapi-1.0.24-py3-none-any.whl/lark_oapi/api/task/v2/resource/__init__.py
@@ -0,0 +1,7 @@
+from .comment import *
+from .custom_field import *
+from .custom_field_option import *
+from .section import *
+from .task import *
+from .task_subtask import *
+from .tasklist import *
