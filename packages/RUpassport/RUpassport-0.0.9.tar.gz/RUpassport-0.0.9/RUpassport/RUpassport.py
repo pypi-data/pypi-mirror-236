21392c8933d174dde02cccfb7c87657944900fac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-23 04:09:04 +0000

    Add package RUpassport/0.0.9/RUpassport-0.0.9.tar.gz

---
+++ packages/RUpassport/RUpassport-0.0.9.tar.gz/RUpassport-0.0.9/RUpassport/RUpassport.py
@@ -0,0 +1,38 @@
+import os
+from ultralytics import YOLO
+import easyocr
+import shutil
+import cv2
+
+class Pasport:
+    def __init__(self):
+        self.__reader = easyocr.Reader(['ru'])
+        module_dir = os.path.dirname(__file__)
+        model_file_path = os.path.join(module_dir, 'pasport_model.pt')
+        self.__model = YOLO(model_file_path)
+
+    def recognize_pasport(self, img: str, id: str):
+        """
+        :param img: path to passport img
+        :param id: folder id (random value)
+        :return: info from passport
+        """
+        self.__model(img, retina_masks=True, save_crop=True, project="file", name=id)
+
+        info = {}
+        for dir in os.listdir(f"file/{id}/crops"):
+            if dir != "pass-ort":
+                info[dir] = f"file/{id}/crops/{dir}/" + os.listdir(f"file/123/crops/{dir}/")[0]
+
+        img = cv2.imread(info["num"])
+        img = cv2.rotate(img, cv2.ROTATE_90_COUNTERCLOCKWISE)
+        cv2.imwrite(info["num"], img)
+        img = cv2.imread(info["ser"])
+        img = cv2.rotate(img, cv2.ROTATE_90_COUNTERCLOCKWISE)
+        cv2.imwrite(info["ser"], img)
+
+        for i in info.keys():
+            info[i] = "".join(self.__reader.readtext(info[i], detail=0)).lower().capitalize()
+
+        shutil.rmtree(f"file/{id}")
+        return info
