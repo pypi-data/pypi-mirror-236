97a4c7ee114f33a962cc5c66b4468f69c558a562
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-23 17:09:40 +0000

    Add package outsystems-pipeline/0.7.0/outsystems-pipeline-0.7.0.tar.gz

---
+++ packages/outsystems-pipeline/outsystems-pipeline-0.7.0.tar.gz/outsystems-pipeline-0.7.0/setup.py
@@ -0,0 +1,105 @@
+from distutils.core import setup
+import os
+
+NAME = 'outsystems-pipeline'
+DESCRIPTION = 'Python package to accelerate the integration of OutSystems with third-party CI/CD tools'
+LONG_DESCRIPTION = '''The outsystems-pipeline Python package provides functions to support the creation of OutSystems CI/CD pipelines using your DevOps automation tool of choice.
+
+Visit the `project repository <https://github.com/OutSystems/outsystems-pipeline>`_ on GitHub for instructions on how to build example OutSystems CI/CD pipelines with common DevOps automation tools, as well as documentation that will help you adapt the examples to your particular scenarios.
+
+
+What's new
+==========
+
+**Scan Test Endpoints Script**
+* New function to discover Client Side and Server Side BDD test flows through the CI/CD Probe.
+
+**CI/CD Probe Integration Enhancements**
+For enhanced BDD test execution, flexibility and security, two new parameters were added:
+* --exclude_pattern: to specify the exclude pattern (using a regular expression) for the BDD test flows.
+* --cicd_probe_key: to enhance the security of the CI/CD Probe API calls.
+
+**Bug Fixes**
+* Fixed the issue related with loading the manifest file when the path directories included spaces.
+* Fixed the evaluate_test_results script to correctly use the provided input parameter instead of relying on default value.
+
+
+Installing and upgrading
+========================
+
+Install or upgrade outsystems-pipeline to the latest available version as follows:
+::
+
+    pip install -U outsystems-pipeline
+
+'''
+AUTHOR = u'OutSystems'
+EMAIL = u'cicd.integrations@outsystems.com'
+URL = 'https://github.com/OutSystems/outsystems-pipeline'
+LICENSE = 'Apache License 2.0'
+PYTHON_REQUIRES = '>=3.7'
+KEYWORDS = [
+    '',
+]
+
+CLASSIFIERS = [
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'Intended Audience :: System Administrators',
+    'License :: OSI Approved :: Apache Software License',
+    'Programming Language :: Python',
+    'Topic :: Software Development :: Build Tools',
+    'Topic :: Software Development :: Quality Assurance',
+    'Topic :: Software Development :: Testing',
+    'Topic :: Software Development :: Testing :: Acceptance',
+    'Topic :: Software Development :: Testing :: BDD',
+    'Topic :: Software Development :: Testing :: Unit',
+    'Topic :: System :: Software Distribution'
+]
+
+REQUIREMENTS = [
+    'python-dateutil==2.8.2',
+    'requests==2.31.0',
+    'unittest-xml-reporting==3.2.0',
+    'xunitparser==1.3.4',
+    'toposort==1.10'
+]
+
+PACKAGES = [
+    'outsystems',
+    'outsystems.architecture_dashboard',
+    'outsystems.bdd_framework',
+    'outsystems.cicd_probe',
+    'outsystems.exceptions',
+    'outsystems.file_helpers',
+    'outsystems.lifetime',
+    'outsystems.manifest',
+    'outsystems.osp_tool',
+    'outsystems.pipeline',
+    'outsystems.properties',
+    'outsystems.vars'
+]
+
+if __name__ == '__main__':  # Do not run setup() when we import this module.
+    if os.path.isfile("VERSION"):
+        with open("VERSION", 'r') as version_file:
+            version = version_file.read().replace('\n', '')
+    else:
+        # dummy version
+        version = '1.0.0'
+
+    setup(
+        name=NAME,
+        version='0.7.0',
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        keywords=' '.join(KEYWORDS),
+        author=AUTHOR,
+        author_email=EMAIL,
+        url=URL,
+        license=LICENSE,
+        python_requires=PYTHON_REQUIRES,
+        classifiers=CLASSIFIERS,
+        packages=PACKAGES,
+        install_requires=REQUIREMENTS
+    )
