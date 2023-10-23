907838964d8e8935509c5c165b5b070265ffd88d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-23 16:07:31 +0000

    Add package url-local/0.0.36/url_local-0.0.36-py3-none-any.whl

---
+++ packages/url-local/url_local-0.0.36-py3-none-any.whl/url_local/component_name_enum.py
@@ -0,0 +1,13 @@
+from enum import Enum
+
+
+class ComponentName(Enum):
+    AUTHENTICATION = "authentication"  # login
+    EVENT = "event"
+    GENDER_DETECTION = "gender-detection"
+    GROUP = "group"
+    GROUP_PROFILE = "group-profile"
+    LOGGER = "logger"
+    MARKETPLACE_GOODS = "marketplace-goods"
+    TIMELINE = "timeline"
+    USER_REGISTRATION = "user-registration"
