d945d3c4b95421eb227b35eb4592c7d3941003d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-23 04:09:27 +0000

    Add package ProVar/0.0.3/ProVar-0.0.3.tar.gz

---
+++ packages/ProVar/ProVar-0.0.3.tar.gz/ProVar-0.0.3/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+classifiers = [
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Science/Research',
+    'License :: Other/Proprietary License',
+    'Operating System :: Microsoft :: Windows',
+    'Programming Language :: Python :: 3',
+]
+
+setup(
+    name='ProVar',
+    version='0.0.3',
+    description='procedural variable system',
+    long_description='This package provides tools for working with probabilistic variants in genomics data.',
+    long_description_content_type='text/markdown',
+    url='',
+    author='Alexander Eriksen',
+    author_email='Alex.eriksen@live.no',
+    license='Privat Lisence',
+    classifiers=classifiers,
+    keywords='procedural variable',
+    package=find_packages(),
+    install_requires=['']
+)
