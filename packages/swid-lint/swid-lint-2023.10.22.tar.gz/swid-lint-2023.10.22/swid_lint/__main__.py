7599ce29cacff5f00c08b08ddde55d80a605b811
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-23 03:11:51 +0000

    Add package swid-lint/2023.10.22/swid-lint-2023.10.22.tar.gz

---
+++ packages/swid-lint/swid-lint-2023.10.22.tar.gz/swid-lint-2023.10.22/swid_lint/__main__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+import sys
+
+from swid_lint.cli import main
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv[1:]))  # pragma: no cover
---
+++ packages/swid-lint/swid-lint-2023.10.22.tar.gz/swid-lint-2023.10.22/test/test_lint.py
@@ -0,0 +1,5 @@
+from swid_lint.lint import main
+
+
+def test_main():
+    assert main() is NotImplemented
