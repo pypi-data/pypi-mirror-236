b205a580d0cc8cf6ff729ec870e3b253d351b922
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-23 04:09:01 +0000

    Add package autowired/0.1.6/autowired-0.1.6.tar.gz

---
+++ packages/autowired/autowired-0.1.6.tar.gz/autowired-0.1.6/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="autowired",
+    version="0.1.6",
+    description="A minimalistic dependency injection library for Python",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Nicolai Großer",
+    author_email="nicolai.grosser@googlemail.com",
+    url="https://github.com/npgrosser/autowired",
+    license="MIT",
+    packages=find_packages(),
+    install_requires=[],
+    python_requires=">=3.8",
+)
