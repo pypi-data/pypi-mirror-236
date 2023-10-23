b480ea1b0b7183edc01cfbc073cb266ab101dca2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-23 03:12:31 +0000

    Add package rocket-abluus/1.0.0/rocket-abluus-1.0.0.tar.gz

---
+++ packages/rocket-abluus/rocket-abluus-1.0.0.tar.gz/rocket-abluus-1.0.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='rocket-abluus',  # Replace 'rocket' with your actual package name
+    version='1.0.0',
+    author='Abla Hagani',  # Replace with your actual name
+    author_email='ja_hagani@esi.dz',  # Replace with your actual email
+    packages=['rocket-abluus'],  # Replace 'rocket' with the actual name of your package
+    url='http://pypi.python.org/pypi/rocket-abluus/',  # Replace 'rocket' with your actual package name
+    license='LICENSE.txt',
+    description='An awesome package that does something',
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+   
+)
