8a31f15b29bbdd4057afe257460cf8a38c015add
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-23 14:08:35 +0000

    Add package fastutils-hmarcuzzo/0.4.3/fastutils_hmarcuzzo-0.4.3.tar.gz

---
+++ packages/fastutils-hmarcuzzo/fastutils_hmarcuzzo-0.4.3.tar.gz/fastutils_hmarcuzzo-0.4.3/fastutils_hmarcuzzo/decorators/pagination_decorator.py
@@ -0,0 +1,15 @@
+from typing import TypeVar
+
+from starlette.requests import Request
+
+from fastutils_hmarcuzzo.types.find_many_options import FindManyOptions
+from fastutils_hmarcuzzo.utils.pagination_utils import PaginationUtils
+
+E = TypeVar("E")
+
+
+class GetPagination(object):
+    def __call__(self, request: Request) -> FindManyOptions:
+        paging_params = PaginationUtils.get_pagination_from_request(request)
+
+        return PaginationUtils.format_pagination_options(paging_params)
