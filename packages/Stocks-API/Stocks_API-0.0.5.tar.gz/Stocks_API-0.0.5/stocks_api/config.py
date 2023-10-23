67b41cb35fadfb477d4e4e1733b2b2e6504951b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-23 04:09:13 +0000

    Add package Stocks-API/0.0.5/Stocks_API-0.0.5.tar.gz

---
+++ packages/Stocks-API/Stocks_API-0.0.5.tar.gz/Stocks_API-0.0.5/stocks_api/config.py
@@ -0,0 +1,7 @@
+'''
+You must create a free API key from http://www.polygon.io/ to use this tool The key
+must be entered into the API_KEY='' variable below betweem the quotes ''
+
+'''
+
+API_KEY=''
