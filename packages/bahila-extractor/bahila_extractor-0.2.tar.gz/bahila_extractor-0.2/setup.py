3c1aa8ca265a2b64009b7fe20a1948283e2f97ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-23 03:11:54 +0000

    Add package bahila-extractor/0.2/bahila_extractor-0.2.tar.gz

---
+++ packages/bahila-extractor/bahila_extractor-0.2.tar.gz/bahila_extractor-0.2/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='bahila_extractor',
+    version='0.2',
+    packages=find_packages(),
+    install_requires=[
+        'theguardian',
+        # 'theguardian-api-python',
+        'requests',
+        'openai',
+        'bs4',
+        'langchain',
+        # any other dependencies
+    ],
+)
