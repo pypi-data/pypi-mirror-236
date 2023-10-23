8d1879a72c962df53a27347480d508be368ec0bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-23 04:09:02 +0000

    Add package command-saver-terminal-program/2.1.4/command-saver-terminal-program-2.1.4.tar.gz

---
+++ packages/command-saver-terminal-program/command-saver-terminal-program-2.1.4.tar.gz/command-saver-terminal-program-2.1.4/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setup(
+    name="command-saver-terminal-program",
+    version="2.1.4",
+    description="Simple terminal helper program to save long terminal commands. See git: https://github.com/Zzanetiite/CommandSaver",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'cs = command_saver.cs:main'
+        ],
+    },
+    install_requires=[
+        'rich==13.3.3',
+    ],
+)
