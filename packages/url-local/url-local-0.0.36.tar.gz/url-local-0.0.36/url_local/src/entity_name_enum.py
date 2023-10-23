18b6fae5ebea932517802f8766118dbedf74b6e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-23 16:07:31 +0000

    Add package url-local/0.0.36/url-local-0.0.36.tar.gz

---
+++ packages/url-local/url-local-0.0.36.tar.gz/url-local-0.0.36/url_local/src/entity_name_enum.py
@@ -0,0 +1,13 @@
+from enum import Enum
+
+
+class EntityName(Enum):
+    AUTH_LOGIN = "auth"
+    EVENT = "event"
+    GENDER_DETECTION = "gender-detection"
+    GROUP = "group"
+    LOGGER = "logger"
+    MARKETPLACE_GOODS = "marketplace-goods"
+    TIMELINE = "timeline"
+    USER_REGISTRATION = "user-registration"
+    GROUP_PROFILE = "group-profile"
