c8a03146120d4ba88700f3e25eab85656ce3a878
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-23 03:12:30 +0000

    Add package netix/1.11/netix-1.11-py3-none-any.whl

---
+++ packages/netix/netix-1.11-py3-none-any.whl/netix/lib/tasks.py
@@ -0,0 +1,17 @@
+
+import asyncio
+
+
+class ServerTasks:
+    def __init__(self, tasks, loop=None):
+        if loop is None:
+            loop = asyncio.get_event_loop()
+
+        self._loop = loop
+        self._tasks = tasks
+
+    def create(self, coro):
+        task = self._loop.create_task(coro)
+        self._tasks.append(task.cancel)
+
+        return task
