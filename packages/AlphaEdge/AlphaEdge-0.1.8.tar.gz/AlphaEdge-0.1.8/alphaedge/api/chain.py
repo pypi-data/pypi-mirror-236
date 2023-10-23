f2b44c9e8be4d72506f85b3a2722e42c6f0f9b0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-23 15:06:51 +0000

    Add package AlphaEdge/0.1.8/AlphaEdge-0.1.8.tar.gz

---
+++ packages/AlphaEdge/AlphaEdge-0.1.8.tar.gz/AlphaEdge-0.1.8/alphaedge/api/chain.py
@@ -0,0 +1,4 @@
+from alphaedge.plugins.chain import AtomLauncher
+from alphaedge.plugins.chain import S1Launcher
+
+__all__ = ['AtomLauncher', 'S1Launcher']
---
+++ packages/AlphaEdge/AlphaEdge-0.1.8.tar.gz/AlphaEdge-0.1.8/alphaedge/plugins/chain/__init__.py
@@ -0,0 +1,4 @@
+from alphaedge.plugins.chain.atom.launcher import Launcher as AtomLauncher
+from alphaedge.plugins.chain.alpha_s1.launcher import Launcher as S1Launcher
+
+__all__ = ['AtomLauncher', 'S1Launcher']
