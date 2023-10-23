d443ce6e6906889f748609ccd1a97eda56773bc1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-23 03:12:30 +0000

    Add package rocket-ablus/1.0.0/rocket-ablus-1.0.0.tar.gz

---
+++ packages/rocket-ablus/rocket-ablus-1.0.0.tar.gz/rocket-ablus-1.0.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='rocket-ablus',  # Replace 'rocket' with your actual package name
+    version='1.0.0',
+    author='Abla Hagani',  # Replace with your actual name
+    author_email='ja_hagani@esi.dz',  # Replace with your actual email
+    packages=['rocket-ablus'],  # Replace 'rocket' with the actual name of your package
+    url='http://pypi.python.org/pypi/rocket-ablus/',  # Replace 'rocket' with your actual package name
+    license='LICENSE.txt',
+    description='An awesome package that does something',
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    install_requires=['dependency1', 'dependency2']  # Replace with the actual dependencies
+)
