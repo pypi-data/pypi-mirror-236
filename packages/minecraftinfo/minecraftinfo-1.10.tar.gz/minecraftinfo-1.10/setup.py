0e61aa49f56c3354f08860d9dc86eaeb42bd8610
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-23 03:13:54 +0000

    Add package minecraftinfo/1.10/minecraftinfo-1.10.tar.gz

---
+++ packages/minecraftinfo/minecraftinfo-1.10.tar.gz/minecraftinfo-1.10/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='minecraftinfo',
+    version="1.10",
+    description="Request information Minecraft JavaEdition and BedrockEdition",
+    author='naisu',
+    packages=find_packages(),
+    license='MIT',
+    readme="README.md"
+)
