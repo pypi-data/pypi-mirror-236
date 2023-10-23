5753c02633bde7036f66e1450363241601f8cd2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-23 16:07:23 +0000

    Add package delayed-reactant-labeling/0.2.3/delayed-reactant-labeling-0.2.3.tar.gz

---
+++ packages/delayed-reactant-labeling/delayed-reactant-labeling-0.2.3.tar.gz/delayed-reactant-labeling-0.2.3/README.txt
@@ -0,0 +1,4 @@
+# Delayed Reactant labeling Python
+
+This is a package that assists with the analysis of Delayed Reactant Labeling Experiments. These experiments can provide
+information on kinetics of reactions, and the corresponding enantioselectivity.
---
+++ packages/delayed-reactant-labeling/delayed-reactant-labeling-0.2.3.tar.gz/delayed-reactant-labeling-0.2.3/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+
+setup(
+    install_requires=['numpy',
+                      'moviepy',
+                      'scipy',
+                      'matplotlib',
+                      'scikit-learn',
+                      'numba',
+                      'tqdm',
+                      'openpyxl',
+                      'pandas',
+                      'polars',
+                      'joblib'],
+)
