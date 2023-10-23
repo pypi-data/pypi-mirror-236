c1aa50f1b056f7e1d5d63e09f3ddfb86d272ebae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-23 04:08:41 +0000

    Add package RESTModels/0.0.1/RESTModels-0.0.1.tar.gz

---
+++ packages/RESTModels/RESTModels-0.0.1.tar.gz/RESTModels-0.0.1/tests/test_resources.py
@@ -0,0 +1,42 @@
+import datetime
+
+from src.APIClient.resources import try_parse_response_content
+
+
+def test_try_parse_str_response():
+    data = "text"
+    expected_type = str
+
+    expected_result = "text"
+
+    assert try_parse_response_content(data, expected_type) == expected_result
+
+
+def test_try_parse_int_response():
+    data1 = "5"
+    data2 = 5
+    expected_type = int
+
+    expected_result = 5
+
+    assert try_parse_response_content(data1, expected_type) == expected_result
+    assert try_parse_response_content(data2, expected_type) == expected_result
+
+
+def test_try_parse_datetime_response():
+    data = "2023-10-22T19:50:29.182993"
+    expected_type = datetime.datetime
+
+    expected_result = datetime.datetime.fromisoformat(data)
+
+    assert try_parse_response_content(data, expected_type) == expected_result
+
+# def test_try_parse_tuple_response():
+#     data1 = [1, 2]
+#     data2 = ["1", "2"]
+#     expected_type = tuple[int]
+#
+#     expected_result = (1, 2)
+#
+#     assert try_parse_response_content(data1, expected_type) == expected_result
+#     assert try_parse_response_content(data2, expected_type) == expected_result
