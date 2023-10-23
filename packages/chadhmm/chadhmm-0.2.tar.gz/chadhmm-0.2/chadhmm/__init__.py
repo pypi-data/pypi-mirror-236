f131153f090dc879a10f1d9e484dfee35e1541a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-23 03:13:08 +0000

    Add package chadhmm/0.2/chadhmm-0.2.tar.gz

---
+++ packages/chadhmm/chadhmm-0.2.tar.gz/chadhmm-0.2/chadhmm/__init__.py
@@ -0,0 +1,15 @@
+"""
+ChadHMM
+======
+
+Ultra Chad Implementation of Hidden Markov Models in Pytorch (available only to true sigma males)
+
+But seriously this package needs you to help me make it better. I'm not a professional programmer, I'm just a guy who likes to code. 
+If you have any suggestions, please let me know. I'm open to all ideas.
+"""
+
+# Import HMM objects
+from .hmm.CategoricalHMM import CategoricalHMM
+from .hmm.GaussianHMM import GaussianHMM, GaussianMixtureHMM
+from .hsmm.CategoricalHSMM import CategoricalHSMM
+from .hsmm.GaussianHSMM import GaussianHSMM, GaussianMixtureHSMM
