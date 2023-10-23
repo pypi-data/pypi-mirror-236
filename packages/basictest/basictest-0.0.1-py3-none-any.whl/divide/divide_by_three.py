4f6a457547b9a36ac9dace3b3cc1cf6337eee614
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-23 04:08:10 +0000

    Add package basictest/0.0.1/basictest-0.0.1-py3-none-any.whl

---
+++ packages/basictest/basictest-0.0.1-py3-none-any.whl/divide/divide_by_three.py
@@ -0,0 +1,10 @@
+"""
+This module divides the number by 3.
+"""
+
+def divide_by_three(num):
+    """
+    Takes one parameter: num -> int.
+    Divides number by 3.
+    """
+    return num / 3
---
+++ packages/basictest/basictest-0.0.1-py3-none-any.whl/multiply/multiply_by_three.py
@@ -0,0 +1,11 @@
+"""
+This module multiplies the number by 3.
+"""
+
+def multiply_by_three(num):
+    """
+    Takes one parameter: num -> int.
+    Multiplies number by 3.
+    """
+
+    return num * 3
