89fdd1ccfb292ca3b4fe76ff92a60fde78c88e90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-23 15:06:45 +0000

    Add package anova-ble/0.1.3/anova_ble-0.1.3.tar.gz

---
+++ packages/anova-ble/anova_ble-0.1.3.tar.gz/anova_ble-0.1.3/anova_ble/__main__.py
@@ -0,0 +1,35 @@
+import time
+import bleak
+from . import AnovaBLEPrecisionCooker, _LOGGER
+import asyncio
+
+import logging
+logging.basicConfig(level=logging.DEBUG)
+
+async def discover():
+    "Look for an Anova sous-vide machine"
+    devs = await bleak.BleakScanner.discover()
+
+    for dev in devs:
+        print(dev)
+        if dev.name == "Anova":
+            return dev
+
+async def main():
+    ble_dev = await discover()
+    _LOGGER.debug(ble_dev)
+    anova = AnovaBLEPrecisionCooker(ble_device=ble_dev)
+    
+    time.sleep(1)
+    _LOGGER.info(await anova.update_state())
+    time.sleep(3)
+
+    await asyncio.gather(
+        anova.start(),
+        anova.update_state()
+    )
+
+    await anova.stop()
+
+if __name__ == "__main__":
+    asyncio.run(main())
