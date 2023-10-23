4c7ae668a55567227f0b601e6af196bc356b5212
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-23 14:08:43 +0000

    Add package probality-distributions/1.0/probality_distributions-1.0.tar.gz

---
+++ packages/probality-distributions/probality_distributions-1.0.tar.gz/probality_distributions-1.0/README.md
@@ -0,0 +1,14 @@
+## probality-distributions package
+
+Summary of the package
+
+# Files
+We have 3 files:
+1 for General class for both Gaussian and Binomial classes
+2 Binomial class
+3 Gausiian class
+
+# Installation
+pip install probality-distributions
+
+
---
+++ packages/probality-distributions/probality_distributions-1.0.tar.gz/probality_distributions-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='probality_distributions',
+       version='1.0',
+       description = 'Gaussian and Binomial distributions formulas',
+       packages=['probality_distributions'] ,
+       author = 'Rodolfo Frois',
+       author_email = 'froisrodolfo@gmail.com',
+       zip_safe=False)
