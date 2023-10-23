7edd6e72e1bc7379aa27325ad499bb3969933bd6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-23 16:08:11 +0000

    Add package sqs-queue-package-ya/0.0.1/sqs_queue_package_ya-0.0.1-py3-none-any.whl

---
+++ packages/sqs-queue-package-ya/sqs_queue_package_ya-0.0.1-py3-none-any.whl/sqs_queue/main.py
@@ -0,0 +1,61 @@
+import boto3
+import os
+from dotenv import dotenv_values
+
+env_path = os.getcwd().replace('/src', '') + '/.env'
+Env = dotenv_values(env_path)
+
+os.environ['AWS_ACCESS_KEY_ID'] = Env["AWS_ACCESS_KEY_ID"]
+os.environ['AWS_SECRET_ACCESS_KEY'] = Env["AWS_SECRET_ACCESS_KEY"]
+
+
+class YaQueue:
+    client = None
+
+    def __init__(self, _endpoint_url: str):
+        self.client = boto3.client(
+            service_name=Env["QUEUE_SERVICE_NAME"],
+            endpoint_url=_endpoint_url,
+            region_name=Env["QUEUE_REGION"]
+        )
+
+    def send_message(self, _queue_name, _queue_msg):
+        queue_url = self.client.create_queue(
+            QueueName=_queue_name
+        ).get('QueueUrl')
+
+        return self.client.send_message(
+            QueueUrl=queue_url,
+            MessageBody=_queue_msg,
+        )
+
+    def get_messages(self, _queue_name):
+        queue_url = self.client.create_queue(
+            QueueName=_queue_name
+        ).get('QueueUrl')
+
+        return self.client.receive_message(
+            QueueUrl=queue_url,
+            MaxNumberOfMessages=10,
+            VisibilityTimeout=1200,
+            WaitTimeSeconds=10
+        ).get('Messages')
+
+    def delete_message(self, _queue_name, _recept):
+        queue_url = self.client.create_queue(
+            QueueName=_queue_name
+        ).get('QueueUrl')
+
+        return self.client.delete_message(
+            QueueUrl=queue_url,
+            ReceiptHandle=_recept
+        )
+
+    def delete_all_messages(self, _queue_name):
+        queue_url = self.client.create_queue(
+            QueueName=_queue_name
+        ).get('QueueUrl')
+
+        return self.client.purge_queue(
+            QueueUrl=queue_url
+        )
