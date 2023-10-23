5c19e21f788f6dac85b0cc2b1270a259e0af8636
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-23 16:07:20 +0000

    Add package venv-management/2.2.1/venv_management-2.2.1-py3-none-any.whl

---
+++ packages/venv-management/venv_management-2.2.1-py3-none-any.whl/venv_management/__init__.py
@@ -0,0 +1,40 @@
+
+from .version import __version__, __version_info__
+from .api import (
+    check_environment,
+    has_virtualenvwrapper,
+    list_virtual_envs,
+    make_virtual_env,
+    resolve_virtual_env,
+    virtual_env,
+    ensure_virtual_env,
+    remove_virtual_env,
+    discard_virtual_env,
+    python_executable_path,
+    python_name,
+    python_version,
+)
+
+from .errors import (
+    ImplementationNotFound,
+    CommandNotFound,
+    PythonNotFound,
+)
+
+__all__ = [
+    "check_environment",
+    "has_virtualenvwrapper",
+    "list_virtual_envs",
+    "make_virtual_env",
+    "resolve_virtual_env",
+    "virtual_env",
+    "ensure_virtual_env",
+    "remove_virtual_env",
+    "discard_virtual_env",
+    "python_executable_path",
+    "python_name",
+    "python_version",
+    "ImplementationNotFound",
+    "CommandNotFound",
+    "PythonNotFound",
+]
