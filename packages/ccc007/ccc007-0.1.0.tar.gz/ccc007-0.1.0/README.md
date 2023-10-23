5d34abf6027a56b27b74188a4ead348fc908e667
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-23 03:11:10 +0000

    Add package ccc007/0.1.0/ccc007-0.1.0.tar.gz

---
+++ packages/ccc007/ccc007-0.1.0.tar.gz/ccc007-0.1.0/README.md
@@ -0,0 +1,10 @@
+# ccc007
+
+[![PyPI 版本](https://badge.fury.io/py/your-package-name.svg)](https://pypi.org/project/ccc007/)
+[![许可证：GPL 3.0](https://img.shields.io/badge/License-GPL%203.0-blue.svg)](https://opensource.org/licenses/GPL-3.0)
+
+这是我的工具合集
+
+## 安装
+
+使用软件包管理器 [pip](https://pip.pypa.io/en/stable/) 安装您的包名称。
---
+++ packages/ccc007/ccc007-0.1.0.tar.gz/ccc007-0.1.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ccc007', # 包名
+    version='0.1.0',   # 版本号
+    description='ccc007的工具', # 描述   
+    author='ccc007', # 作者    
+    author_email='mengchao704653@gmail.com', # 邮箱
+    url='https://github.com/1492949083/repo', # 项目主页 
+    packages=find_packages(), # 需要打包的目录模块
+    install_requires=[ # 依赖列表
+        'win32gui', 
+        'win32ui', 
+        'win32con', 
+        'win32api'
+        'os'  
+    ],
+    long_description=open('README.md').read()
+)
