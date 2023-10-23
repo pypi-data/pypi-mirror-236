8153072dafc8f5a9bec26e4ab19c5df0b5f88745
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-23 04:09:48 +0000

    Add package opswork/0.7.2/opswork-0.7.2-py3-none-any.whl

---
+++ packages/opswork/opswork-0.7.2-py3-none-any.whl/opswork/module/palm.py
@@ -0,0 +1,51 @@
+# MIT License
+#
+# Copyright (c) 2023 Clivern
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+import requests
+
+
+class Palm:
+    def __init__(self, api_key):
+        self.api_key = api_key
+        self.url = "https://generativelanguage.googleapis.com/v1beta3/models/text-bison-001:generateText"
+
+    def prompt(self, message):
+        headers = {
+            "Content-Type": "application/json",
+        }
+
+        params = {
+            "key": self.api_key,
+        }
+
+        data = {
+            "prompt": {
+                "text": message,
+            },
+        }
+
+        response = requests.post(self.url, headers=headers, params=params, json=data)
+
+        if response.ok:
+            return response.json()["candidates"][0]["output"]
+        else:
+            response.raise_for_status()
