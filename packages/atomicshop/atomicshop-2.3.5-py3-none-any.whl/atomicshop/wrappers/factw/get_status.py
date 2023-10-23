10993e7d7255ef82952902a736eb4ad3f31e7b6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-23 03:14:03 +0000

    Add package atomicshop/2.3.5/atomicshop-2.3.5-py3-none-any.whl

---
+++ packages/atomicshop/atomicshop-2.3.5-py3-none-any.whl/atomicshop/wrappers/factw/get_status.py
@@ -0,0 +1,41 @@
+import requests
+
+from . import fact_config
+from ... print_api import print_api
+
+
+def get_global_status():
+    """
+    Get status of the FACT service.
+    :return:
+    """
+
+    url: str = f'{fact_config.FACT_ADDRESS}{fact_config.STATUS_ENDPOINT}'
+    response: requests.Response = requests.get(url)
+
+    # Check response status code.
+    if response.status_code == 200:
+        # Print response.
+        print_api(response.json())
+    else:
+        # Print error.
+        print_api('Error: ' + str(response.status_code), error_type=True, logger_method='critical')
+
+    return response
+
+
+def is_uid_exist(uid: str):
+    """
+    Check if the specified FACT UID exists in the database.
+    :param uid: string, FACT UID.
+    :return: boolean, True if exists, False if not.
+    """
+
+    url: str = f'{fact_config.FACT_ADDRESS}{fact_config.FIRMWARE_ENDPOINT}/{uid}'
+    response: requests.Response = requests.get(url)
+
+    # Check response status code.
+    if response.status_code == 200:
+        return True
+    else:
+        return False
