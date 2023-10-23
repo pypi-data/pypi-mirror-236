16f70e2b80465a639ec0e3440352827c86a46122
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-23 15:08:07 +0000

    Add package most-queue/1.30/most_queue-1.30.tar.gz

---
+++ packages/most-queue/most_queue-1.30.tar.gz/most_queue-1.30/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+setup(name='most-queue',
+      version='1.30',
+      description="Software package for calculation and simulation of queuing systems",
+      author='Xabarov Roman',
+      author_email='xabarov1985@gmail.com',
+      url='https://github.com/xabarov/mps',
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.9',
+        'Topic :: Text Processing :: Linguistic',
+      ],
+      license='MIT',
+      packages=find_packages(),
+      install_requires=[
+            "matplotlib",
+            "matplotlib-inline",
+            "numba",
+            "numpy",
+            "pandas",
+            "scipy",
+            "tqdm",
+            "tqdm-stubs",
+            'PyQt5',
+            'qt-material',
+            'PySide2',
+            'networkx',
+            'graphviz',
+            'colorama'
+      ],
+      include_package_data=True,
+      zip_safe=False)
