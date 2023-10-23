4681263ac96b05ef23e83d1df4be323d958a3211
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-23 03:12:48 +0000

    Add package Salman-distributions/1.0/Salman_distributions-1.0.tar.gz

---
+++ packages/Salman-distributions/Salman_distributions-1.0.tar.gz/Salman_distributions-1.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='Salman_distributions',
+      version='1.0',
+      description='Gaussian and Binomial distributions',
+      packages=['Salman_distributions'],
+      author=['Salman Gassem'],
+      zip_safe=False)
