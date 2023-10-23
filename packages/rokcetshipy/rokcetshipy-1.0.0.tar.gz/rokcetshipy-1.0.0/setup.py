7c846f4de55522f442d09ae3cdd995da2f8e777e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-23 03:11:06 +0000

    Add package rokcetshipy/1.0.0/rokcetshipy-1.0.0.tar.gz

---
+++ packages/rokcetshipy/rokcetshipy-1.0.0.tar.gz/rokcetshipy-1.0.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+   name='rokcetshipy',
+   version='1.0.0',
+   author='Abdelmoumen Deghbouche',
+   author_email='a.deghbouche@esi-sba.dz',
+   packages=['rokcetshipy'],
+   url='http://pypi.python.org/pypi/rokcetshipy/',
+   license='LICENSE.txt',
+   description='An awesome package that does something',
+   long_description=open('README.md').read(),
+   long_description_content_type="text/markdown",
+   install_requires=['rokcetshipy']
+)
