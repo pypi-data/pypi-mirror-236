860ab857c1401b062c6890038b3d7504382461f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-23 15:06:56 +0000

    Add package Dockme/0.1.1/Dockme-0.1.1-py3-none-any.whl

---
+++ packages/Dockme/Dockme-0.1.1-py3-none-any.whl/Dockme/__init__.py
@@ -0,0 +1,22 @@
+version = "0.1.1"
+
+
+def hello():
+    print('Hello from Dockme in Version:', version)
+
+
+def get_version():
+    print('Version:', version)
+
+
+
+from .get_image import get_image
+from .run_container import run_container
+from .stop_container import stop_container
+from .delet_container import delet_container
+from .delet_image import delet_image
+from. run_command_in_con import run_command
+
+
+
+__all__ = ['get_image', 'run_container', 'stop_container', 'delet_container', 'delet_image', 'run_command']
