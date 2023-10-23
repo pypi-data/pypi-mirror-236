dd3724755801b2c2500b3b99ac1bb950dc5e8944
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-23 04:09:13 +0000

    Add package Dockme/0.0.9/Dockme-0.0.9.tar.gz

---
+++ packages/Dockme/Dockme-0.0.9.tar.gz/Dockme-0.0.9/src/Dockme/__init__.py
@@ -0,0 +1,20 @@
+version = "0.0.9"
+
+
+def hello():
+    print('Hello from Dockme in Version:', version)
+
+
+
+
+
+
+from .get_image import get_image
+from .run_container import run_container
+from .stop_container import stop_container
+from .delet_container import delet_container
+from .delet_image import delet_image
+
+
+
+__all__ = ['get_image', 'run_container', 'stop_container', 'delet_container', 'delet_image']
