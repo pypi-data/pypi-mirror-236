b0e035e3d6b6b391634a5516a26ba873e579acdf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-23 15:07:58 +0000

    Add package pyramid-oereb/2.4.6/pyramid_oereb-2.4.6-py2.py3-none-any.whl

---
+++ packages/pyramid-oereb/pyramid_oereb-2.4.6-py2.py3-none-any.whl/pyramid_oereb/core/records/general_information.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+import warnings
+
+
+class GeneralInformationRecord(object):
+    """
+    Represents a general information entry with its title and content.
+    Attributes:
+        title (dict of unicode): The title of the information (multilingual)
+        content (dict of unicode): The actual information (multilingual)
+    """
+    def __init__(self, title, content):
+        """
+        Args:
+            title (dict of unicode): The title of the information (multilingual)
+            content (dict of unicode): The actual information (multilingual)
+        """
+
+        if not isinstance(title, dict):
+            warnings.warn('Type of "title" should be "dict", is {}, value {}'
+                          .format(type(title), title))
+
+        if not isinstance(content, dict):
+            warnings.warn('Type of "content" should be "dict", is {}, value {}'
+                          .format(type(content), content))
+
+        self.title = title
+        self.content = content
+
+    def __str__(self):
+        return '<{} -- title: {} content: {}>'.format(
+            self.__class__.__name__, self.title, self.content)
