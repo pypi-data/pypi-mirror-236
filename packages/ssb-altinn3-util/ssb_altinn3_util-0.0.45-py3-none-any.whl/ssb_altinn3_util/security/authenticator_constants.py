fe88b80d7d35bbc913a80b0372ee431aa48c976a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-23 16:07:22 +0000

    Add package ssb-altinn3-util/0.0.45/ssb_altinn3_util-0.0.45-py3-none-any.whl

---
+++ packages/ssb-altinn3-util/ssb_altinn3_util-0.0.45-py3-none-any.whl/ssb_altinn3_util/security/authenticator_constants.py
@@ -0,0 +1,15 @@
+ROLE_USER = "user"
+ROLE_FORM_READER = "form_reader"
+ROLE_FORM_WRITER = "form_writer"
+ROLE_SUPERUSER = "superuser"
+ROLE_ADMIN = "admin"
+
+VALID_ROLES = [
+    ROLE_USER,
+    ROLE_FORM_READER,
+    ROLE_FORM_WRITER,
+    ROLE_SUPERUSER,
+    ROLE_ADMIN,
+]
+
+FORM_ACCESS_ALL = "all_forms"
