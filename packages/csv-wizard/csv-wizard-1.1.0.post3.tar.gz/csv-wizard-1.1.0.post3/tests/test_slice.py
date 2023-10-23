b278c48179e2dca5532321f62b63f77ed74a88df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-23 14:08:32 +0000

    Add package csv-wizard/1.1.0.post3/csv-wizard-1.1.0.post3.tar.gz

---
+++ packages/csv-wizard/csv-wizard-1.1.0.post3.tar.gz/csv-wizard-1.1.0.post3/tests/test_slice.py
@@ -0,0 +1,14 @@
+import pytest
+
+# test slice()
+
+
+def test_slice_type_is_dict(test_file):
+    parts = test_file.slice()
+    assert type(parts) == dict
+
+
+def test_slice(test_file):
+    parts = test_file.slice()
+    half = int(test_file.get_row_count() / 2)
+    assert len(parts["First_Half"]) <= half and len(parts["Second_Half"]) >= half
