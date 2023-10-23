41bf88fca6a8f4109a5cba32c4cf40fa3eb53450
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-23 15:07:51 +0000

    Add package jy_lib/0.1.4/jy_lib-0.1.4.tar.gz

---
+++ packages/jy_lib/jy_lib-0.1.4.tar.gz/jy_lib-0.1.4/jy_lib/__init__.py
@@ -0,0 +1,14 @@
+"""
+Python SDK for jyfund
+"""
+# 客户端类
+from .auth_client import AuthClient
+from .smb_client import SmbClient
+
+# 枚举类
+from .bank import BankSC
+from .country import CountryA2, CountryA3
+from .currency import CurrencyAlphabeticCode
+from .exchange import ExchangeMic, ExchangeAcronym
+
+__version__ = "0.1.4"
