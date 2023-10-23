4a6986e66db4a0ad4366d0371088bc3a571b26a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-23 03:12:14 +0000

    Add package hamas-rocket/1.2.0/hamas_rocket-1.2.0.tar.gz

---
+++ packages/hamas-rocket/hamas_rocket-1.2.0.tar.gz/hamas_rocket-1.2.0/hamas_rocket/__init__.py
@@ -0,0 +1,1 @@
+import hamas_rocket
---
+++ packages/hamas-rocket/hamas_rocket-1.2.0.tar.gz/hamas_rocket-1.2.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+   name='hamas_rocket',
+   version='1.2.0',
+   author='Kassam',
+   author_email='ca.atallah@esi-sba.dz',
+   packages=['hamas_rocket'],
+   url='http://pypi.python.org/pypi/hamas_rocket/',
+   license='LICENSE.txt',
+   description='An awesome package that does something',
+   long_description=open('README.md').read(),
+   long_description_content_type="text/markdown",
+   install_requires=['hamas_rocket']
+)
