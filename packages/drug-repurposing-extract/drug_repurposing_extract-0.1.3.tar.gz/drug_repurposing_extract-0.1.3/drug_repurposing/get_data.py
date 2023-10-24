54641f10b653a06269c54ccdaeba2177dcf2575a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-24 05:08:41 +0000

    Add package drug-repurposing-extract/0.1.3/drug_repurposing_extract-0.1.3.tar.gz

---
+++ packages/drug-repurposing-extract/drug_repurposing_extract-0.1.3.tar.gz/drug_repurposing_extract-0.1.3/drug_repurposing/get_data.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import requests
+import csv
+
+def generate_data(cids=[]):
+    results = []
+    fails = []
+    for cid in cids:
+        try:
+            resp = requests.get("https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/cid/" + str(cid) + "/property/MolecularWeight,MolecularFormula,IsomericSMILES,IUPACName/JSON")
+            content = resp.json()
+            desc_fp = from_smiles(content['PropertyTable']['Properties'][0]['IsomericSMILES'], fingerprints=True)
+            desc_fp['cid'] = cid
+            results.append(desc_fp)
+        except Exception as e:
+            # print(e)
+            # print(content)
+            fails.append(cid)
+    print("Fails CIDS: ", fails)
+
+    csv_file = 'complete_data.csv'
+    field_names = results[0].keys()
+    with open(csv_file, 'w', newline='') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=field_names)
+        writer.writeheader()
+        for row in results:
+            writer.writerow(row)
