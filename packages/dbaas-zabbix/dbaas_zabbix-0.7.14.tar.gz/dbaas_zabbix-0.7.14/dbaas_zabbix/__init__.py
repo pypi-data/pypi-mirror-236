638243eabe8360ce7e0dd26004954b335b257d85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-24 04:07:28 +0000

    Add package dbaas-zabbix/0.7.14/dbaas_zabbix-0.7.14.tar.gz

---
+++ packages/dbaas-zabbix/dbaas_zabbix-0.7.14.tar.gz/dbaas_zabbix-0.7.14/dbaas_zabbix/__init__.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from dbaas_zabbix.dbaas_api import DatabaseAsAServiceApi
+from dbaas_zabbix.provider_factory import ProviderFactory
+from pyzabbix import ZabbixAPI
+from dbaas_zabbix.provider import ZabbixProvider
+
+
+def factory_for(**kwargs):
+    databaseinfra = kwargs['databaseinfra']
+    credentials = kwargs['credentials']
+    del kwargs['databaseinfra']
+    del kwargs['credentials']
+
+    zabbix_api = ZabbixAPI
+    if kwargs.get('zabbix_api'):
+        zabbix_api = kwargs.get('zabbix_api')
+        del kwargs['zabbix_api']
+
+    dbaas_api = DatabaseAsAServiceApi(databaseinfra, credentials)
+
+    # TUDO ISSO PRA RETORNAR...
+    # ZabbixProvider(dbaas_api=dbaas_api, zabbix_api=zabbix_api, **kwargs)
+    return ProviderFactory.factory(dbaas_api, zabbix_api=zabbix_api, **kwargs)
