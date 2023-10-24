56592fdab69e8abd4c0b02ad97a67b15f1763502
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-24 04:07:43 +0000

    Add package tharos-pytools/0.0.19/tharos-pytools-0.0.19.tar.gz

---
+++ packages/tharos-pytools/tharos-pytools-0.0.19.tar.gz/tharos-pytools-0.0.19/tharospytools/__init__.py
@@ -0,0 +1,8 @@
+'Quality-of-life functions for Python projects'
+from .multithreading import futures_collector, limit_memory
+from .matplotlib_tools import get_palette, get_palette_from_list
+from .overloading import overload
+from .list_tools import flatten
+from .signatures import types_checker
+from .bio_tools import revcomp
+from .path_tools import path_allocator
