d0ca65d1c1aedfb51706dc5fb9d875c004bee6e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-24 03:13:21 +0000

    Add package autolens/2023.10.23.3/autolens-2023.10.23.3-py3-none-any.whl

---
+++ packages/autolens/autolens-2023.10.23.3-py3-none-any.whl/autolens/analysis/visualizer.py
@@ -0,0 +1,317 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.stats import norm
+from os import path
+import os
+from typing import Dict
+
+import autoarray as aa
+import autogalaxy.plot as aplt
+
+from autogalaxy.analysis.visualizer import plot_setting
+
+from autogalaxy.analysis.visualizer import Visualizer as AgVisualizer
+
+from autolens.lens.ray_tracing import Tracer
+from autolens.lens.plot.ray_tracing_plotters import TracerPlotter
+
+
+class Visualizer(AgVisualizer):
+    """
+    Visualizes the maximum log likelihood model of a model-fit, including components of the model and fit objects.
+
+    The methods of the `Visualizer` are called throughout a non-linear search using the `Analysis`
+    classes `visualize` method.
+
+    The images output by the `Visualizer` are customized using the file `config/visualize/plots.ini`.
+
+    Parameters
+    ----------
+    visualize_path
+        The path on the hard-disk to the `image` folder of the non-linear searches results.
+    """
+
+    def visualize_tracer(
+        self, tracer: Tracer, grid: aa.type.Grid2DLike, during_analysis: bool
+    ):
+        """
+        Visualizes a `Tracer` object.
+
+        Images are output to the `image` folder of the `visualize_path` in a subfolder called `ray_tracing`. When
+        used with a non-linear search the `visualize_path` points to the search's results folder and this function
+        visualizes the maximum log likelihood `Tracer` inferred by the search so far.
+
+        Visualization includes individual images of attributes of the tracer (e.g. its image, convergence, deflection
+        angles) and a subplot of all these attributes on the same figure.
+
+        The images output by the `Visualizer` are customized using the file `config/visualize/plots.ini` under the
+        [ray_tracing] header.
+
+        Parameters
+        ----------
+        tracer
+            The maximum log likelihood `Tracer` of the non-linear search.
+        grid
+            A 2D grid of (y,x) arc-second coordinates used to perform ray-tracing, which is the masked grid tied to
+            the dataset.
+        during_analysis
+            Whether visualization is performed during a non-linear search or once it is completed.
+        """
+
+        def should_plot(name):
+            return plot_setting(section="ray_tracing", name=name)
+
+        mat_plot_2d = self.mat_plot_2d_from(subfolders="ray_tracing")
+
+        tracer_plotter = TracerPlotter(
+            tracer=tracer,
+            grid=grid,
+            mat_plot_2d=mat_plot_2d,
+            include_2d=self.include_2d,
+        )
+
+        if should_plot("subplot_ray_tracing"):
+            tracer_plotter.subplot_tracer()
+
+        if should_plot("subplot_plane_images"):
+            tracer_plotter.subplot_plane_images()
+
+        tracer_plotter.figures_2d(
+            image=should_plot("image"),
+            source_plane=should_plot("source_plane_image"),
+            convergence=should_plot("convergence"),
+            potential=should_plot("potential"),
+            deflections_y=should_plot("deflections"),
+            deflections_x=should_plot("deflections"),
+            magnification=should_plot("magnification"),
+        )
+
+        if not during_analysis and should_plot("all_at_end_png"):
+            mat_plot_2d = self.mat_plot_2d_from(
+                subfolders=path.join("ray_tracing", "end"),
+            )
+
+            tracer_plotter = TracerPlotter(
+                tracer=tracer,
+                grid=grid,
+                mat_plot_2d=mat_plot_2d,
+                include_2d=self.include_2d,
+            )
+
+            tracer_plotter.figures_2d(
+                image=True,
+                source_plane=True,
+                convergence=True,
+                potential=True,
+                deflections_y=True,
+                deflections_x=True,
+                magnification=True,
+            )
+
+        if not during_analysis and should_plot("all_at_end_fits"):
+            mat_plot_2d = self.mat_plot_2d_from(
+                subfolders=path.join("ray_tracing", "fits"), format="fits"
+            )
+
+            tracer_plotter = TracerPlotter(
+                tracer=tracer,
+                grid=grid,
+                mat_plot_2d=mat_plot_2d,
+                include_2d=self.include_2d,
+            )
+
+            tracer_plotter.figures_2d(
+                image=True,
+                source_plane=True,
+                convergence=True,
+                potential=True,
+                deflections_y=True,
+                deflections_x=True,
+                magnification=True,
+            )
+
+    def visualize_image_with_positions(
+        self, image: aa.Array2D, positions: aa.Grid2DIrregular
+    ):
+        """
+        Visualizes the positions of a model-fit, where these positions are used to resample lens models where
+        the positions to do trace within an input threshold of one another in the source-plane.
+
+        Images are output to the `image` folder of the `visualize_path` in a subfolder called `positions`. When
+        used with a non-linear search the `visualize_path` points to the search's results folder.
+
+        The visualization is an image of the strong lens with the positions overlaid.
+
+        The images output by the `Visualizer` are customized using the file `config/visualize/plots.ini` under the
+        [ray_tracing] header.
+
+        Parameters
+        ----------
+        imaging
+            The imaging dataset whose image the positions are overlaid.
+        position
+            The 2D (y,x) arc-second positions used to resample inaccurate mass models.
+        """
+
+        def should_plot(name):
+            return plot_setting(section=["positions"], name=name)
+
+        mat_plot_2d = self.mat_plot_2d_from(subfolders="positions")
+
+        if positions is not None:
+            visuals_2d = aplt.Visuals2D(positions=positions)
+
+            image_plotter = aplt.Array2DPlotter(
+                array=image,
+                mat_plot_2d=mat_plot_2d,
+                include_2d=self.include_2d,
+                visuals_2d=visuals_2d,
+            )
+            image_plotter.set_filename("image_with_positions")
+            if should_plot("image_with_positions"):
+                image_plotter.figure_2d()
+
+    def visualize_adapt_images(
+        self,
+        adapt_galaxy_image_path_dict: Dict[str, aa.Array2D],
+        adapt_model_image: aa.Array2D,
+    ):
+        """
+        Visualizes the adapt-images and adapt dataset inferred by a model-fit.
+
+        Images are output to the `image` folder of the `visualize_path` in a subfolder called `adapt`. When
+        used with a non-linear search the `visualize_path` points to the search's results folder.
+
+        Visualization includes individual images of attributes of the adapt dataset (e.g. the adapt image) and
+        a subplot of all galaxy images on the same figure.
+
+        The images output by the `Visualizer` are customized using the file `config/visualize/plots.ini` under the
+        [adapt] header.
+
+        Parameters
+        ----------
+        adapt_galaxy_image_path_dict
+            A dictionary mapping the path to each galaxy (e.g. its name) to its corresponding galaxy image.
+        adapt_model_image
+            The adapt image which corresponds to the sum of galaxy images.
+        """
+
+        def should_plot(name):
+            return plot_setting(section="adapt", name=name)
+
+        mat_plot_2d = self.mat_plot_2d_from(subfolders="adapt")
+
+        adapt_plotter = aplt.AdaptPlotter(
+            mat_plot_2d=mat_plot_2d, include_2d=self.include_2d
+        )
+
+        if should_plot("model_image"):
+            adapt_plotter.figure_adapt_model_image(adapt_model_image=adapt_model_image)
+
+        if should_plot("images_of_galaxies"):
+            adapt_plotter.subplot_adapt_images_of_galaxies(
+                adapt_galaxy_image_path_dict=adapt_galaxy_image_path_dict
+            )
+
+    def visualize_contribution_maps(self, tracer: Tracer):
+        """
+        Visualizes the contribution maps that are used for adapt features which adapt a model to the dataset it is
+        fitting.
+
+        Images are output to the `image` folder of the `visualize_path` in a subfolder called `adapt`. When
+        used with a non-linear search the `visualize_path` points to the search's results folder and this function
+        visualizes the maximum log likelihood contribution maps inferred by the search so far.
+
+        Visualization includes individual images of attributes of the adapt dataset (e.g. the contribution map of
+        each galaxy) and a subplot of all contribution maps on the same figure.
+
+        The images output by the `Visualizer` are customized using the file `config/visualize/plots.ini` under the
+        [adapt] header.
+
+        Parameters
+        ----------
+        tracer
+            The maximum log likelihood `Tracer` of the non-linear search which is used to plot the contribution maps.
+        """
+
+        def should_plot(name):
+            return plot_setting(section="adapt", name=name)
+
+        mat_plot_2d = self.mat_plot_2d_from(subfolders="adapt")
+
+        adapt_plotter = aplt.AdaptPlotter(
+            mat_plot_2d=mat_plot_2d, include_2d=self.include_2d
+        )
+
+        if hasattr(tracer, "contribution_map_list"):
+            if should_plot("contribution_map_list"):
+                adapt_plotter.subplot_contribution_map_list(
+                    contribution_map_list_list=tracer.contribution_map_list
+                )
+
+    def visualize_stochastic_histogram(
+        self,
+        stochastic_log_likelihoods: np.ndarray,
+        max_log_evidence: float,
+        histogram_bins: int = 10,
+    ):
+        """
+        Certain `Inversion`'s have stochasticity in their log likelihood estimate.
+
+        For example, the `VoronoiBrightnessImage` pixelization, which changes the likelihood depending on how different
+        KMeans seeds change the pixel-grid.
+
+        A log likelihood cap can be applied to model-fits performed using these `Inversion`'s to improve error and
+        posterior estimates. This log likelihood cap is estimated from a list of stochastic log likelihoods, where
+        these log likelihoods are computed using the same model but with different KMeans seeds.
+
+        This function plots a histogram representing the distribution of these stochastic log likelihoods with a 1D
+        Gaussian fit to the likelihoods overlaid. This figure can be used to determine how subject the fit to this
+        dataset is to the stochastic likelihood effect.
+
+        Parameters
+        ----------
+        stochastic_log_likelihoods
+            The stochastic log likelihood which are used to plot the histogram and Gaussian.
+        max_log_evidence
+            The maximum log likelihood value of the non-linear search, which will likely be much larger than any of the
+            stochastic log likelihoods as it will be boosted high relative to most samples.
+        histogram_bins
+            The number of bins in the histogram used to visualize the distribution of stochastic log likelihoods.
+
+        Returns
+        -------
+        float
+            A log likelihood cap which is applied in a stochastic model-fit to give improved error and posterior
+            estimates.
+        """
+        if stochastic_log_likelihoods is None:
+            return
+
+        if plot_setting("other", "stochastic_histogram"):
+            file_path = path.join(self.visualize_path, "other")
+
+            try:
+                os.makedirs(file_path)
+            except FileExistsError or IsADirectoryError:
+                pass
+
+            filename = path.join(file_path, "stochastic_histogram.png")
+
+            if path.exists(filename):
+                try:
+                    os.rmdir(filename)
+                except Exception:
+                    pass
+
+            (mu, sigma) = norm.fit(stochastic_log_likelihoods)
+            n, bins, patches = plt.hist(
+                x=stochastic_log_likelihoods, bins=histogram_bins, density=1
+            )
+            y = norm.pdf(bins, mu, sigma)
+            plt.plot(bins, y, "--")
+            plt.xlabel("log evidence")
+            plt.title("Stochastic Log Evidence Histogram")
+            plt.axvline(max_log_evidence, color="r")
+            plt.savefig(filename, bbox_inches="tight")
+            plt.close()
---
+++ packages/autolens/autolens-2023.10.23.3-py3-none-any.whl/autolens/lens/to_inversion.py
@@ -0,0 +1,243 @@
+from typing import Dict, List, Optional, Tuple, Type, Union
+
+from autoconf import cached_property
+
+import autoarray as aa
+import autogalaxy as ag
+
+from autoarray.inversion.inversion.factory import inversion_unpacked_from
+
+from autolens.analysis.preloads import Preloads
+
+
+class TracerToInversion(ag.AbstractToInversion):
+    def __init__(
+        self,
+        tracer,
+        dataset: Optional[Union[aa.Imaging, aa.Interferometer]] = None,
+        data: Optional[Union[aa.Array2D, aa.Visibilities]] = None,
+        noise_map: Optional[Union[aa.Array2D, aa.VisibilitiesNoiseMap]] = None,
+        w_tilde: Optional[Union[aa.WTildeImaging, aa.WTildeInterferometer]] = None,
+        settings_pixelization=aa.SettingsPixelization(),
+        settings_inversion: aa.SettingsInversion = aa.SettingsInversion(),
+        preloads=Preloads(),
+        run_time_dict: Optional[Dict] = None,
+    ):
+        self.tracer = tracer
+
+        super().__init__(
+            dataset=dataset,
+            data=data,
+            noise_map=noise_map,
+            w_tilde=w_tilde,
+            settings_pixelization=settings_pixelization,
+            settings_inversion=settings_inversion,
+            preloads=preloads,
+            run_time_dict=run_time_dict,
+        )
+
+    @property
+    def planes(self):
+        return self.tracer.planes
+
+    @cached_property
+    @aa.profile_func
+    def traced_grid_2d_list_of_inversion(self) -> List[aa.type.Grid2DLike]:
+        return self.tracer.traced_grid_2d_list_from(grid=self.dataset.grid_pixelization)
+
+    @cached_property
+    def lp_linear_func_list_galaxy_dict(
+        self,
+    ) -> Dict[ag.LightProfileLinearObjFuncList, ag.Galaxy]:
+        if not self.tracer.perform_inversion:
+            return {}
+
+        lp_linear_galaxy_dict_list = {}
+
+        traced_grids_of_planes_list = self.tracer.traced_grid_2d_list_from(
+            grid=self.dataset.grid
+        )
+
+        if self.dataset.blurring_grid is not None:
+            traced_blurring_grids_of_planes_list = self.tracer.traced_grid_2d_list_from(
+                grid=self.dataset.blurring_grid
+            )
+        else:
+            traced_blurring_grids_of_planes_list = [None] * len(
+                traced_grids_of_planes_list
+            )
+
+        for plane_index, plane in enumerate(self.planes):
+            plane_to_inversion = ag.PlaneToInversion(
+                plane=plane,
+                dataset=self.dataset,
+                grid=traced_grids_of_planes_list[plane_index],
+                blurring_grid=traced_blurring_grids_of_planes_list[plane_index],
+            )
+
+            lp_linear_galaxy_dict_of_plane = (
+                plane_to_inversion.lp_linear_func_list_galaxy_dict
+            )
+
+            lp_linear_galaxy_dict_list = {
+                **lp_linear_galaxy_dict_list,
+                **lp_linear_galaxy_dict_of_plane,
+            }
+
+        return lp_linear_galaxy_dict_list
+
+    def cls_pg_list_from(self, cls: Type) -> List:
+        return [plane.cls_list_from(cls=cls) for plane in self.planes]
+
+    @cached_property
+    def adapt_galaxy_image_pg_list(self) -> List:
+        return [
+            plane.adapt_galaxies_with_pixelization_image_list for plane in self.planes
+        ]
+
+    @cached_property
+    @aa.profile_func
+    def sparse_image_plane_grid_pg_list(self) -> List[List]:
+        """
+        Specific pixelizations, like the `VoronoiMagnification`, begin by determining what will become its the
+        source-pixel centres by calculating them  in the image-plane. The `VoronoiBrightnessImage` pixelization
+        performs a KMeans clustering.
+        """
+
+        sparse_image_plane_grid_list_of_planes = []
+
+        for plane in self.planes:
+            plane_to_inversion = ag.PlaneToInversion(
+                plane=plane,
+                grid_pixelization=self.dataset.grid,
+                settings_pixelization=self.settings_pixelization,
+            )
+
+            sparse_image_plane_grid_list = (
+                plane_to_inversion.sparse_image_plane_grid_list
+            )
+            sparse_image_plane_grid_list_of_planes.append(sparse_image_plane_grid_list)
+
+        return sparse_image_plane_grid_list_of_planes
+
+    @cached_property
+    @aa.profile_func
+    def traced_sparse_grid_pg_list(self) -> Tuple[List[List], List[List]]:
+        """
+        Ray-trace the sparse image plane grid used to define the source-pixel centres by calculating the deflection
+        angles at (y,x) coordinate on the grid from the galaxy mass profiles and then ray-trace them from the
+        image-plane to the source plane.
+        """
+        if (
+            self.preloads.sparse_image_plane_grid_pg_list is None
+            or self.settings_pixelization.is_stochastic
+        ):
+            sparse_image_plane_grid_pg_list = self.sparse_image_plane_grid_pg_list
+
+        else:
+            sparse_image_plane_grid_pg_list = (
+                self.preloads.sparse_image_plane_grid_pg_list
+            )
+
+        traced_sparse_grid_pg_list = []
+
+        for plane_index, plane in enumerate(self.planes):
+            if sparse_image_plane_grid_pg_list[plane_index] is None:
+                traced_sparse_grid_pg_list.append(None)
+            else:
+                traced_sparse_grids_list = []
+
+                for sparse_image_plane_grid in sparse_image_plane_grid_pg_list[
+                    plane_index
+                ]:
+                    try:
+                        traced_sparse_grids_list.append(
+                            self.tracer.traced_grid_2d_list_from(
+                                grid=sparse_image_plane_grid
+                            )[plane_index]
+                        )
+                    except AttributeError:
+                        traced_sparse_grids_list.append(None)
+
+                traced_sparse_grid_pg_list.append(traced_sparse_grids_list)
+
+        return traced_sparse_grid_pg_list, sparse_image_plane_grid_pg_list
+
+    @cached_property
+    def mapper_galaxy_dict(self) -> Dict[aa.AbstractMapper, ag.Galaxy]:
+        mapper_galaxy_dict = {}
+
+        if self.preloads.traced_grids_of_planes_for_inversion is None:
+            traced_grids_of_planes_list = self.traced_grid_2d_list_of_inversion
+        else:
+            traced_grids_of_planes_list = (
+                self.preloads.traced_grids_of_planes_for_inversion
+            )
+
+        if self.preloads.traced_sparse_grids_list_of_planes is None:
+            (
+                traced_sparse_grids_list_of_planes,
+                sparse_image_plane_grid_list,
+            ) = self.traced_sparse_grid_pg_list
+        else:
+            traced_sparse_grids_list_of_planes = (
+                self.preloads.traced_sparse_grids_list_of_planes
+            )
+            sparse_image_plane_grid_list = self.preloads.sparse_image_plane_grid_list
+
+        for plane_index, plane in enumerate(self.planes):
+            if plane.has(cls=aa.Pixelization):
+                plane_to_inversion = ag.PlaneToInversion(
+                    plane=plane,
+                    grid_pixelization=traced_grids_of_planes_list[plane_index],
+                    settings_pixelization=self.settings_pixelization,
+                    preloads=self.preloads,
+                )
+
+                galaxies_with_pixelization_list = plane.galaxies_with_cls_list_from(
+                    cls=aa.Pixelization
+                )
+
+                for mapper_index in range(
+                    len(traced_sparse_grids_list_of_planes[plane_index])
+                ):
+                    pixelization_list = self.cls_pg_list_from(cls=aa.Pixelization)
+
+                    mapper = plane_to_inversion.mapper_from(
+                        mesh=pixelization_list[plane_index][mapper_index].mesh,
+                        regularization=pixelization_list[plane_index][
+                            mapper_index
+                        ].regularization,
+                        source_plane_mesh_grid=traced_sparse_grids_list_of_planes[
+                            plane_index
+                        ][mapper_index],
+                        image_plane_mesh_grid=sparse_image_plane_grid_list[plane_index][
+                            mapper_index
+                        ],
+                        adapt_galaxy_image=self.adapt_galaxy_image_pg_list[plane_index][
+                            mapper_index
+                        ],
+                    )
+
+                    galaxy = galaxies_with_pixelization_list[mapper_index]
+
+                    mapper_galaxy_dict[mapper] = galaxy
+
+        return mapper_galaxy_dict
+
+    @cached_property
+    def inversion(self):
+        inversion = inversion_unpacked_from(
+            dataset=self.dataset,
+            data=self.data,
+            noise_map=self.noise_map,
+            w_tilde=self.w_tilde,
+            linear_obj_list=self.linear_obj_list,
+            settings=self.settings_inversion,
+            preloads=self.preloads,
+            run_time_dict=self.tracer.run_time_dict,
+        )
+
+        inversion.linear_obj_galaxy_dict = self.linear_obj_galaxy_dict
+
+        return inversion
