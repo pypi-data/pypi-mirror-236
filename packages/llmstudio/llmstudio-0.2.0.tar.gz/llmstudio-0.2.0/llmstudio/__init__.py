94d88eee9b8bdac9efbf2f4dd2bc213fe693412c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-24 04:07:19 +0000

    Add package llmstudio/0.2.0/llmstudio-0.2.0.tar.gz

---
+++ packages/llmstudio/llmstudio-0.2.0.tar.gz/llmstudio-0.2.0/llmstudio/__init__.py
@@ -0,0 +1,22 @@
+name = "version"
+__version__ = "0.2.0"
+
+__requirements__ = [
+    # core
+    "pydantic",
+    "requests",
+    "pydantic",
+    "numpy",
+    "torch",
+    "sentence-transformers",
+    "fastapi",
+    "uvicorn",
+    "PyYaml",
+    # engine
+    "openai",
+    "tiktoken",
+    "google-auth",
+    "google-cloud-aiplatform",
+    "fastapi",
+    "boto3",
+]
