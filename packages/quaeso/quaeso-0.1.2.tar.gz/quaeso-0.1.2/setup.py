7e3f390d30ca6c600b7ae6604c3d30af8bb6c845
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-24 04:08:17 +0000

    Add package quaeso/0.1.2/quaeso-0.1.2.tar.gz

---
+++ packages/quaeso/quaeso-0.1.2.tar.gz/quaeso-0.1.2/setup.py
@@ -0,0 +1,40 @@
+# pip3 install setuptools twine wheel
+# python3 setup.py sdist bdist_wheel
+# twine upload dist/*
+
+
+import pathlib
+from setuptools import setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+README = (HERE / "README.md").read_text()
+
+with (HERE / "requirements.txt").open() as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name="quaeso",
+    version="0.1.2",
+    description="python cli program to send requests",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/zahash/quaeso",
+    author="zahash",
+    author_email="zahash.z@gmail.com",
+    license="MIT",
+    entry_points={
+        'console_scripts': [
+            'quaeso = quaeso.__main__:main',
+        ],
+    },
+    python_requires='>=3.8',
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+    ],
+    packages=["quaeso"],
+    include_package_data=True,
+    install_requires=requirements,
+)
