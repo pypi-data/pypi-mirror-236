cbff67c36eb99f70bcceb39f235da01028a606e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-24 03:14:27 +0000

    Add package g1879/0.5.3b1/g1879-0.5.3b1.tar.gz

---
+++ packages/g1879/g1879-0.5.3b1.tar.gz/g1879-0.5.3b1/setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+from setuptools import setup, find_packages
+
+# with open("README.md", "r", encoding='utf-8') as fh:
+#     long_description = fh.read()
+
+setup(
+    name="g1879",
+    version="0.5.3beta1",
+    author="g1879",
+    author_email="g1879@qq.com",
+    description="A personal toolkit.",
+    # long_description=long_description,
+    # long_description_content_type="text/markdown",
+    license="MIT",
+    keywords="g1879",
+    url="https://gitee.com/g1879/g1879",
+    include_package_data=True,
+    packages=find_packages(),
+    # install_requires=[
+    #     "selenium",
+    #     "lxml",
+    #     "tldextract",
+    #     "requests"
+    # ],
+    classifiers=[
+        "Programming Language :: Python :: 3.8",
+        "Development Status :: 4 - Beta",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: MIT License",
+    ],
+    python_requires='>=3.8'
+)
