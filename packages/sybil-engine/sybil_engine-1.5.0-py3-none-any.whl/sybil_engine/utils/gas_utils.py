052cb714bf9312d883a6fb910251d92c6cd9650b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-24 03:13:24 +0000

    Add package sybil-engine/1.5.0/sybil_engine-1.5.0-py3-none-any.whl

---
+++ packages/sybil-engine/sybil_engine-1.5.0-py3-none-any.whl/sybil_engine/utils/gas_utils.py
@@ -0,0 +1,40 @@
+from loguru import logger
+from web3 import Web3
+
+from sybil_engine.domain.balance.balance_utils import from_wei_to_gwei
+from sybil_engine.utils.utils import randomized_sleeping
+
+web3_main = Web3(Web3.HTTPProvider('https://rpc.ankr.com/eth'))
+
+
+def check_gas_price(chain_instance, web3):
+    while True:
+        try:
+            return verify_gas_price(chain_instance, web3)
+        except GasPriceToHigh as e:
+            logger.info(e)
+            randomized_sleeping({'from': 60 * 4, 'to': 60 * 8})
+
+
+def verify_gas_price(chain_instance, web3):
+    gas_price_wei = web3.eth.gas_price
+    l1_gas_price_wei = web3_main.eth.gas_price
+
+    l1_max_gas_price = chain_instance['l1_gas_price_gwei']
+    zksync_max_gas_price = chain_instance['gas_price_gwei']
+
+    if from_wei_to_gwei(l1_gas_price_wei) > l1_max_gas_price:
+        raise GasPriceToHigh(
+            f"L1 Gas price is to high: {from_wei_to_gwei(l1_gas_price_wei)}Gwei, max: {l1_max_gas_price}Gwei")
+
+    if from_wei_to_gwei(gas_price_wei) > zksync_max_gas_price:
+        raise GasPriceToHigh(
+            f"Gas price is to high: {from_wei_to_gwei(gas_price_wei)}Gwei, max: {zksync_max_gas_price}Gwei")
+
+    return gas_price_wei
+
+
+class GasPriceToHigh(Exception):
+    def __init__(self, message):
+        self.message = message
+        super().__init__(self.message)
