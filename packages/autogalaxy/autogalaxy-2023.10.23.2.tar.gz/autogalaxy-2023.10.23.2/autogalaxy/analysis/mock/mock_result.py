6fce9076cc2f2fbe6479f4217898726128c42da8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-24 03:12:27 +0000

    Add package autogalaxy/2023.10.23.2/autogalaxy-2023.10.23.2.tar.gz

---
+++ packages/autogalaxy/autogalaxy-2023.10.23.2.tar.gz/autogalaxy-2023.10.23.2/autogalaxy/analysis/mock/mock_result.py
@@ -0,0 +1,39 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING, Dict
+
+if TYPE_CHECKING:
+    from autogalaxy import mock
+
+import autofit as af
+import autogalaxy as ag
+
+
+class MockResult(af.m.MockResult):
+    def __init__(
+        self,
+        samples: mock.MockSamples = None,
+        instance: af.Instance = None,
+        model: af.Model = None,
+        analysis: mock.MockAnalysis = None,
+        search: af.mock.MockSearch = None,
+        adapt_galaxy_image_path_dict: Dict[ag.Galaxy, ag.Array2D] = None,
+        adapt_model_image: ag.Array2D = None,
+        max_log_likelihood_plane: ag.Plane = None,
+        max_log_likelihood_tracer=None,
+    ):
+        super().__init__(
+            samples=samples,
+            instance=instance,
+            model=model,
+            analysis=analysis,
+            search=search,
+        )
+
+        self.adapt_galaxy_image_path_dict = adapt_galaxy_image_path_dict
+        self.adapt_model_image = adapt_model_image
+        self.max_log_likelihood_plane = max_log_likelihood_plane
+        self.max_log_likelihood_tracer = max_log_likelihood_tracer
+
+    @property
+    def last(self):
+        return self
