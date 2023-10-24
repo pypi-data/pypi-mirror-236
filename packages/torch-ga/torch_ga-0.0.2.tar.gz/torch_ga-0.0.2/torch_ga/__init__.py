f1c2bfd2183b65067f5d3ca03a209d1944747df1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-24 04:07:26 +0000

    Add package torch-ga/0.0.2/torch_ga-0.0.2.tar.gz

---
+++ packages/torch-ga/torch_ga-0.0.2.tar.gz/torch_ga-0.0.2/torch_ga/__init__.py
@@ -0,0 +1,11 @@
+"""
+PyTorch Geometric Algebra
+
+An python library for Geometric Algebra in Pytorch
+"""
+
+__version__ = "0.0.2"
+__author__ = 'Francesco Alesiani'
+__credits__ = 'TF-GA (https://doi.org/10.5281/zenodo.3902404)'
+
+from .torch_ga import GeometricAlgebra
