101cd1cc2d4509d1e22fa42503e6427780e7832c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-24 04:08:16 +0000

    Add package vitirover/0.1.1/vitirover-0.1.1.tar.gz

---
+++ packages/vitirover/vitirover-0.1.1.tar.gz/vitirover-0.1.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="vitirover",
+    version="0.1.1",
+    packages=find_packages(),
+    install_requires=[
+        'pygame',
+        'protobuf',
+        'socket',
+        'math',
+        'random',
+    ],
+    author="Jorand Gallou",
+    author_email="info@vitirover.com",
+    description="Commande du robot Vitirover avec le clavier.",
+    license="MIT",
+    keywords="vitirover robot control pygame",
+    url="https://github.com/votreusername/vitirover",
+)
