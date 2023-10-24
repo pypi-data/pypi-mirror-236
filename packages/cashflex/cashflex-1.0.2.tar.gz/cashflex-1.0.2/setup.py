54582e11d47b46b85e509d658117fc12e8eb2847
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-24 03:12:55 +0000

    Add package cashflex/1.0.2/cashflex-1.0.2.tar.gz

---
+++ packages/cashflex/cashflex-1.0.2.tar.gz/cashflex-1.0.2/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+from setuptools import find_packages
+
+setup( 
+    name='cashflex', 
+    version='1.0.2', 
+    description='Cashflex Money Manager', 
+    author='Gary Barnes', 
+    author_email='gary.barnes2023@gmail.com', 
+    python_requires='>=3.10',
+    packages=['cashflex'],
+    include_package_data=True,
+    install_requires=[ 
+        'matplotlib>=3.8', 
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: End Users/Desktop',
+        'License :: Free for non-commercial use',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Topic :: Office/Business :: Financial :: Accounting',
+    ],
+    project_urls={
+        'Documentation': 'https://github.com/gary-1959/cashflex/blob/main/README.md',
+        'Source': 'https://github.com/gary-1959/cashflex',
+        'Tracker': 'https://github.com/gary-1959/cashflex/issues',
+    },
+) 
