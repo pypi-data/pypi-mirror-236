b7da373a10d6febfb00e5fc81e825479e47fa0ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-24 03:15:32 +0000

    Add package faker_food/0.3.0/faker_food-0.3.0.tar.gz

---
+++ packages/faker_food/faker_food-0.3.0.tar.gz/faker_food-0.3.0/faker_food/categories.py
@@ -0,0 +1,26 @@
+"""
+List of recipe categories
+"""
+
+categories = [
+    "Appetizers",
+    "Beverages",
+    "Breads",
+    "Breakfasts",
+    "Cakes",
+    "Casseroles",
+    "Cocktails",
+    "Condiments",
+    "Cookies & Candies",
+    "Desserts",
+    "Entrees",
+    "Holidays",
+    "Pasta",
+    "Pies",
+    "Salads",
+    "Sandwiches",
+    "Sauces",
+    "Slow Cooker",
+    "Soups & Stews",
+    "Vegetables",
+]
