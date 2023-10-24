a18902dda9ec3e8231849876421a6b17ad457013
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-24 03:16:24 +0000

    Add package sphinxcontrib-opendataservices-jsonschema/0.6.1/sphinxcontrib-opendataservices-jsonschema-0.6.1.tar.gz

---
+++ packages/sphinxcontrib-opendataservices-jsonschema/sphinxcontrib-opendataservices-jsonschema-0.6.1.tar.gz/sphinxcontrib-opendataservices-jsonschema-0.6.1/setup.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+long_desc = open('README.rst').read()
+
+setup(
+    name='sphinxcontrib-opendataservices-jsonschema',
+    version='0.6.1',
+    url='https://github.com/OpenDataServices/sphinxcontrib-opendataservices-jsonschema',
+    license='BSD',
+    author='Takeshi KOMIYA & Open Data Services Co-operative',
+    author_email='code@opendataservices.coop',
+    description='Sphinx extension to define data structure using JSON Schema',
+    long_description=long_desc,
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Framework :: Sphinx :: Extension',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Topic :: Documentation',
+        'Topic :: Documentation :: Sphinx',
+    ],
+    platforms='any',
+    packages=find_packages(),
+    include_package_data=True,
+    python_requires='>=3.8',
+    install_requires=[
+        'docutils',
+        'jsonref',
+        'jsonpointer',
+        'myst-parser',
+    ],
+    extras_require={
+        'test': [
+            'flake8<6',
+            'lxml',
+            'pytest',
+        ],
+    },
+    namespace_packages=['sphinxcontrib'],
+)
