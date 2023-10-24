863ee2d290ca7b8b6d2b5a8d3ac737bb9043b094
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-24 05:08:31 +0000

    Add package qcb/0.1/qcb-0.1.tar.gz

---
+++ packages/qcb/qcb-0.1.tar.gz/qcb-0.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='qcb',
+    version='0.1',
+    description='Quilt circuit boards',
+    author='Your Name',
+    author_email='dev@quilter.ai',
+    url='https://quilter.ai',
+    # packages=find_packages(),
+    install_requires=[
+        # Add any dependencies your package requires here
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
