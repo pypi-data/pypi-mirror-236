bec841241699860b52f350c75e0ef8f0deb2def2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-24 03:13:22 +0000

    Add package pydqt/0.1.8/pydqt-0.1.8.tar.gz

---
+++ packages/pydqt/pydqt-0.1.8.tar.gz/pydqt-0.1.8/pydqt/__init__.py
@@ -0,0 +1,25 @@
+from pydqt.pydqt import (
+    set_workdir,
+    workspace,
+    env_reload,
+    env_edit,
+    QueryTemplateParams,
+    QueryParams,
+    Query,
+    Sql
+)
+
+from pydqt import pydqt
+
+
+__all__ = [
+    "set_workdir",
+    "workspace",
+    "env_reload",
+    "env_edit",
+    "QueryTemplateParams",
+    "QueryParams",
+    "Query",
+    "Sql",
+    "pydqt"
+]
