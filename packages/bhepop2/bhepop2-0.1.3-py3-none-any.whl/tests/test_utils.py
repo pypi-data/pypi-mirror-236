4def143582c94042b5bc73e90b95f5fd535453f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-24 04:07:23 +0000

    Add package bhepop2/0.1.3/bhepop2-0.1.3-py3-none-any.whl

---
+++ packages/bhepop2/bhepop2-0.1.3-py3-none-any.whl/tests/test_utils.py
@@ -0,0 +1,10 @@
+from bhepop2.bhepop2_enrichment import Bhepop2Enrichment
+from bhepop2.utils import *
+
+
+def test_add_defaults_and_validate_against_schema(test_parameters):
+    """
+    Test the json schema validation.
+    """
+
+    add_defaults_and_validate_against_schema(test_parameters, Bhepop2Enrichment.parameters_schema)
