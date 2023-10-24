9bf9560bdeeaab9387253eb5c164d52c988badb9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-24 05:08:46 +0000

    Add package spreadsheet-wrangler/0.1.4/spreadsheet_wrangler-0.1.4.tar.gz

---
+++ packages/spreadsheet-wrangler/spreadsheet_wrangler-0.1.4.tar.gz/spreadsheet_wrangler-0.1.4/setup.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    LONG_DESCRIPTION = fh.read()
+
+try:
+    requirements = []
+    with open("requirements.txt", "r") as f:
+        for line in f:
+            requirements.append(line.strip())
+except FileNotFoundError:
+    requirements = [
+        "click",
+        "pandas",
+        "pyexcel_ods3",
+        "openpyxl"
+    ]
+
+setup(name='spreadsheet_wrangler',
+      version='0.1.4',
+      description='Spreadsheet editing tools',
+      long_description=LONG_DESCRIPTION,
+      long_description_content_type="text/markdown",
+      url='https://github.com/snhobbs/spreadsheet-wrangler',
+      author='Simon Hobbs',
+      author_email='simon.hobbs@electrooptical.net',
+      license='MIT',
+      packages=find_packages(),
+      classifiers=[
+          "Programming Language :: Python :: 3",
+          "License :: OSI Approved :: MIT License",
+          "Operating System :: OS Independent",
+      ],
+      python_requires='>=3.6',
+      install_requires=requirements,
+      test_suite='nose.collector',
+      tests_require=['nose'],
+      scripts=["spreadsheet_wrangler.py"],
+      entry_points={
+        "console_scripts": [
+            "spreadsheet_wrangler=spreadsheet_wrangler:main",
+        ],
+      },
+      include_package_data=False,
+      zip_safe=True)
