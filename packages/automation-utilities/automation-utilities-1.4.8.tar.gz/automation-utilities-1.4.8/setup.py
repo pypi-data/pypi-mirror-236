cba918e5d522a18e8dfd614f0309ae2c4f21fc6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-24 03:14:19 +0000

    Add package automation-utilities/1.4.8/automation-utilities-1.4.8.tar.gz

---
+++ packages/automation-utilities/automation-utilities-1.4.8.tar.gz/automation-utilities-1.4.8/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='automation-utilities',
+    version='1.4.8',
+    author='Hazem Oukal',
+    description='Some automation tools',
+    packages=['automation_utilities'],
+    install_requires=['phonenumbers', 'names', 'requests']
+)
