b8d0d88fb1425d1d690db008a1d91b241e9b3fd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-24 04:08:02 +0000

    Add package hydradx-api/0.2.0/hydradx_api-0.2.0-py3-none-any.whl

---
+++ packages/hydradx-api/hydradx_api-0.2.0-py3-none-any.whl/hydradxapi/api.py
@@ -0,0 +1,14 @@
+from hydradxapi.pallets.omnipool import Omnipool
+
+
+class API:
+    """
+    HydraDX chain API
+    """
+
+    def __init__(self, client):
+        self._client = client
+
+    @property
+    def omnipool(self):
+        return Omnipool(self._client)
