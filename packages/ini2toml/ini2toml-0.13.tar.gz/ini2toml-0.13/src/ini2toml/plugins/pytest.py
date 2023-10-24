0e62e01f44f97540f3a873e39aa8c61887a80f09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-24 03:16:18 +0000

    Add package ini2toml/0.13/ini2toml-0.13.tar.gz

---
+++ packages/ini2toml/ini2toml-0.13.tar.gz/ini2toml-0.13/src/ini2toml/plugins/pytest.py
@@ -0,0 +1,67 @@
+# https://docs.pytest.org/en/latest/reference/reference.html#configuration-options
+# https://docs.pytest.org/en/latest/reference/customize.html#config-file-formats
+from collections.abc import MutableMapping
+from functools import partial
+from typing import TypeVar
+
+from ..transformations import coerce_scalar, split_list
+from ..types import IntermediateRepr, Translator
+
+R = TypeVar("R", bound=IntermediateRepr)
+
+split_spaces = partial(split_list, sep=" ")
+split_lines = partial(split_list, sep="\n")
+# ^ most of the list values in pytest use whitespace separators,
+#   but markers/filterwarnings are a special case.
+
+
+def activate(translator: Translator):
+    plugin = Pytest()
+    for file in ("setup.cfg", "tox.ini", "pytest.ini"):
+        translator[file].intermediate_processors.append(plugin.process_values)
+    translator["pytest.ini"].help_text = plugin.__doc__ or ""
+
+
+class Pytest:
+    """Convert settings to 'pyproject.toml' ('ini_options' table)"""
+
+    LINE_SEPARATED_LIST_VALUES = (
+        "markers",
+        "filterwarnings",
+    )
+    SPACE_SEPARATED_LIST_VALUES = (
+        "norecursedirs",
+        "python_classes",
+        "python_files",
+        "python_functions",
+        "required_plugins",
+        "testpaths",
+        "usefixtures",
+    )
+
+    DONT_TOUCH = ("minversion",)
+
+    def process_values(self, doc: R) -> R:
+        candidates = [
+            (("pytest", "ini_options"), "pytest", doc),
+            (("tool", "pytest", "ini_options"), "tool:pytest", doc),
+            (("tool", "pytest", "ini_options"), ("tool", "pytest"), doc),
+            (("pytest", "ini_options"), "pytest", doc.get("tool", {})),
+        ]
+        for new_key, old_key, parent in candidates:
+            section = parent.get(old_key)
+            if section:
+                self.process_section(section)
+                parent.rename(old_key, new_key)
+        return doc
+
+    def process_section(self, section: MutableMapping):
+        for field in section:
+            if field in self.DONT_TOUCH:
+                continue
+            if field in self.LINE_SEPARATED_LIST_VALUES:
+                section[field] = split_lines(section[field])
+            elif field in self.SPACE_SEPARATED_LIST_VALUES:
+                section[field] = split_spaces(section[field])
+            else:
+                section[field] = coerce_scalar(section[field])
---
+++ packages/ini2toml/ini2toml-0.13.tar.gz/ini2toml-0.13/tests/plugins/test_pytest.py
@@ -0,0 +1,54 @@
+from textwrap import dedent
+
+import tomli
+
+from ini2toml.drivers import full_toml, lite_toml
+from ini2toml.plugins import pytest
+from ini2toml.translator import Translator
+
+
+def test_pytest():
+    example = """\
+    [pytest]
+    minversion = 6.0
+    addopts = -ra -q --cov ini2toml
+    testpaths = tests
+    python_files = test_*.py check_*.py example_*.py
+    required_plugins = pytest-django>=3.0.0,<4.0.0 pytest-html pytest-xdist>=1.0.0
+    norecursedirs =
+        dist
+        build
+        .tox
+    filterwarnings=
+        error
+        ignore:Please use `dok_matrix` from the `scipy\\.sparse` namespace, the `scipy\\.sparse\\.dok` namespace is deprecated.:DeprecationWarning
+    """  # noqa
+    expected = """\
+    [pytest]
+    [pytest.ini_options]
+    minversion = "6.0"
+    addopts = "-ra -q --cov ini2toml"
+    testpaths = ["tests"]
+    python_files = ["test_*.py", "check_*.py", "example_*.py"]
+    required_plugins = ["pytest-django>=3.0.0,<4.0.0", "pytest-html", "pytest-xdist>=1.0.0"]
+    norecursedirs = [
+        "dist", 
+        "build", 
+        ".tox", 
+    ]
+    filterwarnings = [
+        "error",
+        'ignore:Please use `dok_matrix` from the `scipy\\.sparse` namespace, the `scipy\\.sparse\\.dok` namespace is deprecated.:DeprecationWarning',
+    ]
+    """  # noqa
+    for convert in (lite_toml.convert, full_toml.convert):
+        translator = Translator(plugins=[pytest.activate], toml_dumps_fn=convert)
+        out = translator.translate(dedent(example), "pytest.ini").strip()
+        expected = dedent(expected).strip()
+        print("expected=\n" + expected + "\n***")
+        print("out=\n" + out)
+        try:
+            assert expected in out
+        except AssertionError:
+            # At least the Python-equivalents when parsing should be the same
+            assert tomli.loads(expected) == tomli.loads(out)
