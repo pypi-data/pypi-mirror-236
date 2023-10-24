531f1beb36b943a0084c3f6f6e3975c67fb0580e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-24 04:07:22 +0000

    Add package nonebot_plugin_chatrecorder/0.5.1/nonebot_plugin_chatrecorder-0.5.1.tar.gz

---
+++ packages/nonebot_plugin_chatrecorder/nonebot_plugin_chatrecorder-0.5.1.tar.gz/nonebot_plugin_chatrecorder-0.5.1/nonebot_plugin_chatrecorder/consts.py
@@ -0,0 +1,34 @@
+from nonebot_plugin_localstore import get_cache_dir
+from strenum import StrEnum
+
+CACHE_DIR = get_cache_dir("nonebot_plugin_chatrecorder")
+IMAGE_CACHE_DIR = CACHE_DIR / "images"
+RECORD_CACHE_DIR = CACHE_DIR / "records"
+VIDEO_CACHE_DIR = CACHE_DIR / "videos"
+
+IMAGE_CACHE_DIR.mkdir(parents=True, exist_ok=True)
+RECORD_CACHE_DIR.mkdir(parents=True, exist_ok=True)
+VIDEO_CACHE_DIR.mkdir(parents=True, exist_ok=True)
+
+
+class SupportedAdapter(StrEnum):
+    onebot_v11 = "OneBot V11"
+    onebot_v12 = "OneBot V12"
+    console = "Console"
+    kaiheila = "Kaiheila"
+    qqguild = "QQ Guild"
+    telegram = "Telegram"
+    feishu = "Feishu"
+    red = "RedProtocol"
+    discord = "Discord"
+
+
+class SupportedPlatform(StrEnum):
+    qq = "qq"
+    console = "console"
+    kaiheila = "kaiheila"
+    qqguild = "qqguild"
+    telegram = "telegram"
+    feishu = "feishu"
+    discord = "discord"
+    unknown = "unknown"
