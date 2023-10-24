06069db68df64f0a79b82c3a1eebfb3ac988ea59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-24 04:07:40 +0000

    Add package PAMM/0.0.2/PAMM-0.0.2.tar.gz

---
+++ packages/PAMM/PAMM-0.0.2.tar.gz/PAMM-0.0.2/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='PAMM',
+      version='0.0.2',
+      description='Preproccesing, analyze, synchronization and mathematic modeling for time-series',
+      packages=['PAMM'],
+      author_email='rmrs1@yandex.ru',
+      zip_safe=False)
