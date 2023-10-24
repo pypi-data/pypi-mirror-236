cfd9ef575509998b60d33e2f0c326f25f2626ba2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-24 04:07:16 +0000

    Add package moabb/1.0.0/moabb-1.0.0-py3-none-any.whl

---
+++ packages/moabb/moabb-1.0.0-py3-none-any.whl/moabb/pipelines/__init__.py
@@ -0,0 +1,46 @@
+"""Pipeline defines all steps required by an algorithm to obtain predictions.
+
+Pipelines are typically a chain of sklearn compatible transformers and
+end with a sklearn compatible estimator.
+"""
+
+# flake8: noqa
+from mne.utils import warn
+
+from .classification import SSVEP_CCA, SSVEP_TRCA, SSVEP_MsetCCA
+from .features import FM, AugmentedDataset, ExtendedSSVEPSignal, LogVariance
+from .utils import FilterBank, create_pipeline_from_config
+
+
+try:
+    from .deep_learning import (
+        KerasDeepConvNet,
+        KerasEEGITNet,
+        KerasEEGNet_8_2,
+        KerasEEGNeX,
+        KerasEEGTCNet,
+        KerasShallowConvNet,
+    )
+    from .utils_deep_model import EEGNet, TCN_block
+except ModuleNotFoundError as err:
+    warn(
+        "Tensorflow is not installed. "
+        "You won't be able to use these MOABB pipelines if you attempt to do "
+        "so.",
+        category=ModuleNotFoundError,
+        module="moabb.pipelines",
+    )
+
+
+try:
+    from .utils_pytorch import (
+        BraindecodeDatasetLoader,
+        InputShapeSetterEEG,
+        get_shape_from_baseconcat,
+    )
+except ModuleNotFoundError as err:
+    print(
+        "To use the get_shape_from_baseconcar, InputShapeSetterEEG, BraindecodeDatasetLoader"
+        "you need to install `braindecode`."
+        "`pip install braindecode` or Please refer to `https://braindecode.org`."
+    )
