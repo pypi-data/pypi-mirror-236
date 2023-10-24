4a198b2758c0f4da666b3a31ed540d80681a7244
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-24 04:07:50 +0000

    Add package goldhandtest/1.0/goldhandtest-1.0.tar.gz

---
+++ packages/goldhandtest/goldhandtest-1.0.tar.gz/goldhandtest-1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="goldhandtest",
+    version="1.0",
+    author="Mihaly",
+    author_email="ormraat.pte@gmail.com",
+    description="A description of your package",
+    license="MIT",
+    install_requires=[],
+    packages=find_packages(),
+)
