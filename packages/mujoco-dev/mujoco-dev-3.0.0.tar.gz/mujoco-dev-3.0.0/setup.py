fcf0216cf03970381853a41bd9f99415f5c0ec47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-24 03:16:10 +0000

    Add package mujoco-dev/3.0.0/mujoco-dev-3.0.0.tar.gz

---
+++ packages/mujoco-dev/mujoco-dev-3.0.0.tar.gz/mujoco-dev-3.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+raise RuntimeError("""
+\u001b[1;31;43m                                                                                \u001b[0m
+\u001b[1;31;43m  The mujoco-dev package on PyPI is DEPRECATED and is no longer being updated.  \u001b[0m
+\u001b[1;31;43m  To install a pre-release version of the MuJoCo Python bindings, please use:   \u001b[0m
+\u001b[1;31;43m                                                                                \u001b[0m
+\u001b[1;31;43m         pip install mujoco --pre -f https://py.mujoco.org/                     \u001b[0m
+\u001b[1;31;43m                                                                                \u001b[0m""")
+
+setup()
