7657013d07ea8c9293d32e1263666851cfa4f4ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-24 03:12:28 +0000

    Add package foxplot/0.4.0/foxplot-0.4.0-py3-none-any.whl

---
+++ packages/foxplot/foxplot-0.4.0-py3-none-any.whl/foxplot/fox.py
@@ -0,0 +1,218 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# Copyright 2023 Inria
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Main class to manipulate dictionary-series data."""
+
+import logging
+import sys
+import webbrowser
+from typing import BinaryIO, List, Optional, TextIO, Union
+
+from .decoders.json import decode_json
+from .decoders.msgpack import decode_msgpack
+from .generate_html import generate_html
+from .indexed_series import IndexedSeries
+from .node import Node
+from .write_tmpfile import write_tmpfile
+
+
+class Fox:
+    """Frequent Observation diXionaries, our main class.
+
+    Our main class to read, access and manipulate series of dictionary data.
+    """
+
+    __file: Optional[str]
+    __time: Optional[str]
+    data: Node
+    length: int
+
+    def __init__(
+        self,
+        from_file: Optional[str] = None,
+        time: Union[None, str, IndexedSeries] = None,
+    ):
+        """Initialize series.
+
+        Args:
+            from_file: If set, read data from this path.
+            time: Label of time index in input dictionaries.
+        """
+        self.__file = from_file
+        self.__time = None
+        self.data = Node("/")
+        self.length = 0
+        if from_file:
+            self.read_from_file(from_file)
+        if time is not None:
+            self.set_time(time)
+
+    def get_series(self, label: str) -> IndexedSeries:
+        """Get time-series data from a given label.
+
+        Args:
+            label: Label to the data in input dictionaries, for example
+                ``/observation/cpu_temperature``.
+
+        Returns:
+            Corresponding time series.
+        """
+        keys = label.strip("/").split("/")
+        return self.data._get_child(keys)
+
+    @property
+    def labels(self) -> List[str]:
+        """List of all labels present in the data."""
+        return self.data._list_labels()
+
+    def plot(
+        self,
+        left: Union[IndexedSeries, List[IndexedSeries]],
+        right: Optional[List[IndexedSeries]] = None,
+        title: str = "",
+        left_axis_unit: str = "",
+        right_axis_unit: str = "",
+        open_new_tab: bool = True,
+    ) -> None:
+        """Plot a set of indexed series.
+
+        Args:
+            left: Series to plot on the left axis.
+            right: Series to plot on the right axis.
+            title: Plot title.
+            left_axis_unit: Unit label for the left axis.
+            right_axis_unit: Unit label for the right axis.
+            open_new_tab: If true (default), open plot in a new browser tab.
+        """
+        if isinstance(left, IndexedSeries):
+            left = [left]
+        times = (
+            self.get_series(self.__time)._get(self.length)
+            if self.__time is not None
+            else [float(x) for x in range(self.length)]
+        )
+
+        def list_to_dict(series_list):
+            series_dict = {}
+            for series in series_list:
+                if isinstance(series, IndexedSeries):
+                    series_dict[series._label] = series._get(self.length)
+                elif isinstance(series, Node):
+                    for key, child in series._items():
+                        label = series._label + f"/{key}"
+                        if isinstance(child, IndexedSeries):
+                            series_dict[label] = child._get(self.length)
+                        else:
+                            logging.warn(
+                                "Skipping '%s' as it is not an indexed series",
+                                label,
+                            )
+                else:
+                    raise TypeError(
+                        f"Series '{series._label}' "
+                        f"has unknown type {type(series)}"
+                    )
+            return series_dict
+
+        left_series = list_to_dict(left)
+        right_series = list_to_dict(right) if right is not None else {}
+        html = generate_html(
+            times,
+            left_series,
+            right_series,
+            title,
+            left_axis_unit,
+            right_axis_unit,
+            timestamped=self.__time is not None,
+        )
+
+        if open_new_tab:
+            filename = write_tmpfile(html)
+            webbrowser.open_new_tab(filename)
+            print("New tab opened in your web browser! ", end="")
+
+        print("The command line is to produce it directly is:\n")
+        left_args = " ".join(left_series.keys())
+        right_args = (
+            f"-r {' '.join(right_series.keys())} " if right_series else ""
+        )
+        file = self.__file if self.__file is not None else ""
+        timestamp = f"-t {self.__time} " if self.__time is not None else ""
+        print(f"foxplot {timestamp}-l {left_args} {right_args}{file}")
+
+    def read_from_file(self, filename: str) -> None:
+        """Process time series data.
+
+        Args:
+            filename: Name of a file to read time series from.
+        """
+        if filename == "stdin":
+            self.read_from_json(sys.stdin)
+        elif filename.endswith(".json"):
+            with open(filename, "r", encoding="utf-8") as file:
+                self.read_from_json(file)
+        elif filename.endswith(".mpack"):
+            with open(filename, "rb") as file:
+                self.read_from_msgpack(file)
+
+    def read_from_json(self, file: TextIO) -> None:
+        """Process time series data from a JSON stream.
+
+        Args:
+            file: JSON stream to read time series from.
+        """
+        for unpacked in decode_json(file=file):
+            self.unpack(unpacked)
+
+    def read_from_msgpack(self, file: BinaryIO) -> None:
+        """Process time series data from a MessagePack stream.
+
+        Args:
+            file: MessagePack stream to read time series from.
+        """
+        for unpacked in decode_msgpack(file=file):
+            self.unpack(unpacked)
+
+    def set_time(self, time: Union[str, IndexedSeries]):
+        """Set label of time index in input dictionaries.
+
+        Args:
+            time: Time index as a series or its label in input dictionaries.
+        """
+        label = time._label if isinstance(time, IndexedSeries) else time
+        self.__time = label
+
+    def unpack(self, unpacked: dict) -> None:
+        """Append data from an unpacked dictionary.
+
+        Args:
+            unpacked: Unpacked dictionary.
+        """
+        self.data._update(self.length, unpacked)
+        self.length += 1
+
+    def detect_time(self) -> None:
+        """Search for a time key in root keys."""
+        candidates = ("time", "timestamp")
+        for key in candidates:
+            if key in self.data.__dict__:
+                self.set_time(self.data.__dict__[key])
+                print(
+                    f'Detected "{key}" as time key from the input '
+                    "(call `fox.set_time` to select a different one)"
+                )
+                return
---
+++ packages/foxplot/foxplot-0.4.0-py3-none-any.whl/foxplot/indexed_series.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# Copyright 2023 Inria
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Series data unpacked from input dictionaries."""
+
+from typing import Any, Dict, List
+
+
+class IndexedSeries:
+    """Indexed time-series values."""
+
+    __data: Dict[int, Any]
+    __label: str
+
+    def __init__(self, label: str):
+        """Initialize a new indexed series.
+
+        Args:
+            label: Label of the series in the input data.
+        """
+        self.__data = {}
+        self.__label = label
+
+    @property
+    def _label(self) -> str:
+        """Label of the series in the input data."""
+        return self.__label
+
+    def __len__(self):
+        """Length of the indexed series."""
+        return len(self.__data)
+
+    def __repr__(self):
+        """String representation of the series."""
+        values = list(self.__data.values())
+        return f"Time series with values: {values}"
+
+    def _get(self, max_index: int):
+        return [self.__data.get(index, None) for index in range(max_index)]
+
+    def _list_labels(self) -> List[str]:
+        return [self.__label]
+
+    def _update(self, index: int, value: Any):
+        self.__data[index] = value
