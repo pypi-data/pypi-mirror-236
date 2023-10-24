70bcb8359dce208c0e1d0e063881177266361cb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-24 05:08:34 +0000

    Add package galaxy-web-apps/23.0.6/galaxy_web_apps-23.0.6-py3-none-any.whl

---
+++ packages/galaxy-web-apps/galaxy_web_apps-23.0.6-py3-none-any.whl/tool_shed/util/tool_util.py
@@ -0,0 +1,13 @@
+from galaxy.tool_shed.util.tool_util import (
+    copy_sample_file,
+    copy_sample_files,
+    generate_message_for_invalid_tools,
+    new_state,
+)
+
+__all__ = (
+    "copy_sample_file",
+    "copy_sample_files",
+    "generate_message_for_invalid_tools",
+    "new_state",
+)
