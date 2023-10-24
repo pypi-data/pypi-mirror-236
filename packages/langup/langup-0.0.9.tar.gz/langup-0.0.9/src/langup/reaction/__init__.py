0feea88f462bd9b36821c89e245d67b18cf52d10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-24 04:08:19 +0000

    Add package langup/0.0.9/langup-0.0.9.tar.gz

---
+++ packages/langup/langup-0.0.9.tar.gz/langup-0.0.9/src/langup/reaction/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from langup.reaction.bilibili import CommentReaction
+from langup.reaction.voice import TTSSpeakReaction
+
+
+__all__ = [
+    'CommentReaction',
+    'TTSSpeakReaction'
+]
+
---
+++ packages/langup/langup-0.0.9.tar.gz/langup-0.0.9/src/langup/reaction/bilibili.py
@@ -0,0 +1,15 @@
+from bilibili_api import Credential
+
+from langup import base, config
+from langup.api.bilibili import comment
+
+
+class CommentReaction(base.Reaction):
+
+    aid: int
+    content: str
+
+    async def areact(self):
+        # 发送评论
+        await comment.send_comment(text=self.content, type_=comment.CommentResourceType.VIDEO, oid=self.aid, credential=config.credential)
+
